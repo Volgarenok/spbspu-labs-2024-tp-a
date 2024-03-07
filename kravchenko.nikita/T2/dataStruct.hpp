e561cfce77b8e0b2a12a168d413085bf9364609e
Glaz0k <nikitakravcenko892@gmail.com>
2024-03-07 21:58:00 +0300

    added dataStruct.hpp

---
+++ kravchenko.nikita/T2/dataStruct.hpp
@@ -0,0 +1,21 @@
+#ifndef DATA_STRUCT_H
+#define DATA_STRUCT_H
+
+#include <complex>
+#include <string>
+#include <istream>
+#include <ostream>
+
+namespace dataStruct
+{
+	struct DataStruct
+	{
+		long long key1;
+		std::complex< double > key2;
+		std::string key3;
+	};
+	std::istream& operator>>(std::istream& in, DataStruct& value);
+	std::ostream& operator<<(std::ostream& out, const DataStruct& value);
+}
+
+#endif
