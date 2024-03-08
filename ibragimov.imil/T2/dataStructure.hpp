4d2c06141687d382ccae41ce7c7eefdf781e4106
StantStantov <ibragimovimil0@gmail.com>
2024-03-08 21:28:43 +0300

    Add definition of virtual struct DataStructure

---
+++ ibragimov.imil/T2/dataStructure.hpp
@@ -0,0 +1,15 @@
+#ifndef DATA_STRUCTURE_HPP
+#define DATA_STRUCTURE_HPP
+
+//TODO: DBL LIT, CMP LSP
+namespace ibr
+{
+  struct DataStructure
+  {
+  public:
+  virtual ~DataStructure() = default;
+  };
+
+}
+
+#endif
