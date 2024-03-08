d2acee5101314d213ccd3e7e4524b371e4212f6b
Glaz0k <nikitakravcenko892@gmail.com>
2024-03-08 22:55:45 +0300

    added stringDelimeterI

--- kravchenko.nikita/T2/delimeterI.cpp
+++ kravchenko.nikita/T2/delimeterI.cpp
@@ -2,16 +2,30 @@
 
 std::istream& kravchenko::operator>>(std::istream& in, DelimeterI&& exp)
 {
-	std::istream::sentry sentry(in);
-	if (!sentry)
-	{
-		return in;
-	}
-	char c = 0;
-	in >> c;
-	if (c != exp.expected)
-	{
-		in.setstate(std::ios::failbit);
-	}
-	return in;
+  std::istream::sentry sentry(in);
+  if (!sentry)
+  {
+    return in;
+  }
+  char c = 0;
+  in >> c;
+  if (c != exp.expected)
+  {
+    in.setstate(std::ios::failbit);
+  }
+  return in;
+}
+
+std::istream& kravchenko::operator>>(std::istream& in, StringDelimeterI&& exp)
+{
+  std::istream::sentry sentry(in);
+  if (!sentry)
+  {
+    return in;
+  }
+  for (size_t i = 0; i < exp.expected.size() && in; ++i)
+  {
+    in >> DelimeterI{ exp.expected[i] };
+  }
+  return in;
 }
--- kravchenko.nikita/T2/delimeterI.hpp
+++ kravchenko.nikita/T2/delimeterI.hpp
@@ -2,6 +2,7 @@
 #define DELIMETER_I_HPP
 
 #include <istream>
+#include <string>
 
 namespace kravchenko
 {
@@ -10,6 +11,12 @@ namespace kravchenko
     char expected;
   };
   std::istream& operator>>(std::istream& in, DelimeterI&& exp);
+
+  struct StringDelimeterI
+  {
+    std::string expected;
+  };
+  std::istream& operator>>(std::istream& in, StringDelimeterI&& exp);
 }
 
 #endif
