eb6677629e809c60f6040ae1387f95c7dba553cb
Glaz0k <nikitakravcenko892@gmail.com>
2024-03-08 03:27:02 +0300

    added realization for parseFunctions and delimeterI

---
+++ kravchenko.nikita/T2/delimeterI.cpp
@@ -0,0 +1,17 @@
+#include "DelimeterI.hpp"
+
+std::istream& delimeterI::operator>>(std::istream& in, delimeterI::DelimeterI&& exp)
+{
+	std::istream::sentry guard(in);
+	if (!guard)
+	{
+		return in;
+	}
+	char c = 0;
+	in >> c;
+	if (c != exp.expected)
+	{
+		in.setstate(std::ios::failbit);
+	}
+	return in;
+}
---
+++ kravchenko.nikita/T2/parseFunctions.cpp
@@ -0,0 +1,41 @@
+#include "ParseFunctions.hpp"
+#include "DelimeterI.hpp"
+
+void parseSignedLL(std::istream& in, long long& value)
+{
+	std::istream::sentry guard(in);
+	if (!guard)
+	{
+		return;
+	}
+	long long temp = 0;
+	in >> temp;
+	char c[2] = { 0 };
+	in >> c[0] >> c[1];
+	if (tolower(c[0]) != 'l' || c[0] != c[1])
+	{
+		in.setstate(std::ios::failbit);
+		return;
+	}
+	if (in)
+	{
+		value = temp;
+	}
+}
+
+void parseComplex(std::istream& in, std::complex< double >& value)
+{
+	std::istream::sentry guard(in);
+	if (!guard)
+	{
+		return;
+	}
+	using cDel = delimeterI::DelimeterI;
+	double re = 0;
+	double im = 0;
+	in >> cDel{ '#' } >> cDel{ 'c' } >> cDel{ '(' } >> re >> im >> cDel{ ')' };
+	if (in)
+	{
+		value = { re, im };
+	}
+}
