c069355fa1fac68015ae768de4579503e3e0dbb3
Glaz0k <nikitakravcenko892@gmail.com>
2024-03-08 11:48:23 +0300

    added advanced input

--- kravchenko.nikita/T2/main.cpp
+++ kravchenko.nikita/T2/main.cpp
@@ -2,13 +2,23 @@
 #include <vector>
 #include <algorithm>
 #include <iterator>
-#include "DataStruct.hpp"
+#include "dataStruct.hpp"
 
 int main()
 {
   using namespace kravchenko::dataStruct;
   using inputItT = std::istream_iterator< DataStruct >;
-  std::vector< DataStruct > data(inputItT{ std::cin }, inputItT{});
+  std::vector< DataStruct > data;
+  while (!std::cin.eof())
+  {
+    if (std::cin.fail())
+    {
+      std::cin.clear();
+      std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+    }
+    data.insert(data.cend(), inputItT{ std::cin }, inputItT{});
+  }
+
   using outputItT = std::ostream_iterator< DataStruct >;
   std::copy(data.cbegin(), data.cend(), outputItT{ std::cout, "\n" });
   return 0;
