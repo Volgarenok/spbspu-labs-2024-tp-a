b459d0bbe27abd31a115fbc552732b2495789b13
StantStantov <ibragimovimil0@gmail.com>
2024-03-08 22:17:37 +0300

    Add definition of class DBL LIT DataStructure

---
+++ ibragimov.imil/T2/dLDataStructure.hpp
@@ -0,0 +1,30 @@
+#ifndef DBL_LIT_DATA_STRUCTURE_HPP
+#define DBL_LIT_DATA_STRUCTURE_HPP
+
+#include "dataStructure.hpp"
+#include <iostream>
+#include <string>
+
+namespace ibr
+{
+  struct DLDataStructure: public DataStructure
+  {
+  public:
+    DLDataStructure();
+    DLDataStructure(const DLDataStructure&);
+    DLDataStructure(DLDataStructure&&);
+    DLDataStructure(double, double, std::string);
+    ~DLDataStructure();
+    DLDataStructure& operator=(const DLDataStructure&);
+    DLDataStructure& operator=(DLDataStructure&&);
+
+  private:
+    double key1_;
+    double key2_;
+    std::string key3_;
+  };
+  std::istream& operator>>(std::istream&, const DLDataStructure&);
+  std::ostream& operator<<(std::ostream&, DLDataStructure&);
+}  
+
+#endif
