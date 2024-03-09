13e2b9d50c3827e761501132c3649409ad926aa4
StantStantov <ibragimovimil0@gmail.com>
2024-03-09 17:41:15 +0300

    Change completly definitions of DSs
    Apparently, I can't f**king read. Changed DataStructure and deleted
    unnecessary DL- and CL- DataStructures

--- ibragimov.imil/T2/dataStructure.hpp
+++ ibragimov.imil/T2/dataStructure.hpp
@@ -1,15 +1,30 @@
 #ifndef DATA_STRUCTURE_HPP
 #define DATA_STRUCTURE_HPP
 
-//TODO: DBL LIT, CMP LSP
+#include <complex>
+#include <iostream>
+#include <string>
+
 namespace ibragimov
 {
   struct DataStructure
   {
   public:
-  virtual ~DataStructure() = default;
-  };
+    DataStructure();
+    DataStructure(const DataStructure&);
+    DataStructure(DataStructure&&);
+    DataStructure(double, double, std::string);
+    ~DataStructure();
+    DataStructure& operator=(const DataStructure&);
+    DataStructure& operator=(DataStructure&&);
 
+  private:
+    double key1_;
+    std::complex<double> key2_;
+    std::string key3_;
+  };
+  std::istream& operator>>(std::istream&, DataStructure&);
+  std::ostream& operator<<(std::ostream&, const DataStructure&);
 }
 
 #endif
