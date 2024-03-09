df9249efaa9ad2360a78537f2c4ad4ed8b05aea3
sbabinov <serafimbabinov106@gmail.com>
2024-03-09 20:32:58 +0300

    Implementation of operators
    * implemented and tested the behavior of the DataStruct arithmetic operators

--- babinov.serafim/T2/dataStruct.cpp
+++ babinov.serafim/T2/dataStruct.cpp
@@ -1,11 +1,42 @@
 #include "dataStruct.hpp"
 
-bool babinov::DataStruct::operator>(const DataStruct& right) const {};
-bool babinov::DataStruct::operator<(const DataStruct& right) const {};
-bool babinov::DataStruct::operator>=(const DataStruct& right) const {};
-bool babinov::DataStruct::operator<=(const DataStruct& right) const {};
-bool babinov::DataStruct::operator==(const DataStruct& right) const {};
-bool babinov::DataStruct::operator!=(const DataStruct& right) const {};
+bool babinov::DataStruct::operator>(const DataStruct& right) const
+{
+  if (key1 != right.key1)
+  {
+    return key1 > right.key1;
+  }
+  if (key2 != right.key2)
+  {
+    return key2 > right.key2;
+  }
+  return key3 > right.key3;
+}
+
+bool babinov::DataStruct::operator<(const DataStruct& right) const
+{
+  return (right > *this);
+}
+
+bool babinov::DataStruct::operator>=(const DataStruct& right) const
+{
+  return !(*this < right);
+}
+
+bool babinov::DataStruct::operator<=(const DataStruct& right) const
+{
+  return !(right < *this);
+}
+
+bool babinov::DataStruct::operator==(const DataStruct& right) const
+{
+  return ((right <= *this) && (*this <= right));
+}
+
+bool babinov::DataStruct::operator!=(const DataStruct& right) const
+{
+  return !(*this == right);
+}
 
 std::istream& babinov::operator>>(std::istream& in, DataStruct& data) {};
 std::ostream& babinov::operator<<(std::ostream& in, const DataStruct& data) {};
--- babinov.serafim/T2/main.cpp
+++ babinov.serafim/T2/main.cpp
@@ -1,10 +1,21 @@
 #include <iostream>
+#include <bitset>
+#include <string>
 
 #include "dataStruct.hpp"
-#include "delimiters.hpp"
-#include "dataProcessing.hpp"
 
 int main()
 {
   using namespace babinov;
+
+  std::cout << "DataStruct operators test:" << '\n';
+  DataStruct struct1{81, 25, "test"};
+  DataStruct struct2{81, 25, "test"};
+  std::cout << (struct1 == struct2) << '\n';
+  struct1.key2 = 37;
+  std::cout << (struct1 < struct2) << '\n';
+  struct2.key1 = 100;
+  std::cout << (struct1 <= struct2) << '\n';
+  struct2.key3 = "testtest";
+  std::cout << (struct1 != struct2) << '\n';
 }
