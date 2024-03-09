8ab9319db401b55743af10e8f90ee7507c3c5baa
C3mR4de <NVadim2005@gmail.com>
2024-03-10 02:20:22 +0300

    Added implementation for StringDelimiterI input

---
+++ novikov.vadim/T2/stringDelimiterI.cpp
@@ -0,0 +1,21 @@
+#include "stringDelimiterI.hpp"
+
+std::istream& novikov::operator>>(std::istream& in, const StringDelimiterI& exp)
+{
+  std::istream::sentry sentry(in);
+  if (sentry)
+  {
+    bool success = true;
+    for (const char* i = exp.expected; *i != '\0' && success; ++i)
+    {
+      char ch = '\0';
+      in >> ch;
+      if (ch != *i)
+      {
+        in.setstate(std::ios::failbit);
+        success = false;
+      }
+    }
+  }
+  return in;
+}
