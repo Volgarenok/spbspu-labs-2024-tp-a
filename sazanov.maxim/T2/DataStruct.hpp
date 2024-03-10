3dac22076e48d226fce36fbbd6a3fbfafdcce108
MaxSazanov <makssazanov31@gmail.com>
2024-03-10 04:28:28 +0300

    changed DataStruct

--- sazanov.maxim/T2/DataStruct.hpp
+++ sazanov.maxim/T2/DataStruct.hpp
@@ -1,12 +1,22 @@
 #ifndef DATA_STRUCT_HPP
 #define DATA_STRUCT_HPP
 
+#include <string>
+#include <utility>
+
 namespace sazanov
 {
   struct DataStruct
   {
+    DataStruct(unsigned long long otherKey1, char otherKey2, std::string  otherKey3):
+      key1(otherKey1),
+      key2(otherKey2),
+      key3(std::move(otherKey3))
+    {}
+
     unsigned long long key1;
     char key2;
+    std::string key3;
   };
 }
 
