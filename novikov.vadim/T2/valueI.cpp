94ce66dbcb3b63e8d06b428f748cec923fe3040a
C3mR4de <NVadim2005@gmail.com>
2024-03-11 01:01:03 +0300

    Added input and output implementations for values
    * Added operator<< and operator>> functions implementations
      for OctValueI, HexValueI and StrValueI structs

---
+++ novikov.vadim/T2/valueI.cpp
@@ -0,0 +1,82 @@
+#include "valueI.hpp"
+#include "charDelimiterI.hpp"
+#include "stringDelimiterI.hpp"
+#include "formatGuard.hpp"
+
+std::istream& novikov::operator>>(std::istream& in, OctValueI&& exp)
+{
+  std::istream::sentry sentry(in);
+  if (sentry)
+  {
+    using chr_del = CharDelimiterI;
+    using oct_val = OctValueI;
+    FormatGuard fmt_guard(in);
+    in >> chr_del{'0'};
+    in >> std::oct;
+    in >> oct_val{exp.expected};
+  }
+  return in;
+}
+
+std::ostream& novikov::operator<<(std::ostream& out, const OctValueI& exp)
+{
+  std::ostream::sentry sentry(out);
+  if (sentry)
+  {
+    FormatGuard fmt_guard(out);
+    out << std::oct;
+    out << '0' << exp.expected;
+  }
+  return out;
+}
+
+std::istream& novikov::operator>>(std::istream& in, HexValueI&& exp)
+{
+  std::istream::sentry sentry(in);
+  if (sentry)
+  {
+    using str_del = StringDelimiterI;
+    using hex_val = HexValueI;
+    FormatGuard fmt_guard(in);
+    in >> str_del{"0x", true};
+    in >> std::hex;
+    in >> hex_val{exp.expected};
+  }
+  return in;
+}
+
+std::ostream& novikov::operator<<(std::ostream& out, const HexValueI& exp)
+{
+  std::ostream::sentry sentry(out);
+  if (sentry)
+  {
+    FormatGuard fmt_guard(out);
+    out << std::hex << std::uppercase;
+    out << "0x" << exp.expected;
+  }
+  return out;
+}
+
+std::istream& novikov::operator>>(std::istream& in, StrValueI&& exp)
+{
+  std::istream::sentry sentry(in);
+  if (sentry)
+  {
+    using chr_del = CharDelimiterI;
+    FormatGuard fmt_guard(in);
+    in >> chr_del{'"'};
+    in >> exp.expected;
+    in >> chr_del{'"'};
+  }
+  return in;
+}
+
+std::ostream& novikov::operator<<(std::ostream& out, const StrValueI& exp)
+{
+  std::ostream::sentry sentry(out);
+  if (sentry)
+  {
+    out << '"' << exp.expected << '"';
+  }
+  return out;
+}
