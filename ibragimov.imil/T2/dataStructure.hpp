e628353d69de37a5fa78e694827101328fb69de0
StantStantov <ibragimovimil0@gmail.com>
2024-03-10 08:38:58 +0300

    Delete constructor for struct dataStructure
    DataStructure does not need custom constructor, at least now.

--- ibragimov.imil/T2/dataStructure.hpp
+++ ibragimov.imil/T2/dataStructure.hpp
@@ -9,14 +9,9 @@ namespace ibragimov
 {
   struct DataStructure
   {
-  public:
     double key1_;
     std::complex<double> key2_;
     std::string key3_;
-
-    DataStructure(const double&, const std::complex<double>&, const std::string&);
-
-  private:
   };
   std::istream& operator>>(std::istream&, DataStructure&);
   std::ostream& operator<<(std::ostream&, const DataStructure&);
