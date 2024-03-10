4eaf38f16cc7faf61bf560063f162d8cff5c9f41
C3mR4de <NVadim2005@gmail.com>
2024-03-11 00:59:41 +0300

    Added ignore case to StringDelimiterI struct
    * Added ignore_case field of type bool
    * Edited implementation of input of StringDelimiterI struct

--- novikov.vadim/T2/stringDelimiterI.cpp
+++ novikov.vadim/T2/stringDelimiterI.cpp
@@ -10,7 +10,9 @@ std::istream& novikov::operator>>(std::istream& in, const StringDelimiterI& exp)
     {
       char ch = '\0';
       in >> ch;
-      if (ch != *i)
+      bool error_ignore = ignore_case && (ch != std::tolower(*i));
+      bool error_no_ignore = !ignore_case && (ch != *i);
+      if (error_ignore || error_no_ignore)
       {
         in.setstate(std::ios::failbit);
         success = false;
--- novikov.vadim/T2/stringDelimiterI.hpp
+++ novikov.vadim/T2/stringDelimiterI.hpp
@@ -8,6 +8,7 @@ namespace novikov
   struct StringDelimiterI
   {
     const char* expected;
+    bool ignore_case = false;
   };
   std::istream& operator>>(std::istream& in, const StringDelimiterI& exp);
 }
