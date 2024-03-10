0025fbb974e87282e3ed2cb818e579a0953aafc0
MaxSazanov <makssazanov31@gmail.com>
2024-03-10 03:10:56 +0300

    added DataStruct

---
+++ sazanov.maxim/T2/DataStruct.hpp
@@ -0,0 +1,13 @@
+#ifndef DATA_STRUCT_HPP
+#define DATA_STRUCT_HPP
+
+namespace sazanov
+{
+  struct DataStruct
+  {
+    unsigned long long key1;
+    char key2;
+  };
+}
+
+#endif //DATA_STRUCT_HPP
