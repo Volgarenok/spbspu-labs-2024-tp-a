d598134c68f98ba942d2d6632ce5e1959088624c
MaxSazanov <makssazanov31@gmail.com>
2024-03-11 14:38:35 +0300

    added input into vector without sorting

--- sazanov.maxim/T2/main.cpp
+++ sazanov.maxim/T2/main.cpp
@@ -1,9 +1,33 @@
 #include "DataStruct.hpp"
 #include <iostream>
+#include <vector>
+#include <iterator>
+#include <algorithm>
+#include <limits>
 
 int main()
 {
-  sazanov::DataStruct exp{0, 0, ""};
-  std::cin >> exp;
-  std::cout << exp.key1 << ' ' << exp.key2 << ' ' << exp.key3;
+  std::vector< sazanov::DataStruct > data;
+
+  using input_it_t = std::istream_iterator< sazanov::DataStruct >;
+  while (!std::cin.eof())
+  {
+    if (std::cin.fail())
+    {
+      std::cin.clear();
+      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    }
+    std::copy(
+      input_it_t{std::cin},
+      input_it_t{},
+      std::back_inserter(data)
+    );
+  }
+
+  using output_it_t = std::ostream_iterator< sazanov::DataStruct >;
+  std::copy(
+    data.cbegin(),
+    data.cend(),
+    output_it_t{std::cout, "\n"}
+  );
 }
