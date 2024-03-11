afb259211a840089e697ce744a2f392cc3f0b2d2
C3mR4de <NVadim2005@gmail.com>
2024-03-11 14:12:18 +0300

    Added check for incorrect input in main.cpp file
    * Included <limits> header
    * Added while (std::cin.eof()) cycle
    * Added clear and ignore in while (std::cin.eof()) cycle

--- novikov.vadim/T2/main.cpp
+++ novikov.vadim/T2/main.cpp
@@ -1,17 +1,23 @@
 #include <vector>
 #include <algorithm>
 #include <iterator>
+#include <limits>
 #include "dataStruct.hpp"
 
 int main()
 {
   using namespace novikov;
-  std::vector< DataStruct > data;
-  std::copy(
-    std::istream_iterator< DataStruct >{std::cin},
-    std::istream_iterator< DataStruct >{},
-    std::back_inserter(data)
-  );
+  std::vector <DataStruct> data;
+  while (!std::cin.eof())
+  {
+    std::copy(
+      std::istream_iterator < DataStruct > {std::cin},
+      std::istream_iterator < DataStruct > {},
+      std::back_inserter(data)
+    );
+    std::cin.clear();
+    std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+  }
   std::sort(data.begin(), data.end());
   std::copy(
     data.cbegin(),
