7521718460dc4543d31f0ce3afd27ef98b462ea1
C3mR4de <NVadim2005@gmail.com>
2024-03-11 19:48:25 +0300

    Added if (std::cin.fail()) condition in main.cpp file

--- novikov.vadim/T2/main.cpp
+++ novikov.vadim/T2/main.cpp
@@ -10,18 +10,20 @@ int main()
   using namespace novikov;
   using input_it_t = std::istream_iterator< DataStruct >;
   using output_it_t = std::ostream_iterator< DataStruct >;
+
   std::vector< DataStruct > data;
   while (!std::cin.eof())
   {
-    std::copy(
-      input_it_t{ std::cin },
-      input_it_t{},
-      std::back_inserter(data)
-    );
-    std::cin.clear();
-    std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+    std::copy(input_it_t{ std::cin }, input_it_t{}, std::back_inserter(data));
+    if (std::cin.fail())
+    {
+      std::cin.clear();
+      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    }
   }
+
   std::sort(data.begin(), data.end());
+
   std::copy(
     data.cbegin(),
     data.cend(),
