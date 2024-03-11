7d92c670a2c8299995efb118f6e434dc2a33a10a
C3mR4de <NVadim2005@gmail.com>
2024-03-11 13:17:39 +0300

    Added comparison operators declarations for DataStruct
    * Added operator<, operator>, operator<=, operator>=,
      operator==, operator!= declarations to dataStruct.hpp file

--- novikov.vadim/T2/dataStruct.hpp
+++ novikov.vadim/T2/dataStruct.hpp
@@ -12,6 +12,12 @@ namespace novikov
     ull key1;
     ull key2;
     std::string key3;
+    bool operator<(const DataStruct& other) const;
+    bool operator>(const DataStruct& other) const;
+    bool operator<=(const DataStruct& other) const;
+    bool operator>=(const DataStruct& other) const;
+    bool operator==(const DataStruct& other) const;
+    bool operator!=(const DataStruct& other) const;
   };
   std::istream& operator>>(std::istream& in, DataStruct& value);
   std::ostream& operator<<(std::ostream& out, const DataStruct& value);
