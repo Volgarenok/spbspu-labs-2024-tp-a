4bf7bfc347eda5a0fbd034a3e4102a3a43a66a83
C3mR4de <NVadim2005@gmail.com>
2024-03-11 13:34:11 +0300

    Added comparison operators implementations for DataStruct
    * Added operator<, operator>, operator<=, operator>=,
      operator== and operator!= implementations in dataStruct.cpp
      file

--- novikov.vadim/T2/dataStruct.cpp
+++ novikov.vadim/T2/dataStruct.cpp
@@ -4,6 +4,50 @@
 #include "valueI.hpp"
 #include "valueO.hpp"
 
+bool novikov::operator<(const DataStruct& other) const
+{
+  if (key1 == other.key1)
+  {
+    if (key2 == other.key2)
+    {
+      return (key3.size() < other.key3.size());
+    }
+    return (key2 < other.key2);
+  }
+  return (key1 < other.key1);
+}
+
+bool novikov::operator>(const DataStruct& other) const
+{
+  return (other < *this);
+}
+
+bool novikov::operator<=(const DataStruct& other) const
+{
+  return !(other < *this);
+}
+
+bool novikov::operator>=(const DataStruct& other) const
+{
+  return !(*this < other);
+}
+
+bool novikov::operator==(const DataStruct& other) const
+{
+  if (key1 == other.key1)
+  {
+    if (key2 == other.key2)
+    {
+      return key3.size() == other.key3.size();
+    }
+  }
+}
+
+bool novikov::operator!=(const DataStruct& other) const
+{
+  return !(*this == other);
+}
+
 std::istream& novikov::operator>>(std::istream& in, DataStruct& value)
 {
   std::istream::sentry sentry(in);
