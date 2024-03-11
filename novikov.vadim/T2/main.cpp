8c65b6d7c687e65cc50d339140f901edb7dfbf0e
C3mR4de <NVadim2005@gmail.com>
2024-03-11 12:49:37 +0300

    Added basic implementation for main.cpp file

--- novikov.vadim/T2/main.cpp
+++ novikov.vadim/T2/main.cpp
@@ -1,2 +1,23 @@
+#include <vector>
+#include <algorithm>
+#include <iterator>
+#include "dataStruct.hpp"
+#include "valueI.hpp"
+#include "charDelimiterI.hpp"
+#include "stringDelimiterI.hpp"
+
 int main()
-{}
+{
+  using namespace novikov;
+  std::vector< DataStruct > data;
+  std::copy(
+    std::istream_iterator< DataStruct >{std::cin},
+    std::istream_iterator< DataStruct >{},
+    std::back_inserter(data)
+  );
+  std::copy(
+    data.cbegin(),
+    data.cend(),
+    std::ostream_iterator< DataStruct >{std::cout, "\n"}
+  );
+}
