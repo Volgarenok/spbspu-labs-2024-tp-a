11a1cfb88a76768b128968320f1efc5d674b1872
artemkpwr <netd698@gmail.com>
2024-03-13 02:57:56 +0300

    added delimeters and input double

--- kornienko.artem/T2/main.cpp
+++ kornienko.artem/T2/main.cpp
@@ -2,11 +2,86 @@
 #include <vector>
 #include <algorithm>
 #include <iterator>
+#include <cctype>
 
 struct DataStruct
 {
-  private:
-    double key1_;
+  double key1;
+  unsigned long long key2;
+  std::string key3;
+};
+
+struct DelimeterI
+{
+  char expected;
+};
+
+struct DelimeterIgnoreRegister
+{
+  char expected;
+};
+
+std::istream & operator>>(std::istream & in, DelimeterI && exp)
+{
+  std::istream::sentry guard(in);
+  if (!guard)
+  {
+    return in;
+  }
+  char c = 0;
+  in >> c;
+  if (c != exp.expected)
+  {
+    in.setstate(std::ios::failbit);
+  }
+  return in;
+}
+
+std::istream & operator>>(std::istream & in, DelimeterIgnoreRegister && exp)
+{
+  std::istream::sentry guard(in);
+  if (!guard)
+  {
+    return in;
+  }
+  char c = 0;
+  in >> c;
+  if (std::tolower(c) != exp.expected)
+  {
+    in.setstate(std::ios::failbit);
+  }
+  return in;
+}
+
+std::istream & operator>>(std::istream & in, DataStruct & value)
+{
+  std::istream::sentry guard(in);
+  if (!guard)
+  {
+    return in;
+  }
+  using del = DelimeterI;
+  using delIgnReg = DelimeterIgnoreRegister;
+  double key1 = 0;
+  unsigned long long key2 = 0;
+  std::string key3 = "";
+  in >> key1 >> delIgnReg{'d'};
+  if (in)
+  {
+    value.key1 = key1;
+  }
+  return in;
+}
+
+std::ostream & operator<<(std::ostream & out, const DataStruct & value)
+{
+  std::ostream::sentry guard(out);
+  if (!guard)
+  {
+    return out;
+  }
+  out << value.key1 << "d";
+  return out;
 }
 
 int main()
@@ -14,6 +89,7 @@ int main()
   using input_it_t = std::istream_iterator< DataStruct >;
   std::vector< DataStruct > data(input_it_t{std::cin}, input_it_t{});
 
-  using output_it_t = std::ostream_iterator< DataStruct >;
+  using output_it_t = std::ostream_iterator< DataStruct  >;
   std::copy(data.cbegin(), data.cend(), output_it_t{std::cout, "\n"});
+  return 0;
 }
