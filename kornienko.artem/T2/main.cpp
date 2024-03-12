cc9aaf5865f3c2bf49b811f9b8faed8ec8c20565
artemkpwr <netd698@gmail.com>
2024-03-12 15:03:45 +0300

    input and output void DataStruct

--- kornienko.artem/T2/main.cpp
+++ kornienko.artem/T2/main.cpp
@@ -1,2 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+
+struct DataStruct
+{
+  private:
+    double key1_;
+}
+
 int main()
-{}
+{
+  using input_it_t = std::istream_iterator< DataStruct >;
+  std::vector< DataStruct > data(input_it_t{std::cin}, input_it_t{});
+
+  using output_it_t = std::ostream_iterator< DataStruct >;
+  std::copy(data.cbegin(), data.cend(), output_it_t{std::cout, "\n"});
+}
