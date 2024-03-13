6552e796c0877c6c0bd7f51ce1c41417e2f1c8b1
Glaz0k <nikitakravcenko892@gmail.com>
2024-03-13 20:58:32 +0300

    changed vector to list

--- kravchenko.nikita/T2/main.cpp
+++ kravchenko.nikita/T2/main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <vector>
+#include <list>
 #include <algorithm>
 #include <iterator>
 #include "dataStruct.hpp"
@@ -8,7 +8,7 @@ int main()
 {
   using namespace kravchenko;
   using inputItT = std::istream_iterator< DataStruct >;
-  std::vector< DataStruct > data;
+  std::list< DataStruct > data;
   while (!std::cin.eof())
   {
     std::copy(inputItT{ std::cin }, inputItT{}, std::back_inserter(data));
@@ -16,7 +16,7 @@ int main()
     std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
   }
 
-  std::sort(data.begin(), data.end());
+  data.sort();
 
   using outputItT = std::ostream_iterator< DataStruct >;
   std::copy(data.cbegin(), data.cend(), outputItT{ std::cout, "\n" });
