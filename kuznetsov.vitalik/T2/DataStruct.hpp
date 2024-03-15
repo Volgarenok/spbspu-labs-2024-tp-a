182daf94cd4055aaa0d9e84073c21d1b3bcf3ec6
VitalikKuznetsov <ssiromas@gmail.com>
2024-03-15 19:21:01 +0300

    DataStruct with operator<<

--- kuznetsov.vitalik/T2/DataStruct.hpp
+++ kuznetsov.vitalik/T2/DataStruct.hpp
@@ -1,5 +1,5 @@
-#ifndef DATA_STRUCT_H
-#define DATA_STRUCT_H
+#ifndef DATA_STRUCT_HPP
+#define DATA_STRUCT_HPP
 
 #include <iostream>
 #include <complex>
@@ -9,15 +9,18 @@ namespace kuznetsov
 {
   struct DataStruct
   {
-    DataStruct():
-     key1(0),
-     key2(0),
-     key3("")
+    DataStruct() :
+      key1(1),
+      key2(1.0, -1.0),
+      key3("hi")
     {}
+
     unsigned long long key1;
     std::complex<double> key2;
     std::string key3;
   };
+
+  std::ostream& operator<<(std::ostream& out, const DataStruct& data);
 }
 
-#endif //DATA_STRUCT_H
+#endif
