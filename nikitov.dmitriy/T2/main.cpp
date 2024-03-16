0c9eb81f8b2d7053799df50a5aebfaff52a48d68
Dmitriy <Dmitry5335@yandex.ru>
2024-03-16 18:27:06 +0300

    Add iterators, base input and output

--- nikitov.dmitriy/T2/main.cpp
+++ nikitov.dmitriy/T2/main.cpp
@@ -1,2 +1,15 @@
+#include <iostream>
+#include <iterator>
+#include <vector>
+#include <algorithm>
+#include "data_struct.hpp"
+
 int main()
-{}
+{
+  using namespace nikitov;
+  using input_it_t = std::istream_iterator< DataStruct >;
+  std::vector< DataStruct > data(input_it_t{std::cin}, input_it_t{});
+
+  using output_it_t = std::ostream_iterator< DataStruct >;
+  std::copy(data.cbegin(), data.cend(), output_it_t{std::cout, "\n"});
+}
