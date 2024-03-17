6cd52c6959b748957f7210c3c0374c796e29e810
Eliseyz <elisevz@mail.ru>
2024-03-17 22:02:29 +0300

    Added streamGuard

---
+++ zaparin.elisei/T2/streamGuard.hpp
@@ -0,0 +1,20 @@
+#ifndef STREAM_GUARD
+#define STREAM_GUARD
+
+#include<ios>
+
+namespace zaparin
+{
+  class StreamGuard {
+  private:
+    std::basic_ios<char>& s_;
+    std::streamsize precision_;
+    std::basic_ios<char>::fmtflags flags_;
+
+  public:
+    StreamGuard(std::basic_ios<char>& s);
+    ~StreamGuard();
+  };
+}
+
+#endif
