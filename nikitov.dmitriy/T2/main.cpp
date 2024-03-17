d66af223993975e904f22a1ec3da7dc017a75a91
Dmitriy <Dmitry5335@yandex.ru>
2024-03-17 13:02:07 +0300

    Change vector to list

--- nikitov.dmitriy/T2/main.cpp
+++ nikitov.dmitriy/T2/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <iterator>
-#include <vector>
+#include <list>
 #include <algorithm>
 #include "data_struct.hpp"
 
@@ -8,7 +8,7 @@ int main()
 {
   using namespace nikitov;
   using input_it_t = std::istream_iterator< DataStruct >;
-  std::vector< DataStruct > data(input_it_t{std::cin}, input_it_t{});
+  std::list< DataStruct > data(input_it_t{std::cin}, input_it_t{});
   while (!std::cin.eof())
   {
     if (std::cin.fail())
