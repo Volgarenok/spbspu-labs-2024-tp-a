bb953dfd8a387c27da014c60f5039b88e781adcc
k-daria <kuzminada1005@gmail.com>
2024-03-18 00:17:03 +0300

    Added output

--- kuzmina.daria/T2/DataStruct.cpp
+++ kuzmina.daria/T2/DataStruct.cpp
@@ -1,5 +1,5 @@
 #include "DataStruct.hpp"
-
+#include <iostream>
 #include "DelimiterI.hpp"
 
 bool kuzmina::DataStruct::operator<(const DataStruct& other) const
@@ -17,4 +17,18 @@ bool kuzmina::DataStruct::operator<(const DataStruct& other) const
 
 std::istream& kuzmina::operator>>(std::istream&, DataStruct&) {}
 
-std::ostream& kuzmina::operator<<(std::ostream&, const DataStruct&) {}
+std::ostream& kuzmina::operator<<(std::ostream&, const DataStruct&)
+std::ostream& kuzmina::operator<<(std::ostream& out, const DataStruct& value)
+{
+  std::ostream::sentry guard(out);
+  if (!guard)
+  {
+    return out;
+  }
+
+  out << "(:key1 " << value.key1 << "ll"
+      << ":key2 (:N " << value.key2.first << ":D " << value.key2.second<< ":)"
+      << ":key3 \"" << value.key3 << "\":)";
+
+  return out;
+}
