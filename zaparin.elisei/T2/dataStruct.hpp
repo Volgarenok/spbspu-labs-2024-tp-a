c4d3cb265bfe794493405cebfc5f36de3803be63
Eliseyz <elisevz@mail.ru>
2024-03-17 22:27:36 +0300

    Added dataStruct.hpp

---
+++ zaparin.elisei/T2/dataStruct.hpp
@@ -0,0 +1,20 @@
+#ifndef DATA_STRUCT
+#define DATA_STRUCT
+
+#include<string>
+#include<istream>
+#include<ostream>
+
+namespace zaparin
+{
+  struct DataStruct {
+    unsigned long long key1;
+    std::pair<long long, unsigned long long> key2;
+    std::string key3;
+  };
+
+  std::istream& operator>>(std::istream& in, DataStruct& data);
+  std::ostream& operator<<(std::ostream& out, DataStruct& data);
+}
+
+#endif
