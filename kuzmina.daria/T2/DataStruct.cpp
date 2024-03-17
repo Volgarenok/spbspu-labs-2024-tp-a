6dca18c5facde14963a88b6b3ca0ad3911726f65
k-daria <kuzminada1005@gmail.com>
2024-03-17 22:57:08 +0300

    Added operator<

--- kuzmina.daria/T2/DataStruct.cpp
+++ kuzmina.daria/T2/DataStruct.cpp
@@ -1,3 +1,20 @@
 #include "DataStruct.hpp"
 
 #include "DelimiterI.hpp"
+
+bool kuzmina::DataStruct::operator<(const DataStruct& other) const
+{
+  if (key1 == other.key1)
+  {
+    if (key2.first / key2.second == other.key2.first / other.key2.second)
+    {
+      return key3.length() < other.key3.length();
+    }
+    return key2.first / key2.second < other.key2.first / other.key2.second;
+  }
+  return key1 < other.key1;
+}
+
+std::istream& kuzmina::operator>>(std::istream&, DataStruct&) {}
+
+std::ostream& kuzmina::operator<<(std::ostream&, const DataStruct&) {}
