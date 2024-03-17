3d028dd33f0afac1d9611783a0b7b00bae37f201
TheOneFoxAgo <nikitad2005@gmail.com>
2024-03-17 19:29:00 +0300

    Always init your structs kids!

--- demidenko.nikita/T2/main.cpp
+++ demidenko.nikita/T2/main.cpp
@@ -4,7 +4,7 @@
 int main()
 {
   // Пока введу хотя б один дата структ, а там посмотрим
-  demidenko::DataStruct data;
+  demidenko::DataStruct data{};
   std::cin >> data;
   std::cout << data;
   return 0;
