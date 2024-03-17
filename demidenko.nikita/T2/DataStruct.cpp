77b45e2018166a475b7e8ee61efbaad69d2da290
TheOneFoxAgo <nikitad2005@gmail.com>
2024-03-17 16:47:20 +0300

    Implemented input for DataStruct with dummy types. Added headers of "T2 enterprise edition"

---
+++ demidenko.nikita/T2/DataStruct.cpp
@@ -0,0 +1,40 @@
+#include "DataStruct.hpp"
+#include <iomanip>
+#include <ios>
+#include <iostream>
+#include <istream>
+#include "DataStructBuilder.hpp"
+#include "Delimeter.hpp"
+#include "StreamGuard.hpp"
+
+std::istream& demidenko::operator>>(std::istream& in, DataStruct& data)
+{
+  std::istream::sentry sentry(in);
+  if (!sentry)
+  {
+    return in;
+  }
+  demidenko::StreamGuard guard(in);
+  in >> std::setprecision(1) >> std::fixed >> std::noskipws;
+
+  demidenko::DataStructBuilder builder(data);
+  using del = demidenko::DelimeterI;
+  using key = demidenko::KeyParser;
+  using field = demidenko::FieldParser;
+
+  in >> del{"(:"};
+  for (int i = 0; i < 3; i++)
+  {
+    in >> key(builder) >> del{" "} >> field(builder) >> del{":"};
+  }
+  in >> del{")"};
+  if (!builder.isDone())
+  {
+    in.setstate(std::ios::failbit);
+  }
+  return in;
+}
+std::ostream& demidenko::operator<<(std::ostream& out, DataStruct& data)
+{
+  return out;
+}
---
+++ demidenko.nikita/T2/DataStructBuilder.hpp
@@ -0,0 +1,44 @@
+#ifndef DATA_STRUCT_BUILDER_HPP
+#define DATA_STRUCT_BUILDER_HPP
+
+#include "DataStruct.hpp"
+#include "KeysEnum.hpp"
+namespace demidenko
+{
+  class DataStructBuilder
+  {
+  public:
+    DataStructBuilder(DataStruct& data);
+    bool isDone() const noexcept;
+
+  private:
+    DataStruct& data_;
+    KeysEnum current_key_;
+    unsigned int keys_;
+
+    friend class FieldParser;
+    friend class KeyParser;
+  };
+
+  class KeyParser
+  {
+  public:
+    KeyParser(DataStructBuilder& builder);
+
+  private:
+    DataStructBuilder& builder_;
+  };
+
+  class FieldParser
+  {
+  public:
+    FieldParser(DataStructBuilder& builder);
+
+  private:
+    DataStructBuilder& builder_;
+  };
+
+  std::istream& operator>>(std::istream& in, KeyParser&& parser);
+  std::istream& operator>>(std::istream& in, FieldParser&& parser);
+}
+#endif
