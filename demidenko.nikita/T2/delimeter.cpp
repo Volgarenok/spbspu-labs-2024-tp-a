5eba656c414d15c69ffc020d62aa5237260624e7
TheOneFoxAgo <nikitad2005@gmail.com>
2024-03-17 17:20:32 +0300

    Implemented delimeter. I can't care less about inefficiency of always using strings. DRY

---
+++ demidenko.nikita/T2/delimeter.cpp
@@ -0,0 +1,26 @@
+#include "delimeter.hpp"
+#include <ios>
+#include <istream>
+#include "streamGuard.hpp"
+
+std::istream& demidenko::operator>>(std::istream& in, DelimeterI&& del)
+{
+  demidenko::StreamGuard guard(in);
+  in >> std::noskipws;
+  std::istream::sentry sentry(in);
+  if (!sentry)
+  {
+    return in;
+  }
+  char current_character = ' ';
+  while (in && *del.delimeter_)
+  {
+    in >> current_character;
+    if (*del.delimeter_ != current_character)
+    {
+      in.setstate(std::ios::failbit);
+      return in;
+    }
+  }
+  return in;
+}
