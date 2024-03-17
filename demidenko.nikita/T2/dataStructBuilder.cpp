3050845f48612da029b3f9d533a791e10ff75e29
TheOneFoxAgo <nikitad2005@gmail.com>
2024-03-17 18:36:34 +0300

    Actually done something useful. Implemented FieldParser.

--- demidenko.nikita/T2/dataStructBuilder.cpp
+++ demidenko.nikita/T2/dataStructBuilder.cpp
@@ -1,4 +1,6 @@
 #include "dataStructBuilder.hpp"
+#include <complex>
+#include <iomanip>
 #include "dataStruct.hpp"
 #include "delimeter.hpp"
 #include "keysEnum.hpp"
@@ -16,11 +18,6 @@ bool demidenko::DataStructBuilder::isDone() const noexcept
 
 std::istream& demidenko::operator>>(std::istream& in, KeyParser&& parser)
 {
-  if (parser.builder_.isDone())
-  {
-    in.setstate(std::ios::failbit);
-    return in;
-  }
   demidenko::StreamGuard guard(in);
   in >> std::noskipws;
   std::istream::sentry sentry(in);
@@ -28,10 +25,14 @@ std::istream& demidenko::operator>>(std::istream& in, KeyParser&& parser)
   {
     return in;
   }
+
   int key_number = 0;
   in >> DelimeterI{"key"} >> key_number;
   key_number <<= key_number;
-  if (!key_number || key_number > demidenko::KeysEnum::ALL)
+
+  bool is_in_range = !key_number || key_number > demidenko::KeysEnum::ALL;
+  bool is_unique = !(key_number & parser.builder_.keys_);
+  if (!(is_in_range || is_unique))
   {
     in.setstate(std::ios::failbit);
     return in;
@@ -40,4 +41,44 @@ std::istream& demidenko::operator>>(std::istream& in, KeyParser&& parser)
   return in;
 }
 
-std::istream& demidenko::operator>>(std::istream& in, FieldParser&& parser);
+std::istream& demidenko::operator>>(std::istream& in, FieldParser&& parser)
+{
+  demidenko::StreamGuard guard(in);
+  in >> std::noskipws;
+  std::istream::sentry sentry(in);
+  if (!sentry)
+  {
+    return in;
+  }
+
+  KeysEnum key = parser.builder_.current_key_;
+  DataStruct& data = parser.builder_.data_;
+  using del = DelimeterI;
+  switch (key)
+  {
+  case ULL_OCT:
+    in >> del{"0"} >> std::oct >> data.key1;
+    break;
+  case CMP_LSP:
+  {
+    in >> del{"#c("} >> std::setprecision(1);
+    double real = 0.0;
+    double imaginary = 0.0;
+    in >> real >> del{" "} >> imaginary >> del{")"};
+    data.key2 = std::complex< double >(real, imaginary);
+    break;
+  }
+  case STRING:
+    in >> del{"\""} >> data.key3 >> del{"\""};
+    break;
+  default:
+    in.setstate(std::ios::failbit);
+    return in;
+  }
+
+  if (in.good())
+  {
+    parser.builder_.keys_ |= key;
+  }
+  return in;
+}
