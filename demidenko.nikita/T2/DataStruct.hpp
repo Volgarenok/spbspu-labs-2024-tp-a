f8b505c2731950a60b03bc957f042898334966c7
TheOneFoxAgo <nikitad2005@gmail.com>
2024-03-17 14:19:02 +0300

    Implemented simple main.cpp and added DataStruct.hpp

---
+++ demidenko.nikita/T2/DataStruct.hpp
@@ -0,0 +1,20 @@
+#ifndef DATA_STRUCT_HPP
+#define DATA_STRUCT_HPP
+
+#include <complex>
+#include <iosfwd>
+#include <string>
+
+namespace demidenko
+{
+  struct DataStruct
+  {
+    unsigned long long key1;
+    std::complex< double > key2;
+    std::string key3;
+  };
+
+  std::istream& operator>>(std::istream& in, DataStruct& data);
+  std::ostream& operator<<(std::ostream& out, DataStruct& data);
+}
+#endif
--- demidenko.nikita/T2/main.cpp
+++ demidenko.nikita/T2/main.cpp
@@ -1,4 +1,11 @@
+#include <iostream>
+#include "DataStruct.hpp"
+
 int main()
 {
+  // Пока введу хотя б один дата структ, а там посмотрим
+  demidenko::DataStruct data;
+  std::cin >> data;
+  std::cout << data;
   return 0;
 }
