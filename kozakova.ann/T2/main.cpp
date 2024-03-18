3b0a72f953275332c1e61b1839863803441d7862
KozakovaAnn <kozakova.ann@mail.ru>
2024-03-18 17:45:50 +0300

    + all enter

--- kozakova.ann/T2/main.cpp
+++ kozakova.ann/T2/main.cpp
@@ -2,12 +2,14 @@
 #include <string>
 #include <cstring>
 #include <algorithm>
+
 namespace kozakova {
-  struct Data
+  struct DataStruct
   {
-    Data(unsigned long long k1, unsigned long long k2) :
+    DataStruct(unsigned long long k1, unsigned long long k2, std::string k3) :
       key1(k1),
-      key2(k2)
+      key2(k2),
+      key3(k3)
     {
     }
     unsigned long long get_Key1() const
@@ -18,15 +20,32 @@ namespace kozakova {
     {
       return key2;
     }
+    std::string get_Key3() const
+    {
+      return key3;
+    }
+    void set_Key1(unsigned long long k1)
+    {
+      key1 = k1;
+    }
+    void set_Key2(unsigned long long k2)
+    {
+      key2 = k2;
+    }
+    void set_Key3(std::string k3)
+    {
+      key3 = k3;
+    }
   private:
     unsigned long long key1;
     unsigned long long key2;
-    //std::string key3;
+    std::string key3;
   };
 
   struct DelimiterString
   {
     std::string expected;
+    bool f;
   };
 
   std::istream& operator>>(std::istream& in, DelimiterString&& exp)
@@ -37,12 +56,23 @@ namespace kozakova {
       return in;
     }
     std::string s = "";
-    std::string expUp = exp.expected;
-    std::transform(exp.expected.begin(), exp.expected.end(), exp.expected.begin(), ::tolower);
-    in >> s;
-    if ((strcmp(s.c_str(), exp.expected.c_str()) && strcmp(s.c_str(), expUp.c_str()))) 
+    if (exp.f)
     {
-      in.setstate(std::ios::failbit);
+      std::getline(in, s, ':');
+      std::string expUp = exp.expected;
+      std::transform(exp.expected.begin(), exp.expected.end(), exp.expected.begin(), ::tolower);
+      if ((strcmp(s.c_str(), exp.expected.c_str()) && strcmp(s.c_str(), expUp.c_str())))
+      {
+        in.setstate(std::ios::failbit);
+      }
+    }
+    else
+    {
+      in >> s;
+      if (strcmp(s.c_str(), exp.expected.c_str()))
+      {
+        in.setstate(std::ios::failbit);
+      }
     }
     return in;
   };
@@ -68,7 +98,46 @@ namespace kozakova {
     return in;
   };
 
-  std::istream& operator>>(std::istream& in, Data& value)
+  void setKeys(std::istream& in, std::string key, DataStruct& value)
+  {
+    using dels = DelimiterString;
+    using delc = DelimiterChar;
+    if (key == "key1")
+    {
+      unsigned long long a = 0;
+      in >> std::dec >> a >> dels{ "ULL", true };
+      if (in)
+      {
+        value.set_Key1(a);
+      }
+    }
+    else if (key == "key2")
+    {
+      unsigned long long b = 0;
+      in >> delc{ '0' } >> delc{ 'X' } >> std::hex >> b >> delc{ ':' };
+      if (in)
+      {
+        value.set_Key2(b);
+      }
+    }
+    else if (key == "key3")
+    {
+      std::string s = "";
+      in >> delc{ '"' };
+      std::getline(in, s, '"');
+      in >> delc{ ':' };
+      if (in)
+      {
+        value.set_Key3(s);
+      }
+    }
+    else
+    {
+      in.setstate(std::ios::failbit);
+    }
+  }
+
+  std::istream& operator>>(std::istream& in, DataStruct& value)
   {
     std::istream::sentry guard(in);
     if (!guard)
@@ -77,31 +146,35 @@ namespace kozakova {
     }
     using dels = DelimiterString;
     using delc = DelimiterChar;
-    unsigned long long a = 0;
-    unsigned long long b = 0;
-    in >> a >> dels{ "ULL" } >> delc{ '0' } >> delc{ 'X' } >> std::hex >> b;
-    if (in)
+    std:: string key = "";
+    in >> delc{ '(' } >> delc{ ':' };
+    for (int i = 0; i < 3 && in; i++)
     {
-      value = Data(a, b);
+      in >> key;
+      setKeys(in, key, value);
     }
+    in >> delc{ ')' };
     return in;
   };
 
-  std::ostream& operator<<(std::ostream& out, const Data& value)
+  std::ostream& operator<<(std::ostream& out, const DataStruct& value)
   {
     std::ostream::sentry guard(out);
     if (!guard)
     {
       return out;
     }
-    out << value.get_Key1() << "ull" << " " << "0x" << std::uppercase << std::hex << value.get_Key2();
+    out << "(:key1 " << value.get_Key1() << "ull";
+    out << ":key2 " << "0x" << std::uppercase << std::hex << value.get_Key2();
+    out << ":key3 \"" << value.get_Key3() << "\":)";
     return out;
   };
+
 }
 
 int main()
 {
-  kozakova::Data a(0,0);
+  kozakova::DataStruct a(0,0, "");
   std::cin >> a;
   std::cout << a;
   return 0;
