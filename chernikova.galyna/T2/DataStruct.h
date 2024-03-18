0789cb3b0e6d618cc9d38aa05ffc006185652218
chernikovaG <chernikova.gv@edu.spbstu.ru>
2024-03-18 15:09:42 +0300

    Added DataStruct

---
+++ chernikova.galyna/T2/DataStruct.h
@@ -0,0 +1,16 @@
+#ifndef DATA_STRUCT_H
+#define DATA_STRUCT_H
+
+#include <string>
+
+namespace chernikova
+{
+  struct DataStruct
+  {
+    double key1;
+    unsigned long long key2;
+    std::string key3;
+  };
+}
+
+#endif
