cbe89bdf2d24294451adc951581d0172b9199810
kastiela <kristinazak150@gmail.com>
2024-03-18 17:18:12 +0300

    add struct delimeter

--- zakozhurnikova.kristina/T2/dataStruct.hpp
+++ zakozhurnikova.kristina/T2/dataStruct.hpp
@@ -22,3 +22,4 @@ namespace zakozhurnikova
   std::istream& operator>>(std::istream& in, DataStruct& data);
   std::ostream& operator<<(std::ostream& out, const DataStruct& data);
 }
+#endif
---
+++ zakozhurnikova.kristina/T2/delimeter.hpp
@@ -0,0 +1,11 @@
+#ifndef DELIMETERS_HPP
+#define DELIMETERS_HPP
+
+namespace zakozhurnikova
+{
+  struct Delimeter
+  {
+    char expected;
+  };
+}
+#endif
