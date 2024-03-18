fcdd1cc34dfbd2c8e50760da3959af129de8095a
Anyanari <a.erfurt02@mail.ru>
2024-03-18 15:27:02 +0300

    Added DataStruct

---
+++ erfurt.anna/T2/DataStruct.hpp
@@ -0,0 +1,22 @@
+#ifndef DATA_STRUCT_HPP
+#define DATA_STRUCT_HPP
+
+#include <string>
+
+namespace erfurt
+{
+  struct DataStruct
+  {
+    long long key1 = 0;
+    unsigned long long key2 = 0;
+    std::string key3 = "";
+
+    bool operator<(const DataStruct& data) const;
+  };
+
+  std::istream &operator>>(std::istream &in, DataStruct &value);
+  std::ostream &operator<<(std::ostream &out, const DataStruct &value);
+}
+
+#endif //DATA_STRUCH_HPP 
+
