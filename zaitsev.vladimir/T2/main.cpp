688ee79afa7f936f0ecf55fb40061c50d2a8a419
v-zaycev <70699650+v-zaycev@users.noreply.github.com>
2024-03-18 20:08:06 +0300

    Main added

--- zaitsev.vladimir/T2/main.cpp
+++ zaitsev.vladimir/T2/main.cpp
@@ -1,2 +1,25 @@
+#include <iostream>
+#include <list>
+#include <limits>
+#include "data_struct.hpp"
+
 int main()
-{}
+{
+  using namespace zaitsev;
+  using input_it = std::istream_iterator< DataStruct >;
+  using output_it = std::ostream_iterator< DataStruct >;
+
+  std::list< DataStruct > structs;
+  while (!std::cin.eof())
+  {
+    if (std::cin.fail())
+    {
+      std::cin.clear();
+      std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+    }
+    std::copy(input_it(std::cin), input_it(), std::back_inserter(structs));
+  }
+  structs.sort();
+  std::copy(structs.begin(), structs.end(), output_it(std::cout, "\n"));
+  return 0;
+}
