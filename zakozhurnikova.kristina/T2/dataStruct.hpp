ba3c9264f6a0d0edaff58ec0dabe8c4ad7da7012
kastiela <kristinazak150@gmail.com>
2024-03-18 15:33:45 +0300

    realization struct DataStruct (types key)

---
+++ zakozhurnikova.kristina/T2/dataStruct.hpp
@@ -0,0 +1,14 @@
+#ifndef DATA_STRUCT_HPP
+#define DATA_STRUCT_HPP
+
+namespace zakozhurnikova
+{
+  struct DataStruct
+  {
+    using ull = unsigned long long;
+
+    double key1;
+    ull key2;
+    std::string key3;
+  }
+}
