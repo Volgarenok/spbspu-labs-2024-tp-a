7acc059d0ebd4dd8819547e9e17bbbfcb8cdd67a
Anyanari <a.erfurt02@mail.ru>
2024-03-18 15:44:09 +0300

    Realize Delimeter

---
+++ erfurt.anna/T2/Delimeter.cpp
@@ -0,0 +1,23 @@
+#include "Delimeter.hpp"
+
+#include <cstring>
+
+std::istream &erfurt::operator>>(std::istream &in, Delimeter &&exp)
+{
+  std::istream::sentry guard(in);
+  if (!guard)
+  {
+    returt in;
+  }
+  char c = 0;
+  for (size_t i = 0; exp.expected[i] && in; ++i)
+  {
+    in >> c;
+    c = tolower(c);
+    if (c != exp.expected[i])
+    {
+      in.setstate(std::ios::failbit);
+    }
+  }
+  return in;
+}
