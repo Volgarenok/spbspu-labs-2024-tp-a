368eee511b148f027ee7a5510729c2f12f8ede3d
chernikovaG <chernikova.gv@edu.spbstu.ru>
2024-03-18 18:25:43 +0300

    Added Delimiter

---
+++ chernikova.galyna/T2/Delimiter.cpp
@@ -0,0 +1,2 @@
+#include "Delimiter.h"
+
---
+++ chernikova.galyna/T2/Delimiter.h
@@ -0,0 +1,14 @@
+#ifndef DELIMITER_H
+#define DELIMITER_H
+
+#include <iostream>
+
+namespace chernikova
+{
+  struct DelimiterChar
+  {
+    const char expected;
+  };
+}
+
+#endif
