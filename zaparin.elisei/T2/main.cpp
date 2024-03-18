624f3c7ef310eaa43dbbc1fcefc2bdf80b869828
Eliseyz <elisevz@mail.ru>
2024-03-18 18:57:36 +0300

    Trying to fix CG

--- zaparin.elisei/T2/main.cpp
+++ zaparin.elisei/T2/main.cpp
@@ -1,7 +1,24 @@
 #include"delimeter.hpp"
 #include<iostream>
 
+#include<fstream>
+#include<string>
+
 int main()
 {
-  return 0;
+  int num = 0b010101;
+  std::string text = "";
+
+  std::ifstream in;
+  in.open("text.txt");
+  if (in)
+  {
+    std::cout << "Good";
+  }
+  else
+  {
+    std::cerr << "Error!";
+  }
+
+  in.close();
 }
