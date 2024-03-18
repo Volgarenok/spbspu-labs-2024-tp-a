80e0a134bb0192edeaf354b2cc09cae5da87b3a9
MaxArtemev <maxartemev@mail.ru>
2024-03-18 13:34:09 +0300

    add DataStruct.hpp

---
+++ artemev.max/T2/DataStruct.hpp
@@ -0,0 +1,16 @@
+#ifndef DATA_STRUCT_HPP
+#define DATA_STRUCT_HPP
+
+#include "Delimiter.hpp"
+
+namespace artemev
+{
+  struct DataStruct
+  {
+    double key1;
+    double key2;
+    std::string key3;
+  };
+}
+
+#endif
