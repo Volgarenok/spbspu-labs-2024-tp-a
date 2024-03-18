c46f147678b1f97c6eca6893dd02a3cae19e815e
KozakovaAnn <kozakova.ann@mail.ru>
2024-03-18 14:47:42 +0300

    + check hex

--- kozakova.ann/T2/main.cpp
+++ kozakova.ann/T2/main.cpp
@@ -5,17 +5,22 @@
 namespace kozakova {
   struct Data
   {
-    Data(unsigned long long k) :
-      key1(k)
+    Data(unsigned long long k1, unsigned long long k2) :
+      key1(k1),
+      key2(k2)
     {
     }
     unsigned long long get_Key1() const
     {
       return key1;
     }
+    unsigned long long get_Key2() const
+    {
+      return key2;
+    }
   private:
     unsigned long long key1;
-    //unsigned long long key2;
+    unsigned long long key2;
     //std::string key3;
   };
 
@@ -35,7 +40,28 @@ namespace kozakova {
     std::string expUp = exp.expected;
     std::transform(exp.expected.begin(), exp.expected.end(), exp.expected.begin(), ::tolower);
     in >> s;
-    if ((strcmp(s.c_str(), exp.expected.c_str()) && strcmp(s.c_str(), expUp.c_str()))) // подумать
+    if ((strcmp(s.c_str(), exp.expected.c_str()) && strcmp(s.c_str(), expUp.c_str()))) 
+    {
+      in.setstate(std::ios::failbit);
+    }
+    return in;
+  };
+
+  struct DelimiterChar
+  {
+    char expected;
+  };
+
+  std::istream& operator>>(std::istream& in, DelimiterChar&& exp)
+  {
+    std::istream::sentry guard(in);
+    if (!guard)
+    {
+      return in;
+    }
+    char c = 0;
+    in >> c;
+    if (!(c == exp.expected || c == tolower(exp.expected)))
     {
       in.setstate(std::ios::failbit);
     }
@@ -49,12 +75,14 @@ namespace kozakova {
     {
       return in;
     }
-    using del = DelimiterString;
+    using dels = DelimiterString;
+    using delc = DelimiterChar;
     unsigned long long a = 0;
-    in >> a >> del{ "ULL" };
+    unsigned long long b = 0;
+    in >> a >> dels{ "ULL" } >> delc{ '0' } >> delc{ 'X' } >> std::hex >> b;
     if (in)
     {
-      value = Data(a);
+      value = Data(a, b);
     }
     return in;
   };
@@ -66,14 +94,14 @@ namespace kozakova {
     {
       return out;
     }
-    out << value.get_Key1() << "ull";
+    out << value.get_Key1() << "ull" << " " << "0x" << std::uppercase << std::hex << value.get_Key2();
     return out;
   };
 }
 
 int main()
 {
-  kozakova::Data a(0);
+  kozakova::Data a(0,0);
   std::cin >> a;
   std::cout << a;
   return 0;
