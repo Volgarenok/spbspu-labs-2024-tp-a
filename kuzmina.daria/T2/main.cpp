dd45cb43435de6018cfad5a4531139662d0c6c08
k-daria <kuzminada1005@gmail.com>
2024-03-19 22:47:42 +0300

    Added input/output of vector< DataStruct >

--- kuzmina.daria/T2/main.cpp
+++ kuzmina.daria/T2/main.cpp
@@ -1,14 +1,35 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+#include <limits>
 #include "DataStruct.hpp"
 #include "DelimiterI.hpp"
 
 int main()
 {
   using namespace kuzmina;
-  DataStruct ds{ 5, {5, 5}, "555" };
 
-  std::cin >> ds;   // (:key1 77LL:key3 "hi :":key2 (:N 8:D 9:):)
-  std::cout << ds;  // (:key1 77ll:key2 (:N 8:D 9:):key3 "hi :":)
+  std::vector< DataStruct > data(std::istream_iterator< DataStruct >(std::cin),
+                                 std::istream_iterator< DataStruct >{});
+
+  while (!std::cin.eof())
+  {
+    std::copy(
+      std::istream_iterator< DataStruct >(std::cin),
+      std::istream_iterator< DataStruct >(),
+      std::back_inserter(data)
+    );
+
+    std::cin.clear();
+    std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+  }
+
+  std::copy(
+    std::begin(data),
+    std::end(data),
+    std::ostream_iterator< DataStruct >(std::cout, "\n")
+  );
 
   return 0;
 }
