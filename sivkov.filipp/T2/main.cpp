3e7ff4bda5534593b98b502295536dc57c398125
exxiiint <filipp.sivkov@yandex.ru>
2024-03-20 23:13:37 +0300

    add DataStruct

--- sivkov.filipp/T2/main.cpp
+++ sivkov.filipp/T2/main.cpp
@@ -1,3 +1,19 @@
+#include <iostream>
+#include <limits>
+#include <string>
+#include <complex>
+#include <iomanip>
+#include <iterator>
+#include <list>
+#include <algorithm>
+
+struct DataStruct
+{
+  double key1;
+  std::complex<double> key2;
+  std::string key3;
+};
+
 int main()
 {
 }
