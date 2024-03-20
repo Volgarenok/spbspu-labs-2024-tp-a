044a81b3cfb312c7bbde3086741240c0189a90e8
Eliseyz <elisevz@mail.ru>
2024-03-20 19:35:32 +0300

    Rewrited main for the task

--- zaparin.elisei/T2/main.cpp
+++ zaparin.elisei/T2/main.cpp
@@ -1,11 +1,35 @@
-#include"delimeter.hpp"
-#include<iostream>
+#include"dataStruct.hpp"
 
+#include<iostream>
 #include<fstream>
 #include<string>
+#include<list>
+#include<algorithm>
+#include<limits>
+
 
 int main()
 {
-  return 0;
+  using namespace zaparin;
+
+  std::list< DataStruct > data;
+
+  while (!std::cin.eof())
+  {
+    std::copy(
+      std::istream_iterator< DataStruct >(std::cin),
+      std::istream_iterator< DataStruct >(),
+      std::back_inserter(data));
+
+    if (std::cin.fail())
+    {
+      std::cin.clear();
+      std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+    }
+  }
+
+  data.sort();
+
+  std::copy(data.begin(), data.end(), std::ostream_iterator< DataStruct >(std::cout, "\n"));
 }
 
