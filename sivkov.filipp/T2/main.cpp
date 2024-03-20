2fd08ff9409cb0eae1981223bfedb4c51d343f68
exxiiint <filipp.sivkov@yandex.ru>
2024-03-20 23:29:44 +0300

    add operators >>

--- sivkov.filipp/T2/main.cpp
+++ sivkov.filipp/T2/main.cpp
@@ -17,6 +17,37 @@ struct DelimiterStr
   const char * expected;
 };
 
+std::istream& operator>>(std::istream& in, DelimiterI&& exp)
+{
+  std::istream::sentry guard(in);
+  if (!guard)
+  {
+    return in;
+  }
+
+  char c = 0;
+  in >> c;
+
+  if (in && std::tolower(c) != std::tolower(exp.expected))
+  {
+    in.setstate(std::ios::failbit);
+  }
+  return in;
+}
+
+std::istream& operator>>(std::istream& in, DelimiterStr&& exp)
+{
+  std::istream::sentry guard(in);
+  if (!guard)
+  {
+    return in;
+  }
+  for (size_t i = 0; exp.expected[i] != '\0'; ++i)
+  {
+    in >> DelimiterI{ exp.expected[i] };
+  }
+}
+
 struct DataStruct
 {
   double key1;
