18dacc94a55e296bd1002280fe1260072a05ef41
exxiiint <filipp.sivkov@yandex.ru>
2024-03-22 00:18:13 +0300

    fix output complex

--- sivkov.filipp/T2/DataStruct.cpp
+++ sivkov.filipp/T2/DataStruct.cpp
@@ -30,7 +30,7 @@ std::ostream& operator<<(std::ostream& out, const DataStruct& value)
   }
   out << std::setprecision(1) << std::fixed;
   out << "(:key1 " << value.key1 << "D";
-  out << ":key2 #c" << value.key2 << ":";
+  out << ":key2 #c" << value.key2.real() << " " << value.key2.imag() << ":";
   out << "key3 \"" << value.key3 << "\":)";
   return out;
 }
