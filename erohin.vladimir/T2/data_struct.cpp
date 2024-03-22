daeeeecc3010356dba81bf246551333410957b13
WarriorVortex <erohinvova01010111@gmail.com>
2024-03-22 22:17:19 +0300

    Added DataStruct with some input realization

--- erohin.vladimir/T2/data_struct.cpp
+++ erohin.vladimir/T2/data_struct.cpp
@@ -1,11 +1,14 @@
+#include "data_struct.hpp"
 #include <iostream>
+#include "format.hpp"
+#include "delimeter.hpp"
 
 std::istream & erohin::operator>>(std::istream & input, DataStruct & dest)
 {
-  std::istream::sentry sentry;
+  std::istream::sentry sentry(input);
   if (!sentry)
   {
     return input;
   }
-  return input >> Delimeter{'('} >> Delimeter{')'};
+  return input >> Delimeter{'('} >> dest.key1 >> Delimeter{')'};
 }
