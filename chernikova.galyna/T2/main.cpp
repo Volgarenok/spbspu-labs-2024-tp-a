44931a0c0af19d2ebab879ddd73f90f53f07135f
chernikovaG <chernikova.gv@edu.spbstu.ru>
2024-03-25 21:27:44 +0300

    Added input and output to main.cpp

--- chernikova.galyna/T2/main.cpp
+++ chernikova.galyna/T2/main.cpp
@@ -1,2 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <iterator>
+#include <algorithm>
+#include "DataStruct.h"
+
+
 int main()
-{}
+{
+  using namespace chernikova;
+
+  std::vector< DataStruct > data;
+  std::istringstream iss("{ \"key1\": 1.0d, \"key2\": \"Let madness release you\" }");
+
+  std::copy(
+    std::istream_iterator< DataStruct >(iss),
+    std::istream_iterator< DataStruct >(),
+    std::back_inserter(data)
+  );
+
+  std::cout << "Data:\n";
+  std::copy(
+    std::begin(data),
+    std::end(data),
+    std::ostream_iterator< DataStruct >(std::cout, "\n")
+  );
+
+  return 0;
+}
