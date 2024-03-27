86276fe335d0d679a010a99aba2f1bae74626388
kirillnamestnikov <kir.namestnikov@gmail.com>
2024-03-27 21:27:17 +0300

    change data_struct.hpp

--- namestnikov.kirill/T2/data_struct.hpp
+++ namestnikov.kirill/T2/data_struct.hpp
@@ -14,6 +14,5 @@ namespace namestnikov
   };
   std::istream & operator>>(std::istream & in, DataStruct & data);
   std::ostream & operator<<(std::ostream & out, const DataStruct & data);
-  std::istream & operator>>(std::istream & in, std::string & s);
 }
 #endif
