9bd8b9aa7fbac77bdb49fb28837d63368706363a
MayaSakovskaya <zloyejik05@gmail.com>
2024-03-29 18:35:19 +0300

    added data_struct.hpp

---
+++ sakovskaia.maiia/T2/data_struct.hpp
@@ -0,0 +1,13 @@
+#ifndef DATA_STRUCT_HPP
+#define DATA_STRUCT_HPP
+#include <cstring>
+namespace sakovskaia
+{
+  struct DataStruct
+  {
+    unsigned long long key1;
+    char key2;
+    std::string key3;
+  };
+}
+#endif
