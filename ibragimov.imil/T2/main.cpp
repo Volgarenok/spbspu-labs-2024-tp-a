f4f80f9a8aea358ba847e58c01bca90a922f97a7
StantStantov <ibragimovimil0@gmail.com>
2024-03-30 18:02:50 +0300

    Add Input and Output

--- ibragimov.imil/T2/main.cpp
+++ ibragimov.imil/T2/main.cpp
@@ -1,4 +1,26 @@
-int main() {
+#include <ios>
+#include <iterator>
+#include <limits>
+#include <vector>
+#include "dataStructure.hpp"
+
+int main()
+{
+  using namespace ibragimov;
+  using is_iterator = std::istream_iterator<DataStructure>;
+  using os_iterator = std::ostream_iterator<DataStructure>;
+
+  std::vector<DataStructure> data;
+  while (!std::cin.eof()) {
+    std::copy(is_iterator{std::cin}, is_iterator{}, std::back_inserter(data));
+    if (!std::cin)
+    {
+      std::cin.clear();
+      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    }
+  }
+
+  std::copy(std::begin(data), std::end(data), os_iterator(std::cout, "\n"));
 
   return 0;
 }
