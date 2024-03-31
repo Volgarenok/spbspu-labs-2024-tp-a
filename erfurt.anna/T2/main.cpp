4b7446232e9d89492ef8cba235eb028df07e66cc
Anyanari <a.erfurt02@mail.ru>
2024-03-31 12:31:56 +0300

    Another fix

--- erfurt.anna/T2/main.cpp
+++ erfurt.anna/T2/main.cpp
@@ -18,7 +18,7 @@ int main()
     if (std::cin.fail())
     {
       std::cin.clear();
-      std::cin.ignore(std::numeric_limits< std::streamsize > ::max(), '\n');
+      std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
     }
     std::copy(
       input_it_t{ std::cin },
