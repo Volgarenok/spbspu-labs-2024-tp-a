77bf4633ffcbfa63ae6cbbc28c03a9a295dbd3a7
osakasonyboy@gmail.com <sensh0god@gmail.com>
2024-04-01 15:33:36 +0300

    hw - done. (read until there is a valid input)

--- arakelyan.daniil/T2/main.cpp
+++ arakelyan.daniil/T2/main.cpp
@@ -1,45 +1,48 @@
 #include <iostream>
+#include <limits>
 
-struct SomeDataS
-{
-  SomeDataS() = default;
-  SomeDataS(int aa, int bb):
-    a(aa),
-    b(bb)
-  {}
-  int a;
-  int b;
-};
 
-std::istream &operator>>(std::istream &in, SomeDataS &data)
+namespace arakelyan
 {
-  std::istream::sentry guard(in);
-  if (!guard)
+  struct SomeDataS
   {
+    SomeDataS() = default;
+    SomeDataS(int aa, int bb):
+      a(aa),
+      b(bb)
+    {}
+    int a;
+    int b;
+  };
+
+  std::istream &operator>>(std::istream &in, SomeDataS &data)
+  {
+    std::istream::sentry guard(in);
+    if (!guard)
+    {
+      return in;
+    }
+    int a = 0;
+    int b = 0;
+    in >> a >> b;
+    if (in)
+    {
+      data = SomeDataS(a,b);
+    }
     return in;
   }
-  int a = 0;
-  int b = 0;
-  in >> a >> b;
-  if (in)
-  {
-    data = SomeDataS(a,b);
-  }
-  return in;
 }
-
 
 int main()
 {
+  using namespace arakelyan;
   SomeDataS data;
-  if (!(std::cin >> data))
+  while(!(std::cin >> data))
   {
-    std::cin.clear();
-    std::cin.ignore(1);
-    if (!(std::cin >> data))
+    if (!std::cin)
     {
-      std::cerr << "Error input\n";
-      return 1;
+      std::cin.clear();
+      std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
     }
   }
   std::cout << data.a << " - " << data.b << "\n";
