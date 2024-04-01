f9a931067dddb86db0e503b1d1fe64e1cfa023b4
chernikovaG <chernikova.gv@edu.spbstu.ru>
2024-04-01 16:41:59 +0300

    Added ValueI.cpp

---
+++ chernikova.galyna/T2/ValueI.cpp
@@ -0,0 +1,100 @@
+#include "ValueI.h"
+#include <string>
+#include <cmath>
+
+std::istream& chernikova::operator>>(std::istream& in, 
+chernikova::DelimiterI&& exp)
+{
+  std::istream::sentry guard(in);
+  if (!guard)
+  {
+    return in;
+  }
+  char c = 0;
+  in >> c;
+  if ((!exp.variability) && (c != exp.expected) || exp.variability && 
+(std::tolower(c) != std::tolower(exp.expected)))
+  {
+    in.setstate(std::ios::failbit);
+  }
+  return in;
+}
+
+std::istream& chernikova::operator>>(std::istream& in, 
+chernikova::DoubleI&& dest)
+{
+  std::istream::sentry guard(in);
+  if (!guard)
+  {
+    return in;
+  }
+  double mantissa = 0;
+  int exponent = 0;
+  char symbol = ' ';
+  std::string strMantissa;
+  std::string strExponent;
+
+  while (!(in >> chernikova::DelimiterI{'e', true}))
+  {
+    in >> symbol;
+    strMantissa += symbol;
+    ++dest.ref;
+  }
+  while (in)
+  {
+    in >> symbol;
+    strExponent += symbol;
+    ++dest.ref;
+  }
+
+  mantissa = std::stod(strMantissa);
+  exponent = std::stod(strExponent);
+  dest.ref = mantissa * pow(10, exponent);
+  return in;
+}
+
+int octalToDecimal(int octalNumber)
+{
+  int decimalNumber = 0;
+  int i = 0;
+  int rem = 0;
+  while (octalNumber != 0)
+  {
+    rem = octalNumber % 10;
+    octalNumber /= 10;
+    decimalNumber += rem * pow(8, i);
+    ++i;
+  }
+  return decimalNumber;
+}
+
+std::istream& chernikova::operator>>(std::istream& in, 
+chernikova::UnsignedllI&& exp)
+{
+  std::istream::sentry guard(in);
+  if (!guard)
+  {
+    return in;
+  }
+  int num = 0;
+  in >> num;
+  exp.ref = octalToDecimal(num);
+  return in;
+}
+
+std::istream& chernikova::operator>>(std::istream& in, 
+chernikova::StringI&& dest)
+{
+  std::istream::sentry guard(in);
+  if (!guard)
+  {
+    return in;
+  }
+
+  while (*dest.ref != '\0')
+  {
+    in >> DelimiterI{*dest.ref};
+    ++dest.ref;
+  }
+  return in;
+}
