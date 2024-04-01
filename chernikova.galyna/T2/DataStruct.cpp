5b305e18338bbe92346aa1b702dd5a3ab412e856
chernikovaG <chernikova.gv@edu.spbstu.ru>
2024-04-01 15:39:51 +0300

    Added key comparison to the DataStruct.cpp

--- chernikova.galyna/T2/DataStruct.cpp
+++ chernikova.galyna/T2/DataStruct.cpp
@@ -1,1 +1,22 @@
 #include "DataStruct.h"
+#include <string>
+#include <iostream>
+#include "Delimiter.h"
+#include "StreamGuard.h"
+
+bool chernikova::compare(const DataStruct& value1, const DataStruct& 
+value2)
+{
+  if (value1.key1 != value2.key1)
+  {
+    return (value1.key1 < value2.key1);
+  }
+  else if (value1.key2 != value2.key2)
+  {
+    return (value1.key2 < value2.key2);
+  }
+  else
+  {
+    return strlen(value2.key3) < strlen(value2.key3);
+  }
+}
