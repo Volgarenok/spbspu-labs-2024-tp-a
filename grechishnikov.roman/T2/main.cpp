5b63aeca2dc8a408c6a52ce87daa475b30b708c6
Steuger7 <steuger7@mail.ru>
2024-04-01 22:41:24 +0300

    cin and cout to std::copy

--- grechishnikov.roman/T2/main.cpp
+++ grechishnikov.roman/T2/main.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
+#include <vector>
+#include <iterator>
 #include "dataStruct.hpp"
+
 
 int main()
 {
   using namespace grechishnikov;
+  std::vector< DataStruct > data{ {10, { 3.1, 3.003 }, "aaa" } };
 
-  DataStruct a { 10, { 3.1, 3.003 }, "aaa" };
-  std::cout << a << '\n';
+//  using input_it_t = std::istream_iterator< DataStruct >;
+//  std::copy(input_it_t{ std::sin }, input_it_t{}, std::back_inserter(data));
+
+  using output_it_t = std::ostream_iterator< DataStruct >;
+  std::copy(data.cbegin(), data.cend(), output_it_t{ std::cout, "\n" });
 }
