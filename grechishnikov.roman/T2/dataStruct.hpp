5d09a9e2cba83d38004120e0cf43a5365ed1a328
Steuger7 <steuger7@mail.ru>
2024-04-01 17:38:28 +0300

    add initial dataStruct

---
+++ grechishnikov.roman/T2/dataStruct.hpp
@@ -0,0 +1,12 @@
+#include <cstddef>
+#include <complex>
+
+namespace grechishnikov
+{
+  struct Data
+  {
+    size_t key1;
+    std::complex< double > key2;
+    std::string key3;
+  };
+}
--- grechishnikov.roman/T2/main.cpp
+++ grechishnikov.roman/T2/main.cpp
@@ -1,2 +1,11 @@
+#include <iostream>
+#include "dataStruct.hpp"
+
 int main()
-{}
+{
+  using namespace grechishnikov;
+
+  Data a;
+  std::cin >> a.key2;
+  std::cout << a.key2;
+}
