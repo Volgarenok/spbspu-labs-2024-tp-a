13385dcb0d013233f52dd8551c78561c0b83f708
chernikovaG <chernikova.gv@edu.spbstu.ru>
2024-04-01 16:51:24 +0300

    Added DelimiterI to the Delimiter.h

--- chernikova.galyna/T2/Delimiter.h
+++ chernikova.galyna/T2/Delimiter.h
@@ -5,11 +5,17 @@
 
 namespace chernikova
 {
+  struct DelimiterI
+  {
+    const char expected;
+    bool variability = false;
+  };
+
   struct StringDelimiterI
   {
     const char* exp;
   };
-
+  std::istream& operator>>(std::istream& in, chernikova::DelimiterI&& exp);
   std::istream& operator>>(std::istream& in, chernikova::StringDelimiterI&& dest);
 }
 
