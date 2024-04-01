5aae1495ea184652a09a7f4105abce23c8d20e37
iamneefee <neefee.mail@gmail.com>
2024-04-02 00:02:25 +0300

    Add implementation of input in dataStruct

--- kozlov.nikita/T2/dataStruct.cpp
+++ kozlov.nikita/T2/dataStruct.cpp
@@ -11,8 +11,29 @@ std::istream& kozlov::operator>>(std::istream& in, DataStruct& data)
   }
   StreamGuard streamGuard(in);
   using del = Delimiter;
-  int keyCount = 0;
   DataStruct temp{0, 0, ""};
+  int keyCount = 3;
+  in >> del{"("};
+  for (int i = 0; i < keyCount; i++)
+  {
+    int keyNum = 0;
+    in >> del{":key"} >> keyNum;
+    if (keyNum = 1)
+    {
+      in >> std::hex;
+      in >> del{"0x"} >> temp.key1;
+    }
+    if (keyNum = 2)
+    {
+      in >> del{"\""} >> temp.key2 >> del{"\""};
+    }
+    if (keyNum = 3)
+    {
+      in >> del{"\""};
+      std::getline(in, temp.key3, '\"');
+    }
+  }
+  in >> del{":)"};
   data = temp;
   return in;
 }
