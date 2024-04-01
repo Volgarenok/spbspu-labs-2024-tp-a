7b618547f6d4d1f99f8acf47e39ddae70f48f405
chernikovaG <chernikova.gv@edu.spbstu.ru>
2024-04-01 16:53:43 +0300

    Added input operator to the Delimiter.cpp

--- chernikova.galyna/T2/Delimiter.cpp
+++ chernikova.galyna/T2/Delimiter.cpp
@@ -2,6 +2,24 @@
 #include <iostream>
 #include "ValueI.h"
 
+std::istream& chernikova::operator>>(std::istream& in, 
+chernikova::DelimiterI&& exp)
+{
+  std::istream::sentry guard(in);
+  if (!guard)
+  {
+    return in;
+  }
+  char c = 0;
+  in >> c;
+  if ((!exp.variability) && (c != exp.expected) || exp.variability && 
+(std::tolower(c) != std::tolower(exp.expected)))
+  {
+    in.setstate(std::ios::failbit);
+  }
+  return in;
+}
+
 std::istream& chernikova::operator>>(std::istream &in, 
 chernikova::StringDelimiterI &&dest)
 {
@@ -17,4 +35,3 @@ chernikova::StringDelimiterI &&dest)
   }
   return in;
 }
-
