b4079cc177bd29082a74765ce645194015a45065
chernikovaG <chernikova.gv@edu.spbstu.ru>
2024-04-01 15:48:41 +0300

    Added input and output operators to the DataStruct.h

--- chernikova.galyna/T2/DataStruct.h
+++ chernikova.galyna/T2/DataStruct.h
@@ -9,8 +9,11 @@ namespace chernikova
   {
     double key1;
     unsigned long long key2;
-    std::string key3;
+    char* key3;
   };
+  std::istream& operator>>(std::istream& in, DataStruct& value);
+  std::ostream& operator<<(std::ostream& out, const DataStruct& value);
+  bool compare(const DataStruct& value1, const DataStruct& value2);
 }
 
 #endif
