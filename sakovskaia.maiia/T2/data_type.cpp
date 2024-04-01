dbf9f940e2bd2bb548553c685d5be9685486db4c
MayaSakovskaya <zloyejik05@gmail.com>
2024-04-02 00:03:10 +0300

    added input for different types

---
+++ sakovskaia.maiia/T2/data_type.cpp
@@ -0,0 +1,29 @@
+#include "data_type.hpp"
+
+std::istream & sakovskaia:: operator>>(std::istream & input, unsigned long long key);
+{
+  std::istream::sentry guard(input);
+  using delimiter = delimiter_t;
+  StreamGuard ios_guard(input);
+  input >> std::bitset<8> >> delimiter{'0'} >> delimiter{'b'} >> key;
+  return input;
+}
+
+std::istream & sakovskaia:: operator>>(std::istream & input, char key);
+{
+  std::istream::sentry guard(input);
+  using delimiter = delimiter_t;
+  StreamGuard ios_guard(input);
+  input >> delimiter{'\''} >> key >> delimiter{'\''};
+  return input;
+}
+
+std::istream & sakovskaia:: operator>>(std::istream & input, std::string key);
+{
+  std::istream::sentry guard(input);
+  using delimiter = delimiter_t;
+  StreamGuard ios_guard(input);
+  input >> delimiter{'"'};
+  input >> key >> delimiter{'"'};
+  return input;
+}
