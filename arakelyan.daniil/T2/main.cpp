4a67801ffdb99366a4628d8ebc2e4babf78e71f6
osakasonyboy@gmail.com <sensh0god@gmail.com>
2024-04-01 22:08:09 +0300

    added vector and input while not eof

--- arakelyan.daniil/T2/main.cpp
+++ arakelyan.daniil/T2/main.cpp
@@ -1,27 +1,45 @@
+#include <algorithm>
 #include <iostream>
 #include <limits>
+#include <vector>
+#include <iterator>
 
 #include "dataStruct.hpp"
 
 int main()
 {
-  //in format -> (:key1 10:key2 30)
+  //in format -> (:key1 076:key2 'a':key3 "hiOsaka":)
   using namespace arakelyan;
-  DataStruct data;
-  // while(!(std::cin >> data))
-  // {
-  //   if (!(std::cin))
-  //   {
-  //     std::cin.clear();
-  //     std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
-  //   }
-  // }
-  std::cin >> data;
-  if (!std::cin)
+  std::vector< DataStruct > data;
+
+  while (!std::cin.eof())
   {
-    std::cerr << "Incorrect format!\n";
-    return 1;
+    std::copy(
+    std::istream_iterator< DataStruct >{std::cin},
+    std::istream_iterator< DataStruct >{},
+    std::back_inserter(data)
+    );
+    if (!std::cin)
+    {
+      std::cin.clear();
+      std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+    }
   }
-  std::cout << data << "\n";
+
+  // std::copy(
+  //   data.begin(),
+  //   data.end(),
+  //   std::ostream_iterator< DataStruct >(std::cout, "\n")
+  // );
+
+  std::cout << "data: \n";
+  auto it = data.begin();
+  while (it != data.cend())
+  {
+    std::cout << *it << "\n";
+    ++it;
+  }
+  // std::cout << *data.begin() << "\n";
+
   return 0;
 }
