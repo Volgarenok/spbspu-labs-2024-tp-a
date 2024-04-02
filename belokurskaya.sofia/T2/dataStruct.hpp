be5ae2cec86d49e08a76b5ce2e68c44ad78c673f
sonia66666 <147131510+sonia66666@users.noreply.github.com>
2024-04-02 12:05:34 +0300

    added dataStruct.hpp

---
+++ belokurskaya.sofia/T2/dataStruct.hpp
@@ -0,0 +1,13 @@
+#ifndef DATASTRUCT_H
+#define DATASTRUCT_H
+
+#include <iostream>
+
+struct DataStruct
+{
+  char key1;
+  std::pair< long long, unsigned long long > key2;
+  std::string key3;
+};
+
+#endif
