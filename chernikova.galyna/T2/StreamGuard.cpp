067000404ec024404ef6e6593de31dcf5c3b4e00
chernikovaG <chernikova.gv@edu.spbstu.ru>
2024-04-02 22:58:23 +0300

    Fixed CS in StreamGuard.cpp

--- chernikova.galyna/T2/StreamGuard.cpp
+++ chernikova.galyna/T2/StreamGuard.cpp
@@ -1,6 +1,6 @@
 #include "StreamGuard.hpp"
 
-chernikova::StreamGuard::StreamGuard(std::basic_ios<char>& s):
+chernikova::StreamGuard::StreamGuard(std::basic_ios< char >& s):
   s_(s),
   precision_(s.precision()),
   flags_(s.flags())
