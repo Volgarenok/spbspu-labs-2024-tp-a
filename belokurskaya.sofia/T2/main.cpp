3854e15508fd4add87d8d6081850f9be3652ceed
sonia66666 <147131510+sonia66666@users.noreply.github.com>
2024-04-02 12:16:53 +0300

    Update main.cpp file

--- belokurskaya.sofia/T2/main.cpp
+++ belokurskaya.sofia/T2/main.cpp
@@ -1,4 +1,35 @@
-﻿int main()
-{
+﻿#include <iostream>
+#include <sstream>
+#include <cassert>
+#include <iterator>
+#include <vector>
+#include <iomanip>
+#include <algorithm>
 
+#include "dataStruct.hpp"
+#include "reader.hpp"
+
+int main()
+{
+  std::vector< DataStruct > dataStructVector;
+  while (!std::cin.eof())
+  {
+    std::copy(std::istream_iterator< DataStruct >(std::cin),
+      std::istream_iterator< DataStruct >(),
+      std::back_inserter(dataStructVector));
+
+    if (!std::cin)
+    {
+      std::cin.clear();
+      std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+    }
+  }
+
+  std::sort(dataStructVector.begin(), dataStructVector.end());
+
+  std::copy(dataStructVector.begin(),
+    dataStructVector.end(),
+    std::ostream_iterator< DataStruct >(std::cout, "\n"));
+
+  return 0;
 }
