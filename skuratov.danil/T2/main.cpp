e22d62fb4578ba30b1aeec03b58e5d5e764fcf3c
Skuratov09 <skyratovdaniil28@gmail.com>
2024-04-02 14:16:10 +0300

    Trying recovery

--- skuratov.danil/T2/main.cpp
+++ skuratov.danil/T2/main.cpp
@@ -5,8 +5,13 @@ int main()
   int a = 0, b = 0;
   if (!(std::cin >> a >> b))
   {
-    std::cerr << "Error\n";
-    return 1;
+    std::cin.clear();
+    std::cin.ignore(1);
+    if (!(std::cin >> a >> b))
+    {
+      std::cerr << "Error\n";
+      return 1;
+    }
   }
   std::cout << a << " " << b << '\n';
   return 0;
