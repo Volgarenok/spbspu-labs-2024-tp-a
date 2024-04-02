20cf8ac4b662cde4455a041e822f946ab9b51c2e
pashko2303 <baranov-p.b@yandex.ru>
2024-04-02 23:23:03 +0300

    Add dataStruct

---
+++ baranov.pavel/T2/dataStruct.hpp
@@ -0,0 +1,15 @@
+#ifndef DATA_STRUCT_HPP
+#define DATA_STRUCT_HPP
+
+namespace baranov
+{
+  struct DataStruct
+  {
+    unsigned long long key1;
+    std::complex< double > key2;
+    std::string key3;
+  };
+}
+
+#endif
+
