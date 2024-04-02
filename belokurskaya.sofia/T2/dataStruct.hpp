519bf2e641c0995afc6435449ea0e265e5f2da4a
sonia66666 <147131510+sonia66666@users.noreply.github.com>
2024-04-02 12:06:47 +0300

    Update dataStruct.hpp

--- belokurskaya.sofia/T2/dataStruct.hpp
+++ belokurskaya.sofia/T2/dataStruct.hpp
@@ -8,6 +8,43 @@ struct DataStruct
   char key1;
   std::pair< long long, unsigned long long > key2;
   std::string key3;
+
+  DataStruct& operator=(const DataStruct& other)
+  {
+    if (this != &other)
+    {
+      key1 = other.key1;
+      key2 = other.key2;
+      key3 = other.key3;
+    }
+    return *this;
+  }
+
+  bool operator<(const DataStruct& other) const
+  {
+    if (key1 < other.key1)
+    {
+      return true;
+    }
+    else if (key1 == other.key1)
+    {
+      if (key2 < other.key2)
+      {
+        return true;
+      }
+      else if (key2 == other.key2)
+      {
+        return key3 < other.key3;
+      }
+      return false;
+    }
+    return false;
+  }
+
+  bool operator>(const DataStruct& other) const
+  {
+    return !(*this < other);
+  }
 };
 
 #endif
