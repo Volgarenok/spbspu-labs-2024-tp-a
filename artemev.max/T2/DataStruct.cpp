99bf18e835d496b081e2f91182ad157f486694fe
MaxArtemev <maxartemev@mail.ru>
2024-04-02 14:42:16 +0300

    Define operator>>

--- artemev.max/T2/DataStruct.cpp
+++ artemev.max/T2/DataStruct.cpp
@@ -32,3 +32,58 @@ bool artemev::DataStruct::operator<(const DataStruct& other) const
 
   return (key3.length() <= other.key3.length());
 }
+
+std::istream& artemev::operator>>(std::istream& in, DataStruct& value)
+{
+  std::istream::sentry sentry(in);
+  if (!sentry)
+  {
+    return in;
+  }
+
+  ScopeGuard scopeGuard(in);
+
+  using delChar = DelimiterCharI;
+  using delString = DelimiterStringI;
+
+  const size_t KEY_NUMBER = 3;
+  size_t keyI = 0;
+
+  in >> delChar{ '(' };
+
+  for (size_t i = 0; i < KEY_NUMBER; ++i)
+  {
+    in >> delString{ ":key" } >> keyI;
+    switch (keyI)
+    {
+      case 1:
+      {
+        in >> value.key1 >> delChar{ 'd' };
+        break;
+      }
+      case 2:
+      {
+        std::bitset< 64 > bin;
+        in >> delString{ "0b" } >> bin;
+        if (in)
+        {
+          value.key2 = bin.to_ullong();
+        }
+        break;
+      }
+      case 3:
+      {
+        in >> delChar{ '"' };
+        std::getline(in, value.key3, '"');
+        break;
+      }
+      default:
+      {
+        in.setstate(std::ios::failbit);
+      }
+    }
+  }
+
+  in >> delString{ ":)" };
+  return in;
+}
