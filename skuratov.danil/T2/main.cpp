5fc2be5b95e837cdfbbb1788264e40fcba7284bc
Skuratov09 <skyratovdaniil28@gmail.com>
2024-04-02 22:11:46 +0300

    Operator overloads

--- skuratov.danil/T2/main.cpp
+++ skuratov.danil/T2/main.cpp
@@ -1,18 +1,55 @@
 #include <iostream>
 
+namespace skuratov
+{
+  struct Data
+  {
+    Data(int aa, int bb):
+      a(aa),
+      b(bb)
+    {}
+    int getA() const
+    {
+      return a;
+    }
+    int getB() const
+    {
+      return b;
+    }
+  private:
+    int a, b;
+  };
+  std::istream & operator>>(std::istream& in, Data& value)
+  {
+    std::istream::sentry guard(in);
+    if (!guard)
+    {
+      return in;
+    }
+    int a = 0, b = 0;
+    in >> a >> b;
+    if (in)
+    {
+      value = Data(a, b);
+    }
+    return in;
+  }
+  std::ostream& operator<<(std::ostream& out, const Data& value)
+  {
+    std::ostream::sentry guard(out);
+    if (!guard)
+    {
+      return out;
+    }
+    out << value.getA() << " " << value.getB();
+    return out;
+  }
+}
+
 int main()
 {
-  int a = 0, b = 0;
-  if (!(std::cin >> a >> b))
-  {
-    std::cin.clear();
-    std::cin.ignore(1);
-    if (!(std::cin >> a >> b))
-    {
-      std::cerr << "Error\n";
-      return 1;
-    }
-  }
-  std::cout << a << " " << b << '\n';
+  skuratov::Data d(0, 0);
+  std::cin >> d;
+  std::cout << d << '\n';
   return 0;
 }
