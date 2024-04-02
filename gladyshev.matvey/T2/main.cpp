311e36060f3c90ceefde09797679b12f4c673b87
magggottt <gladyshevmatvey@gmail.com>
2024-04-02 23:48:06 +0700

    final rez

--- gladyshev.matvey/T2/main.cpp
+++ gladyshev.matvey/T2/main.cpp
@@ -1,3 +1,29 @@
+#include <iostream>
+#include <iterator>
+#include <list>
+#include <limits>
+
+#include "delimiter.hpp"
+#include "datastruct.hpp"
+#include "streamguard.hpp"
+
 int main()
 {
+  using namespace gladyshev;
+  std::list< DataStruct > structures;
+  while (!std::cin.eof())
+  {
+    if (!std::cin)
+    {
+      std::cin.clear();
+      std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+    }
+    std::copy(
+      std::istream_iterator< DataStruct >{std::cin},
+      std::istream_iterator< DataStruct >{},
+      std::back_inserter(structures)
+    );
+  }
+  structures.sort();
+  std::copy(structures.cbegin(), structures.cend(), std::ostream_iterator< DataStruct >{std::cout, "\n"});
 }
