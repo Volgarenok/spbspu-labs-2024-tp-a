26f993a2893933066d5e0f5f6e9bcaf70ba77fab
iamneefee <neefee.mail@gmail.com>
2024-04-02 18:41:58 +0300

    Added vector input and output using std::copy, added checking for correct input + sort

--- kozlov.nikita/T2/main.cpp
+++ kozlov.nikita/T2/main.cpp
@@ -1,2 +1,26 @@
+#include "dataStruct.hpp"
+#include <iostream>
+#include <vector>
+#include <iterator>
+#include <limits>
+#include <algorithm>
+
+using namespace kozlov;
 int main()
-{}
+{
+  std::vector< DataStruct > data;
+  using iIterator = std::istream_iterator< DataStruct >;
+  while (!std::cin.eof())
+  {
+     std::copy(iIterator{std::cin}, iIterator{}, std::back_inserter(data));
+     if (!std::cin)
+     {
+       std::cin.clear();
+       std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+     }
+  }
+  std::sort(data.begin(), data.end());
+  using oIterator = std::ostream_iterator< DataStruct >;
+  std::copy(data.begin(), data.end(), oIterator(std::cout, "\n"));
+  return 0;
+}
