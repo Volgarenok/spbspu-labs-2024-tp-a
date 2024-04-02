2773f4005f2fc8cf00f73c7045e799addec65d67
Skuratov09 <skyratovdaniil28@gmail.com>
2024-04-02 12:52:44 +0300

    Input two ints

--- skuratov.danil/T2/main.cpp
+++ skuratov.danil/T2/main.cpp
@@ -1,4 +1,13 @@
 #include <iostream>
 
-int  main()
-{}
+int main()
+{
+  int a = 0, b = 0;
+  if (!(std::cin >> a >> b))
+  {
+    std::cerr << "Error\n";
+    return 1;
+  }
+  std::cout << a << " " << b << '\n';
+  return 0;
+}
