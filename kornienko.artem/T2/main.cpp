629c4105b28a8cfcd85993bc2e52700cbfe9c53f
artemkpwr <netd698@gmail.com>
2024-04-03 09:24:12 +0300

    input while eof

--- kornienko.artem/T2/main.cpp
+++ kornienko.artem/T2/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
-#include <vector>
-//#include <algorithm>
+#include <list>
 #include <iterator>
+#include <limits>
 
 #include "DataStruct.hpp"
 
@@ -9,9 +9,17 @@ int main()
 {
   using namespace kornienko;
   using input_it_t = std::istream_iterator< DataStruct >;
-  std::vector< DataStruct > data(input_it_t{std::cin}, input_it_t{});
-
   using output_it_t = std::ostream_iterator< DataStruct  >;
+  std::list< DataStruct > data(input_it_t{std::cin}, input_it_t{});
+  while (!std::cin.eof())
+  {
+    if (std::cin.fail())
+    {
+      std::cin.clear();
+      std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+    }
+    std::copy(input_it_t{ std::cin }, input_it_t{}, std::back_inserter(data));
+  }
   std::copy(data.cbegin(), data.cend(), output_it_t{std::cout, "\n"});
   return 0;
 }
