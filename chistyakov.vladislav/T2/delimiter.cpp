6b181d966811c4e7b6b0fd22e71a44ce5de38a10
jiggerman <liggerman@mail.ru>
2024-04-03 16:54:22 +0300

    Add delimiter cpp

---
+++ chistyakov.vladislav/T2/delimiter.cpp
@@ -0,0 +1,23 @@
+#include "delimiter.hpp"
+#include <cstring>
+
+std::istream & chistyakov::operator>>(std::istream & in, Delimiter && exp)
+{
+  std::istream::sentry guard(in);
+  if (!guard)
+  {
+    return in;
+  }
+
+  for (size_t i = 0; exp.expected[i] && in; ++i)
+  {
+    char c = '0';
+    in >> c;
+    c = std::tolower(c);
+    if (c != exp.expected[i])
+    {
+      in.setstate(std::ios::failbit);
+    }
+  }
+  return in;
+}
