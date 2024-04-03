4015c968ca7a9c3470de4edb247496d4aba4cdb4
jiggerman <liggerman@mail.ru>
2024-04-03 23:29:38 +0300

    Add dataTypes hpp

---
+++ chistyakov.vladislav/T2/dataTypes.hpp
@@ -0,0 +1,23 @@
+#ifndef DATATYPES_HPP
+#define DATATYPES_HPP
+
+#include <iostream>
+#include <utility>
+#include <cstring>
+
+namespace chistyakov
+{
+  struct UllBin
+  {
+    unsigned long long & binaryNumber;
+  };
+  std::istream & operator>>(std::istream & in, UllBin & binNum);
+
+  struct RatLsp
+  {
+    std::pair< long long, unsigned long long > & pairLlAndUll;
+  };
+  std::istream & operator>>(std::istream & in, RatLsp & pair);
+}
+
+#endif
