92c3c80c19d13949c65856677695ad0e7c7aa1bb
plitkkka <anastasiatimchishina@yandex.ru>
2024-04-04 00:45:53 +0300

    Add defenitions of overloaded operators

--- timchishina.anastasia/T2/dataStruct.hpp
+++ timchishina.anastasia/T2/dataStruct.hpp
@@ -1,6 +1,7 @@
 #ifndef DATASTRUCT_HPP
 #define DATASTRUCT_HPP
 #include <string>
+#include <iostream>
 
 namespace timchishina
 {
@@ -10,5 +11,7 @@ namespace timchishina
     unsigned long long key2;
     std::string key3;
   };
+  std::istream & operator>>(std::istream & in, DataStruct & data);
+  std::ostream & operator<<(std::ostream & out, const DataStruct & data);
 }
 #endif
