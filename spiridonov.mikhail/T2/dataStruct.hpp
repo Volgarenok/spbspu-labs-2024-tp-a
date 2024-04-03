d2ebf96e636977f7c3f234e015379a0aa4c8806e
mordw1n <lolpovar0@gmail.com>
2024-04-03 16:47:27 +0300

    hpp fix

--- spiridonov.mikhail/T2/dataStruct.hpp
+++ spiridonov.mikhail/T2/dataStruct.hpp
@@ -1,9 +1,21 @@
 #ifndef DATASTRUCT_HPP
 #define DATASTRUCT_HPP
+#include <iostream>
+#include <string>
+#include <iomanip>
 
 namespace spiridonov
 {
-
+  struct DataStruct
+  {
+    double key1;
+    unsigned long long key2;
+    std::string key3;
+    bool operator<(const DataStruct& data) const;
+  };
+  std::istream& operator>>(std::istream& in, DataStruct& data);
+  std::istream& operator>>(std::istream& in, std::string& exp);
+  std::ostream& operator<<(std::ostream& out, const DataStruct& value);
 }
 
 #endif
