ce939566e573244e359c0018bf315b2988186482
plitkkka <anastasiatimchishina@yandex.ru>
2024-04-03 23:48:33 +0300

    Add dataStruct.hpp

---
+++ timchishina.anastasia/T2/dataStruct.hpp
@@ -0,0 +1,14 @@
+#ifndef DATASTRUCT_HPP
+#define DATASTRUCT_HPP
+#include <string>
+
+namespace timchishina
+{
+  struct DataStruct
+  {
+    long long key1;
+    unsigned long long key2;
+    std::string key3;
+  };
+}
+#endif
