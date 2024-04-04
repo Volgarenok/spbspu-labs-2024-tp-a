72ce73434ce570d2e05fbf84ae29146c9fdce075
jiggerman <liggerman@mail.ru>
2024-04-04 15:42:19 +0300

    Make input and output data

--- chistyakov.vladislav/T2/main.cpp
+++ chistyakov.vladislav/T2/main.cpp
@@ -1,13 +1,36 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
 #include <iterator>
+#include <string>
+#include <list>
+#include <algorithm>
+#include <limits>
+#include "dataStruct.hpp"
 
 int main()
 {
-  using input_it_t = std::istream_iterator< int >;
-  std::vector< int > data(input_it_t{std::cin}, input_it_t{});
+  using namespace chistyakov;
+  std::list< DataStruct > data;
 
-  using output_it_t = std::ostream_iterator< int >;
-  std::copy(data.cbegin(), data.cend(), output_it_t{std::cout, "\n"});
+  using input_it_t = std::istream_iterator< DataStruct >;
+  while (!std::cin.eof())
+  {
+    if (std::cin.fail())
+    {
+      std::cin.clear();
+      std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+    }
+    std::copy(
+      input_it_t{std::cin},
+      input_it_t{},
+      std::back_inserter(data)
+    );
+  }
+
+  using output_it_t = std::ostream_iterator< DataStruct >;
+  std::copy(
+    data.cbegin(),
+    data.cend(),
+    output_it_t{std::cout, "\n"}
+  );
+  return 0;
 }
