8618735e49e44c6aae296ff15935ec4b546f2c57
plitkkka <anastasiatimchishina@yandex.ru>
2024-04-04 22:50:49 +0300

    Added the input

--- timchishina.anastasia/T2/dataStruct.cpp
+++ timchishina.anastasia/T2/dataStruct.cpp
@@ -1,5 +1,41 @@
 #include "dataStruct.hpp"
+#include "delimiter.hpp"
 #include "streamGuard.hpp"
+
+std::istream& timchishina::operator>>(std::istream & in, DataStruct & data)
+{
+  std::istream::sentry guard(in);
+  if (!guard)
+  {
+    return in;
+  }
+  using del = Delimiter;
+  in >> del{'('};
+  for (int i = 0; i < 3; i++)
+  {
+    int num = 0;
+    in >> del{':'} >> del{'k'} >> del{'e'} >> del{'y'};
+    in >> num;
+    if (num == 1)
+    {
+      in >> data.key1 >> del{'l'} >> del{'l'};
+    }
+    else if (num == 2)
+    {
+      in >> std::oct >> data.key2;
+    }
+    else if (num == 3)
+    {
+      in >> del{"\""} >> data.key3 >> del{"\""};
+    }
+    else
+    {
+      in.setstate(std::ios::failbit);
+    }
+  }
+  in >> del{':'} >> del{')'};
+  return in;
+}
 
 std::ostream & timchishina::operator<<(std::ostream & out, const DataStruct & data)
 {
