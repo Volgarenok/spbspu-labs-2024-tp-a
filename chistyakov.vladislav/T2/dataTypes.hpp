3e8978d7a9a9ec26ace99af303496972b2b45dbb
jiggerman <liggerman@mail.ru>
2024-04-04 15:38:03 +0300

    fix dataTypes operators

--- chistyakov.vladislav/T2/dataTypes.hpp
+++ chistyakov.vladislav/T2/dataTypes.hpp
@@ -9,21 +9,21 @@ namespace chistyakov
 {
   struct UllBin
   {
-    unsigned long long & binaryNumber;
+    unsigned long long binaryNumber;
   };
-  std::istream & operator>>(std::istream & in, UllBin && binNum);
+  std::istream & operator>>(std::istream & in, UllBin & binNum);
 
   struct RatLsp
   {
-    std::pair< long long, unsigned long long > & pairLlAndUll;
+    std::pair< long long, unsigned long long > pairLlAndUll;
   };
-  std::istream & operator>>(std::istream & in, RatLsp && pair);
+  std::istream & operator>>(std::istream & in, RatLsp & pair);
 
   struct StringT
   {
     std::string str;
   };
-  std::istream & operator>>(std::istream&, StringT && str);
+  std::istream & operator>>(std::istream &, StringT & str);
 }
 
 #endif
