b17407a13c4663441f08eb767d8596ac1eb1c19d
StantStantov <ibragimovimil0@gmail.com>
2024-04-05 00:29:00 +0300

    Update dataStructure
    1.Add StreamGuard
    1.Change operator<

--- ibragimov.imil/T2/dataStructure.cpp
+++ ibragimov.imil/T2/dataStructure.cpp
@@ -1,6 +1,7 @@
 #include "dataStructure.hpp"
 
 #include <iomanip>
+#include <ios>
 #include <tuple>
 #include "delimiters.hpp"
 #include "inputFormatters.hpp"
@@ -16,10 +17,11 @@ std::istream& ibragimov::operator>>(std::istream& in, DataStructure& value)
     int keysAmount = 3;
     bool isInputed[3] = {false, false, false};
     int keyId = 0;
+    in >> std::noskipws;
     in >> DelimiterI{"("};
     while ((in) && (keysAmount != 0))
     {
-      in >> DelimiterI{":key"} >> keyId;
+      in >> DelimiterI{":key"} >> keyId >> std::ws;
       if ((keyId == 1) && (isInputed[keyId - 1] == false))
       {
         in >> DoubleLitI{value.key1};
@@ -47,6 +49,7 @@ std::istream& ibragimov::operator>>(std::istream& in, DataStructure& value)
 std::ostream& ibragimov::operator<<(std::ostream& out, const DataStructure& value)
 {
   using namespace ibragimov;
+  StreamGuard sGuard(out);
   out << std::fixed << std::setprecision(1);
   out << "(:key1 " << value.key1 << "d";
   out << ":key2 " << "#c(" << value.key2.real() << ' ' << value.key2.imag() << ")";
@@ -56,14 +59,6 @@ std::ostream& ibragimov::operator<<(std::ostream& out, const DataStructure& valu
 
 bool ibragimov::operator<(const DataStructure& l, const DataStructure& r)
 {
-  double lComplex = std::abs(l.key2);
-  double rComplex = std::abs(r.key2);
-  size_t lStr = l.key3.size();
-  size_t rStr = r.key3.size();
-  return std::tie(l.key1, lComplex, lStr) < std::tie(r.key1, rComplex, rStr);
-}
-
-bool ibragimov::operator==(const DataStructure& l, const DataStructure& r)
-{
-  return (!(l < r) && !(r < l));
+  return std::forward_as_tuple(l.key1, std::abs(l.key2), l.key3.size()) 
+      < std::forward_as_tuple(l.key1, std::abs(l.key2), r.key3.size());
 }
