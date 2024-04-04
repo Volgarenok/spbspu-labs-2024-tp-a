d00597e21d2de9bda34e50865a166160cdee5a4c
jiggerman <liggerman@mail.ru>
2024-04-04 15:41:26 +0300

    first try main...

--- chistyakov.vladislav/T2/main.cpp
+++ chistyakov.vladislav/T2/main.cpp
@@ -1,2 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+
 int main()
-{}
+{
+  using input_it_t = std::istream_iterator< int >;
+  std::vector< int > data(input_it_t{std::cin}, input_it_t{});
+
+  using output_it_t = std::ostream_iterator< int >;
+  std::copy(data.cbegin(), data.cend(), output_it_t{std::cout, "\n"});
+}
