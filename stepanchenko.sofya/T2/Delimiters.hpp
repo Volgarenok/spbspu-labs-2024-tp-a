7b12f0987f9d36b5164977ae487d82ab5b9be5c1
loveShrek <sofia.kunaeva@gmail.com>
2024-04-08 01:19:54 +0300

    Added Delimiters.hpp file back

---
+++ stepanchenko.sofya/T2/Delimiters.hpp
@@ -0,0 +1,22 @@
+#ifndef DELIMETERS
+#define DELIMETERS
+
+#include <iostream>
+
+namespace stepanchenko
+{
+  struct DelimiterI
+  {
+    const char expected;
+  };
+
+  struct StringDelimiterI
+  {
+    const char* expected;
+  };
+  
+  std::istream& operator>>(std::istream& in, DelimiterI&& del);
+  std::istream& operator>>(std::istream& in, StringDelimiterI&& del);
+}
+
+#endi
