32b659b62e9d2f952c07960dca9eaaf4692c410f
SofiyLop <lopatina.sa@edu.spbstu.ru>
2024-04-07 15:42:53 +0300

    Many tries

--- lopatina.sofia/T2/main.cpp
+++ lopatina.sofia/T2/main.cpp
@@ -5,16 +5,17 @@ int main()
 {
   int a = 0, b = 0;
   char c = 0;
-  if (!(std::cin >> a >> b))
+  while (!(std::cin.eof()))
   {
-    std::cin.clear(std::ios::goodbit);
-    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
     if (!(std::cin >> a >> b))
     {
-      std::cerr << "Error\n";
-      return 1;
+      std::cin.clear(std::ios::goodbit);
+      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    }
+    else
+    {
+      std::cout << a << ' ' << b << '\n';
+      return 0;
     }
   }
-  std::cout << a << ' ' << b << '\n';
-  return 0;
 }
