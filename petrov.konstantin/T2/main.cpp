29af88029515e0da0d27b7095e1e139209800068
BrainR <constantin.m.petroo@gmail.com>
2024-04-07 19:08:15 +0300

    CG in main

--- petrov.konstantin/T2/main.cpp
+++ petrov.konstantin/T2/main.cpp
@@ -4,41 +4,30 @@
 #include <vector>
 #include <algorithm>
 #include <iterator>
+#include <limits>
 #include "DataStruct.h"
 
 int main()
 {
   using petrov::DataStruct;
-  std::vector< DataStruct > dataStruct;
+  std::vector< DataStruct > dataStructs;
   while (!std::cin.eof())
   {
-    std::copy
-    (
-      std::istream_iterator< DataStruct >(std::cin),
-      std::istream_iterator< DataStruct >(),
-      std::back_inserter(dataStruct)
-    );
+    std::istream_iterator< DataStruct > cinItFirst(std::cin);
+    std::istream_iterator< DataStruct > cinItLast;
+    std::copy(cinItFirst, cinItLast, std::back_inserter(dataStructs));
     if (std::cin.fail())
     {
       std::cin.clear();
-      std::cin.ignore
-      (
-        std::numeric_limits<std::streamsize>::max(),
-        '\n'
-      );
+      auto nLMax = std::numeric_limits< std::streamsize >::max();
+      std::cin.ignore(nLMax, '\n');
     }
   }
-  std::sort
-  (
-    dataStruct.begin(),
-    dataStruct.end()
-  );
-  std::copy
-  (
-    std::begin(dataStruct),
-    std::end(dataStruct),
-    std::ostream_iterator< DataStruct >(std::cout, "\n")
-  );
+  std::sort(dataStructs.begin(), dataStructs.end());
+  auto dsItBegin = std::begin(dataStructs);
+  auto dsItEnd = std::end(dataStructs);
+  std::ostream_iterator< DataStruct > coutIt(std::cout, "\n");
+  std::copy(dsItBegin, dsItEnd, coutIt);
   return 0;
 }
 
