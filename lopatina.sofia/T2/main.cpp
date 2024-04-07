67bcdff81b251154fd4c72ba5ccb48fc8b3a25d4
SofiyLop <lopatina.sa@edu.spbstu.ru>
2024-04-07 15:30:00 +0300

    Input and second try

--- lopatina.sofia/T2/main.cpp
+++ lopatina.sofia/T2/main.cpp
@@ -1,2 +1,20 @@
+#include <iostream>
+#include <limits>
+
 int main()
-{}
+{
+  int a = 0, b = 0;
+  char c = 0;
+  if (!(std::cin >> a >> b))
+  {
+    std::cin.clear(std::ios::goodbit);
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    if (!(std::cin >> a >> b))
+    {
+      std::cerr << "Error\n";
+      return 1;
+    }
+  }
+  std::cout << a << ' ' << b << '\n';
+  return 0;
+}
