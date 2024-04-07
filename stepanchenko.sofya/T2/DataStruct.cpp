09c99184803c4d3a58521fc48d17bc5ca14dd013
loveShrek <sofia.kunaeva@gmail.com>
2024-04-08 02:07:10 +0300

    Fixed sort function

--- stepanchenko.sofya/T2/DataStruct.cpp
+++ stepanchenko.sofya/T2/DataStruct.cpp
@@ -55,13 +55,13 @@ std::ostream& stepanchenko::operator<<(std::ostream& out, const DataStruct& data
 
 bool stepanchenko::operator<(const DataStruct& data1, const DataStruct& data2)
 {
-  if (data1.key1_ == data2.key2_)
+  if (data1.key1_ != data2.key1_)
   {
-    if (data1.key2_ == data2.key2_)
-    {
-      return data1.key3_.length() < data2.key3_.length();
-    }
+    return data1.key1_ < data2.key1_;
+  }
+  if (data1.key2_ != data2.key2_)
+  {
     return data1.key2_ < data2.key2_;
   }
-  return data1.key1_ < data2.key1_;
+  return data1.key3_.length() < data2.key3_.length();
 }
