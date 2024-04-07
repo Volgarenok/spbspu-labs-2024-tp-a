0885bf8cb16e1a589294a77ffe86343e2380d3ed
loveShrek <sofia.kunaeva@gmail.com>
2024-04-07 22:49:01 +0300

    Input and output algorithm

--- stepanchenko.sofya/T2/main.cpp
+++ stepanchenko.sofya/T2/main.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
+#include <vector>
+#include <iterator>
+#include <limits>
+#include <algorithm>
+
+#include "DataStruct.h"
 
 int main()
 {
+  using namespace stepanchenko;
+  std::vector< DataStruct > data;
+  while (!std::cin.eof())
+  {
+    std::copy(
+	std::istream_iterator< DataStruct >{std::cin},
+	std::istream_iterator< DataStruct >(),
+	std::back_inserter(data)
+    );
+  }
+
+  std::sort(data.begin(), data.end());
+  std::copy(
+	data.begin(),
+	data.end(),
+	std::ostream_iterator< DataStruct >(std::cout, "\n")
+  );
+
   return 0;
 }
