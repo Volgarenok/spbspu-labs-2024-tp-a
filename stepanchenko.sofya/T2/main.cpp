8fe3f95b82c83d30f7b54635c52b3d54fa1f800e
loveShrek <sofia.kunaeva@gmail.com>
2024-04-07 22:50:48 +0300

    Wrong input case

--- stepanchenko.sofya/T2/main.cpp
+++ stepanchenko.sofya/T2/main.cpp
@@ -17,6 +17,12 @@ int main()
 	std::istream_iterator< DataStruct >(),
 	std::back_inserter(data)
     );
+
+    if (std::cin.fail())
+    {
+      std::cin.clear();
+      std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+    }
   }
 
   std::sort(data.begin(), data.end());
