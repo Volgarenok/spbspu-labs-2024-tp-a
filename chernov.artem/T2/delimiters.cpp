7ffc7f8e79cacbcef5199ed6026fd247a97765be
CA21lmFlame <chernov.ar@spbstu.ru>
2024-04-07 21:44:23 +0300

    implemented the input operator for Unsigned long long

--- chernov.artem/T2/delimiters.cpp
+++ chernov.artem/T2/delimiters.cpp
@@ -1,9 +1,17 @@
 #include "delimiters.hpp"
 #include "streamGuard.hpp"
 #include <iostream>
+#include <cmath>
 
 std::istream& chernov::operator>>(std::istream& in, UnsignedLongLongIO&& exp)
-{}
+{
+  std::istream::sentry sentry(in);
+  if (!sentry)
+  {
+    return in;
+  }
+  return in >> CharIO{ '0' } >> CharIO{ 'x' } >> std::hex >> exp.value_;
+}
 
 std::istream& chernov::operator>>(std::istream& in, CharIO&& exp)
 {}
