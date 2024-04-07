21b9d5828b8778b0e09c3942ce7822ab16f0fdc2
loveShrek <sofia.kunaeva@gmail.com>
2024-04-07 22:24:02 +0300

    Added input operator for StringI

--- stepanchenko.sofya/T2/Formats.cpp
+++ stepanchenko.sofya/T2/Formats.cpp
@@ -14,6 +14,7 @@ std::istream& stepanchenko::operator>>(std::istream& in, LongLongI&& ch)
   }
   using strDel = StringDelimeterI;
   in >> ch.longlong_key >> strDel{ "ull" };
+  
   return in;
 }
 
@@ -27,7 +28,22 @@ std::istream& stepanchenko::operator>>(std::istream& in, CharI&& ch)
   }
   using del = DelimeterI;
   in >> del{ '\'' } >> ch.char_key >> del{ '\'' };
+  
   return in;
 }
 
 
+std::istream& stepanchenko::operator>>(std::istream& in, StringI&& str)
+{
+  std::istream::sentry guard(in);
+  if (!guard)
+  {
+    in.setstate(std::ios::failbit);
+  }
+  using del = DelimeterI;
+  in >> del{ '\"' };
+  std::getline(in, str.string_key, '"');
+  in >> del{ '\"' };
+
+  return in;
+}
