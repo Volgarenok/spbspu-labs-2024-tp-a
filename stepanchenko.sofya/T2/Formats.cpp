3f00aae06a3390aa2aabd8bd223d75f477c86657
loveShrek <sofia.kunaeva@gmail.com>
2024-04-07 22:22:22 +0300

    Added input operator for CharI

--- stepanchenko.sofya/T2/Formats.cpp
+++ stepanchenko.sofya/T2/Formats.cpp
@@ -16,3 +16,18 @@ std::istream& stepanchenko::operator>>(std::istream& in, LongLongI&& ch)
   in >> ch.longlong_key >> strDel{ "ull" };
   return in;
 }
+
+
+std::istream& stepanchenko::operator>>(std::istream& in, CharI&& ch)
+{
+  std::istream::sentry guard(in);
+  if (!guard)
+  {
+    in.setstate(std::ios::failbit);
+  }
+  using del = DelimeterI;
+  in >> del{ '\'' } >> ch.char_key >> del{ '\'' };
+  return in;
+}
+
+
