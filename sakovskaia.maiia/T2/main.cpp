b40e1c3cfe026a3e5060aca4ffa4da1ba1786d63
MayaSakovskaya <zloyejik05@gmail.com>
2024-04-08 11:18:02 +0300

    added main.cpp

--- sakovskaia.maiia/T2/main.cpp
+++ sakovskaia.maiia/T2/main.cpp
@@ -1,2 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <iterator>
+#include <algorithm>
+#include "data_struct.hpp"
+
+using namespace sakovskaia;
 int main()
-{}
+{
+  try
+  {
+    std::vector< DataStruct > data_struct;
+    while (!std::cin.eof())
+    {
+      std::copy(std::istream_iterator< DataStruct >(std::cin), std::istream_iterator< DataStruct >(), std::back_inserter(data_struct));
+      if (!std::cin)
+      {
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits< std::streamsize >::max(), "\n");
+      }
+    }
+    std::sort(data_struct.begin(), data_struct.end());
+    std::copy(data_struct.cbegin(), data_struct.cend(), std::ostream_iterator< DataStruct >(std::cout, "\n"));
+  }
+}
