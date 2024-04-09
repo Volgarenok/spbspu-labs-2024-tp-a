e42c1c6f03f2ea24b4d91e75628289faae383441
CA21lmFlame <chernov.ar@spbstu.ru>
2024-04-09 19:17:31 +0300

    small changes

--- chernov.artem/T2/delimiters.hpp
+++ chernov.artem/T2/delimiters.hpp
@@ -1,5 +1,5 @@
 #ifndef DELIMITERS_HPP
-#define	DELIMITERS_HPP
+#define DELIMITERS_HPP
 #include <string>
 #include <iostream>
 
@@ -9,18 +9,22 @@ namespace chernov
   {
     double& value_;
   };
+
   struct UnsignedLongLongIO
   {
     unsigned long long& value_;
   };
+
   struct CharIO
   {
     char value_;
   };
+
   struct StringIO
   {
-    std::string& value_;
+    std::string value_;
   };
+
   std::istream& operator>>(std::istream& in, CharIO&& exp);
   std::istream& operator>>(std::istream& in, DoubleIO&& exp);
   std::istream& operator>>(std::istream& in, UnsignedLongLongIO&& exp);
