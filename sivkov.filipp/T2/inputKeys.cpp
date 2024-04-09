12c38447d55cc9fe6068bea9e3b3c70189012440
exxiiint <filipp.sivkov@yandex.ru>
2024-04-09 21:56:47 +0300

    add input keys

---
+++ sivkov.filipp/T2/inputKeys.cpp
@@ -0,0 +1,57 @@
+#include "inputKeys.hpp"
+#include "delimiter.hpp"
+
+std::istream& sivkov::operator>>(std::istream& input, DoubleKey&& key)
+{
+  std::istream::sentry sentry(in);
+  if (!sentry)
+  {
+    return input;
+  }
+
+  using del = DelimiterI;
+  using delStr = DelimiterStr;
+  double num = 0;
+
+  in >> num >> del{ 'd' };
+  if (in)
+  {
+    key.num = num;
+  }
+  return input;
+}
+
+std::istream& sivkov::operator>>(std::istream& input, ComplexKey&& key)
+{
+  std::istream::sentry sentry(in);
+  if (!sentry)
+  {
+    return input;
+  }
+
+  using del = DelimiterI;
+  using delStr = DelimiterStr;
+  double real = 0;
+  double imag = 0;
+
+  in >> delStr{ "#c(" } >> real >> imag >> del{ ')' };
+
+  if (in)
+  {
+    key.num = { real, imag };
+  }
+  return input;
+}
+
+std::istream& sivkov::operator>>(std::istream& input, StringKey&& key)
+{
+  std::istream::sentry guard(in);
+  if (!guard)
+  {
+    return input;
+  }
+
+  std::getline(in, key.string, '"');
+  return input;
+}
+
---
+++ sivkov.filipp/T2/inputKeys.hpp
@@ -0,0 +1,30 @@
+#ifndef INPUTKEYS_HPP
+#define INPUTKEYS_HPP
+
+#include <istream>
+#include <complex>
+#include <string>
+
+namespace sivkov
+{
+  struct DoubleKey
+  {
+    double& num;
+  }
+
+  struct ComplexKey
+  {
+    std::complex< double >& num;
+  }
+
+  struct StringKey
+  {
+    std::string& string;
+  };
+
+  std::istream& operator>>(std::istream& in, DoubleKey&& key);
+  std::istream& operator>>(std::istream& in, ComplexKey&& key);
+  std::istream& operator>>(std::istream& in, StringKey&& key);
+}
+
+#endif
