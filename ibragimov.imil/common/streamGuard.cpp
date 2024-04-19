b735e2ee46f1d359cff4a230d4507348b97b25e0
StantStantov <ibragimovimil0@gmail.com>
2024-04-20 00:31:06 +0300

    [feat!] Add streamGuard
    1. Create common directory
    2. Breaking Change: Transfer streamGuard from T2 to common

--- ibragimov.imil/common/streamGuard.cpp
+++ ibragimov.imil/common/streamGuard.cpp
@@ -1,12 +1,12 @@
 #include "streamGuard.hpp"
 
-ibragimov::StreamGuard::StreamGuard(std::basic_ios< char >& s):
+ibragimov::detail::StreamGuard::StreamGuard(std::basic_ios< char >& s):
   s_(s),
   precision_(s.precision()),
   flags_(s.flags())
 {}
 
-ibragimov::StreamGuard::~StreamGuard()
+ibragimov::detail::StreamGuard::~StreamGuard()
 {
   s_.precision(precision_);
   s_.flags(flags_);
