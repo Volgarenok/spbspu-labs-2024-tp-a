dfa45a51d5af550b7e048db82f470378d8c2032c
StantStantov <ibragimovimil0@gmail.com>
2024-04-20 00:37:59 +0300

    [feat] Add Delimiter struct

---
+++ ibragimov.imil/T3/delimiter.cpp
@@ -0,0 +1,20 @@
+#include "delimiter.hpp"
+
+#include "../common/streamGuard.hpp"
+
+std::istream& ibragimov::detail::operator>>(std::istream& in, Delimiter && rhs)
+{
+  std::istream::sentry guard(in);
+  if (!guard) 
+  {
+    return in;
+  }
+  StreamGuard sguard(in);
+  char c = '0';
+  in >> c;
+  if ((in) && (c != rhs.expected))
+  {
+    in.setstate(std::ios::failbit);
+  }
+  return in;
+}
