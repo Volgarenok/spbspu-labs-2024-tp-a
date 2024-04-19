b864979307312f8f539f681d5e0d59499b3edd7e
StantStantov <ibragimovimil0@gmail.com>
2024-04-20 00:38:25 +0300

    [refactor] Update Polygon's operator<<
    1. Add StreamGuard
    2. Add Delimiter

--- ibragimov.imil/T3/point.cpp
+++ ibragimov.imil/T3/point.cpp
@@ -1,12 +1,18 @@
 #include "point.hpp"
 
+#include "../common/streamGuard.hpp"
+#include "delimiter.hpp"
+
 std::istream& ibragimov::operator>>(std::istream& in, Point& rhs)
 {
   std::istream::sentry guard(in);
   if (!guard) {
     return in;
   }
-  in >> rhs.x >> rhs.y;
+  using namespace detail;
+  StreamGuard sguard(in);
+  in >> std::noskipws;
+  in >> Delimiter{'('} >> rhs.x >> Delimiter{';'} >> rhs.y >> Delimiter{')'};
   if (!in)
   {
     in.setstate(std::ios::failbit);
