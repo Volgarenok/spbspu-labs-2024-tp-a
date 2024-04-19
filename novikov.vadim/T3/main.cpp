e7686e232f39a92cd6c585b39f70e49f448de297
C3mR4de <NVadim2005@gmail.com>
2024-04-19 23:14:06 +0300

    Added basic opening file test to main.cpp file

--- novikov.vadim/T3/main.cpp
+++ novikov.vadim/T3/main.cpp
@@ -1,2 +1,20 @@
-int main()
-{}
+#include <iostream>
+#include <fstream>
+
+int main(int argc, const char* argv[])
+{
+  if (argc != 2)
+  {
+    std::cerr << "Error: invalid argument provided!\n";
+    return 1;
+  }
+
+  std::ifstream file(argv[1]);
+  if (!file.is_open())
+  {
+    std::cerr << "Error: can't open the file!\n";
+    return 2;
+  }
+
+  return 0;
+}
