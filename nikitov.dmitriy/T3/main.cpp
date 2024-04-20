82a2c6874e47132e01cafccd6b5b3fbd7cb7e6e1
Dmitriy <Dmitry5335@yandex.ru>
2024-04-20 18:22:55 +0300

    Filename check and base for figures input

--- nikitov.dmitriy/T3/main.cpp
+++ nikitov.dmitriy/T3/main.cpp
@@ -1,2 +1,21 @@
-int main()
-{}
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <iterator>
+#include "figures_struct.hpp"
+
+int main(int argc, char* argv[])
+{
+  using namespace nikitov;
+  if (argc == 2)
+  {
+    std::ifstream input(argv[1]);
+    using input_it_t = std::istream_iterator< Polygon >;
+    std::vector< Polygon > data(input_it_t{ input }, input_it_t{});
+  }
+  else
+  {
+    std::cerr << "Error: Wrong input parameters" << '\n';
+    return 1;
+  }
+}
