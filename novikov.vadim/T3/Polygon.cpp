e518f597d845bb9c1c7fa2f21be4ed6afe926e5a
C3mR4de <NVadim2005@gmail.com>
2024-04-20 16:04:45 +0300

    Added input implementation for Polygon struct

---
+++ novikov.vadim/T3/Polygon.cpp
@@ -0,0 +1,21 @@
+#include "Polygon.hpp"
+#include <algorithm>
+#include <iterator>
+
+std::istream& novikov::operator>>(std::istream& in, Polygon& polygon)
+{
+  std::istream::sentry sentry(in);
+  if (!sentry)
+  {
+    return in;
+  }
+  std::size_t n;
+  in >> n;
+  std::vector< Point > points;
+  std::copy_n(std::istream_iterator< Point >{ in }, n, std::back_inserter(points));
+  if (in)
+  {
+    polygon.points = std::move(points);
+  }
+  return in;
+}
--- novikov.vadim/T3/Polygon.hpp
+++ novikov.vadim/T3/Polygon.hpp
@@ -11,7 +11,6 @@ namespace novikov
   {
     std::vector< Point > points;
   };
-
   std::istream& operator>>(std::istream& in, Polygon& polygon);
 }
 
