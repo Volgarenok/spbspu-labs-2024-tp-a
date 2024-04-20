ee62b1579ac9947d164bddad12fb44700cac35a2
Dmitriy <Dmitry5335@yandex.ru>
2024-04-21 00:03:46 +0300

    Move vector creating out of input parameters check

--- nikitov.dmitriy/T3/main.cpp
+++ nikitov.dmitriy/T3/main.cpp
@@ -7,11 +7,13 @@
 int main(int argc, char* argv[])
 {
   using namespace nikitov;
+
+  std::vector< Polygon > data;
   if (argc == 2)
   {
     std::ifstream input(argv[1]);
     using input_it_t = std::istream_iterator< Polygon >;
-    std::vector< Polygon > data(input_it_t{ input }, input_it_t{});
+    data.insert(data.begin(), input_it_t{ input }, input_it_t{});
   }
   else
   {
