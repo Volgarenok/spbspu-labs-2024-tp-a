7dbe74112ce1ee31c74b7fa7dbebd4a0e4d3bfbe
Dmitriy <Dmitry5335@yandex.ru>
2024-04-20 21:21:40 +0300

    Operator >> for Point

--- nikitov.dmitriy/T3/figures_struct.cpp
+++ nikitov.dmitriy/T3/figures_struct.cpp
@@ -1,10 +1,22 @@
 #include "figures_struct.hpp"
+#include <limits>
 #include <istream>
+#include <iostream>
+#include <iterator>
+#include <algorithm>
 #include <delimiter.hpp>
 
 std::istream& nikitov::operator>>(std::istream& input, Point& value)
 {
-  
+  std::istream::sentry guard(input);
+  if (!guard)
+  {
+    return input;
+  }
+  using DelChar = DelimiterChar;
+  input >> DelChar({'('}) >> value.x >> DelChar({';'}) >> value.y >> DelChar({')'});
+
+  return input;
 }
 
 std::istream& nikitov::operator>>(std::istream& input, Polygon& value)
