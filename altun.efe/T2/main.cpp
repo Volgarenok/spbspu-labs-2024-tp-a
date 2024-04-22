6d3a3fb4349ba60cfeb89627f2b19e5543ddee7e
efeshka <rokie12312@gmail.com>
2024-04-22 20:34:07 +0300

    Main body added.

--- altun.efe/T2/main.cpp
+++ altun.efe/T2/main.cpp
@@ -1,19 +1,25 @@
 #include <iostream>
-#include <iomanip>
-#include <string>
-#include <ios>
 #include <vector>
 #include <limits>
 #include <algorithm>
 #include <iterator>
-#include "streamGuard.hpp"
-#include "delimiter.hpp"
 #include "dataStruct.hpp"
 
 int main()
 {
-  using namespace altun;
-  DataStruct a{0, 0, ""};
-  std::cin >> a;
-  std::cout << a << '\n';
+  using altun::DataStruct;
+  std::vector< DataStruct > data;
+  while (!std::cin.eof())
+  {
+    using input_it = std::istream_iterator< DataStruct >;
+    std::copy(input_it{std::cin}, input_it{}, std::back_inserter(data));
+    if (!std::cin)
+    {
+      std::cin.clear();
+      std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+    }
+  }
+  std::sort(data.begin(), data.end());
+  using output_it = std::ostream_iterator< DataStruct >;
+  std::copy(data.cbegin(), data.cend(), output_it{std::cout, "\n"});
 }
