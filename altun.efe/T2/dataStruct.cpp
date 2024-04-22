4b6b30ddaca48217f06c763f33833e39fff33201
efeshka <rokie12312@gmail.com>
2024-04-22 23:07:54 +0300

    a little bit bigger fix

--- altun.efe/T2/dataStruct.cpp
+++ altun.efe/T2/dataStruct.cpp
@@ -1,4 +1,5 @@
 #include "dataStruct.hpp"
+#include "dataStruct.hpp"
 #include <iomanip>
 #include "streamGuard.hpp"
 #include "delimiter.hpp"
@@ -24,3 +25,69 @@ std::istream& altun::operator>>(std::istream& in, DataStruct& data)
     {
       in >> data.key1 >> del{'d'};
     }
+    else if (dnumber == 2)
+    {
+      in >> data.key2;
+    }
+    else if (dnumber == 3)
+    {
+      in >> del{'"'};
+      std::getline(in, data.key3, '"');
+    }
+  }
+  in >> del{':'} >> del{')'};
+  return in;
+}
+std::ostream& altun::stabilizeScientific(std::ostream& out, double dnum)
+{
+  StreamGuard s_guard(out);
+  out << std::fixed << std::setprecision(1);
+  int exp = 0;
+  char sign = 0;
+  if (dnum >= 1.0)
+  {
+    while (dnum > 1.0)
+    {
+      dnum /= 10;
+      exp++;
+    }
+    sign = '+';
+  }
+  else
+  {
+    while (dnum < 1.0)
+    {
+      dnum *= 10;
+      exp++;
+    }
+    sign = '-';
+  }
+  out << dnum << 'e' << sign << exp;
+  return out;
+}
+std::ostream& altun::operator<<(std::ostream& out, const DataStruct& data)
+{
+  std::ostream::sentry guard(out);
+  if (!guard)
+  {
+    return out;
+  }
+  StreamGuard s_guard(out);
+  out << "(:key1 " <<  std::setprecision(1) << std::fixed << data.key1;
+  out << "d:key2 ";
+  stabilizeScientific(out, data.key2);
+  out << ":key3 " << '"' << data.key3 << "\":)";
+  return out;
+}
+bool altun::operator<(const DataStruct& lhs, const DataStruct& rhs)
+{
+  if (lhs.key1 != rhs.key1)
+  {
+    return lhs.key1 < rhs.key1;
+  }
+  if (lhs.key2 != rhs.key2)
+  {
+    return lhs.key2 < rhs.key2;
+  }
+  return lhs.key3.size() < rhs.key3.size();
+}
