cc6068eab9948391589a1faa40b8187ac9e5f290
MaxSazanov <makssazanov31@gmail.com>
2024-04-24 00:58:50 +0300

    added structs Point and Polygon

---
+++ sazanov.maxim/T3/Point.hpp
@@ -0,0 +1,13 @@
+#ifndef POINT_HPP
+#define POINT_HPP
+
+namespace sazanov
+{
+  struct Point
+  {
+    int x;
+    int y;
+  };
+}
+
+#endif
---
+++ sazanov.maxim/T3/Polygon.hpp
@@ -0,0 +1,15 @@
+#ifndef POLYGON_HPP
+#define POLYGON_HPP
+
+#include <vector>
+#include "Point.hpp"
+
+namespace sazanov
+{
+  class Polygon
+  {
+    std::vector< Point > points;
+  };
+}
+
+#endif
