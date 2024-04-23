d0ba3a036a5ea862e14089e97acb95ed1510f8b1
RozayYakshieva <rozay.yakshieva.22@mail.ru>
2024-04-23 11:51:05 +0300

    Streamguard explicit repair

---
+++ yakshieva.rozay/T2/Streamguard.hpp
@@ -0,0 +1,21 @@
+#ifndef STREAM_GUARD_HPP
+#define STREAM_GUARD_HPP
+
+#include <iostream>
+
+namespace yakshieva
+{
+  class Streamguard
+  {
+  public:
+    explicit Streamguard(std::basic_ios< char >& s);
+    ~Streamguard();
+
+  private:
+    std::basic_ios< char >& s_;
+    std::streamsize precision_;
+    std::basic_ios< char >::fmtflags fmt_;
+  };
+}
+
+#endif
