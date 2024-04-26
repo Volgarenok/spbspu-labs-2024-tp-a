1f39e3699de909938c72dd00c1a70a685171f97a
yoojek <deminsn2004@gmail.com>
2024-04-26 11:22:52 +0300

    formats fix

--- demin.slava/T2/formats.cpp
+++ demin.slava/T2/formats.cpp
@@ -1,38 +1,41 @@
-#include "dataFormats.hpp"
 #include "formats.hpp"
+#include "delimiter.hpp"
 #include "streamGuard.hpp"
 
-std::istream &operator>>(std::istream &in, DblLit &&key)
+std::istream &demin::operator>>(std::istream &in, DblLit &&key)
 {
   std::istream::sentry guard(in);
   if (!guard)
   {
     return in;
   }
-  in >> key.value >> Delimiter{"d"};
+  using del = demin::Delimiter;
+  in >> key.value >> del{"d"};
   return in;
 }
 
-std::istream &operator>>(std::istream &in, UllOct &&key)
+std::istream &demin::operator>>(std::istream &in, UllOct &&key)
 {
   std::istream::sentry guard(in);
   if (!guard)
   {
     return in;
   }
-  StreamGuard streamGuard(in);
-  in >> Delimiter{"0"} >> std::oct >> oct;
+  demin::StreamGuard streamGuard(in);
+  using del = demin::Delimiter;
+  in >> del{"0"} >> std::oct >> key.value;
   return in;
 }
 
-std::istream &operator>>(std::istream &in, StringVal &&key)
+std::istream &demin::operator>>(std::istream &in, StringVal &&key)
 {
   std::istream::sentry guard(in);
   if (!guard)
   {
     return in;
   }
-  std::getline(in >> Delimiter{"\""}, str, '\"');
+  using del = demin::Delimiter;
+  std::getline(in >> del{"\""}, key.value, '\"');
   return in;
 }
 
