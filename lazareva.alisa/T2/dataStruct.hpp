2cb8ced2ba70b783a9f348d2208849b2c9dcf0d3
AlisaLazarevapi8 <lazarevaalisa283@gmail.com>
2024-04-26 19:39:58 +0300

    roma-- fix

---
+++ lazareva.alisa/T2/dataStruct.hpp
@@ -0,0 +1,23 @@
+#ifndef DATASTRUCT_HPP
+#define DATASTRUCT_HPP
+#include <iostream>
+#include <string>
+namespace lazareva
+{
+ struct DataStruct
+  {
+    double key1;
+    long long key2;
+    std::string key3;
+  };
+
+  bool operator<(const DataStruct& lhs, const DataStruct& rhs);
+  std::istream& operator>>(std::istream& in, DataStruct& data);
+  std::ostream& operator<<(std::ostream& out, const DataStruct& data);
+
+}
+
+#endif
+
+
+
---
+++ lazareva.alisa/T2/streamGuard.hpp
@@ -0,0 +1,21 @@
+#ifndef STREAMGUARD_HPP
+#define STREAMGUARD_HPP
+#include <ios>
+
+namespace lazareva
+{
+  struct StreamGuard
+  {
+    public:
+      StreamGuard(std::basic_ios< char >& s);
+      ~StreamGuard();
+
+    private:
+      std::basic_ios< char >& s_;
+      std::streamsize precision_;
+      std::basic_ios< char >::fmtflags flags_;
+  };
+}
+#endif
+
+
