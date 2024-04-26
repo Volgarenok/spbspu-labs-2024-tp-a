9c3adc5c6904cd766bd8839d9c351dbac06c14fc
yoojek <deminsn2004@gmail.com>
2024-04-26 11:00:37 +0300

    streamguard 2nd fix

--- demin.slava/T2/streamGuard.hpp
+++ demin.slava/T2/streamGuard.hpp
@@ -7,11 +7,11 @@ namespace demin
   class StreamGuard
   {
   public:
-    StreamGuard(std::basic_ios< char > &s);
+    explicit StreamGuard(std::basic_ios< char > &s);
     ~StreamGuard();
 
   private:
-    explicit std::basic_ios< char > &s_;
+    std::basic_ios< char > &s_;
     std::streamsize precision_;
     std::basic_ios< char >::fmtflags flags_;
   };
