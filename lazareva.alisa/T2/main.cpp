dc5dbfe97bbcfedf87404e56d5ec307e5770325f
AlisaLazarevapi8 <lazarevaalisa283@gmail.com>
2024-04-26 18:05:17 +0300

    first working version

--- lazareva.alisa/T2/main.cpp
+++ lazareva.alisa/T2/main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include <iomanip>
 #include <string>
-
+#include <algorithm>
+#include <limits>
+#include <vector>
+#include <iterator>
 namespace lazareva
 {
 
@@ -10,6 +14,11 @@ namespace lazareva
   };
   std::istream& operator>>(std::istream& in, Delimiter&& exp)
   {
+    std::istream::sentry guard(in);
+    if (!guard)
+    {
+      return in;
+    }
     char c = 0;
     in >> c;
     if (c != exp.expected)
@@ -25,50 +34,94 @@ namespace lazareva
     long long key2;
     std::string key3;
   };
+
+  bool operator<(const DataStruct& lhs, const DataStruct& rhs)
+  {
+    if (lhs.key1 != rhs.key1)
+    {
+      return lhs.key1 < rhs.key1;
+    }
+    if (lhs.key2 != rhs.key2)
+    {
+      return lhs.key2 < rhs.key2;
+    }
+    return lhs.key3.size() < rhs.key3.size();
+  }
+
   std::istream& operator>>(std::istream& in, DataStruct& data)
   {
+    std::istream::sentry guard(in);
+    if (!guard)
+    {
+      return in;
+    }
+
     using del = lazareva::Delimiter;
-    in >> del{'('} >> del{':'} >> del{'k'} >> del{'e'} >> del{'y'};
-    int i = 0;
-    in >> i;
-    if (i == 1)
+    in >> del{'('};
+    for (int b = 0; b < 3; b++)
     {
-      double value = 0;
-      in >> value;
-      data.key1 = value;
-      in >> del{'d'};
+      in  >> del{':'} >> del{'k'} >> del{'e'} >> del{'y'};
+      int i = 0;
+      in >> i;
+      if (i == 1)
+      {
+        double value = 0;
+        in >> value;
+        data.key1 = value;
+        in >> del{'d'};
+      }
+      else if (i == 2)
+      {
+        long long value = 0;
+        in >> value;
+        data.key2 = value;
+        in >> del{'l'} >> del{'l'};
+      }
+      else if (i == 3)
+      {
+        std::string value = "";
+        in >> del{'"'};
+        std::getline(in, value, '\"');
+        data.key3 = value;
+      }
+      else
+      {
+        in.setstate(std::ios::failbit);
+      }
     }
-    else if (i == 2)
-    {
-      long long value = 0;
-      in >> value;
-      data.key2 = value;
-      in >> del{'l'} >> del{'l'};
-    }
-    else if (i == 3)
-    {
-      std::string value = "";
-      in >> del{'"'};
-      std::getline(in, value, '\"');
-    }
-    else
-    {
-      in.setstate(std::ios::failbit);
-    }
+    in >> del{':'} >> del{')'};
     return in;
   }
 
   std::ostream& operator<<(std::ostream& out, const DataStruct& data)
   {
-    out << "(:key1 " << data.key1 << ":key2 " << data.key2 << ":key3 \"" << data.key3 << "\":)";
+    std::ostream::sentry guard(out);
+    if (!guard)
+    {
+      return out;
+    }
+    out << std::fixed;
+    out << "(:key1 " << std::setprecision(1) <<  data.key1 << "d:key2 " << data.key2 << "ll:key3 \"" << data.key3 << "\":)";
     return out;
   }
-
 }
+
 
 int main()
 {
-  lazareva::DataStruct datastruct{0,2,""};
-  std::cin >> datastruct;
-  std::cout << datastruct;
+  using lazareva::DataStruct;
+  std::vector< DataStruct > data;
+  while (!std::cin.eof())
+  {
+    using input_it = std::istream_iterator< DataStruct >;
+    std::copy(input_it{std::cin}, input_it{}, std::back_inserter(data));
+    if (!std::cin)
+    {
+      std::cin.clear();
+      std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+    }
+  }
+  std::sort(data.begin(), data.end());
+  using output_it = std::ostream_iterator< DataStruct >;
+  std::copy(data.cbegin(), data.cend(), output_it{std::cout, "\n"});
 }
