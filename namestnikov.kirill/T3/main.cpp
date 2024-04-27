6356565a1a3afe9e4a58f9ab08c3d5c35bbf1736
kirillnamestnikov <kir.namestnikov@gmail.com>
2024-04-27 19:48:40 +0300

    started T3 realization

--- namestnikov.kirill/T3/main.cpp
+++ namestnikov.kirill/T3/main.cpp
@@ -1,2 +1,20 @@
-int main()
-{}
+#include <iostream>
+#include <fstream>
+
+int main(int argc, char * argv[])
+{
+  if (argc != 2)
+  {
+    std::cerr << "Wrong command line arguments\n";
+    return 1;
+  }
+  else
+  {
+    std::ifstream in(argv[1]);
+    if (!in.is_open())
+    {
+      std::cerr << "Can not open file\n";
+      return 1;
+    }
+  }
+}
