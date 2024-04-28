ba058ea325b395f0dd9bae7ec53ea90a391fa5d4
C3mR4de <NVadim2005@gmail.com>
2024-04-28 14:46:49 +0300

    Added get_area function and area accumulator

--- novikov.vadim/T3/point.cpp
+++ novikov.vadim/T3/point.cpp
@@ -1,24 +1,46 @@
 #include "point.hpp"
+#include <cmath>
+#include <ostream>
 #include <delimiterI.hpp>
 
-std::istream& novikov::operator>>(std::istream& in, Point& point)
+std::istream& novikov::operator>>(std::istream& in, Point& rhs)
 {
   std::istream::sentry sentry(in);
   if (!sentry)
   {
     return in;
   }
+
   Point tmp{ 0, 0 };
   using del = StrictCaseCharDelimiterI;
   in >> del{ '(' } >> tmp.x >> del{ ';' } >> tmp.y >> del{ ')' };
   if (in)
   {
-    point = tmp;
+    rhs = tmp;
   }
+
   return in;
+}
+
+std::ostream& novikov::operator<<(std::ostream& out, const Point& rhs)
+{
+  std::ostream::sentry sentry(out);
+  if (!sentry)
+  {
+    return out;
+  }
+  out << '(' << rhs.x << ';' << rhs.y << ')';
+  return out;
 }
 
 bool novikov::operator==(const Point& lhs, const Point& rhs)
 {
   return lhs.x == rhs.x && lhs.y == rhs.y;
+}
+
+double novikov::AreaAccumulator::operator()(double val, const Point& p2, const Point& p3)
+{
+  val += 0.5 * std::abs((p2.x - p1.x) * (p3.y - p1.y) - (p3.x - p1.x) * (p2.y - p1.y));
+  p1 = p2;
+  return val;
 }
--- novikov.vadim/T3/point.hpp
+++ novikov.vadim/T3/point.hpp
@@ -9,8 +9,15 @@ namespace novikov
   {
     int x, y;
   };
-  std::istream& operator>>(std::istream& in, Point& point);
+  std::istream& operator>>(std::istream& in, Point& rhs);
+  std::ostream& operator<<(std::ostream& out, const Point& rhs);
   bool operator==(const Point& lhs, const Point& rhs);
+
+  struct AreaAccumulator
+  {
+    Point p1;
+    double operator()(double val, const Point& p2, const Point& p3);
+  };
 }
 
 #endif
