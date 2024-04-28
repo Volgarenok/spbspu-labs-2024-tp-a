ca148540cf8e0e6b5c273098c05e11c0347015d2
C3mR4de <NVadim2005@gmail.com>
2024-04-28 18:44:09 +0300

    Added implementation for all basic commands

--- novikov.vadim/T3/commands.cpp
+++ novikov.vadim/T3/commands.cpp
@@ -1,4 +1,5 @@
 #include "commands.hpp"
+#include <algorithm>
 #include <iomanip>
 #include <numeric>
 #include <functional>
@@ -6,7 +7,7 @@
 #include "polygon.hpp"
 #include "predicates.hpp"
 
-void novikov::cmd::area(const subcommands_t& cmds, const poly_vec_t& vec, std::istream& in, std::ostream& out)
+void novikov::cmd::area(const area_args_t& args, const poly_vec_t& vec, std::istream& in, std::ostream& out)
 {
   std::string arg;
   in >> arg;
@@ -17,12 +18,12 @@ void novikov::cmd::area(const subcommands_t& cmds, const poly_vec_t& vec, std::i
   {
     std::size_t size = std::stoul(arg);
     using namespace std::placeholders;
-    AreaPredicate acc_pred = std::bind(vertexes_count, _1, size);
+    Predicate acc_pred = std::bind(vertexes_count, _1, size);
     area_accumulator = std::bind(cmd::acc_area_if, _1, _2, acc_pred);
   }
   catch (const std::invalid_argument&)
   {
-    area_accumulator = cmds.at(arg);
+    area_accumulator = args.at(arg);
   }
 
   FormatGuard guard(out);
@@ -30,9 +31,9 @@ void novikov::cmd::area(const subcommands_t& cmds, const poly_vec_t& vec, std::i
   out << std::accumulate(vec.cbegin(), vec.cend(), 0.0, area_accumulator) << "\n";
 }
 
-double novikov::cmd::acc_area_if(double val, const Polygon& rhs, AreaPredicate pred)
+double novikov::cmd::acc_area_if(double val, const Polygon& rhs, Predicate pred)
 {
-  return pred(rhs) ? val + get_area(rhs) : 0.0;
+  return pred(rhs) ? val + get_area(rhs) : val;
 }
 
 double novikov::cmd::acc_area_mean(double val, const Polygon& rhs, std::size_t size)
@@ -40,45 +41,68 @@ double novikov::cmd::acc_area_mean(double val, const Polygon& rhs, std::size_t s
   return val + get_area(rhs) / size;
 }
 
-/*
-void novikov::cmd::Area::operator()(const subcommands& cmds, const poly_vec& vec, std::istream& in, std::ostream& out)
+void novikov::cmd::max(const max_args_t& args, const poly_vec_t& vec, std::istream& in, std::ostream& out)
+{
+  std::string arg;
+  in >> arg;
+  args.at(arg)(vec, out);
+}
+
+void novikov::cmd::max_area(const poly_vec_t& vec, std::ostream& out)
+{
+  FormatGuard guard(out);
+  auto res = std::max_element(vec.cbegin(), vec.cend(), compare_areas);
+  out << std::setprecision(1) << std::fixed;
+  out << (res != vec.cend() ? get_area(*res) : 0.0) << "\n";
+}
+
+void novikov::cmd::max_vertexes(const poly_vec_t& vec, std::ostream& out)
+{
+  FormatGuard guard(out);
+  auto res = std::max_element(vec.cbegin(), vec.cend(), compare_vertexes);
+  out << (res != vec.cend() ? res->points.size() : 0) << "\n";
+}
+
+void novikov::cmd::min(const min_args_t& args, const poly_vec_t& vec, std::istream& in, std::ostream& out)
+{
+  std::string arg;
+  in >> arg;
+  args.at(arg)(vec, out);
+}
+
+void novikov::cmd::min_area(const poly_vec_t& vec, std::ostream& out)
+{
+  FormatGuard guard(out);
+  auto res = std::min_element(vec.cbegin(), vec.cend(), compare_areas);
+  out << std::setprecision(1) << std::fixed;
+  out << (res != vec.cend() ? get_area(*res) : 0.0) << "\n";
+}
+
+void novikov::cmd::min_vertexes(const poly_vec_t& vec, std::ostream& out)
+{
+  FormatGuard guard(out);
+  auto res = std::min_element(vec.cbegin(), vec.cend(), compare_vertexes);
+  out << (res != vec.cend() ? res->points.size() : 0) << "\n";
+}
+
+void novikov::cmd::count(const count_args_t& args, const poly_vec_t& vec, std::istream& in, std::ostream& out)
 {
   std::string arg;
   in >> arg;
 
-  double res{};
-
-  try
-  {
-    auto cmd_func = cmds.at(arg);
-    auto acc_func = std::bind(cmd_func, _1, get_area(_2));
-    res = std::accumulate(vec.cbegin(), vec.cend(), 0.0, acc_func);
-  }
-  catch (const std::out_of_range&)
-  {}
+  std::function< bool(const Polygon&) > count_pred;
 
   try
   {
     std::size_t size = std::stoul(arg);
     using namespace std::placeholders;
-    auto predicate = std::bind(vertexes_count, _1, size);
-    auto acc_func = std::bind(acc_area_if, _1, std::bind(get_area, _2), predicate);
-    res = std::accumulate(vec.cbegin(), vec.cend(), 0.0, acc_func);
+    count_pred = std::bind(vertexes_count, _1, size);
   }
   catch (const std::invalid_argument&)
   {
-    throw;
+    count_pred = args.at(arg);
   }
 
-  out << res << "\n";
+  FormatGuard guard(out);
+  out << std::count_if(vec.cbegin(), vec.cend(), count_pred) << "\n";
 }
-
-double novikov::cmd::acc_area_if(double val, const Polygon& rhs, bool(*pred)(const Polygon&))
-{
-  return pred(rhs) ? val + get_area(rhs) : 0.0;
-}
-
-double novikov::cmd::acc_area_mean(double val, const Polygon& rhs, std::size_t size)
-{
-  return val + get_area(rhs) / size;
-}*/
--- novikov.vadim/T3/commands.hpp
+++ novikov.vadim/T3/commands.hpp
@@ -10,13 +10,26 @@ namespace novikov
 {
   namespace cmd
   {
-    using subcommands_t = std::map< std::string, std::function< double(double, const Polygon&) > >;
     using poly_vec_t = std::vector< Polygon >;
 
-    void area(const subcommands_t& cmds, const poly_vec_t& vec, std::istream& in, std::ostream& out);
-    using AreaPredicate = std::function< bool(const Polygon&) >;
-    double acc_area_if(double val, const Polygon& rhs, AreaPredicate pred);
+    using area_args_t = std::map< std::string, std::function< double(double, const Polygon&) > >;
+    void area(const area_args_t& cmds, const poly_vec_t& vec, std::istream& in, std::ostream& out);
+    using Predicate = std::function< bool(const Polygon&) >;
+    double acc_area_if(double val, const Polygon& rhs, Predicate pred);
     double acc_area_mean(double val, const Polygon& rhs, std::size_t size);
+
+    using max_args_t = std::map< std::string, std::function< void(const std::vector< Polygon >&, std::ostream&) > >;
+    void max(const max_args_t& args, const poly_vec_t& vec, std::istream& in, std::ostream& out);
+    void max_area(const poly_vec_t& vec, std::ostream& out);
+    void max_vertexes(const poly_vec_t& vec, std::ostream& out);
+
+    using min_args_t = std::map< std::string, std::function< void(const std::vector< Polygon >&, std::ostream&) > >;
+    void min(const max_args_t& args, const poly_vec_t& vec, std::istream& in, std::ostream& out);
+    void min_area(const poly_vec_t& vec, std::ostream& out);
+    void min_vertexes(const poly_vec_t& vec, std::ostream& out);
+
+    using count_args_t = std::map< std::string, std::function< bool(const Polygon&) > >;
+    void count(const count_args_t& args, const poly_vec_t& vec, std::istream& in, std::ostream& out);
   }
 }
 
