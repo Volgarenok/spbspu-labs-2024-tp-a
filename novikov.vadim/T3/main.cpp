79d13b54d873e3c2612edb396d547ca701604302
C3mR4de <NVadim2005@gmail.com>
2024-04-28 18:44:41 +0300

    Added all basic commands calls in main.cpp file

--- novikov.vadim/T3/main.cpp
+++ novikov.vadim/T3/main.cpp
@@ -9,7 +9,6 @@
 #include "polygon.hpp"
 #include "predicates.hpp"
 
-
 int main(int argc, const char *argv[])
 {
   if (argc != 2)
@@ -36,16 +35,34 @@ int main(int argc, const char *argv[])
     file.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
   }
 
-  std::map< std::string, std::function< double(double, const Polygon&) > > area_subcommands;
+  cmd::area_args_t area_arguments;
 
   using namespace std::placeholders;
-  area_subcommands["EVEN"] = std::bind(cmd::acc_area_if, _1, _2, even_vertexes);
-  area_subcommands["ODD"] = std::bind(cmd::acc_area_if, _1, _2, odd_vertexes);
-  area_subcommands["MEAN"] = std::bind(cmd::acc_area_mean, _1, _2, polygons.size());
+  area_arguments["EVEN"] = std::bind(cmd::acc_area_if, _1, _2, even_vertexes);
+  area_arguments["ODD"] = std::bind(cmd::acc_area_if, _1, _2, odd_vertexes);
+  area_arguments["MEAN"] = std::bind(cmd::acc_area_mean, _1, _2, polygons.size());
+
+  cmd::max_args_t max_arguments;
+
+  max_arguments["AREA"] = std::bind(cmd::max_area, _1, _2);
+  max_arguments["VERTEXES"] = std::bind(cmd::max_vertexes, _1, _2);
+
+  cmd::min_args_t min_arguments;
+
+  min_arguments["AREA"] = std::bind(cmd::min_area, _1, _2);
+  min_arguments["VERTEXES"] = std::bind(cmd::min_vertexes, _1, _2);
+
+  cmd::count_args_t count_arguments;
+
+  count_arguments["EVEN"] = even_vertexes;
+  count_arguments["ODD"] = odd_vertexes;
 
   std::map< std::string, std::function< void(std::istream&, std::ostream&) > > commands;
 
-  commands["AREA"] = std::bind(cmd::area, std::cref(area_subcommands), std::cref(polygons), _1, _2);
+  commands["AREA"] = std::bind(cmd::area, std::cref(area_arguments), std::cref(polygons), _1, _2);
+  commands["MAX"] = std::bind(cmd::max, std::cref(max_arguments), std::cref(polygons), _1, _2);
+  commands["MIN"] = std::bind(cmd::min, std::cref(min_arguments), std::cref(polygons), _1, _2);
+  commands["COUNT"] = std::bind(cmd::count, std::cref(count_arguments), std::cref(polygons), _1, _2);
 
   std::string cmd;
 
