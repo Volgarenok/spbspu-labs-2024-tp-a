caa6c0fcdb4906d709715766c8dc498a760b2206
DanilNovokhatskiy <karuk.vova2005@mail.ru>
2024-04-30 13:27:26 +0300

    Add out_of_range for map.at()

--- novokhatskiy.danil/T3/main.cpp
+++ novokhatskiy.danil/T3/main.cpp
@@ -59,6 +59,10 @@ int main(int argc, char** argv)
       commands.at(argument)(std::cin, std::cout);
       std::cout << '\n';
     }
+    catch (const std::out_of_range&)
+    {
+      std::cout << "<INVALID COMMAND>" << '\n';
+    }
     catch (const std::invalid_argument&)
     {
       std::cout << "<INVALID COMMAND>" << '\n';
