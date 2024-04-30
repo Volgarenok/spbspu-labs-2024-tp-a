493d634781f4b45d27a98c00edb2ab343a2ff16f
C3mR4de <NVadim2005@gmail.com>
2024-04-30 15:39:37 +0300

    Temporarily replaced copy_n to for and added comparator for polygon

--- novikov.vadim/T3/polygon.cpp
+++ novikov.vadim/T3/polygon.cpp
@@ -24,10 +24,29 @@ std::istream& novikov::operator>>(std::istream& in, Polygon& rhs)
 
   std::vector< Point > points;
   points.reserve(n);
-  std::copy_n(std::istream_iterator< Point >{ in }, n, std::back_inserter(points));
-  if (in)
+  for (std::size_t i = 0; in && i < n; ++i)
+  {
+    Point p{ 0, 0 };
+    if (in.peek() == '\n')
+    {
+      in.setstate(std::ios::failbit);
+    }
+    if (in >> p)
+    {
+      points.push_back(p);
+    }
+  }
+  if (in.peek() != '\n' && !in.eof())
+  {
+    in.setstate(std::ios::failbit);
+  }
+  if (in && points.size() == n)
   {
     rhs.points = std::move(points);
+  }
+  else
+  {
+    in.setstate(std::ios::failbit);
   }
 
   return in;
@@ -41,8 +60,17 @@ std::ostream& novikov::operator<<(std::ostream& out, const Polygon& rhs)
     return out;
   }
   out << rhs.points.size() << " ";
-  std::copy(rhs.points.cbegin(), rhs.points.cend(), std::ostream_iterator< Point >{out, " "});
+  std::copy(rhs.points.cbegin(), rhs.points.cend(), std::ostream_iterator< Point >{ out, " " });
   return out;
+}
+
+bool novikov::operator==(const Polygon& lhs, const Polygon& rhs)
+{
+  if (lhs.points.size() != rhs.points.size())
+  {
+    return false;
+  }
+  return std::equal(lhs.points.cbegin(), lhs.points.cend(), rhs.points.cbegin());
 }
 
 double novikov::get_area(const Polygon& rhs)
--- novikov.vadim/T3/polygon.hpp
+++ novikov.vadim/T3/polygon.hpp
@@ -13,6 +13,7 @@ namespace novikov
   };
   std::istream& operator>>(std::istream& in, Polygon& rhs);
   std::ostream& operator<<(std::ostream& out, const Polygon& rhs);
+  bool operator==(const Polygon& lhs, const Polygon& rhs);
   double get_area(const Polygon& rhs);
 }
 
