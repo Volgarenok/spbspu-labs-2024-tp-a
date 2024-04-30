dbf07d644c898d68119155862cff62484814a3e0
Ravinskij-Vladimir <ravunskiy0412@gmail.com>
2024-04-30 10:53:44 +0300

    Tricks with i/o again

--- ravinskij.vladimir/T3/main.cpp
+++ ravinskij.vladimir/T3/main.cpp
@@ -1,17 +1,43 @@
 #include <iostream>
 #include <iterator>
 #include <algorithm>
+#include <fstream>
+#include <cstddef>
+#include <limits>
 #include "polygon.hpp"
-int main()
+
+int main(int argc, char* argv[])
 {
   using namespace ravinskij;
-  int n = 0;
-  using input_it_t = std::istream_iterator< Point >;
-  using output_it_t = std::ostream_iterator< Point >;
-  std::cin >> n;
+  if (argc < 2)
+	{
+    std::cerr << "No file is provided\n";
+    return 1;
+  }
+  std::ifstream file(argv[1]);
+  if (!file.is_open())
+  {
+    std::cerr << "Couldn't open file\n";
+    return 2;
+  }
+
   Polygon polygon;
-  std::copy_n(input_it_t{std::cin}, n, std::back_inserter(polygon.points));
-  std::copy(polygon.points.cbegin(), polygon.points.cend(), output_it_t{std::cout, " "});
-  std::cout << '\n';
+  using input_it_t = std::istream_iterator< Point >;
+  std::size_t vertexCount = 0;
+  while (!file.eof())
+  {
+    file >> vertexCount;
+    std::copy_n(input_it_t{ file }, vertexCount, std::back_inserter(polygon.points));
+    if (file)
+    {
+      std::copy(polygon.points.cbegin(), polygon.points.cend(),
+      std::ostream_iterator< Point >(std::cout, " "));
+      std::cout << '\n';
+    }
+    polygon.points.clear();
+    file.clear();
+    file.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+  }
+  file.close();
   return 0;
 }
