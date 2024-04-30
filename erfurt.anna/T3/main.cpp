4bf789dfa494b9b6f83807ded7a32779401c3554
Anyanari <a.erfurt02@mail.ru>
2024-04-30 22:02:01 +0300

    Update main

--- erfurt.anna/T3/main.cpp
+++ erfurt.anna/T3/main.cpp
@@ -1,4 +1,76 @@
-int main()
+#include "Polygon.hpp"
+#include "Commands.hpp"
+#include <fstream>
+#include <functional>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <map>
+#include <string>
+#include <vector>
+#include <numeric>
+
+int main(int argc, char* argv[])
 {
+  if (argc < 2)
+  {
+     std::cerr << "Required filename argument\n";
+     return 1;
+  }
+  std::ifstream file(argv[1]);
+  if (!file.is_open())
+  {
+    std::cerr << "File cannot be opened\n";
+    return 2;
+  }
+
+  using namespace erfurt;
+  using namespace std::placeholders;
+
+  if (!file.is_open())
+  {
+    throw std::invalid_argument("File is not open\n");
+  }
+
+  std::vector<Polygon> polygons;
+
+  using input_it_it = std::istream_iterator< Polygon >;
+  while (!file.eof())
+  {
+    std::copy(input_it_it{file}, input_it_it{}, std::back_inserter(polygons));
+    file.clear();
+    file.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+  }
+
+  std::map<std::string, std::function<void(std::istream &, std::ostream &)>> commands;
+  commands["AREA"] = std::bind(area, std::cref(polygons), _1, _2);
+  commands["MAX"] = std::bind(max, std::cref(polygons), _1, _2);
+  commands["MIN"] = std::bind(min, std::cref(polygons), _1, _2);
+  commands["COUNT"] = std::bind(count, std::cref(polygons), _1, _2);
+  commands["PERMS"] = std::bind(perm, std::cref(polygons), _1, _2);
+  commands["RMECHO"] = std::bind(rmecho, std::ref(polygons), _1, _2);;
+
+  std::string cmd;
+  while (std::cin >> cmd)
+  {
+    try
+    {
+      commands.at(cmd)(std::cin, std::cout);
+      std::cout << '\n';
+    }
+    catch (const std::overflow_error & e)
+    {
+      std::cerr << "<ERROR: " << e.what() << ">\n";
+    }
+    catch (const std::out_of_range&)
+    {
+      std::cerr << "<INVALID COMMAND>\n";
+      std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+    }
+    catch (const std::invalid_argument & e)
+    {
+      std::cerr << "<ERROR: " << e.what() << ">\n";
+    }
+  }
   return 0;
 }
