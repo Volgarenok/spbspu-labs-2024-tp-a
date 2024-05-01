d981fa010d1d2bf18b2df2974a53c69d056edf39
Piyavva <piyavkin05@bk.ru>
2024-05-01 15:23:12 +0300

    Add file processing

--- piyavkin.anton/T3/main.cpp
+++ piyavkin.anton/T3/main.cpp
@@ -1,2 +1,21 @@
-int main()
-{}
+#include <fstream>
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+  if (argc != 2)
+  {
+    std::cerr << "File entered incorrectly\n";
+    return 1;
+  }
+  std::ifstream in(argv[1]);
+  if (!in.is_open())
+  {
+    std::cerr << "The file is not open\n";
+    return 2;
+  }
+  std::string name = "";
+  while (std::cin >> name)
+  {
+  }
+}
