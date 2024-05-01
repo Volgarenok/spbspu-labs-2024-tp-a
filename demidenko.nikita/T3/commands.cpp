c9dc9b764ef127ea0e1691e7d9cb1d4f6a864a6d
TheOneFoxAgo <nikitad2005@gmail.com>
2024-05-01 22:07:38 +0300

    Impl minmax

--- demidenko.nikita/T3/commands.cpp
+++ demidenko.nikita/T3/commands.cpp
@@ -7,28 +7,19 @@
 #include <numeric>
 #include <stdexcept>
 #include <vector>
-#include <streamGuard.hpp>
 #include "area.hpp"
 #include "geometry.hpp"
 
 namespace demidenko
 {
   const char* ERROR_MESSAGE = "Error accured\n";
-  double sumArea(const std::vector< Polygon >& polygons)
-  {
-    using namespace std::placeholders;
-    return std::accumulate(polygons.begin(), polygons.end(), 0.0, std::bind(std::plus<>(), _1, polygonArea));
-  }
-  std::size_t polygonSize(const Polygon& polygon)
-  {
-    return polygon.points.size();
-  }
+  double sumArea(const std::vector< Polygon >& polygons);
+  std::size_t polygonSize(const Polygon& polygon);
+  template < class Comp >
+  void extremum(std::istream& in, std::ostream& out, const std::vector< Polygon >& polygons, Comp&& comp);
 }
 void demidenko::area(std::istream& in, std::ostream& out, const std::vector< Polygon >& polygons)
 {
-  StreamGuard guard(out);
-  out << std::fixed << std::setprecision(1);
-
   using namespace std::placeholders;
   std::vector< Polygon > temp;
   int numOfVertices = 0;
@@ -74,8 +65,46 @@ void demidenko::area(std::istream& in, std::ostream& out, const std::vector< Pol
     }
   }
 }
-void demidenko::max(std::istream& in, std::ostream& out, const std::vector< Polygon >& polygons);
-void demidenko::min(std::istream& in, std::ostream& out, const std::vector< Polygon >& polygons);
+double demidenko::sumArea(const std::vector< Polygon >& polygons)
+{
+  using namespace std::placeholders;
+  return std::accumulate(polygons.begin(), polygons.end(), 0.0, std::bind(std::plus<>(), _1, polygonArea));
+}
+std::size_t demidenko::polygonSize(const Polygon& polygon)
+{
+  return polygon.points.size();
+}
+void demidenko::max(std::istream& in, std::ostream& out, const std::vector< Polygon >& polygons)
+{
+  extremum(in, out, polygons, std::less<>());
+}
+void demidenko::min(std::istream& in, std::ostream& out, const std::vector< Polygon >& polygons)
+{
+  extremum(in, out, polygons, std::greater<>());
+}
+template < class Comp >
+void demidenko::extremum(std::istream& in, std::ostream& out, const std::vector< Polygon >& polygons, Comp&& comparator)
+{
+  std::string cmd;
+  in >> cmd;
+  using namespace std::placeholders;
+  if (cmd == "AREA")
+  {
+    out << polygonArea(
+      *std::max_element(polygons.begin(), polygons.end(), std::bind(comparator, polygonArea, polygonArea))
+    );
+  }
+  else if (cmd == "VERTEXES")
+  {
+    out << polygonSize(
+      *std::max_element(polygons.begin(), polygons.end(), std::bind(comparator, polygonSize, polygonSize))
+    );
+  }
+  else
+  {
+    throw std::runtime_error(ERROR_MESSAGE);
+  }
+}
 void demidenko::count(std::istream& in, std::ostream& out, const std::vector< Polygon >& polygons);
 void demidenko::echo(std::istream& in, std::ostream& out, std::vector< Polygon >& polygons);
 void demidenko::rightShapes(std::istream& in, std::ostream& out, const std::vector< Polygon >& polygons);
