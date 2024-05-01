d75f38548d13b1b84f4bcafc539f17d310da6d07
TheOneFoxAgo <nikitad2005@gmail.com>
2024-05-01 17:47:30 +0300

    Done input. Prepared for command impl

---
+++ demidenko.nikita/T3/commands.hpp
@@ -0,0 +1,18 @@
+#ifndef COMMANDS_HPP
+#define COMMANDS_HPP
+
+#include <iosfwd>
+#include <vector>
+#include "geometry.hpp"
+
+namespace demidenko
+{
+  void area(std::istream& in, std::ostream& out, const std::vector< Polygon >& polygons);
+  void max(std::istream& in, std::ostream& out, const std::vector< Polygon >& polygons);
+  void min(std::istream& in, std::ostream& out, const std::vector< Polygon >& polygons);
+  void count(std::istream& in, std::ostream& out, const std::vector< Polygon >& polygons);
+  void echo(std::istream& in, std::ostream& out, std::vector< Polygon >& polygons);
+  void rightShapes(std::istream& in, std::ostream& out, const std::vector< Polygon >& polygons);
+}
+
+#endif
--- demidenko.nikita/T3/main.cpp
+++ demidenko.nikita/T3/main.cpp
@@ -1,10 +1,12 @@
 #include <algorithm>
 #include <fstream>
+#include <functional>
 #include <iostream>
 #include <iterator>
 #include <map>
 #include <string>
 #include <vector>
+#include "commands.hpp"
 #include "geometry.hpp"
 
 int main(int argc, char** argv)
@@ -20,6 +22,25 @@ int main(int argc, char** argv)
     return -1;
   }
   std::string cmd;
-  using Command = void (*)(std::istream&, std::ostream&);
-  std::map< std::string, Command > commands{};
+  using namespace std::placeholders;
+  std::map< std::string, std::function< void(std::istream&, std::ostream&) > > commands{
+    { "AREA", std::bind(demidenko::area, _1, _2, std::ref(polygons)) },
+    { "MAX", std::bind(demidenko::max, _1, _2, std::ref(polygons)) },
+    { "MIN", std::bind(demidenko::min, _1, _2, std::ref(polygons)) },
+    { "COUNT", std::bind(demidenko::count, _1, _2, std::ref(polygons)) },
+    { "ECHO", std::bind(demidenko::echo, _1, _2, std::ref(polygons)) },
+    { "RIGHTSHAPES", std::bind(demidenko::rightShapes, _1, _2, std::ref(polygons)) },
+  };
+  while (!std::cin.eof())
+  {
+    std::cin >> cmd;
+    try
+    {
+      commands.at(cmd)(std::cin, std::cout);
+    }
+    catch (...)
+    {
+      std::cout << "<INVALID COMMAND>\n";
+    }
+  }
 }
