1504362db8fd36a305b6146f2ac9d02a472b063c
true-slon <160644649+true-slon@users.noreply.github.com>
2024-05-01 22:00:24 +0300

    Update delimeter.hpp

--- felk.bogdan/T2/delimeter.hpp
+++ felk.bogdan/T2/delimeter.hpp
@@ -11,7 +11,25 @@ namespace felk
     bool caseStrict;
     Delimeter(char exp, bool strict = true);
   };
+
+  struct WrapperLL
+  {
+    long long data&;
+  }
+
+  struct WrapperHex
+  {
+    size_t data&;
+  }
+
+  struct WrapperStr
+  {
+    std::string data&;
+  }
+
   std::istream& operator>>(std::istream& in, Delimeter&& exp);
-}
+  std::istream& operator>>(std::istream& in, WrapperLL&& exp);
+  std::istream& operator>>(std::istream& in, WrapperHex&& exp);
+  std::istream& operator>>(std::istream& in, WrapperStr&& exp);
 
 #endif
