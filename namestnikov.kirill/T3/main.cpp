c15913d486c7205a94e8d7977e8bdd245fe36d9b
kirillnamestnikov <kir.namestnikov@gmail.com>
2024-05-02 19:44:16 +0300

    got it

--- namestnikov.kirill/T3/main.cpp
+++ namestnikov.kirill/T3/main.cpp
@@ -45,7 +45,7 @@ int main(int argc, char * argv[])
       }
       catch (...)
       {
-        std::cerr << "<INVALID COMMAND>\n";
+        std::cout << "<INVALID COMMAND>\n";
       }
       std::cin.clear();
       std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
