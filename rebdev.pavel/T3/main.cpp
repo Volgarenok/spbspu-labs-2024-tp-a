11699a053604cfef1809a1579450b54a7f202678
User1235321 <pavreb1235321@gmail.com>
2024-05-02 10:25:50 +0300

    Template of main

--- rebdev.pavel/T3/main.cpp
+++ rebdev.pavel/T3/main.cpp
@@ -3,24 +3,73 @@
 #include <algorithm>
 #include <vector>
 #include <fstream>
+#include <map>
+#include <string>
 
 #include "polygon.hpp"
 
 int main(int argc, char ** argv)
 {
+  if (argc != 1)
+  {
+    std::cerr << "I work only with one programm argumet!";
+    return 1;
+  }
+
   std::ifstream inFile(argv[1]);
   using inputItT = std::istream_iterator< rebdev::Polygon >;
-  std::vector< rebdev::Polygon > polygonsVector(inputItT{ inFile }, inputItT{});
-  auto it = polygonsVector.begin();
-  while (it != polygonsVector.end())
+  using polyVec = std::vector< rebdev::Polygon >;
+  polyVec polygonsVector(inputItT{ inFile }, inputItT{});
+
+  std::map< std::string, std::function< void(const polyVec &, std::istream &, std::ostream &) > > commandMap;
+  commandMap["AREA EVEN"] = rebdev::areaEven;
+  commandMap["AREA ODD"] = rebdev::areaOdd;
+  commandMap["AREA MEAN"] = rebdev::areaMean;
+  size_t param = 0;
+  commandMap["AREA NUM"] = std::bind(rebdev::areaNum, param, _1, _2, _3);
+  commandMap["MAX AREA"] = rebdev::maxArea;
+  commandMap["MAX VERTEXES"] = rebdev::maxVertexes;
+  commandMap["MIN AREA"] = rebdev::minArea;
+  commandMap["MIN VERTEXES"] = rebdev::minVertexes;
+  commandMap["COUNT EVEN"] = rebdev::countEven;
+  commandMap["COUNT ODD"] = rebdev::countOdd;
+  commandMap["COUNT NUM"] = std::bind(rebdev::countNum, param, _1, _2, _3);
+  commandMap["RECTS"] = rebdev::rects;
+  commandMap["INFRAME"] = rebdev::inframe;
+
+  std::string inStr;
+  while (std::cin >> inStr)
   {
-    auto it2 = (*it).points.begin();
-    while (it2 != ((*it).points.end()))
+    try
     {
-      std::cout << "x: " << (*it2).x << "y: " << (*it2).y << '\n';
-      ++it2;
+      commandMap.at(instr)(polygonVector, std::cin, std::cout);
     }
-    ++it;
+    catch (std::out_of_range & e)
+    {
+      std::string secondInStr;
+      std::cin >> secondInStr;
+
+      if (std::isdigit(secondInStr[0]))
+      {
+        inStr += " NUM";
+        param = (secondInStr[0] - '0');
+      }
+      else
+      {
+        inStr += (" " + secondInStr);
+      }
+
+      try
+      {
+          commandMap.at(instr)(polygonVector, std::cin, std::cout);
+      }
+      catch (std::out_of_range & e)
+      {
+        std::cerr << "<INVALID COMMAND>\n";
+        std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+      }
+    }
+    param = 0;
   }
   return 0;
 }
