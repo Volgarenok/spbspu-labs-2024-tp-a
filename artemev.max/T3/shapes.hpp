6f44f301456b43afc65d7bd168412ba432c648f2
MaxArtemev <maxartemev@mail.ru>
2024-05-02 22:58:53 +0300

    add operator== for Point and Polygon

---
+++ artemev.max/T3/shapes.hpp
@@ -0,0 +1,24 @@
+#ifndef SHAPES_HPP
+#define SHAPES_HPP
+
+#include <vector>
+#include <iostream>
+
+namespace artemev
+{
+  struct Point
+  {
+    int x, y;
+    bool operator==(const Point& oth) const;
+  };
+  std::istream& operator>>(std::istream& in, Point& point);
+
+  struct Polygon
+  {
+    std::vector< Point > points;
+    bool operator==(const Polygon& oth) const;
+  };
+  std::istream& operator>>(std::istream& in, Polygon& polygon);
+}
+
+#endif
