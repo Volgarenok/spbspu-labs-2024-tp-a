2aa3ab1a68570f5cb2dfb2d0574c98310e9d0881
User1235321 <pavreb1235321@gmail.com>
2024-05-02 08:39:29 +0300

    Commit, to fetch upstream, to use delimeter from T2

--- rebdev.pavel/T3/main.cpp
+++ rebdev.pavel/T3/main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
+#include <algorithm>
+#include <vector>
+#include <fstream>
 
-int main()
+#include "polygon.hpp"
+
+int main(int argc, char ** argv)
 {
+  std::ifstream inFile(argv[1]);
+  std::vector< rebdev::Polygon > polygonsVector;
+  using inputItT = std::istream_iterator< rebdev::Polygon >;
+  std::copy(inputItT{ inFile }, inputItT{}, std::back_inserter(polygonsVector));
+  
   return 0;
 }
