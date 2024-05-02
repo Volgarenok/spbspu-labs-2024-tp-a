7ec4ec854a16d29a66a0821377f26babbb918acc
WarriorVortex <erohinvova01010111@gmail.com>
2024-05-02 23:22:39 +0300

    Added read from file

--- erohin.vladimir/T3/main.cpp
+++ erohin.vladimir/T3/main.cpp
@@ -1,2 +1,34 @@
-int main()
-{}
+#include <iostream>
+#include <map>
+#include <functional>
+#include <limits>
+#include <fstream>
+
+int main(int argc, char ** argv)
+{
+  if (argc != 2)
+  {
+    std::cerr << "Wrong CLA number\n";
+    return 1;
+  }
+  std::fstream file(argv[1]);
+  file.close();
+  using func = std::function< void(std::istream &, std::ostream &) >;
+  std::map< std::string, func > command;
+  std::string command_name;
+  std::cin >> command_name;
+  while (!std::cin.eof())
+  {
+    try
+    {
+      command.at(command_name)(std::cin, std::cout);
+    }
+    catch (const std::exception & e)
+    {
+      std::cout << "<INVALID COMMAND>\n";
+      std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+    }
+    std::cin >> command_name;
+  }
+  return 0;
+}
