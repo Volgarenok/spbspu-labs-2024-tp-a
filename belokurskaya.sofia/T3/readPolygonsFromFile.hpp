d466cad4f6617dc01b0f4ffc522555d62eb265c1
sonia66666 <147131510+sonia66666@users.noreply.github.com>
2024-05-03 23:21:04 +0300

    added reading polygons from file

---
+++ belokurskaya.sofia/T3/readPolygonsFromFile.hpp
@@ -0,0 +1,38 @@
+#ifndef READPOLYGONSFROMFILE_HPP
+#define READPOLYGONSFROMFILE_HPP
+
+#include <fstream>
+
+#include "polygon.hpp"
+
+namespace belokurskaya
+{
+  std::vector< Polygon > readPolygonsFromFile(std::string pathToFile)
+  {
+    std::ifstream file(pathToFile);
+    if (!file.is_open())
+    {
+      throw std::runtime_error("Error reading the file");
+    }
+    std::vector< Polygon > polygons;
+    while (!file.eof())
+    {
+      std::copy
+      (
+        std::istream_iterator< Polygon >{ file },
+        std::istream_iterator< Polygon >{},
+        std::back_inserter(polygons)
+      );
+      if (file.fail() && !file.eof())
+      {
+        file.clear();
+        file.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+      }
+    }
+    file.close();
+    polygons.shrink_to_fit();
+    return polygons;
+  }
+}
+
+#endif
