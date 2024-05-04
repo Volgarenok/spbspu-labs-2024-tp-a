d5df9a2a69c9c409d46dce53b9af5c234c33cffa
AuntMotYA <ikozyanag.2004@gmail.com>
2024-05-05 00:19:22 +0300

    add Polygon.hpp

---
+++ kozlova.yana/T3/Polygon.hpp
@@ -0,0 +1,23 @@
+#ifndef POLYGON_HPP
+#define POLYGON_HPP
+
+#include <vector>
+#include <istream>
+
+namespace kozlova
+{
+  struct Point
+  {
+      int x, y;
+  };
+  std::istream& operator>>(std::istream& in, Point& point);
+
+  struct Polygon
+  {
+      std::vector< Point > points;
+  };
+
+  std::istream& operator>>(std::istream& in, Polygon& polygon);
+}
+
+#endif
