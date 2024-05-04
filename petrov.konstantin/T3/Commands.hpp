5f78a933a6638298b28f0c6c3093e07a9ff0ca90
BrainR <constantin.m.petroo@gmail.com>
2024-05-04 23:40:39 +0300

    Small change in function name

--- petrov.konstantin/T3/Commands.hpp
+++ petrov.konstantin/T3/Commands.hpp
@@ -11,7 +11,7 @@ namespace petrov
   double getAreaAverage(const std::vector< Polygon >& polygons);
   double getAreaNumOfVertexes(const std::vector< Polygon >& polygons, size_t numOfVertexes);
   double getExtremum(const std::vector< Polygon >& polygons, bool forArea, bool forMax);
-  double countEO(const std::vector< Polygon >& polygons, bool forEven);
+  double countPolygonsEO(const std::vector< Polygon >& polygons, bool forEven);
   double countPolygonsNumOfVertexes(const std::vector< Polygon >& Polygons, size_t numOfVertexes);
   double rmEcho(std::vector< Polygon >& polygons, const Polygon& mask);
   double countSame(const std::vector< Polygon >& polygons, const Polygon& mask);
