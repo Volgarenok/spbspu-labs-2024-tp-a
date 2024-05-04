2bf8da3e876c969700b5286cbb935a2eb37063d0
MaxArtemev <maxartemev@mail.ru>
2024-05-04 13:12:23 +0300

    Realization cmd AREA ODD

--- artemev.max/T3/commands.cpp
+++ artemev.max/T3/commands.cpp
@@ -1,5 +1,8 @@
 #include "commands.hpp"
 #include <istream>
+#include <iomanip>
+#include <functional>
+#include <numeric>
 
 bool isOdd(const artemev::Polygon& shape)
 {
@@ -9,4 +12,28 @@ bool isOdd(const artemev::Polygon& shape)
 bool isEven(const artemev::Polygon& shape)
 {
   return !(shape.points.size() % 2);
+}
+
+double conditionAccumulatePolygon(double result, const artemev::Polygon& figure, std::function< bool(const artemev::Polygon&) > predicate)
+{
+  if (predicate(figure))
+  {
+    result += figure.getArea();
+  }
+  return result;
+}
+
+void artemev::area(const std::vector< Polygon >& data, std::istream& input, std::ostream& output)
+{
+  output << std::fixed << std::setprecision(1);
+  std::string command;
+  input >> command;
+
+  using namespace std::placeholders;
+  if (command == "ODD")
+  {
+    std::function< double(double, const Polygon&) > accum = std::bind(conditionAccumulatePolygon, _1, _2, isOdd);
+    output << std::accumulate(data.cbegin(), data.cend(), 0.0, accum);
+  }
+   
 }
