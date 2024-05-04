8413ce5c83df15f5470eb0a304c8fa6110baabee
AuntMotYA <ikozyanag.2004@gmail.com>
2024-05-05 00:24:31 +0300

    realise operator >> for Polygon in Polygon.cpp

--- kozlova.yana/T3/Polygon.cpp
+++ kozlova.yana/T3/Polygon.cpp
@@ -16,3 +16,31 @@ std::istream& kozlova::operator>>(std::istream& in, Point& point)
   in >> DelimiterCharIO{ '(' } >> point.x >> DelimiterCharIO{ ';' } >> point.y >> DelimiterCharIO{ ')' };
   return in;
 }
+
+std::istream& kozlova::operator>>(std::istream& in, Polygon& polygon)
+{
+  std::istream::sentry guard(in);
+  if (!guard)
+  {
+      return in;
+  }
+
+  std::size_t num = 0;
+  in >> num;
+  if (num < 3)
+  {
+      in.setstate(std::ios::failbit);
+      return in;
+  }
+
+  std::vector< Point > point;
+  point.reserve(num);
+  std::copy_n(std::istream_iterator< Point > { in }, num, std::back_inserter(point));
+
+  if (in && point.size() == num)
+  {
+      polygon.points = std::move(point);
+  }
+
+  return in;
+}
