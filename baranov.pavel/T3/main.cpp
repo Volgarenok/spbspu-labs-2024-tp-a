cebbe0fa0dc69f5fa4991af2af2ad360bfa9aa09
pashko2303 <baranov-p.b@yandex.ru>
2024-05-04 17:38:27 +0300

    Input polygons from file

--- baranov.pavel/T3/main.cpp
+++ baranov.pavel/T3/main.cpp
@@ -1,10 +1,30 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <iterator>
+#include <algorithm>
 #include "polygon.hpp"
 
-int main()
-{
+int main(int argc, char * argv[]) {
   using namespace baranov;
-  Polygon poly;
-  std::cin >> poly;
+  if (argc < 2)
+  {
+    std::cerr << "Incorrect CLA\n";
+    return 1;
+  }
+
+  std::ifstream file(argv[1]);
+  if (!file.is_open())
+  {
+    std::cerr << "Invalid file name\n";
+    return 1;
+  }
+
+  std::vector< Polygon > shapes;
+  using in_it_t = std::istream_iterator< Polygon >;
+  while (!file.eof())
+  {
+    std::copy(in_it_t{ file }, in_it_t{}, std::back_inserter(shapes));
+  }
 }
 
