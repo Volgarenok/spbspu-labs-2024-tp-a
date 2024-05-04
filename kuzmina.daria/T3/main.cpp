271d83bc413896b6f38ae5828a1563d0bce08a14
k-daria <kuzminada1005@gmail.com>
2024-05-05 00:24:58 +0300

    Added input from file

--- kuzmina.daria/T3/main.cpp
+++ kuzmina.daria/T3/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <functional>
 #include <vector>
 #include <map>
@@ -7,17 +8,31 @@
 
 using namespace kuzmina;
 
-int main()
+int main(int argc, char* argv[])
 {
   std::vector< Polygon > polygons;
 
-  Polygon p1, p2;
-  std::cin >> p1 >> p2;
+  if (argc < 2)
+  {
+    std::cerr << "No file given\n";
+    return 1;
+  }
 
-  polygons.push_back(p1);
-  polygons.push_back(p2);
+  std::ifstream input(argv[1]);
+  using input_it_t = std::istream_iterator< Polygon >;
 
-  std::map< std::string, std::function< void (std::istream&, std::ostream&, const std::vector< Polygon >&) > > commands;
+  while (!input.eof())
+  {
+    if (input.fail())
+    {
+      input.clear();
+      input.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+    }
+
+    std::copy(input_it_t{ input }, input_it_t{}, std::back_inserter(polygons));
+  }
+
+  std::map< std::string, std::function< void(std::istream&, std::ostream&, const std::vector< Polygon >&) > > commands;
   commands["AREA"] = area;
   commands["MAX"] = max;
   commands["MIN"] = min;
@@ -28,8 +43,17 @@ int main()
   std::string command = "";
   while (std::cin >> command)
   {
-    commands.at(command)(std::cin, std::cout, polygons);
-    std::cout << '\n';
+    try
+    {
+      commands.at(command)(std::cin, std::cout, polygons);
+      std::cout << '\n';
+    }
+    catch (const std::exception&)
+    {
+      std::cout << "<INVALID COMMAND>\n";
+      std::cin.clear();
+      std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+    }
   }
 
   return 0;
