5fbb55e468dd1cf17f3217c615dc7f714a88872c
IlyaBasko <baskoila200@gmail.com>
2024-05-04 19:53:52 +0300

    Added polygon.cpp and added realization operator>> and operator<< for point

---
+++ basko.ilya/T3/polygon.cpp
@@ -0,0 +1,30 @@
+#include "polygon.hpp"
+#include "delimiters.hpp"
+
+std::istream& basko::operator>>(std::istream& in, Point& point)
+{
+  using delChar = DelimiterChar;
+  std::istream::sentry guard(in);
+  if (!guard)
+  {
+    return in;
+  }
+  Point temp = { 0, 0 };
+  in >> delChar{ '(' } >> temp.x >> delChar{ ';' } >> temp.y >> delChar{ ')' };
+  if (in)
+  {
+    point = temp;
+  }
+  return in;
+}
+
+std::ostream& basko::operator<<(std::ostream& out, const Point& point)
+{
+  std::ostream::sentry guard(out);
+  if (!guard)
+  {
+    return out;
+  }
+  out << '(' << point.x << ';' << point.y << ')';
+  return out;
+}
