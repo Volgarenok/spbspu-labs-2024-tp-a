95f81f1d1d6b27b2f3e9a80735a401d0c23e628e
Skuratov09 <skyratovdaniil28@gmail.com>
2024-05-04 18:10:56 +0300

    Created command

--- skuratov.danil/T3/main.cpp
+++ skuratov.danil/T3/main.cpp
@@ -1,6 +1,36 @@
 #include <iostream>
+#include <limits>
+#include <functional>
+#include <map>
+
+namespace skuratov
+{
+	void cmd1(std::istream& in, std::ostream& out)
+	{
+		out << "Hi, this is CMD1" << '\n';
+	}
+	void cmd2(std::istream& in, std::ostream& out)
+	{
+		out << "Hi, this is CMD2" << '\n';
+	}
+}
 
 int main()
 {
-  std::cout << "Hello, may be return 1/0 ;) ?";
+	std::string cmd;
+	while (std::cin >> cmd)
+	{
+		if (cmd == "CMD1")
+		{
+			skuratov::cmd1(std::cin, std::cout);
+		}
+		else if (cmd == "CMD2")
+		{
+			skuratov::cmd2(std::cin, std::cout);
+		}
+		else
+		{
+			std::cerr << "INVALID COMMAND\n";
+		}
+	}
 }
