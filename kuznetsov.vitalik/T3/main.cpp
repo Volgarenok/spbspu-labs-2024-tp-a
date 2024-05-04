651c6101e4444e0279b7156afab77f493b462508
VitalikKuznetsov <ssiromas@gmail.com>
2024-05-04 17:55:47 +0300

    fix main

--- kuznetsov.vitalik/T3/main.cpp
+++ kuznetsov.vitalik/T3/main.cpp
@@ -1,21 +1,37 @@
 #include <iostream>
+#include <fstream>
+#include <iterator>
 #include <map>
 #include <string>
 #include <functional>
 #include <limits>
 #include "commands.hpp"
 
-int main()
+int main(int argc, char* argv[])
 {
   using namespace kuznetsov;
-  Polygon polygon1;
-  Polygon polygon2;
-  Polygon polygon3;
-  std::cin >> polygon1;
-  std::cin >> polygon2;
-  std::cin >> polygon3;
 
-  std::vector< Polygon > polygon{ polygon1, polygon2, polygon3 };
+  std::vector< Polygon > polygon;
+
+  if (argc < 3)
+  {
+    std::ifstream input(argv[1]);
+    while (!input.eof())
+    {
+      if (input.fail())
+      {
+        input.clear();
+        input.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+      }
+      using input_it_t = std::istream_iterator< Polygon >;
+      std::copy(input_it_t{ input }, input_it_t{}, std::back_inserter(polygon));
+    }
+  }
+  else
+  {
+    std::cerr << "Wrong input parameters" << '\n';
+    return 1;
+  }
 
   std::map< std::string, std::function< void(std::istream&, std::ostream&) > > cmds;
   {
@@ -24,8 +40,6 @@ int main()
     cmds["MAX"] = std::bind(getMax, polygon, _1, _2);
     cmds["MIN"] = std::bind(getMin, polygon, _1, _2);
     cmds["COUNT"] = std::bind(getCount, polygon, _1, _2);
-    cmds["INFRAME"];
-    cmds["SAME"];
   }
 
   std::string cmd;
@@ -35,9 +49,10 @@ int main()
     {
       cmds.at(cmd)(std::cin, std::cout);
     }
-    catch (const std::out_of_range&)
+    catch (...)
     {
       std::cerr << "<INVALID COMMAND>\n";
+      std::cin.clear();
       std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
     }
   }
