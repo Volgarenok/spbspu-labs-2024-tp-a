9270961bbed863a1824427bcba87e4d83484fb88
MaxArtemev <maxartemev@mail.ru>
2024-05-05 00:17:46 +0300

    Fix content

--- artemev.max/T3/main.cpp
+++ artemev.max/T3/main.cpp
@@ -56,7 +56,7 @@ int main(int argc, char* argv[])
     catch(const std::exception&)
     {
       std::cout << "<INVALID COMMAND>\n";
-    } 
+    }
     std::cin.clear();
     std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
   }
