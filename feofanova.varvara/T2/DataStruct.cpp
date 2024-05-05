841805cc1e0af2428ebe183edb840ddd18b19216
root <root@DESKTOP-HVA9M9A>
2024-05-05 20:41:41 +0300

    : add

--- feofanova.varvara/T2/DataStruct.cpp
+++ feofanova.varvara/T2/DataStruct.cpp
@@ -56,9 +56,9 @@ std::ostream& feofanova::operator<<(std::ostream& out, const DataStruct& data)
   }
   StreamGuard s_guard(out);
   out << std::setprecision(1) << std::fixed << "(";
-  out << ":key1 " << data.key1 << "d";
+  out << ":key1 " << data.key1 << "d" << ':';
   using ullbin = feofanova::binI;
-  out << "key2 0b" << ullbin{data.key2} << ':';
+  out << "key2 0b" << ullbin{data.key2};
   out << ":key3 \"" << data.key3 << "\":)";
   return out;
 };
