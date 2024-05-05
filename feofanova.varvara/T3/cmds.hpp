856aafbc1d6ca717a83ddb8a1dc616277ca99c37
root <root@DESKTOP-HVA9M9A>
2024-05-05 15:10:52 +0300

    creating cmds.hpp again

---
+++ feofanova.varvara/T3/cmds.hpp
@@ -0,0 +1,15 @@
+#ifndef COMMANDS_HPP
+#define COMMANDS_HPP
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <stream_guard.hpp>
+#include "detail.hpp"
+
+namespace feofanova
+{
+	double feofanova::getArea(const std::vector< Polygon >& data) const;
+	bool feofanova::isAreaLess(const std::vector< Polygon >& input, const std::vector< Polygon >& data) const;
+	bool feofanova::Perms(const std::vector< Polygon >& input, const std::vector< Polygon >& data) const;
+}
+#endif
