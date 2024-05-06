05dbe300abcc4faafd564eff162d6f495b61b93e
StantStantov <ibragimovimil0@gmail.com>
2024-05-06 08:58:05 +0300

    [fix] Fix exception output

--- ibragimov.imil/T3/main.cpp
+++ ibragimov.imil/T3/main.cpp
@@ -88,7 +88,7 @@ int main(int argc, char* argv[])
       }
       catch (const std::exception&)
       {
-        std::cerr << "<INVALID COMMAND>\n";
+        std::cout << "<INVALID COMMAND>\n";
         std::cin.clear();
         std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
       }
