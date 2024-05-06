ee2ab66e7bdc25901d778d8b999aa57f9c69befa
marlions <marishin.lv@edu.spbstu.ru>
2024-05-06 16:07:49 +0300

    exemplary main

--- marishin.leonid/T3/main.cpp
+++ marishin.leonid/T3/main.cpp
@@ -1,72 +1,58 @@
 #include <iostream>
+#include <algorithm>
 #include <functional>
+#include <vector>
+#include <map>
 
 
-namespace poly
+int main(int argc, char* argv[])
 {
-  //Функция
-  //          _1     _2
-  int plus(int a, int b) {
-    return a + b;
+  using namespace marishin;
+  if (argc != 2)
+  {
+    std::cerr << "Invalid command line arguments\n";
+    return 1;
   }
-  //Функтор
-  struct Plus {
-    int operator()(int a, int b) {
-      return plus(a, b);
+  std::ifstream in(argv[1]);
+  if (!in.is_open())
+  {
+    std::cerr << "Unable to open file\n";
+    return 1;
+  }
+  std::vector< Polygon > polygons;
+  using input_it_t = std::istream_iterator< Polygon >;
+  while (!in.eof())
+  {
+    std::copy(input_it_t{ in }, input_it_t{}, std::back_inserter(polygons));
+    in.clear();
+    std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+  }
+  in.close();
+
+  std::map< std::string, std::function< void(std::istream&, std::ostream&) > > cmds;
+  {
+    using namespace std::placeholders;
+    cmds["AREA"] = std::bind(getArea, std::cref(polygons), _1, _2);
+    cmds["COUNT"] = std::bind(getCount, std::cref(polygons), _1, _2);
+    cmds["MIN"] = std::bind(getMin, std::cref(polygons), _1, _2);
+    cmds["MAX"] = std::bind(getMax, std::cref(polygons), _1, _2);
+    cmds["RECTS"] = std::bind(getRects, std::cref(polygons), _1, _2);
+    cmds["INTERSECTIONS"] = std::bind(getIntersections, std::cref(polygons), _1, _2);
+  }
+
+  std::string command = "";
+  while (std::cin >> command)
+  {
+    try
+    {
+      cmds.at(command)(std::cin, std::cout);
+      std::cout << "\n";
     }
-  };
-
-  struct A {
-    //foo(A &, int)
-    void foo(int a) {
-      std::cout << "call foo with " << a << "\n";
+    catch (...)
+    {
+      std::cerr << "<INVALID COMMAND>\n";
     }
-  };
-}
-
-int main() {
-  int x = 10;
-  int y = -5;
-  //x + y
-  std::cout << poly::plus(x, y);
-
-  //x + 5
-  using namespace std::placeholders;
-
-  //plus(5, _1) -> plus5(_1)
-  std::function< int(int) > plus5 = std::bind(poly::plus, 5, _1);
-  std::cout << "\n" << plus5(15);
-
-  auto yaplus5 = std::bind(poly::plus, 5, _2);
-  std::cout << "\n" << yaplus5(-100, 15);
-
-  //f(x) = x + x
-  auto f2x = std::bind(std::plus< int >{}, _1, _1);
-  std::cout << "\n" << f2x(10);
-
-  //f(x) = 2 * x
-  auto yaf2x = std::bind(std::multiplies< int >{}, _1, 2);
-  std::cout << "\n" << yaf2x(20);
-
-  //g0(x, y) = x^2 + y * x
-  //g1(x, y) = x^2 + y * x + y^2
-  auto square = std::bind(std::multiplies< int >{}, _1, _1);
-  auto g0 = std::bind(std::plus< int >{},
-    square,
-    std::bind(std::multiplies< int >{}, _2, _1)
-  );
-  auto g1 = std::bind(std::plus< int >{},
-    g0,
-    std::bind(square, _2)
-  );
-  std::cout << "\n" << g0(2, 3);
-  std::cout << "\n" << g1(2, 3);
-
-  //bind to method
-  poly::A a;
-  auto bindToMethod = std::bind(&poly::A::foo, std::ref(a), _1);
-
-  bindToMethod(100);
-
-  std::cout << "\n";
+    std::cin.clear();
+    std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+  }
 }
