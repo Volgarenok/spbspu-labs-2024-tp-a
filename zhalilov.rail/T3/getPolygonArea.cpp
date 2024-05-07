dea2142a463a58f61ce89a9ccc55b9e63156dd8f
Rail <ksovfox@mail.ru>
2024-05-07 20:34:04 +0300

    Добавлен расчет площади полигона

---
+++ zhalilov.rail/T3/getPolygonArea.cpp
@@ -0,0 +1,21 @@
+#include "getPolygonArea.hpp"
+
+#include "geometry.hpp"
+
+double zhalilov::getPolygonArea(const Polygon &polygon)
+{
+  double area = 0.0;
+  size_t size = polygon.points.size();
+  for (size_t i = 0; i < size - 1; i++)
+  {
+    area -= polygon.points[i].x * polygon.points[i + 1].y;
+  }
+  area -= polygon.points[size - 1].x * polygon.points[0].y;
+  for (size_t i = 0; i < size - 1; i++)
+  {
+    area += polygon.points[i].y * polygon.points[i + 1].x;
+  }
+  area += polygon.points[size - 1].y * polygon.points[0].x;
+  area *= 0.5;
+  return area;
+}
