fcbbff6088e319cc53dd662e3a9a30151d505daf
IlyaBasko <baskoila200@gmail.com>
2024-05-08 14:31:28 +0300

    Fix scopeGuard.cpp

--- basko.ilya/T2/scopeGuard.cpp
+++ basko.ilya/T2/scopeGuard.cpp
@@ -1,6 +1,6 @@
 #include "scopeGuard.hpp"
 
-basko::ScopeGuard::ScopeGuard(std::basic_ios< char >& s) :
+basko::ScopeGuard::ScopeGuard(std::basic_ios< char >& s):
   s_(s),
   precision_(s.precision()),
   flags_(s.flags())
