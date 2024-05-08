2ac2da310d13b8ef91c4e2742e905f7bb3d5498a
chernikovaG <chernikova.gv@edu.spbstu.ru>
2024-05-08 17:03:38 +0300

    Added operator>> fot Point and Polygon

--- chernikova.galyna/T3/polygon.cpp
+++ chernikova.galyna/T3/polygon.cpp
@@ -1,1 +1,41 @@
-#include "Polygon.h"
+#include "polygon.hpp"
+
+std::istream& chernikova::operator>>(std::istream& in, chernikova::Point& dest)
+{
+  std::istream::sentry sentry(in);
+  if (!sentry)
+  {
+    return in;
+  }
+  in >> chernikova::DelimiterI{'(' };
+  in >> dest.x;
+  in >> chernikova::DelimiterI{';' };
+  in >> dest.y;
+  in >> chernikova::DelimiterI{')' };
+  return in;
+}
+
+std::istream& chernikova::operator>>(std::istream& in, chernikova::Polygon& dest)
+{
+  std::istream::sentry sentry(in);
+  if (!sentry)
+  {
+    return in;
+  }
+  size_t count = 0;
+  in >> count;
+  if (count < 3)
+  {
+    in.setstate(std::ios::failbit);
+    return in;
+  }
+  using iter = std::istream_iterator< chernikova::Point >;
+  dest.points.clear();
+  std::copy_n(iter(in), count, std::back_inserter(dest.points));
+  if (dest.points.size() != count)
+  {
+    in.setstate(std::ios::failbit);
+    return in;
+  }
+  return in;
+}
--- chernikova.galyna/T3/polygon.hpp
+++ chernikova.galyna/T3/polygon.hpp
@@ -1,6 +1,11 @@
 #ifndef POLYGON_HPP
 #define POLYGON_HPP
 
+#include <iostream>
+#include <iterator>
+#include <algorithm>
+#include <Delimiter.hpp>
+
 namespace chernikova
 {
   struct Point
@@ -15,6 +20,10 @@ namespace chernikova
   {
     std::vector< Point > points;
   };
+
+  std::istream& operator>>(std::istream& in, chernikova::Point& dest);
+  std::istream& operator>>(std::istream& in, chernikova::Polygon& dest);
+
 }
 
 #endif
