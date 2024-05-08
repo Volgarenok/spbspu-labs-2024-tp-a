695525eb9b595ef3cd17cf5f5aae849691721731
VitalikKuznetsov <ssiromas@gmail.com>
2024-05-08 15:11:01 +0300

    fix

--- kuznetsov.vitalik/T3/main.cpp
+++ kuznetsov.vitalik/T3/main.cpp
@@ -52,7 +52,7 @@ int main(int argc, char* argv[])
     }
     catch (...)
     {
-      std::cerr << "\t<INVALID COMMAND>\n";
+      std::cerr << "<INVALID COMMAND>\n";
       std::cin.clear();
       std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
     }
