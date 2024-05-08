e7975ef053901a7057e1a4b91d385ff7c0360f45
chernikovaG <chernikova.gv@edu.spbstu.ru>
2024-05-08 17:16:20 +0300

    Added getAreaEven

--- chernikova.galyna/T3/polygon.cpp
+++ chernikova.galyna/T3/polygon.cpp
@@ -39,3 +39,38 @@ std::istream& chernikova::operator>>(std::istream& in, chernikova::Polygon& dest
   }
   return in;
 }
+
+bool chernikova::isEven(const chernikova::Polygon& polygon)
+{
+  return polygon.points.size() % 2 == 0;
+}
+
+double chernikova::calcArea(const chernikova::Point& left, const chernikova::Point& right)
+{
+  return 0.5 * (left.x * right.y - right.x * left.y);
+}
+
+double chernikova::getArea(const chernikova::Polygon& polygon)
+{
+  double res = 0.0;
+  for (size_t i = 0; i < polygon.points.size() - 1; ++i)
+  {
+    res += chernikova::calcArea(polygon.points[i], polygon.points[i + 1]);
+  }
+  res += chernikova::calcArea(polygon.points.back(), polygon.points.front());
+  return std::abs(res);
+}
+
+double chernikova::sumArea(double cur, const chernikova::Polygon& polygon)
+{
+  return cur + chernikova::getArea(polygon);
+}
+
+void chernikova::getAreaEven(const std::vector< Polygon >& polygons, std::ostream& out)
+{
+  std::vector< Polygon > even_polygons;
+  std::copy_if(polygons.begin(), polygons.end(), std::back_inserter(even_polygons), chernikova::isEven);
+  StreamGuard streamGuard(out);
+  out << std::fixed << std::setprecision(1);
+  out << std::accumulate(even_polygons.begin(), even_polygons.end(), 0.0, chernikova::sumArea) << "\n";
+}
--- chernikova.galyna/T3/polygon.hpp
+++ chernikova.galyna/T3/polygon.hpp
@@ -4,7 +4,11 @@
 #include <iostream>
 #include <iterator>
 #include <algorithm>
+#include <vector>
+#include <iomanip>
+#include <numeric>
 #include <Delimiter.hpp>
+#include "streamGuard.hpp"
 
 namespace chernikova
 {
@@ -24,6 +28,12 @@ namespace chernikova
   std::istream& operator>>(std::istream& in, chernikova::Point& dest);
   std::istream& operator>>(std::istream& in, chernikova::Polygon& dest);
 
+  void getAreaEven(const std::vector< Polygon >& polygons, std::ostream& out);
+
+  double calcArea(const Point& left, const Point& right);
+  double getArea(const Polygon& polygon);
+  double sumArea(double cur, const Polygon& polygon);
+  bool isEven(const Polygon& polygon);
 }
 
 #endif
