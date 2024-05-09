fefa00c6160751be5298e9037903a8094fd71af8
v-zaycev <70699650+v-zaycev@users.noreply.github.com>
2024-05-09 21:03:59 +0300

    Some changes

--- zaitsev.vladimir/T3/commands.hpp
+++ zaitsev.vladimir/T3/commands.hpp
@@ -8,11 +8,11 @@
 
 namespace zaitsev
 {
-  std::ostream& area_cmd(std::istream& in, std::ostream& out, std::list< Polygon>& shapes);
-  std::ostream& max_cmd(std::istream& in, std::ostream& out, std::list< Polygon>& shapes);
-  std::ostream& min_cmd(std::istream& in, std::ostream& out, std::list< Polygon>& shapes);
-  std::ostream& count_cmd(std::istream& in, std::ostream& out, std::list< Polygon>& shapes);
-  void lessarea_cmd(std::istream& in, std::ostream& out, std::list< Polygon>& shapes);
-  void inframe_cmd(std::istream& in, std::ostream& out, std::list< Polygon>& shapes);
+  std::ostream& area_cmd(std::istream& in, std::ostream& out, std::list< Polygon >& shapes);
+  std::ostream& max_cmd(std::istream& in, std::ostream& out, std::list< Polygon >& shapes);
+  std::ostream& min_cmd(std::istream& in, std::ostream& out, std::list< Polygon >& shapes);
+  std::ostream& count_cmd(std::istream& in, std::ostream& out, std::list< Polygon >& shapes);
+  void lessarea_cmd(std::istream& in, std::ostream& out, std::list< Polygon >& shapes);
+  void inframe_cmd(std::istream& in, std::ostream& out, std::list< Polygon >& shapes);
 }
 #endif
