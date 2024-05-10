f7357bbdef531c0ec3016fefc23a241ab9075faf
C3mR4de <NVadim2005@gmail.com>
2024-05-10 15:07:38 +0300

    Added basic structure for program

---
+++ novikov.vadim/F0/commands.hpp
@@ -0,0 +1,33 @@
+#ifndef COMMANDS_HPP
+#define COMMANDS_HPP
+
+#include <string>
+#include <set>
+#include <map>
+
+namespace novikov
+{
+    using Dictionary = std::map< std::string, std::set< std::string > >;
+
+    void insert();
+    void search();
+    void searchKeys();
+    void searchValues();
+    void remove();
+    void removeKeys();
+    void removeValues();
+
+    void open();
+    void save();
+    void close();
+    void print();
+    void printReflected();
+    void size();
+
+    void merge();
+    void intersect();
+    void filter();
+    void invert();
+}
+
+#endif
--- novikov.vadim/F0/main.cpp
+++ novikov.vadim/F0/main.cpp
@@ -1,2 +1,34 @@
-int main()
-{}
+#include <iostream>
+#include <cstring>
+#include <unordered_map>
+#include "commands.hpp"
+
+int main(int argc, const char* argv[])
+{
+    if (argc > 2)
+    {
+        std::cerr << "Error: invalid arguments provided!\n";
+        return 1;
+    }
+
+    if (argc == 2)
+    {
+        if (!std::strcmp(argv[1], "--help"))
+        {
+            std::cout << "Eng-Rus dictionary help\n";
+            return 0;
+        }
+        else
+        {
+            std::cerr << "Error: invalid arguments provided!\n";
+            return 1;
+        }
+    }
+
+    using namespace novikov;
+    std::unordered_map< std::string, Dictionary > dictionaries;
+
+    std::unordered_map< std::string, std::string > commands;
+
+    return 0;
+}
