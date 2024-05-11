e450b95802df5d6b44d669c3602b0003d148de1d
sonia66666 <147131510+sonia66666@users.noreply.github.com>
2024-05-11 16:31:08 +0300

    added commands in main.cpp

--- belokurskaya.sofia/T3/main.cpp
+++ belokurskaya.sofia/T3/main.cpp
@@ -1,9 +1,70 @@
-﻿#include<iostream>
+﻿#include <fstream>
+#include <functional>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <map>
+#include <vector>
 
-#include"polygon.hpp"
+#include "readPolygonsFromFile.hpp"
+#include "commands.hpp"
 
-int main(int args, char** argv)
+namespace belokurskaya
+{
+  namespace mapCommands
+  {
+    using mapCmd = std::map< std::string, std::function< void(std::istream&, std::ostream&) > >;
+    mapCmd createMapOfCommands(std::vector< Polygon >& polygons, std::istream&, std::ostream&)
+    {
+      mapCmd commands;
+      using namespace std::placeholders;
+      commands["AREA"] = std::bind(cmd::area, std::cref(polygons), _1, _2);
+      commands["MAX"] = std::bind(cmd::max, std::cref(polygons), _1, _2);
+      commands["MIN"] = std::bind(cmd::min, std::cref(polygons), _1, _2);
+      commands["COUNT"] = std::bind(cmd::count, std::cref(polygons), _1, _2);
+      
+      return commands;
+    }
+  }
+}
+
+int main(int argc, char** argv)
 {
   using namespace belokurskaya;
-  std::cout << argv[1];
+  if (argc != 2)
+  {
+    std::cerr << "Incorrect input\n";
+    return 1;
+  }
+  std::vector< Polygon > polygons = readPolygonsFromFile(argv[1]);
+
+  std::copy(polygons.begin(), polygons.end(), std::ostream_iterator<Polygon>(std::cout, "\n"));
+
+  std::string argument;
+
+  std::map< std::string, std::function< void(std::istream&, std::ostream&) > > commands;
+  commands = mapCommands::createMapOfCommands(polygons, std::cin, std::cout);
+
+  while (std::cin >> argument)
+  {
+    if (std::cin.eof())
+    {
+      break;
+    }
+    try
+    {
+      commands.at(argument)(std::cin, std::cout);
+      std::cout << '\n';
+    }
+    catch (const std::out_of_range&)
+    {
+      std::cout << "<INVALID COMMAND>" << '\n';
+    }
+    catch (const std::invalid_argument&)
+    {
+      std::cout << "<INVALID COMMAND>" << '\n';
+    }
+    std::cin.clear();
+    std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+  }
 }
