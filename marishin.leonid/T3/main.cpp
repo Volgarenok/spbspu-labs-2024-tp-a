23b49a49d4dc03d3885de100854345562612b506
marlions <marishin.lv@edu.spbstu.ru>
2024-05-12 23:01:05 +0300

    fix main

--- marishin.leonid/T3/main.cpp
+++ marishin.leonid/T3/main.cpp
@@ -18,48 +18,48 @@ int main(int argc, char* argv[])
     std::cerr << "Invalid command line arguments\n";
     return 1;
   }
-  std::ifstream in(argv[1]);
-  if (!in.is_open())
+  else
   {
-    std::cerr << "Unable to open file\n";
-    return 1;
-  }
-  std::vector< Polygon > polygons;
-  using input_it_t = std::istream_iterator< Polygon >;
-  while (!in.eof())
-  {
-    if (in.fail())
+    std::ifstream in(argv[1]);
+    if (!in.is_open())
     {
+      std::cerr << "Unable to open file\n";
+      return 1;
+    }
+    std::vector< marishin::Polygon > polygons;
+    using input_it_t = std::istream_iterator< Polygon >;
+    while (!in.eof())
+    {
+      std::copy(input_it_t{ in }, input_it_t{}, std::back_inserter(polygons));
       in.clear();
       in.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
     }
-    std::copy(input_it_t{ in }, input_it_t{}, std::back_inserter(polygons));
-  }
 
-  std::map< std::string, std::function< void(std::istream&, std::ostream&) > > cmds;
-  {
-    using namespace std::placeholders;
-    cmds["AREA"] = std::bind(getArea, std::cref(polygons), _1, _2);
-    cmds["MAX"] = std::bind(getMax, std::cref(polygons), _1, _2);
-    cmds["MIN"] = std::bind(getMin, std::cref(polygons), _1, _2);
-    cmds["COUNT"] = std::bind(getCount, std::cref(polygons), _1, _2);
-    cmds["RECTS"] = std::bind(getRects, std::cref(polygons), _2);
-    cmds["INTERSECTIONS"] = std::bind(getIntersections, std::cref(polygons), _1, _2);
-  }
+    std::map< std::string, std::function< void(std::istream&, std::ostream&) > > cmds;
+    {
+      using namespace std::placeholders;
+      cmds["AREA"] = std::bind(getArea, std::cref(polygons), _1, _2);
+      cmds["MAX"] = std::bind(getMax, std::cref(polygons), _1, _2);
+      cmds["MIN"] = std::bind(getMin, std::cref(polygons), _1, _2);
+      cmds["COUNT"] = std::bind(getCount, std::cref(polygons), _1, _2);
+      cmds["RECTS"] = std::bind(getRects, std::cref(polygons), _2);
+      cmds["INTERSECTIONS"] = std::bind(getIntersections, std::cref(polygons), _1, _2);
+    }
 
-  std::string command;
-  std::cout << std::fixed << std::setprecision(1);
-  while (std::cin >> command)
-  {
-    try
+    std::string command;
+    while (std::cin >> command)
     {
-      cmds.at(command)(std::cin, std::cout);
+      try
+      {
+        cmds.at(command)(std::cin, std::cout);
+        std::cout << "\n";
+      }
+      catch (...)
+      {
+        std::cerr << "<INVALID COMMAND>\n";
+      }
+      std::cin.clear();
+      std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
     }
-    catch (...)
-    {
-      std::cerr << "<INVALID COMMAND>\n";
-    }
-    std::cin.clear();
-    std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
   }
 }
