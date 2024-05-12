a8db47a7df5b249451ea4e3da84551ef8734b217
kirillnamestnikov <kir.namestnikov@gmail.com>
2024-05-12 19:24:38 +0300

    trying simple input-output with unordered_map

--- namestnikov.kirill/F0/main.cpp
+++ namestnikov.kirill/F0/main.cpp
@@ -1,2 +1,19 @@
+#include <iostream>
+#include <fstream>
+#include <unordered_map>
+#include <iterator>
+#include <string>
+
 int main()
-{}
+{
+  std::ifstream in("base.txt");
+  std::unordered_map< std::string, std::unordered_map< std::string, std::string > > myMap;
+  std::unordered_map< std::string, std::string > tempMap;
+  tempMap.insert({"cat", "кошка"});
+  tempMap.insert({"dog", "собака"});
+  myMap.insert({"first", tempMap});
+  for (auto itr = myMap["first"].begin(); itr != myMap["first"].end(); itr++) { 
+        std::cout << itr->first 
+             << '\t' << itr->second << '\n'; 
+  }
+}
