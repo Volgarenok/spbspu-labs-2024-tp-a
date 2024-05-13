e21dcc836f07139dba1859533aa444638bb2df43
marlions <marishin.lv@edu.spbstu.ru>
2024-05-13 15:14:05 +0300

    fix

--- marishin.leonid/T3/command.hpp
+++ marishin.leonid/T3/command.hpp
@@ -13,6 +13,19 @@ namespace marishin
   void getMax(const std::vector< marishin::Polygon >& data, std::istream& in, std::ostream& out);
   void getRects(const std::vector< marishin::Polygon >& data, std::ostream& out);
   void getIntersections(const std::vector< marishin::Polygon >& data, std::istream& in, std::ostream& out);
+
+  double accumulateAreaIf(double result, const Polygon& polygon, bool value);
+  double accumulateArea(double result, const Polygon& polygon);
+  double accumulateAreaIfCount(double result, const Polygon& polygon, size_t count);
+  bool isEven(const Polygon& polygon);
+  bool isOdd(const Polygon& polygon);
+  bool isProperSize(const Polygon& polygon, size_t number);
+  bool comparePoints(const Polygon& first, const Polygon& second);
+  bool compareArea(const Polygon& first, const Polygon& second);
+  bool hasIntersection(const Polygon& first, const Polygon& second);
+  bool checkRectangle(const Polygon& ptr);
+  void getRects(const std::vector< Polygon >& data, std::ostream& out);
+  void getIntersections(const std::vector< marishin::Polygon >& data, std::istream& in, std::ostream& out);
 }
 
 #endif
