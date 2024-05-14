7ca243436021d5cd41db9ca3b16348da6268f19a
true-slon <160644649+true-slon@users.noreply.github.com>
2024-05-14 14:59:53 +0300

    Update streamGuard.hpp

--- felk.bogdan/T2/streamGuard.hpp
+++ felk.bogdan/T2/streamGuard.hpp
@@ -8,14 +8,14 @@ namespace felk
   class StreamGuard
   {
   public:
-    explicit StreamGuard(std::basic_ios<char>& s);
+    explicit StreamGuard(std::basic_ios< char >& s);
     ~StreamGuard();
 
   private:
-    std::basic_ios<char>& s_;
+    std::basic_ios< char >& s_;
     char fill_;
     std::streamsize precision_;
-    std::basic_ios<char>::fmtflags flags_;
+    std::basic_ios< char >::fmtflags flags_;
   };
 }
 
