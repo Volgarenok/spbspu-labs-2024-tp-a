bedafd394adf71bb7a57a17db7da9aa1fea15234
Rattlerutrap <dimaaupov2005@gmail.com>
2024-05-14 21:41:52 +0300

    Fix for input Point operator; Added in/output operators for Polygon

--- ayupov.dmitry/T3/polygon.cpp
+++ ayupov.dmitry/T3/polygon.cpp
@@ -1,5 +1,8 @@
 #include "polygon.hpp"
 #include <iostream>
+#include <vector>
+#include <iterator>
+#include <algorithm>
 #include "delimiter.hpp"
 
 std::istream& ayupov::operator>>(std::istream& in, Point& point)
@@ -10,7 +13,12 @@ std::istream& ayupov::operator>>(std::istream& in, Point& point)
     return in;
   }
   using DelC = DelimiterChar;
-  in >> DelC{'('} >> point.x >> DelC{';'} >> point.y >> DelC{')'};
+  Point temp;
+  in >> DelC{'('} >> temp.x >> DelC{';'} >> temp.y >> DelC{')'};
+  if (in)
+  {
+    point = temp;
+  }
   return in;
 }
 std::ostream& ayupov::operator<<(std::ostream& out, const Point& point)
@@ -22,4 +30,37 @@ std::ostream& ayupov::operator<<(std::ostream& out, const Point& point)
   }
   out << '(' << point.x << ';' << point.y << ')';
   return out;
+}
+std::istream& ayupov::operator>>(std::istream& in, Polygon& polygon)
+{
+  std::istream::sentry guard(in);
+  if (!guard)
+  {
+    return in;
+  }
+  int pointsNum = 0;
+  in >> pointsNum;
+  if (pointsNum < 3)
+  {
+    in.setstate(std::ios::failbit);
+  }
+  Polygon temp;
+  using input_it_t = std::istream_iterator<Point>;
+  std::copy_n(input_it_t{in}, pointsNum, std::back_inserter(temp.points));
+  if (in)
+  {
+    polygon = temp;
+  }
+  return in;
+}
+std::ostream& ayupov::operator<<(std::ostream& out, const Polygon& polygon)
+{
+  std::ostream::sentry sentry(out);
+  if (!sentry)
+  {
+    return out;
+  }
+  using output_iterator_t = std::ostream_iterator< Point >;
+  out << polygon.points.size() << " ";
+  std::copy(polygon.points.cbegin(), polygon.points.cend(), output_iterator_t{out, " "});
 }
