c86c9e9086f9b32547c30a5c6fac8fc986847d9a
C3mR4de <NVadim2005@gmail.com>
2024-05-14 19:40:37 +0300

    Fixed CG

--- novikov.vadim/T3/predicates.cpp
+++ novikov.vadim/T3/predicates.cpp
@@ -11,7 +11,7 @@ bool novikov::hasOddVertexesCount(const Polygon& rhs)
   return !hasEvenVertexesCount(rhs);
 }
 
-bool novikov::vertexesCount(const Polygon& rhs, std::size_t num)
+bool novikov::hasVertexesCount(const Polygon& rhs, std::size_t num)
 {
   return rhs.points.size() == num;
 }
