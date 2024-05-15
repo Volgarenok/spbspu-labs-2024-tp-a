94f72638f22b1ef1216d0cfb64ea74c984252044
Eliseyz <elisevz@mail.ru>
2024-05-15 23:05:19 +0300

    Added main

--- zaparin.elisei/F0/main.cpp
+++ zaparin.elisei/F0/main.cpp
@@ -1,2 +1,32 @@
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <functional>
+#include <algorithm>
+#include <limits>
+#include <map>
+
+#include "Hash.hpp"
+#include "Commands.hpp"
+
 int main()
-{}
+{
+  std::map< std::string, zaparin::HashTable > dicts;
+  std::map< std::string, std::function< void(std::istream&, std::ostream&) > > commands;
+
+  std::string command;
+  while (std::cin >> command)
+  {
+    try
+    {
+      commands.at(command)(std::cin, std::cout);
+    }
+    catch (...)
+    {
+      std::cerr << "<INVALID COMMAND>\n";
+    }
+
+    std::cin.clear();
+    std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+  }
+}
