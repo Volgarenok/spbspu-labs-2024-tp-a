833c0fde3f0f85e235a41e78e5478a798419dbb2
VitalikKuznetsov <ssiromas@gmail.com>
2024-05-15 14:44:50 +0300

    fix operator>>

--- kuznetsov.vitalik/T3/datastruct.cpp
+++ kuznetsov.vitalik/T3/datastruct.cpp
@@ -20,19 +20,16 @@ std::istream& kuznetsov::operator>>(std::istream& in, Point& point)
 {
   using delchr = DelimeterChar;
   std::istream::sentry guard(in);
-  if (guard)
+  if (!guard)
   {
-    int x = 0;
-    int y = 0;
-    in >> delchr{ '(' } >> x >> delchr{ ';' } >> y >> delchr{ ')' };
-    if (in)
-    {
-      point = { x, y };
-    }
-    else
-    {
-      in.setstate(std::ios::failbit);
-    }
+    return in;
+  }
+  int x = 0;
+  int y = 0;
+  in >> delchr{ '(' } >> x >> delchr{ ';' } >> y >> delchr{ ')' };
+  if (in)
+  {
+    point = { x, y };
   }
   return in;
 }
@@ -40,25 +37,23 @@ std::istream& kuznetsov::operator>>(std::istream& in, Point& point)
 std::istream& kuznetsov::operator>>(std::istream& in, Polygon& polygon)
 {
   std::istream::sentry guard(in);
-  if (guard)
+  if (!guard)
   {
-    size_t n = 0;
-    in >> n;
-    if (n < 3)
-    {
-      in.setstate(std::ios::failbit);
-    }
-    std::vector< Point > points;
-    using input_it_t = std::istream_iterator< kuznetsov::Point >;
-    std::copy_n(input_it_t{ in }, n, std::back_inserter(points));
-    if (in)
-    {
-      polygon = { points };
-    }
-    else
-    {
-      in.setstate(std::ios::failbit);
-    }
+    return in;
+  }
+  size_t n = 0;
+  in >> n;
+  if (n < 3)
+  {
+    in.setstate(std::ios::failbit);
+    return in;
+  }
+  std::vector< Point > points;
+  using input_it_t = std::istream_iterator< kuznetsov::Point >;
+  std::copy_n(input_it_t{ in }, n, std::back_inserter(points));
+  if (in && n == points.size())
+  {
+    polygon = { points };
   }
   return in;
 }
