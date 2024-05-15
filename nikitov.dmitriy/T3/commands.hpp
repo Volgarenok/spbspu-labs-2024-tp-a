a4cc210053606e1f9b4449c02a7a1e6676e87fe5
Dmitriy <Dmitry5335@yandex.ru>
2024-05-16 01:19:03 +0300

    Extra spaces

--- nikitov.dmitriy/T3/commands.hpp
+++ nikitov.dmitriy/T3/commands.hpp
@@ -8,15 +8,10 @@
 namespace nikitov
 {
   void areaCmd(const std::vector< Polygon >& data, std::istream& input, std::ostream& output);
-
   void maxCmd(const std::vector< Polygon >& data, std::istream& input, std::ostream& output);
-
   void minCmd(const std::vector< Polygon >& data, std::istream& input, std::ostream& output);
-
   void countCmd(const std::vector< Polygon >& data, std::istream& input, std::ostream& output);
-
   void sameCmd(const std::vector< Polygon >& data, std::istream& input, std::ostream& output);
-
   void permsCmd(const std::vector< Polygon >& data, std::istream& input, std::ostream& output);
 }
 #endif
