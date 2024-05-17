648d1699833a42972ee6159446ae2890592ab0c9
SergeyKartamyshev <Sergey.krt@mail.ru>
2024-05-17 14:19:39 +0300

    fix include

--- kartamyshev.sergey/T2/DataStruct.cpp
+++ kartamyshev.sergey/T2/DataStruct.cpp
@@ -1,7 +1,7 @@
 #include "DataStruct.h"
 #include <iomanip>
 #include "Delimiter.h"
-#include "RealisationKeys.h"
+#include "RealizationKeys.h"
 
 bool kartamyshev::DataStruct::operator<(const DataStruct& value) const
 {
