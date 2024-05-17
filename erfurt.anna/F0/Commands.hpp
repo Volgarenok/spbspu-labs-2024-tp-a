a9e8edf180f75ca7384d3b51b3f20bf96ce6e7cd
Anyanari <a.erfurt02@mail.ru>
2024-05-17 17:38:14 +0300

    Create commands facade

---
+++ erfurt.anna/F0/Commands.hpp
@@ -0,0 +1,24 @@
+#ifndef COMMANDS_HPP
+#define COMMANDS_HPP
+
+#include <iosfwd>
+#include <vector>
+#include "Dictionary.hpp"
+
+namespace erfurt
+{
+  void makeInsert(std::istream& in, std::vector<Dictionary>& dictionaries, std::ostream& out);
+  void makeFindTranslate(std::istream& in, std::vector<Dictionary>& dictionaries, std::ostream& out);
+  void makeDelete(std::istream& in, std::vector<Dictionary>& dictionaries, std::ostream& out);
+  void makeMerge(std::istream& in, std::vector<Dictionary>& dictionaries, std::ostream& out);
+  void makeAddTranslate(std::istream& in, std::vector<Dictionary>& dictionaries, std::ostream& out);
+  void makePrint(std::istream& in, std::vector<Dictionary>& dictionaries, std::ostream& out);
+  void makeCommon(std::istream& in, std::vector<Dictionary>& dictionaries, std::ostream& out);
+  void makeUnique(std::istream& in, std::vector<Dictionary>& dictionaries, std::ostream& out);
+  void makeIsTranslate(std::istream& in, std::vector<Dictionary>& dictionaries, std::ostream& out);
+  void makeHelp(std::istream& in, std::vector<Dictionary>& dictionaries, std::ostream& out);
+  void makePrintDictionaries(std::istream& in, std::vector<Dictionary>& dictionaries, std::ostream& out);
+  void makeSave(std::istream& in, std::vector<Dictionary>& dictionaries, std::ostream& out);
+}
+
+#endif
