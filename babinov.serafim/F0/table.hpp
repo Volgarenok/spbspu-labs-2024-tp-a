891c204d732ab14aefad578fc98b481a9cd21427
sbabinov <serafimbabinov106@gmail.com>
2024-05-20 19:15:08 +0300

    Add data types for tables

--- babinov.serafim/F0/table.hpp
+++ babinov.serafim/F0/table.hpp
@@ -1,4 +1,31 @@
 #ifndef TABLE_HPP
 #define TABLE_HPP
+#include <string>
+#include <unordered_map>
+
+namespace babinov
+{
+  enum DataType
+  {
+    PK,
+    INTEGER,
+    REAL,
+    TEXT
+  };
+
+  const std::unordered_map< DataType, std::string > DATA_TYPES_AS_STR = {
+    {PK, "PK"},
+    {INTEGER, "INTEGER"},
+    {REAL, "REAL"},
+    {TEXT, "TEXT"}
+  };
+
+  const std::unordered_map< std::string, DataType > DATA_TYPES_FROM_STR = {
+    {"PK", PK},
+    {"INTEGER", INTEGER},
+    {"REAL", REAL},
+    {"TEXT", TEXT}
+  };
+}
 
 #endif
