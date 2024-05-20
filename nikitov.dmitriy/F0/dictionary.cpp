b8a1e6ed5cbc2fbb86f585b0dabfa260ea750421
Dmitriy <Dmitry5335@yandex.ru>
2024-05-21 00:32:58 +0300

    Add simple delete and edit for words

--- nikitov.dmitriy/F0/dictionary.cpp
+++ nikitov.dmitriy/F0/dictionary.cpp
@@ -4,3 +4,15 @@ void nikitov::Dictionary::addWord(const std::string& word, const std::string& tr
 {
   data_.insert({ word, detail::Word(translation, antonym) });
 }
+
+void nikitov::Dictionary::deleteWord(const std::string& word)
+{
+  data_.erase(word);
+}
+
+void nikitov::Dictionary::editWord(const std::string& word, const std::string& translation, const std::string& antonym)
+{
+  detail::Word toEdit = data_.at(word);
+  toEdit.translation_ = translation;
+  toEdit.antonym_ = antonym;
+}
