0d7f011da577c55ee6d81bbdef61c31b4cbe3cca
chernikovaG <chernikova.gv@edu.spbstu.ru>
2024-05-21 22:27:28 +0300

    Added class Commands

---
+++ chernikova.galyna/T3/commands.hpp
@@ -0,0 +1,32 @@
+#ifndef COMMANDS_H
+#define COMMANDS_H
+
+#include <vector>
+#include <ostream>
+#include <map>
+#include "polygon.hpp"
+
+namespace chernikova
+{
+  class Commands
+  {
+  public:
+    Commands();
+    void doCommand(const std::vector< Polygon >& polygons, const std::string& cmd, std::ostream& out) const;
+    void doCommand(const std::vector< Polygon >& polygons, const std::string& cmd, size_t count, std::ostream& out) const;
+    void doCommand(std::vector< Polygon >& polygons, const std::string& cmd, const Polygon& polygon, std::ostream& out) const;
+    void doCommand(std::vector< Polygon >& polygons, const Commands& dict, const std::string& cmd,
+      std::ostream& out, std::istream& in);
+  private:
+    using com1 = void (*)(const std::vector< Polygon >& polygons, std::ostream& out);
+    using com2 = void (*)(const std::vector< Polygon >& polygons, size_t count, std::ostream& out);
+    using com3 = void (*)(std::vector< Polygon >& polygons, const Polygon& polygon, std::ostream& out);
+    std::map< std::string, com1 > dict1_;
+    std::map< std::string, com2 > dict2_;
+    std::map< std::string, com3 > dict3_;
+  };
+  std::ostream& printError(std::ostream& out);
+  std::string inputCommand(std::istream& in);
+}
+
+#endif
