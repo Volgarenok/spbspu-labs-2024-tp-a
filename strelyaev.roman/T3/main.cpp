88583b5368b7bd6978594414e7f68363bc095f9a
Strelyaev R. <roman.strelyaeff@gmail.com>
2024-05-22 00:16:52 +0300

    max and min added

--- strelyaev.roman/T3/main.cpp
+++ strelyaev.roman/T3/main.cpp
@@ -7,6 +7,33 @@
 #include <functional>
 #include <map>
 #include <numeric>
+#include <iomanip>
+
+namespace strelyaev
+{
+  class StreamGuard
+  {
+  public:
+    StreamGuard(std::basic_ios< char >& s);
+    ~StreamGuard();
+
+  private:
+    std::basic_ios< char >& s_;
+    std::streamsize precision_;
+    std::basic_ios< char >::fmtflags flags_;
+  };
+}
+
+strelyaev::StreamGuard::StreamGuard(std::basic_ios< char >& s):
+  s_(s),
+  precision_(s.precision()),
+  flags_(s.flags())
+{}
+strelyaev::StreamGuard::~StreamGuard()
+{
+  s_.precision(precision_);
+  s_.flags(flags_);
+}
 
 namespace strelyaev
 {
@@ -131,9 +158,24 @@ namespace strelyaev
       std::istream&,
       const std::vector< Polygon >&,
       const std::map< std::string, std::function< bool(const Polygon&) > >&);
+
+  void find_area(std::ostream&,
+      const std::vector< Polygon >&,
+      const std::map< std::string, std::function< bool(const Polygon&) > >&);
+
+void find_verts(std::ostream& out,
+    const std::vector< Polygon >& polygons_vector,
+    std::function< size_t(const std::vector< size_t >&) > func);
+
+void max_cmd(std::ostream& out, std::istream& in,
+    const std::vector< Polygon >& polygons_vector);
+
+void min_cmd(std::ostream& out, std::istream& in,
+    const std::vector< Polygon >& polygons_vector);
+
+bool permutation_polygons(const Polygon& lhs, const Polygon& rhs);
 }
-
-
+////////////////////////////////////////////////////////////////////////////////////
 void strelyaev::count_cmd(std::ostream& out,
     std::istream& in,
     const std::vector< Polygon >& polygons_vector,
@@ -162,14 +204,80 @@ void strelyaev::area_cmd(std::ostream& out, std::istream& in,
   std::function< bool(const Polygon&) > pred;
   std::string str_args = "";
   in >> str_args;
-  pred = args.at(str_args);
+  int devide = 1;
+  if (str_args == "MEAN")
+  {
+    devide = 2;
+  }
+  try
+  {
+    using namespace std::placeholders;
+    size_t ver = std::stoull(str_args);
+    pred = std::bind(std::equal_to< size_t >{}, std::bind(size_getter, _1), ver);
+  }
+  catch (...)
+  {
+    pred = args.at(str_args);
+  }
   std::vector< Polygon > area_polygons;
   std::vector< double > areas;
   std::copy_if(polygons_vector.cbegin(), polygons_vector.cend(), std::back_inserter(area_polygons), pred);
   std::transform(area_polygons.cbegin(), area_polygons.cend(), std::back_inserter(areas), get_area);
-  out << std::accumulate(areas.begin(), areas.end(), 0) / areas.size();
+  out << std::accumulate(areas.begin(), areas.end(), 0) / devide;
 }
 
+void strelyaev::max_cmd(std::ostream& out, std::istream& in,
+      const std::vector< Polygon >& polygons_vector)
+{
+  StreamGuard s_guard(out);
+  std::string str_args = "";
+  in >> str_args;
+  if (str_args == "AREA")
+  {
+    std::vector< double > areas;
+    std::transform(polygons_vector.begin(), polygons_vector.end(), std::back_inserter(areas), get_area);
+    out << std::setprecision(1);
+    out << std::fixed;
+    out << *std::max_element(areas.begin(), areas.end());
+  }
+  if (str_args == "VERTEXES")
+  {
+    std::vector< size_t > areas;
+    std::transform(polygons_vector.begin(), polygons_vector.end(), std::back_inserter(areas), size_getter);
+    out << *std::max_element(areas.begin(), areas.end());
+  }
+}
+
+void strelyaev::min_cmd(std::ostream& out, std::istream& in,
+      const std::vector< Polygon >& polygons_vector)
+{
+  StreamGuard s_guard(out);
+  std::string str_args = "";
+  in >> str_args;
+  if (str_args == "AREA")
+  {
+    std::vector< double > areas;
+    std::transform(polygons_vector.begin(), polygons_vector.end(), std::back_inserter(areas), get_area);
+    out << std::setprecision(1);
+    out << std::fixed;
+    out << *std::min_element(areas.begin(), areas.end());
+  }
+  if (str_args == "VERTEXES")
+  {
+    std::vector< size_t > areas;
+    std::transform(polygons_vector.begin(), polygons_vector.end(), std::back_inserter(areas), size_getter);
+    out << *std::min_element(areas.begin(), areas.end());
+  }
+}
+
+bool strelyaev::permutation_polygons(const Polygon& lhs, const Polygon& rhs)
+{
+  using namespace std::placeholders;
+  auto compare_x = std::bind(std::equal_to< int >{}, std::bind(get_x, _1), std::bind(get_x, _2));
+  auto compare_y = std::bind(std::equal_to< int >{}, std::bind(get_y, _1), std::bind(get_y, _2));
+  auto compare_points = std::bind(std::logical_and<>{}, std::bind(compare_x, _1, _2), std::bind(compare_y, _1, _2));
+  return std::is_permutation(rhs.points.cbegin(), rhs.points.cend(), lhs.points.cbegin(), lhs.points.cend(), compare_points);
+}
 
 int main(int argc, char** argv) //perms, maxseq
 {
@@ -213,6 +321,8 @@ int main(int argc, char** argv) //perms, maxseq
     using namespace std::placeholders;
     cmds["COUNT"] = std::bind(count_cmd, _1, _2, _3, args_count);
     cmds["AREA"] = std::bind(area_cmd, _1, _2, _3, args);
+    cmds["MAX"] = std::bind(max_cmd, _1, _2, _3);
+    cmds["MIN"] = std::bind(min_cmd, _1, _2, _3);
   }
   std::string cmd_name = "";
   while (std::cin >> cmd_name)
