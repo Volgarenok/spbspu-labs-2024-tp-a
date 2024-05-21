30c736e1115ee966a6fa035545a0f1b37e96b77a
chernikovaG <chernikova.gv@edu.spbstu.ru>
2024-05-21 23:53:29 +0300

    Fixed CG

--- chernikova.galyna/T3/commands.cpp
+++ chernikova.galyna/T3/commands.cpp
@@ -28,25 +28,29 @@ std::string chernikova::inputCommand(std::istream& in)
   return command;
 }
 
-void chernikova::Commands::doCommand(const std::vector< Polygon >& polygons, const std::string& cmd, std::ostream& out) const
+void chernikova::Commands::doCommand(const std::vector< Polygon >& polygons,
+  const std::string& cmd, std::ostream& out) const
 {
   auto func = dict1_.at(cmd);
   func(polygons, out);
 }
 
-void chernikova::Commands::doCommand(const std::vector< Polygon >& polygons, const std::string& cmd, size_t count, std::ostream& out) const
+void chernikova::Commands::doCommand(const std::vector< Polygon >& polygons,
+  const std::string& cmd, size_t count, std::ostream& out) const
 {
   auto func  = dict2_.at(cmd);
   func(polygons, count, out);
 }
 
-void chernikova::Commands::doCommand(std::vector< Polygon >& polygons, const std::string& cmd, const Polygon& polygon, std::ostream& out) const
+void chernikova::Commands::doCommand(std::vector< Polygon >& polygons,
+  const std::string& cmd, const Polygon& polygon, std::ostream& out) const
 {
   auto func = dict3_.at(cmd);
   func(polygons, polygon, out);
 }
 
-void chernikova::Commands::doCommand(std::vector< Polygon >& polygons, const Commands& dict, const std::string& cmd, std::ostream& out, std::istream& in)
+void chernikova::Commands::doCommand(std::vector< Polygon >& polygons,
+  const Commands& dict, const std::string& cmd, std::ostream& out, std::istream& in)
 {
   std::cout << "com = " << cmd << '\n';
   std::istringstream iss ( (std::string(cmd)) );
