4a2fe4e7d5d5138c4e8705e5c92668a6a3897ea9
kirillnamestnikov <kir.namestnikov@gmail.com>
2024-05-21 15:43:58 +0300

    visual experience fix

--- namestnikov.kirill/T3/polygon.cpp
+++ namestnikov.kirill/T3/polygon.cpp
@@ -93,16 +93,14 @@ std::istream & namestnikov::operator>>(std::istream & in, Polygon & polygon)
   if (pointsCount < 3)
   {
     in.setstate(std::ios::failbit);
+    return in;
   }
-  else
+  std::vector< Point > tempPolygon;
+  using input_iterator_t = std::istream_iterator< Point >;
+  std::copy_n(input_iterator_t{in}, pointsCount, std::back_inserter(tempPolygon));
+  if (in)
   {
-    std::vector< Point > tempPolygon;
-    using input_iterator_t = std::istream_iterator< Point >;
-    std::copy_n(input_iterator_t{in}, pointsCount, std::back_inserter(tempPolygon));
-    if (in)
-    {
-      polygon.points = tempPolygon;
-    }
+    polygon.points = tempPolygon;
   }
   return in;
 }
