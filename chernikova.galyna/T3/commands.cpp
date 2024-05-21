f0df597d482c6997b91b946c7f7a36bb36f35ef6
chernikovaG <chernikova.gv@edu.spbstu.ru>
2024-05-21 22:30:02 +0300

    Added inputCommand

--- chernikova.galyna/T3/commands.cpp
+++ chernikova.galyna/T3/commands.cpp
@@ -1,1 +1,13 @@
+#include "commands.hpp"
 
+std::string chernikova::inputCommand(std::istream& in)
+{
+  std::string command;
+  getline(in, command);
+  if (!in)
+  {
+    throw std::runtime_error("Error input");
+  }
+
+  return command;
+}
