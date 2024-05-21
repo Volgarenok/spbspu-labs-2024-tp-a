8c62c2f6773b64c4dad4cdadd3b3247c81bfff8f
Glaz0k <nikitakravcenko892@gmail.com>
2024-05-21 16:45:49 +0300

    added commands' reading

---
+++ kravchenko.nikita/F0/commands.hpp
@@ -0,0 +1,12 @@
+#ifndef COMMANDS_HPP
+#define COMMANDS_HPP
+
+#include <map>
+#include <string>
+
+namespace kravchenko
+{
+  using FrequencyDict = std::map< std::string, size_t >;
+}
+
+#endif
--- kravchenko.nikita/F0/main.cpp
+++ kravchenko.nikita/F0/main.cpp
@@ -1,4 +1,36 @@
-int main(int argc, char* argv[])
+#include <map>
+#include <string>
+#include <functional>
+#include <iostream>
+#include <limits>
+#include <exception>
+#include "commands.hpp"
+
+int main()
 {
+  using namespace kravchenko;
+  std::map< std::string, FrequencyDict > dicts;
+
+  std::map< std::string, std::function< void(std::istream&, std::ostream&) > > cmds;
+
+  std::string cmd;
+  while (std::cin >> cmd)
+  {
+    try
+    {
+      cmds.at(cmd)(std::cin, std::cout);
+    }
+    catch(const std::out_of_range&)
+    {
+      std::cout << "<INVALID COMMAND>" << '\n';
+    }
+    catch(const std::invalid_argument& e)
+    {
+      std::cout << e.what() << '\n';
+    }
+    std::cin.clear();
+    std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+  }
+
   return 0;
 }
