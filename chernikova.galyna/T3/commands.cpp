f528153c11f9c2de9171459068eadf289cdf0212
chernikovaG <chernikova.gv@edu.spbstu.ru>
2024-05-21 22:39:57 +0300

    Added Commands()

--- chernikova.galyna/T3/commands.cpp
+++ chernikova.galyna/T3/commands.cpp
@@ -1,6 +1,20 @@
 #include "commands.hpp"
-
 #include <Delimiter.hpp>
+
+chernikova::Commands::Commands()
+{
+  dict1_.insert({ "AREA EVEN", getAreaEven });
+  dict1_.insert({ "AREA ODD", getAreaOdd });
+  dict1_.insert({ "AREA MEAN", getAreaMean });
+  dict2_.insert({ "AREA", getAreaVertexes });
+  dict1_.insert({ "MAX AREA", getMaxArea });
+  dict1_.insert({ "MAX VERTEXES", getMaxVertexes });
+  dict1_.insert({ "MIN AREA", getMinArea });
+  dict1_.insert({ "MIN VERTEXES", getMinVertexes });
+  dict1_.insert({ "COUNT EVEN", getCountEven });
+  dict1_.insert({ "COUNT ODD", getCountOdd });
+  dict2_.insert({ "COUNT", getCountVertexes });
+}
 
 std::string chernikova::inputCommand(std::istream& in)
 {
