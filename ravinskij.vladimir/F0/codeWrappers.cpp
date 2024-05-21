c945c37321c2d9df407f750ff0420040df34db05
ravinskij.vladimir <ravunskij0412@gmail.com>
2024-05-21 14:23:52 +0300

    Implemented CodeWrappers

---
+++ ravinskij.vladimir/F0/codeWrappers.cpp
@@ -0,0 +1,41 @@
+#include "codeWrappers.hpp"
+#include <iostream>
+#include <scopeGuard.hpp>
+#include <delimeters.hpp>
+
+namespace rav = ravinskij;
+std::ostream& rav::operator<<(std::ostream& out, rav::WriteWrapper&& wrapper)
+{
+    std::ostream::sentry sentry(out);
+    if (!sentry)
+    {
+        return out;
+    }
+    out << wrapper.symbol << '-';
+    for (bool bit: wrapper.code)
+    {
+        out << bit;
+    }
+    return out;
+}
+
+std::istream& rav::operator>>(std::istream& in, rav::ReadWrapper&& wrapper)
+{
+    std::istream::sentry sentry(in);
+    if (!sentry)
+    {
+        return in;
+    }
+    wrapper.symbol = in.get();
+    in >> CharDelimeter{'-'};
+    char bit = 0;
+    in >> std::noskipws;
+    while (bit != '\n')
+    {
+        in >> bit;
+        wrapper.code.push_back(static_cast<bool>(bit - '0'));
+    }
+    wrapper.code.pop_back();
+    in >> std::skipws;
+    return in;
+}
---
+++ ravinskij.vladimir/F0/codeWrappers.hpp
@@ -0,0 +1,25 @@
+#ifndef CODE_WRAPPERS_HPP
+#define CODE_WRAPPERS_HPP
+
+#include <iosfwd>
+#include <vector>
+
+namespace ravinskij
+{
+  struct ReadWrapper
+  {
+    char &symbol;
+    std::vector<bool> &code;
+  };
+
+  struct WriteWrapper
+  {
+    const char &symbol;
+    const std::vector<bool> &code;
+  };
+ 
+  std::ostream& operator<<(std::ostream& out, WriteWrapper&& wrapper);
+  std::istream& operator>>(std::istream& in, ReadWrapper&& wrapper);
+}
+
+#endif
