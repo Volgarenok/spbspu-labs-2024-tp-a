510bc631762440e5243cab32f82532b9511b2172
Dmitriy <Dmitry5335@yandex.ru>
2024-05-21 14:43:36 +0300

    Getters for word

--- nikitov.dmitriy/F0/word.cpp
+++ nikitov.dmitriy/F0/word.cpp
@@ -7,6 +7,21 @@ nikitov::detail::Word::Word(const std::string& translation):
   antonym_()
 {}
 
+std::string& nikitov::detail::Word::getPrimaryTranslation()
+{
+  return primaryTranslation_;
+}
+
+std::string& nikitov::detail::Word::getSecondaryTranslation()
+{
+  return secondaryTranslation_;
+}
+
+const std::string& nikitov::detail::Word::getAntonym() const
+{
+  return antonym_;
+}
+
 void nikitov::detail::Word::addTranslation(const std::string& translation)
 {
   if (secondaryTranslation_.empty())
--- nikitov.dmitriy/F0/word.hpp
+++ nikitov.dmitriy/F0/word.hpp
@@ -15,8 +15,14 @@ namespace nikitov
       std::string antonym_;
     public:
       Word(const std::string& translation);
+
+      std::string& getPrimaryTranslation();
+      std::string& getSecondaryTranslation();
+      const std::string& getAntonym() const;
+
       void addTranslation(const std::string& translation);
       void addAntonym(const std::string& antonym);
+      void deleteTranslation(const std::string& translation);
     };
   }
 }
