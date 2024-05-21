2406f1c04b475f85ba3694a028459a545a363144
chernikovaG <chernikova.gv@edu.spbstu.ru>
2024-05-21 22:34:38 +0300

    Added doCommand

--- chernikova.galyna/T3/commands.cpp
+++ chernikova.galyna/T3/commands.cpp
@@ -1,5 +1,7 @@
 #include "commands.hpp"
 
+#include <Delimiter.hpp>
+
 std::string chernikova::inputCommand(std::istream& in)
 {
   std::string command;
@@ -10,4 +12,48 @@ std::string chernikova::inputCommand(std::istream& in)
   }
 
   return command;
+}
+
+void chernikova::Commands::doCommand(std::vector< Polygon >& polygons, const Commands& dict, const std::string& cmd, std::ostream& out, std::istream& in)
+{
+  std::cout << "com = " << cmd << '\n';
+  std::istringstream iss ( (std::string(cmd)) );
+  std::string cmd_str;
+  iss >> cmd_str;
+
+  auto it1 = dict1_.find(cmd);
+
+  if(it1 != dict1_.end())
+  {
+    dict.doCommand(polygons, cmd, out);
+    return;
+  }
+
+  auto it2 = dict2_.find(cmd_str);
+
+  if(it2 != dict2_.end())
+  {
+    size_t num;
+    iss >> num;
+    if (!in)
+    {
+      throw std::invalid_argument("Error input");
+    }
+    dict.doCommand(polygons, cmd_str, num, out);
+    return;
+  }
+
+  auto it3 = dict3_.find(cmd_str);
+
+  if (it3 != dict3_.end())
+  {
+    Polygon polygon;
+    iss >> polygon >> DelimiterI{ '\n' };
+    if (!in)
+    {
+      throw std::invalid_argument("Error input");
+    }
+    dict.doCommand(polygons, cmd_str, polygon, out);
+    return;
+  }
 }
