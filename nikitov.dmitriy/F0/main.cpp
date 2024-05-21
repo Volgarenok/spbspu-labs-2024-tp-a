101a51847236a7f975059e7a1529732a5b7a0d16
Dmitriy <Dmitry5335@yandex.ru>
2024-05-21 12:45:07 +0300

    Make file reading

--- nikitov.dmitriy/F0/main.cpp
+++ nikitov.dmitriy/F0/main.cpp
@@ -1,2 +1,18 @@
-int main()
-{}
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <map>
+#include "dictionary.hpp"
+
+int main(int argc, char* argv[])
+{
+  using namespace nikitov;
+  std::map< std::string, Dictionary > dictOfDicts;
+  for (int i = 1; i != argc; ++i)
+  {
+    std::ifstream fileInput(argv[i]);
+    if (fileInput)
+    {
+    }
+  }
+}
