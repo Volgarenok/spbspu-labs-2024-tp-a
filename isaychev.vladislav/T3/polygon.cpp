4b257c47dbfc96847e6743a598f8f267c61b555c
Z0ltaan <gregnov69@gmail.com>
2024-05-22 20:32:35 +0300

    simple operator>> for polygon

--- isaychev.vladislav/T3/polygon.cpp
+++ isaychev.vladislav/T3/polygon.cpp
@@ -1,9 +1,22 @@
 #include "polygon.hpp"
 #include <iostream>
+#include <iterator>
+#include <algorithm>
 
 std::istream & isaychev::operator>>(std::istream & in, Polygon & p)
 {
-  auto x = p.points.front();
-  x = p.points.front();
+  std::istream::sentry guard(in);
+  if (!guard)
+  {
+    return in;
+  }
+
+  size_t pntNum = 0;
+  in >> pntNum;
+
+  using iterI_t = std::istream_iterator< Point >;
+
+  std::copy_n(iterI_t{in}, pntNum, std::back_inserter(p.points));
+
   return in;
 }
