67d08f5e0215027e50d6fda15c93955f4335e4c5
Z0ltaan <gregnov69@gmail.com>
2024-05-22 17:12:46 +0300

    basic point

---
+++ isaychev.vladislav/T3/point.cpp
@@ -0,0 +1,4 @@
+#include "point.hpp"
+
+std::istream & isaychev::operator>>(std::istream &, Point &)
+{}
---
+++ isaychev.vladislav/T3/point.hpp
@@ -0,0 +1,15 @@
+#ifndef POINT_HPP
+#define POINT_HPP
+
+#include <iosfwd>
+
+namespace isaychev
+{
+  struct Point
+  {
+    int x, y;
+  }
+  std::istream & operator>>(std::istream &, Point &);
+}
+
+#endif
