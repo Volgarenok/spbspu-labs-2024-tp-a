a7669602ddf0263837ae1434bed4cd2a49d4e3a9
KozakovaAnn <kozakova.ann@mail.ru>
2024-05-22 22:01:26 +0300

    update main

--- kozakova.ann/F0/main.cpp
+++ kozakova.ann/F0/main.cpp
@@ -3,23 +3,10 @@
 #include <map>
 #include <functional>
 #include "ERDictionary.hpp"
+#include "Commands.hpp"
 
 #define NOMINMAX
 #include "windows.h"
-
-namespace kozakova
-{
-  void insert(std::map< std::string, kozakova::ERDictionary >& dicts, std::istream& in, std::ostream& out);
-  void search(const std::map< std::string, kozakova::ERDictionary >& dicts, std::istream& in, std::ostream& out);
-  void remove(std::map< std::string, kozakova::ERDictionary >& dicts, std::istream& in, std::ostream& out);
-  void removeTranslation(std::map< std::string, kozakova::ERDictionary >& dicts, std::istream& in, std::ostream& out);
-  void searchShort(const std::map< std::string, kozakova::ERDictionary >& dicts, std::istream& in, std::ostream& out);
-  void searchSame(const std::map< std::string, kozakova::ERDictionary >& dicts, std::istream& in, std::ostream& out);
-  void identical(const std::map< std::string, kozakova::ERDictionary >& dicts, std::istream& in, std::ostream& out);
-  void combine(std::map< std::string, kozakova::ERDictionary >& dicts, std::istream& in, std::ostream& out);
-  void intersection(std::map< std::string, kozakova::ERDictionary >& dicts, std::istream& in, std::ostream& out);
-  void save(const std::map< std::string, kozakova::ERDictionary >& dicts, const std::string& fname);
-}
 
 int main(int argc, char* argv[])
 {
