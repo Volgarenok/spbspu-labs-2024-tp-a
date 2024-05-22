09486498c211b3311bc8ca558e2b8f792153d3a5
exxiiint <filipp.sivkov@yandex.ru>
2024-05-22 21:34:36 +0300

    deleety

--- sivkov.filipp/T3/main.cpp
+++ sivkov.filipp/T3/main.cpp
@@ -1,255 +1,14 @@
 #include <iostream>
 #include <map>
-#include <iomanip>
-#include <vector>
-#include <iterator>
+#include <fstream>
 #include <functional>
 #include <algorithm>
-#include <fstream>
 #include <string>
-#include <iterator>
-#include <numeric>
-#include "polygon.hpp"
-#include "helpCommands.hpp"
-
-
-namespace sivkov
-{
-  void area(std::istream& in, std::ostream& out, const std::vector<Polygon>& polygons)
-  {
-    std::istream::sentry guard(in);
-    if (!guard)
-    {
-      return;
-    }
-    double area = 0.0;
-    std::string argument;
-    in >> argument;
-    std::map< std::string, std::function< double() > > cmd;
-    {
-      using namespace std::placeholders;
-      cmd["EVEN"] = std::bind(getOddEvenMean, polygons, "EVEM");
-      cmd["ODD"] = std::bind(getOddEvenMean, polygons, "ODD");
-      cmd["MEAN"] = std::bind(getOddEvenMean, polygons, "MEAN");
-    }
-    try
-    {
-      if (!std::all_of(argument.cbegin(), argument.cend(), ::isdigit))
-      {
-        throw std::invalid_argument("error argument");
-      }
-      size_t num = std::stoi(argument);
-      if (num < 3)
-      {
-        throw std::invalid_argument("error number vertexes");
-      }
-      std::vector< Polygon > sortShape;
-      auto operation = std::bind(isNumEqSize, std::placeholders::_1, num);
-      std::copy_if(polygons.cbegin(), polygons.cend(), std::back_inserter(sortShape), operation);
-      std::vector< double > areasShape;
-      std::transform(sortShape.cbegin(), sortShape.cend(), std::back_inserter(areasShape), countAreaShape);
-      area = std::accumulate(areasShape.cbegin(), areasShape.cend(), 0.0);
-    }
-    catch (const std::invalid_argument&)
-    {
-      area = cmd[argument]();
-    }
-    out << std::fixed << std::setprecision(1) << area << '\n';
-  }
-
-  void count(std::istream& in, std::ostream& out, const std::vector< Polygon >& polygons)
-  {
-    std::istream::sentry guard(in);
-    if (!guard)
-    {
-      return;
-    }
-    std::string arg = "";
-    in >> arg;
-    size_t count = 0;
-    if (arg == "EVEN")
-    {
-      count = std::count_if(polygons.cbegin(), polygons.cend(), isEvenVertices);
-    }
-    else if (arg == "ODD")
-    {
-      count = std::count_if(polygons.cbegin(), polygons.cend(), isOddVertices);
-    }
-    else
-    {
-      long long ver = 0;
-      try
-      {
-        ver = std::stoull(arg);
-      }
-      catch(const std::invalid_argument&)
-      {
-        throw std::logic_error("<INVALID COMMAND>");
-      }
-      catch (const std::out_of_range&)
-      {
-        throw std::logic_error("<INVALID COMMAND>");
-      }
-      auto hasVertexCountBind = std::bind(hasVertexCount, std::placeholders::_1, ver);
-      count = std::count_if(polygons.cbegin(), polygons.cend(), hasVertexCountBind);
-    }
-    out << count << "\n";
-  }
-  void min(std::istream& in, std::ostream& out, const std::vector<Polygon>& polygons)
-  {
-    std::istream::sentry guard(in);
-    if (!guard)
-    {
-      return;
-    }
-
-    std::string arg;
-    in >> arg;
-
-    if (polygons.empty())
-    {
-      out << "No polygons provided" << std::endl;
-      return;
-    }
-
-    if (arg == "AREA")
-    {
-      auto minIt = std::min_element(polygons.begin(), polygons.end(), MinArea());
-      if (minIt != polygons.end())
-      {
-        double minArea = countAreaShape(*minIt);
-        out << std::fixed << std::setprecision(1) << minArea << '\n';
-      }
-      else
-      {
-        out << "No valid polygon found" << std::endl;
-      }
-    }
-    else if (arg == "VERTEXES")
-    {
-      auto minIt = std::min_element(polygons.begin(), polygons.end(), MinVertices());
-      if (minIt != polygons.end())
-      {
-        size_t minVertices = minIt->points.size();
-        out << minVertices << '\n';
-      }
-      else
-      {
-        out << "No valid polygon found" << std::endl;
-      }
-    }
-    else
-    {
-      out << "<INVALID COMMAND>\n";
-    }
-  }
-  void max(std::istream& in, std::ostream& out, const std::vector<Polygon>& polygons)
-  {
-    std::istream::sentry guard(in);
-    if (!guard)
-    {
-      return;
-    }
-
-    std::string arg;
-    in >> arg;
-
-    if (polygons.empty())
-    {
-      out << "No polygons provided" << std::endl;
-      return;
-    }
-
-    if (arg == "AREA")
-    {
-      auto maxIt = std::max_element(polygons.begin(), polygons.end(), MaxArea());
-      if (maxIt != polygons.end())
-      {
-        double maxArea = countAreaShape(*maxIt);
-        out << std::fixed << std::setprecision(1) << maxArea << '\n';
-      }
-      else
-      {
-        out << "No valid polygon found" << std::endl;
-      }
-    }
-    else if (arg == "VERTEXES")
-    {
-      auto maxIt = std::max_element(polygons.begin(), polygons.end(), MaxVertices());
-      if (maxIt != polygons.end())
-      {
-        size_t maxVertices = maxIt->points.size();
-        out << maxVertices << '\n';
-      }
-      else
-      {
-        out << "No valid polygon found" << std::endl;
-      }
-    }
-    else
-    {
-      out << "<INVALID COMMAND>\n";
-    }
-  }
-
-  void perms(std::istream& in, std::ostream& out, const std::vector<Polygon>& polygons)
-  {
-    std::istream::sentry guard(in);
-    if (!guard)
-    {
-      return;
-    }
-
-    Polygon targetPolygon;
-    if (!(in >> targetPolygon))
-    {
-      out << "<INVALID POLYGON FORMAT>\n";
-      return;
-    }
-    using namespace std::placeholders;
-    size_t count = std::count_if(polygons.begin(), polygons.end(), std::bind(arePointsPermutations, _1, targetPolygon));
-
-    out << count << '\n';
-  }
-
-  void inframe(std::istream& in, std::ostream& out, const std::vector<Polygon>& polygons)
-  {
-    std::istream::sentry guard(in);
-    if (!guard)
-    {
-      return;
-    }
-
-    Polygon targetPolygon;
-    if (!(in >> targetPolygon))
-    {
-      out << "<INVALID POLYGON FORMAT>\n";
-      return;
-    }
-
-    if (polygons.empty())
-    {
-      out << "<FALSE>\n";
-      return;
-    }
-
-    BoundingBox bbox = calculateBoundingBox(polygons);
-
-    if (isPolygonInsideBoundingBox(targetPolygon, bbox))
-    {
-      out << "<TRUE>\n";
-    }
-    else
-    {
-      out << "<FALSE>\n";
-    }
-  }
-}
+#include "commands.hpp"
 
 
 int main(int argc, char* argv[])
 {
-  using namespace sivkov;
   if (argc != 2)
   {
     std::cerr << "error cmd\n";
@@ -263,10 +22,9 @@ int main(int argc, char* argv[])
     return 1;
   }
 
+  using namespace sivkov;
   std::vector< Polygon > polygons;
-
   using input_iter = std::istream_iterator< Polygon >;
-
   while (!in.eof())
   {
     std::copy(input_iter{ in }, input_iter{}, std::back_inserter(polygons));
@@ -274,56 +32,30 @@ int main(int argc, char* argv[])
     in.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
   }
 
+  using treeOfCmd = std::map < std::string, std::function < void(std::istream&, std::ostream&) > >;
+  using namespace std::placeholders;
+  treeOfCmd cmd;
+
+  cmd["MIN"] = std::bind(min, _1, _2, std::cref(polygons));
+  cmd["AREA"] = std::bind(area, _1, _2, std::cref(polygons));
+  cmd["COUNT"] = std::bind(count, _1, _2, std::cref(polygons));
+  cmd["PERMS"] = std::bind(perms, _1, _2, std::cref(polygons));
+  cmd["INFRAME"] = std::bind(inframe, _1, _2, std::cref(polygons));
+
   std::string arg = "";
   while (std::cin >> arg)
   {
     try
     {
-      if (arg == "COUNT")
-      {
-        count(std::cin, std::cout, polygons);
-      }
-      else if (arg == "AREA")
-      {
-        area(std::cin, std::cout, polygons);
-      }
-      else if (arg == "MIN")
-      {
-        min(std::cin, std::cout, polygons);
-      }
-      else if (arg == "MAX")
-      {
-        max(std::cin, std::cout, polygons);
-      }
-      else if (arg == "INFRAME")
-      {
-        inframe(std::cin, std::cout, polygons);
-      }
-      else if (arg == "PERMS")
-      {
-        perms(std::cin, std::cout, polygons);
-      }
-      else
-      {
-        std::cout << "<INVALID COMMAND>\n";
-        std::cin.clear();
-        std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
-      }
+      cmd.at(arg)(std::cin, std::cout);
+      std::cout;
     }
-    catch (const std::out_of_range&)
+    catch (const std::exception&)
     {
-      std::cout << "<INVALID COMMAND>\n";
-      std::cin.clear();
-      std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+      std::cout << "<INVALID COMMAND>" << '\n';
     }
-    catch (const std::logic_error& e)
-    {
-      std::cout << e.what() << '\n';
-      std::cin.clear();
-      std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
-    }
+    std::cin.clear();
+    std::cin.ignore(std::numeric_limits < std::streamsize >::max(), '\n');
   }
-
-  return 0;
 
 }
