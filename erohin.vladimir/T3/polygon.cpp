9225efb9a4cd22cce60d04fbaa4ebfc8a4809e9f
WarriorVortex <erohinvova01010111@gmail.com>
2024-05-24 13:26:18 +0300

    Moved functors to cpp as implementation details

--- erohin.vladimir/T3/polygon.cpp
+++ erohin.vladimir/T3/polygon.cpp
@@ -70,12 +70,31 @@ std::pair< erohin::Point, erohin::Point > erohin::getFrameRect(const Polygon & p
   return std::make_pair(Point{ x_pair.first->x, y_pair.first->y }, Point{ x_pair.second->x, y_pair.second->y });
 }
 
+namespace erohin
+{
+  struct getFrameRectLeftLower
+  {
+    Point operator()(const Polygon & polygon)
+    {
+      return getFrameRect(polygon).first;
+    }
+  };
+
+  struct getFrameRectRightUpper
+  {
+    Point operator()(const Polygon & polygon)
+    {
+      return getFrameRect(polygon).second;
+    }
+  };
+}
+
 std::pair< erohin::Point, erohin::Point > erohin::getFrameRect(const std::vector< Polygon > & context)
 {
   Polygon corner_polygon;
-  std::transform(context.cbegin(), context.cend(), std::back_inserter(corner_polygon.points), detail::getFrameRectLeftLower{});
+  std::transform(context.cbegin(), context.cend(), std::back_inserter(corner_polygon.points), getFrameRectLeftLower{});
   Point left_lower = getFrameRect(corner_polygon).first;
-  std::transform(context.cbegin(), context.cend(), corner_polygon.points.begin(), detail::getFrameRectRightUpper{});
+  std::transform(context.cbegin(), context.cend(), corner_polygon.points.begin(), getFrameRectRightUpper{});
   Point right_upper = getFrameRect(corner_polygon).second;
   return std::make_pair(left_lower, right_upper);
 }
@@ -105,20 +124,51 @@ bool erohin::isLessBySize(const Polygon & lhs, const Polygon & rhs)
   return (lhs.points.size() < rhs.points.size());
 }
 
+namespace erohin
+{
+  struct isRightAngle
+  {
+    Point prev;
+    Point center;
+    bool operator()(const Point & point)
+    {
+      size_t scalar_miltiply = std::abs((point.x - center.x) * (prev.x - center.x) + (point.y - center.y) * (prev.y - center.y));
+      prev = center;
+      center = point;
+      return !scalar_miltiply;
+    }
+  };
+}
+
 bool erohin::hasRightAngles(const Polygon & polygon)
 {
   if (polygon.points.size() < 3)
   {
     throw std::logic_error("Cannot find any angle");
   }
-  auto predicate = detail::isRightAngle{ *(polygon.points.cend() - 2), *(polygon.points.cend() - 1) };
+  auto predicate = isRightAngle{ *(polygon.points.cend() - 2), *(polygon.points.cend() - 1) };
   return std::any_of(polygon.points.cbegin(), polygon.points.cend(), predicate);
+}
+
+namespace erohin
+{
+  struct evaluateArea
+  {
+    Point start;
+    Point prev;
+    double operator()(const Point & point)
+    {
+      double area = std::abs((point.x - start.x) * (prev.y - start.y) - (prev.x - start.x) * (point.y - start.y)) / 2.0;
+      prev = point;
+      return area;
+    }
+  };
 }
 
 double erohin::getArea(const Polygon & polygon)
 {
   const std::vector< Point > & vertex = polygon.points;
-  auto area_functor = detail::evaluateArea{ *vertex.cbegin(), *vertex.cbegin() };
+  auto area_functor = evaluateArea{ *vertex.cbegin(), *vertex.cbegin() };
   std::vector< double > part_area;
   std::transform(vertex.cbegin(), vertex.cend(), std::back_inserter(part_area), area_functor);
   return std::accumulate(part_area.cbegin(), part_area.cend(), 0.0);
@@ -130,29 +180,4 @@ double erohin::getArea(const std::vector< Polygon > & context)
   double (*area_counter)(const Polygon &) = getArea;
   std::transform(context.cbegin(), context.cend(), std::back_inserter(area), area_counter);
   return std::accumulate(area.cbegin(), area.cend(), 0.0);
-}
-
-double erohin::detail::evaluateArea::operator()(const Point & point)
-{
-  double area = std::abs((point.x - start.x) * (prev.y - start.y) - (prev.x - start.x) * (point.y - start.y)) / 2.0;
-  prev = point;
-  return area;
-}
-
-bool erohin::detail::isRightAngle::operator()(const Point & point)
-{
-  size_t scalar_miltiply = std::abs((point.x - center.x) * (prev.x - center.x) + (point.y - center.y) * (prev.y - center.y));
-  prev = center;
-  center = point;
-  return !scalar_miltiply;
-}
-
-erohin::Point erohin::detail::getFrameRectLeftLower::operator()(const Polygon & polygon)
-{
-  return getFrameRect(polygon).first;
-}
-
-erohin::Point erohin::detail::getFrameRectRightUpper::operator()(const Polygon & polygon)
-{
-  return getFrameRect(polygon).second;
 }
--- erohin.vladimir/T3/polygon.hpp
+++ erohin.vladimir/T3/polygon.hpp
@@ -26,32 +26,6 @@ namespace erohin
   bool hasRightAngles(const Polygon & polygon);
   double getArea(const Polygon & polygon);
   double getArea(const std::vector< Polygon > & context);
-  namespace detail
-  {
-    struct evaluateArea
-    {
-      Point start;
-      Point prev;
-      double operator()(const Point & point);
-    };
-
-    struct isRightAngle
-    {
-      Point prev;
-      Point center;
-      bool operator()(const Point & point);
-    };
-
-    struct getFrameRectLeftLower
-    {
-      Point operator()(const Polygon & polygon);
-    };
-
-    struct getFrameRectRightUpper
-    {
-      Point operator()(const Polygon & polygon);
-    };
-  }
 }
 
 #endif
