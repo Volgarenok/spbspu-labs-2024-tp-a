21a14d9789a4e3762fe151a82d514adde29d8fa7
chernikovaG <chernikova.gv@edu.spbstu.ru>
2024-05-25 13:47:49 +0300

    Fixed echo

--- chernikova.galyna/T3/polygon.cpp
+++ chernikova.galyna/T3/polygon.cpp
@@ -1,6 +1,6 @@
 #include "polygon.hpp"
 
-std::istream& chernikova::operator>>(std::istream& in, chernikova::Point& dest)
+std::istream& chernikova::operator>>(std::istream& in, Point& dest)
 {
   std::istream::sentry sentry(in);
   if (!sentry) {
@@ -10,13 +10,13 @@ std::istream& chernikova::operator>>(std::istream& in, chernikova::Point& dest)
   if (!(in >> DelimiterI{'('}) || !(in >> dest.x) || !(in >> DelimiterI{';'}) || !(in >> dest.y) ||
       !(in >> DelimiterI{')'}))
   {
-    in.setstate(std::ios::failbit);
+    throw std::logic_error("<INVALID COMMAND>\n");
   }
 
   return in;
 }
 
-std::istream& chernikova::operator>>(std::istream& in, chernikova::Polygon& dest)
+std::istream& chernikova::operator>>(std::istream& in, Polygon& dest)
 {
   std::istream::sentry sentry(in);
   if (!sentry)
@@ -30,7 +30,7 @@ std::istream& chernikova::operator>>(std::istream& in, chernikova::Polygon& dest
     in.setstate(std::ios::failbit);
     return in;
   }
-  using iter = std::istream_iterator< chernikova::Point >;
+  using iter = std::istream_iterator< Point >;
   dest.points.clear();
   std::copy_n(iter(in), count, std::back_inserter(dest.points));
   if (dest.points.size() != count)
@@ -41,22 +41,22 @@ std::istream& chernikova::operator>>(std::istream& in, chernikova::Polygon& dest
   return in;
 }
 
-bool chernikova::isEven(const chernikova::Polygon& polygon)
+bool chernikova::isEven(const Polygon& polygon)
 {
   return polygon.points.size() % 2 == 0;
 }
 
-bool chernikova::isOdd(const chernikova::Polygon& polygon)
+bool chernikova::isOdd(const Polygon& polygon)
 {
   return !isEven(polygon);
 }
 
-double chernikova::calcArea(const chernikova::Point& left, const chernikova::Point& right)
+double chernikova::calcArea(const Point& left, const Point& right)
 {
   return 0.5 * (left.x * right.y - right.x * left.y);
 }
 
-double chernikova::getArea(const chernikova::Polygon& polygon)
+double chernikova::getArea(const Polygon& polygon)
 {
   double res = 0.0;
   for (size_t i = 0; i < polygon.points.size() - 1; ++i)
@@ -67,12 +67,12 @@ double chernikova::getArea(const chernikova::Polygon& polygon)
   return std::abs(res);
 }
 
-double chernikova::sumArea(double cur, const chernikova::Polygon& polygon)
+double chernikova::sumArea(double cur, const Polygon& polygon)
 {
   return cur + chernikova::getArea(polygon);
 }
 
-bool chernikova::isNecessaryVertex(const chernikova::Polygon& polygon, size_t count)
+bool chernikova::isNecessaryVertex(const Polygon& polygon, size_t count)
 {
   return polygon.points.size() == count;
 }
@@ -89,13 +89,13 @@ size_t chernikova::chooseGreaterVertexes(double cur, const Polygon& polygon)
   return (cur > count) ? cur : count;
 }
 
-double chernikova::chooseLessArea(double cur, const chernikova::Polygon& polygon)
+double chernikova::chooseLessArea(double cur, const Polygon& polygon)
 {
   double area = chernikova::getArea(polygon);
   return (cur < area) ? cur : area;
 }
 
-size_t chernikova::chooseLessVertexes(double cur, const chernikova::Polygon& polygon)
+size_t chernikova::chooseLessVertexes(double cur, const Polygon& polygon)
 {
   size_t count = polygon.points.size();
   return (cur < count) ? cur : count;
@@ -237,7 +237,6 @@ void chernikova::echo(std::vector< Polygon >& polygons, const Polygon& polygon, 
   newPolygons.reserve(polygons.size());
   auto binary_op = std::bind(duplicator, std::ref(polygons), _1, polygon);
   std::transform(polygons.cbegin(), polygons.cend(), std::back_inserter(newPolygons), binary_op);
-  polygons.insert(polygons.end(), newPolygons.begin(), newPolygons.end());
   StreamGuard streamGuard(out);
   out << std::fixed << std::setprecision(1);
   out << count << "\n";
