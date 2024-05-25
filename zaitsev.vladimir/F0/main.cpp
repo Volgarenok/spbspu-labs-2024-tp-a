7d599956fe8f6a71bf7a14be7611b0ce3aa72323
v-zaycev <70699650+v-zaycev@users.noreply.github.com>
2024-05-26 01:47:11 +0300

    Help added

--- zaitsev.vladimir/F0/main.cpp
+++ zaitsev.vladimir/F0/main.cpp
@@ -3,6 +3,8 @@
 #include <iomanip>
 #include <string>
 #include <map>
+#include <string.h>
+#include <stream_guard.hpp>
 
 using unit_t = std::map< std::string, int >;
 using graph_t = std::map< std::string, unit_t >;
@@ -30,7 +32,60 @@ void delete_graph(base_t& graphs, const std::string& graph_name)
   }
 }
 
-std::ostream& print(std::ostream& out, base_t& graphs, const std::string& graph_name)
+std::ostream& list_of_graphs(std::ostream& out, base_t& graphs)
+{
+  if (graphs.empty())
+  {
+    return out << "No saved graphs\n";
+  }
+//  zaitsev::StreamGuard guard(out);
+  auto c_it = graphs.cbegin();
+  for (size_t i = 0; i < graphs.size(); ++i)
+  {
+    out << std::setw(3) << i << ". ";
+    out << c_it->first << '\n';
+  }
+  return out;
+}
+
+void print_help()
+{
+  using namespace std;
+  string indent(2, ' ');
+  cout << "Help:\n";
+  cout << "1.  read <file> <graph-name>\n" << indent;
+  cout << "Read the graph from the file <file>, and assign it the name <graph-name>.\n\n";
+  cout << "2.  write <graph> <file>\n" << indent;
+  cout << "Write a graph <graph>  to a file <file>.\n\n";
+  cout << "3.  list_of_graphs\n" << indent;
+  cout << "Output a lexicographically ordered list of available graphs.\n\n";
+  cout << "4.  print <graph-name>\n" << indent;
+  cout << "Print a description of the graph <graph-name>.\n\n";
+  cout << "5.  create_graph <graph-name>\n" << indent;
+  cout << "Create an empty graph named <graph-name>.\n\n";
+  cout << "6.  delete_graph <graph>\n" << indent;
+  cout << "Delete a graph named <graph-name>.\n\n";
+  cout << "7.  add_vertex <graph> <vertex>  \n" << indent;
+  cout << "Add a vertex <vertex> to the graph <graph-name>.\n\n";
+  cout << "8.  add_edge [-check] <graph> <begin> <end> <value>\n" << indent;
+  cout << "Add an edge to the graph <graph> that connects the vertices <begin> <end> with the value <value>.\n\n";
+  cout << "9.  merge [-check] <new-graph> <graph-1> <graph-2> \n" << indent;
+  cout << "A new graph <new-graph> is created, which is a union of graphs <graph-1> and <graph-2>.\n\n";
+  cout << "10. negative_weight_cycles <graph>\n" << indent;
+  cout << "Output a lexicographically ordered list of available graphs.\n\n"; //to do
+  cout << "11. shortest <graph> <begin> <end>\n" << indent;
+  cout << "Calculate the length of the shortest path from <begin> to <end> in the graph <graph>.\n\n";
+  cout << "12. shortest_trace <graph> <vertex-begin> <vertex-end>\n" << indent;
+  cout << "Print the shortest path from <begin> to <end> in the graph <graph>.\n\n";
+  cout << "13. shortest_path_matrix <graph>\n" << indent;
+  cout << "Output a lexicographically ordered list of available graphs.\n\n"; //to do
+  cout << "14. dump <file>\n" << indent;
+  cout << "Create a file <file> in which all graphs saved in the program are written.\n\n";
+
+  return;
+}
+
+std::ostream& print_graph(std::ostream& out, base_t& graphs, const std::string& graph_name)
 {
   base_t::iterator graph = graphs.find(graph_name);
   if (graph != graphs.end())
@@ -58,4 +113,40 @@ int main(int argc, char** argv)
 {
   base_t graphs;
 
-}
+  if (argc == 3)
+  {
+    if (!strcmp(argv[1], "--help"))
+    {
+      print_help();
+    }
+    else
+    {
+      std::cerr << "Invalid option selected\n";
+    }
+  /*  std::ofstream in(argv[2]);
+    if (!in)
+    {
+
+    }
+    else
+    {
+
+    }*/
+  }
+  if (argc == 2)
+  {
+    std::ofstream in(argv[1]);
+    if (!in)
+    {
+
+    }
+    else
+    {
+
+    }
+  }
+
+
+
+
+ }
