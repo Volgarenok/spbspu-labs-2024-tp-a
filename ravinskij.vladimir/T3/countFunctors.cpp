0d276e648a85b170664a3d4e2e704d6cf01e7101
ravinskij.vladimir <ravunskij0412@gmail.com>
2024-05-25 22:23:18 +0300

    odd is a binded even

--- ravinskij.vladimir/T3/countFunctors.cpp
+++ ravinskij.vladimir/T3/countFunctors.cpp
@@ -8,13 +8,6 @@ bool rav::EvenCountFunctor::operator()(const Polygon& polygon)
   return polygonSize(polygon) % 2 == 0;
 }
 
-
-bool rav::OddCountFunctor::operator()(const Polygon& polygon)
-{
-  return polygonSize(polygon) % 2 != 0;
-}
-
-
 bool rav::VertexNumCountFunctor::operator()(const Polygon& polygon, size_t vertexCount)
 {
   return polygonSize(polygon) == vertexCount;
@@ -24,7 +17,8 @@ rav::GetCount::GetCount(const std::vector< Polygon >& vector):
   polygons(vector)
 {
   using namespace std::placeholders;
-  subCommands["ODD"] = std::bind(OddCountFunctor{}, _1);
+  auto oddFunctor = std::bind(std::logical_not< bool >{}, std::bind(EvenCountFunctor{}, _1));
+  subCommands["ODD"] = std::bind(oddFunctor, _1);
   subCommands["EVEN"] = std::bind(EvenCountFunctor{}, _1);
 }
 
