608c3c672e320086ad986b403f316a9621d55316
WarriorVortex <erohinvova01010111@gmail.com>
2024-05-25 03:54:32 +0300

    Added input of numformat argument

--- erohin.vladimir/F0/main.cpp
+++ erohin.vladimir/F0/main.cpp
@@ -1,2 +1,22 @@
-int main()
-{}
+#include <iostream>
+#include <string>
+
+int main(int argc, char ** argv)
+{
+  if (argc != 2)
+  {
+    std::cerr << "Wrong CLA's number\n";
+    return 1;
+  }
+  std::string format_arg(argv[1]);
+  if (format_arg.substr(0, 12) == "--numformat=")
+  {
+    std::cout << format_arg.substr(12) << "\n";
+  }
+  else
+  {
+    std::cerr << "Wrong number format\n";
+    return 2;
+  }
+  return 0;
+}
