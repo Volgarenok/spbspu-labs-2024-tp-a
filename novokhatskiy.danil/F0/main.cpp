cc86c0a0112eab894434bf47a4a3a2b1550e2962
DanilNovokhatskiy <karuk.vova2005@mail.ru>
2024-05-25 15:57:08 +0300

    Add find, delete

--- novokhatskiy.danil/F0/main.cpp
+++ novokhatskiy.danil/F0/main.cpp
@@ -8,11 +8,51 @@
 using val_t = std::pair< std::string, std::list< std::string > >;
 using mainDict = std::map< std::string, val_t >;
 
-void find(const mainDict& dict, std::string& key)
+void deleteWord(mainDict& dict, std::string& key)
 {
-
+  auto val = dict.find(key);
+  if (val == dict.cend())
+  {
+    throw std::logic_error("Dict can't delete the word, because this word doesn't exist");
+  }
+  dict.erase(key);
 }
 
+void find(const mainDict& dict, std::string& key, std::ostream& out)
+{
+  if (dict.empty())
+  {
+    throw std::logic_error("Empty dict");
+  }
+  auto val = dict.find(key);
+  if (val == dict.cend())
+  {
+    throw std::out_of_range("Dict doesn't have this key");
+  }
+  out << val->second.first << val->second.second.front();
+}
+
+void insert(mainDict& dict, std::istream& in)
+{
+  while (!in.eof())
+  {
+    in.clear();
+    std::string word = {};
+    in >> word;
+    std::string translation = {};
+    while (in >> translation)
+    {
+      std::string example = {};
+      std::getline(in, example);
+      val_t pair;
+      pair.first = translation;
+      pair.second.push_back(example);
+      dict[word] = pair;
+      in >> word;
+    }
+  }
+  in.clear();
+}
 
 void inputDict(std::istream& in, mainDict& dict)
 {
@@ -38,27 +78,42 @@ void inputDict(std::istream& in, mainDict& dict)
 
 void print(const mainDict& dict, std::ostream& out)
 {
+  if (dict.empty())
+  {
+    throw std::logic_error("Empty dict");
+  }
   auto begin = dict.cbegin();
   while (begin != dict.cend())
   {
-    out << begin->first << ' ' << begin->second.first << ' ' << begin->second.second.front() << '\n';
+    out << begin->first << ' ' << begin->second.first << begin->second.second.front() << '\n';
     begin++;
   }
-  out << '\n';
 }
 
 int main()
 {
   setlocale(LC_ALL, "ru");
-  std::cout << "If you want to know about commands, please write --help\n";
-  std::string commandHelp;
-  //std::cin >> commandHelp;
-  mainDict map;
-  std::fstream file("in.txt");
-  inputDict(file, map);
-  print(map, std::cout);
-  if (commandHelp == "--help")
+  try
   {
-    std::cout << "find - parameters < dict name >, < word >.\n";
+    std::cout << "If you want to know about commands, please write --help\n";
+    std::string commandHelp;
+    //std::cin >> commandHelp;
+    mainDict map;
+    std::fstream file("in.txt");
+    inputDict(file, map);
+    //print(map, std::cout);
+    std::string dog = "fkl;saf;l";
+    //find(map, dog, std::cout);
+    deleteWord(map, dog);
+    print(map, std::cout);
+    if (commandHelp == "--help")
+    {
+      std::cout << "find - parameters < dict name >, < word >.\n";
+    }
   }
+  catch (const std::exception& e)
+  {
+    std::cout << e.what() << '\n';
+    return 1;
+  } 
 }
