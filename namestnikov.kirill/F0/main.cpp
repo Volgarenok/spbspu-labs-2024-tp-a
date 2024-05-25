30f4994f7e282f02403b43456fb387d45699c0ba
kirillnamestnikov <kir.namestnikov@gmail.com>
2024-05-25 22:40:14 +0300

    create func

--- namestnikov.kirill/F0/main.cpp
+++ namestnikov.kirill/F0/main.cpp
@@ -4,8 +4,25 @@
 #include <iterator>
 #include <string>
 
+void doCreate(std::istream & in, std::ostream & out, std::unordered_map< std::string, std::unordered_map< std::string, std::string > > & mainMap)
+{
+  std::string newName = "";
+  in >> newName;
+  if (mainMap.find(newName) != mainMap.end())
+  {
+    out << "The dictionary with name " << newName << " already exists.\n";
+    return;
+  }
+  std::unordered_map< std::string, std::string > newDict;
+  mainMap[newName] = newDict;
+  out << newName << " is successfully created.\n";
+}
+
 int main()
 {
+  using pairWords = std::pair< std::string, std::string >;
+  using dictMain = std::unordered_map< std::string, std::unordered_map< std::string, std::string > >;
+
   std::ifstream in("base.txt");
   std::unordered_map< std::string, std::unordered_map< std::string, std::string > > myMap;
   std::unordered_map< std::string, std::string > tempMap;
@@ -16,4 +33,5 @@ int main()
         std::cout << itr->first 
              << '\t' << itr->second << '\n'; 
   }
+  doCreate(std::cin, std::cout, myMap);
 }
