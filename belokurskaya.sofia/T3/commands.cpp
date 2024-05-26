c8e387af4361a93df7ebf295208b3a777c3b4af7
sonia66666 <147131510+sonia66666@users.noreply.github.com>
2024-05-26 23:58:21 +0300

    try fix problems

--- belokurskaya.sofia/T3/commands.cpp
+++ belokurskaya.sofia/T3/commands.cpp
@@ -30,6 +30,10 @@ void belokurskaya::cmd::area(const std::vector< Polygon >& polygons, std::istrea
   if (subcommand.find(option) != subcommand.end())
   {
     resultFuncForArea = subcommand.at(option);
+
+    std::vector< double > areas(polygons.size());
+    std::transform(polygons.begin(), polygons.end(), areas.begin(), resultFuncForArea);
+    out << std::accumulate(areas.begin(), areas.end(), 0.0);
   }
   else
   {
@@ -52,10 +56,6 @@ void belokurskaya::cmd::area(const std::vector< Polygon >& polygons, std::istrea
       throw std::invalid_argument("Need more three vertexes");
     }
   }
-
-  std::vector< double > areas(polygons.size());
-  std::transform(polygons.begin(), polygons.end(), areas.begin(), resultFuncForArea);
-  out << std::accumulate(areas.begin(), areas.end(), 0.0);
 }
 
 void belokurskaya::cmd::min(const std::vector< Polygon >& polygons, std::istream& in, std::ostream& out)
