066584ef8e2e38e70dd73a47137ea8d501623ed1
magggottt <gladyshevmatvey@gmail.com>
2024-05-27 06:16:57 +0700

    added temp main

--- gladyshev.matvey/T3/main.cpp
+++ gladyshev.matvey/T3/main.cpp
@@ -1,2 +1,39 @@
-int main()
-{}
+#include <iostream>
+#include <fstream>
+#include <limits>
+#include <iterator>
+
+#include "polygon.hpp"
+
+int main(int argc, char* argv[])
+{
+  using namespace gladyshev;
+  std::ifstream file;
+  if (argc == 2)
+  {
+    file.open(argv[1]);
+  }
+  else
+  {
+    std::cerr << "Bad file input\n";
+    return 1;
+  }
+  std::vector< Polygon > polygons;
+  while (!file.eof())
+  {
+    if (!file)
+    {
+      file.clear();
+      file.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+    }
+    std::copy(
+      std::istream_iterator< Polygon >{file},
+      std::istream_iterator< Polygon >{},
+      std::back_inserter(polygons)
+    );
+  }
+  for (const auto& area: polygons)
+  {
+    std::cout << findArea(area) << "\n";
+  }
+}
