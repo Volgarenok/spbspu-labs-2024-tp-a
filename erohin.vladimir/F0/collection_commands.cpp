91fb8c7ccc7c64ead22f08223f53f185e0fee66c
WarriorVortex <erohinvova01010111@gmail.com>
2024-05-27 00:49:16 +0300

    Seperated dictionary formatted output and print command implementations

--- erohin.vladimir/F0/collection_commands.cpp
+++ erohin.vladimir/F0/collection_commands.cpp
@@ -14,6 +14,8 @@ namespace erohin
   {
     size_t operator()(const std::pair< std::string, size_t > & pair);
   };
+
+  void printDictionary(const dictionary & dict, std::ostream & output, numformat_t numformat);
 }
 
 void erohin::printCommand(const collection & context, std::istream & input, std::ostream & output, numformat_t numformat)
@@ -21,6 +23,16 @@ void erohin::printCommand(const collection & context, std::istream & input, std:
   std::string dict_name;
   input >> dict_name;
   const dictionary & dict = context.at(dict_name);
+  printDictionary(dict, output, numformat);
+}
+
+size_t erohin::getNumber::operator()(const std::pair< std::string, size_t > & pair)
+{
+  return pair.second;
+}
+
+void erohin::printDictionary(const dictionary & dict, std::ostream & output, numformat_t numformat)
+{
   std::vector< size_t > number_seq;
   std::transform(dict.cbegin(), dict.cend(), std::back_inserter(number_seq), getNumber{});
   size_t total_number = std::accumulate(number_seq.cbegin(), number_seq.cend(), 0);
@@ -31,9 +43,4 @@ void erohin::printCommand(const collection & context, std::istream & input, std:
     std::ostream_iterator< FormattedRecord >(output, "\n"),
     std::bind(createFormattedRecord, std::bind(createRecord, _1), total_number, numformat)
   );
-}
-
-size_t erohin::getNumber::operator()(const std::pair< std::string, size_t > & pair)
-{
-  return pair.second;
 }
