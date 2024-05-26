8f08dac731049adc875f3e7a4a9b5c1979c93f50
ravinskij.vladimir <ravunskij0412@gmail.com>
2024-05-26 16:09:08 +0300

    filtering and accumulating are different things

--- ravinskij.vladimir/T3/areaFunctors.cpp
+++ ravinskij.vladimir/T3/areaFunctors.cpp
@@ -7,19 +7,24 @@ rav::AccumulateArea::AccumulateArea(const std::vector< Polygon >& vector):
   polygons(vector)
 {
   using namespace std::placeholders;
-  subCommands["EVEN"] = std::bind(AreaSummator{}, 0.0, _1, EvenPredicate{});
-  subCommands["ODD"] = std::bind(AreaSummator{}, 0.0, _1, OddPredicate{});
+  subCommands["EVEN"] = std::bind(AreaSummator{}, 0.0, _1);
+  subCommands["ODD"] = std::bind(AreaSummator{}, 0.0, _1);
   subCommands["MEAN"] = std::bind(MeanArea{}, 0.0, _1, polygons.size());
   emptyVectorSupport["EVEN"] = true;
   emptyVectorSupport["ODD"] = true;
   emptyVectorSupport["MEAN"] = false;
+  filters["EVEN"] = EvenPredicate{};
+  filters["ODD"] = OddPredicate{};
+  filters["MEAN"] = nullptr;
 }
 double rav::AccumulateArea::operator()(const std::string& subCommand)
 {
   SubCommand accumulateFunctor;
+  Filter filter;
   try
   {
     accumulateFunctor = subCommands.at(subCommand);
+    filter = filters.at(subCommand);
     if (!emptyVectorSupport[subCommand] && polygons.empty())
     {
       throw std::logic_error("empty vector");
@@ -33,11 +38,18 @@ double rav::AccumulateArea::operator()(const std::string& subCommand)
       throw std::logic_error("invalid size");
     }
     using namespace std::placeholders;
-    Predicate vertexPredicate = std::bind(VertexNumPredicate{}, _1, number);
-    accumulateFunctor = std::bind(AreaSummator{}, 0.0, _1, vertexPredicate);
+    filter = std::bind(VertexNumPredicate{}, _1, number);
   }
+  const std::vector< Polygon >* filtered = &polygons;
+  std::vector< Polygon > temp;
+  if (filter != nullptr)
+  {
+    std::copy_if(polygons.cbegin(), polygons.cend(), std::back_inserter(temp), filter);
+    filtered = &temp;
+  }
+
   std::vector< double > areas;
-  std::transform(polygons.cbegin(), polygons.cend(), std::back_inserter(areas), accumulateFunctor);
+  std::transform(filtered->cbegin(), filtered->cend(), std::back_inserter(areas), accumulateFunctor);
   return std::accumulate(areas.cbegin(), areas.cend(), 0.0);
 }
 
@@ -65,11 +77,8 @@ bool rav::VertexNumPredicate::operator()(const Polygon& polygon, size_t vertexCo
   return polygon.points.size() == vertexCount;
 }
 
-double rav::AreaSummator::operator()(double area, const Polygon& polygon, Predicate pred)
+double rav::AreaSummator::operator()(double area, const Polygon& polygon)
 {
-  if (pred(polygon))
-  {
-    area += getArea(polygon);
-  }
+  area += getArea(polygon);
   return area;
 }
