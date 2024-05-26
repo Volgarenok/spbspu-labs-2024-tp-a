197b5a339a8e45d42f8162a2fbaca4e0c4c08a50
exxiiint <filipp.sivkov@yandex.ru>
2024-05-26 20:03:30 +0300

    fixxxxxx

--- sivkov.filipp/F0/cmd.hpp
+++ sivkov.filipp/F0/cmd.hpp
@@ -7,18 +7,19 @@
 
 namespace sivkov
 {
-  void add_word(std::map< std::string, std::map< std::string, std::string > >& treeOfdic, std::istream& in);
-  void add_translation(std::map< std::string, std::map< std::string, std::string > >& treeOfdic, std::istream& in);
-  void create_dictionary(std::map< std::string, std::map< std::string, std::string > >& treeOfdic, std::istream& in);
-  void remove_word(std::map< std::string, std::map< std::string, std::string > >& treeOfdic, std::istream& in);
-  void list_words(const std::map< std::string, std::map< std::string, std::string > >& treeOfdic, std::istream& in, std::ostream& out);
-  void count_words(const std::map< std::string, std::map< std::string, std::string > >& treeOfdic, std::istream& in, std::ostream& out);
-  void search_words(const std::map< std::string, std::map< std::string, std::string > >& treeOfdic, std::istream& in, std::ostream& out);
-  void delete_dictionary(std::map< std::string, std::map< std::string, std::string > >& treeOfdic, std::istream& in);
-  void rename_dictionary(std::map< std::string, std::map< std::string, std::string > >& treeOfdic, std::istream& in);
-  void merge_dictionaries(std::map< std::string, std::map< std::string, std::string > >& treeOfdic, std::istream& in);
-  void repeating_words(const std::map<std::string, std::map<std::string, std::string>>& treeOfdic, std::istream& in, std::ostream& out);
-  void save(const std::map< std::string, std::map< std::string, std::string > >& treeOfdic, const std::string& filename);
+  using map = std::map< std::string, std::map< std::string, std::string > >;
+  void add_word(map& treeOfdic, std::istream& in);
+  void add_translation(map& treeOfdic, std::istream& in);
+  void create_dictionary(map& treeOfdic, std::istream& in);
+  void remove_word(map& treeOfdic, std::istream& in);
+  void list_words(const map& treeOfdic, std::istream& in, std::ostream& out);
+  void count_words(const map& treeOfdic, std::istream& in, std::ostream& out);
+  void search_words(const map& treeOfdic, std::istream& in, std::ostream& out);
+  void delete_dictionary(map& treeOfdic, std::istream& in);
+  void rename_dictionary(map& treeOfdic, std::istream& in);
+  void merge_dictionaries(map& treeOfdic, std::istream& in);
+  void repeating_words(const map& treeOfdic, std::istream& in, std::ostream& out);
+  void save(const map& treeOfdic, const std::string& filename);
   void help(std::ostream& out);
 }
 
