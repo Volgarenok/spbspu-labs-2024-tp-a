0331460c240c277272b7bf7e0e8a66840d43c239
sonia66666 <147131510+sonia66666@users.noreply.github.com>
2024-05-27 16:31:15 +0300

    added removeDict command

--- belokurskaya.sofia/F0/commands.cpp
+++ belokurskaya.sofia/F0/commands.cpp
@@ -15,10 +15,30 @@ void belokurskaya::cmd::createDict(std::vector< EngRusDict >& vector, std::istre
   vector.push_back(newErd);
 }
 
+void belokurskaya::cmd::removeDict(std::vector< EngRusDict >& vector, std::istream& in)
+{
+  std::string name;
+  in >> name;
+  vector.erase(vector.begin() + subcmd::findIndexDict(vector, name));
+}
+
 bool belokurskaya::cmd::subcmd::containsEngRusDict(std::vector< EngRusDict >& vector, std::string name)
 {
   using namespace std::placeholders;
   return std::any_of(vector.begin(), vector.end(), std::bind(std::equal_to< std::string >(),
     std::bind(&EngRusDict::getName, _1), name)
   );
+}
+
+size_t belokurskaya::cmd::subcmd::findIndexDict(std::vector< EngRusDict >& vector, std::string name)
+{
+  using namespace std::placeholders;
+  auto it = std::find_if(vector.begin(), vector.end(), std::bind(std::equal_to< std::string >(),
+    std::bind(&belokurskaya::EngRusDict::getName, _1), name)
+  );
+  if (it != vector.end())
+  {
+    return std::distance(vector.begin(), it);
+  }
+  throw std::runtime_error("The dictionary with the specified name was not found");
 }
--- belokurskaya.sofia/F0/commands.hpp
+++ belokurskaya.sofia/F0/commands.hpp
@@ -10,10 +10,12 @@ namespace belokurskaya
   namespace cmd
   {
     void createDict(std::vector< EngRusDict >& vector, std::istream& in);
+    void removeDict(std::vector< EngRusDict >& vector, std::istream& in);
 
     namespace subcmd
     {
       bool containsEngRusDict(std::vector< EngRusDict >& vector, std::string name);
+      size_t findIndexDict(std::vector< EngRusDict >& vector, std::string name);
     }
   }
 }
