e0f9e761a1863764f9f86901858a2565621b2bcc
magggottt <gladyshevmatvey@gmail.com>
2024-05-27 07:22:36 +0700

    obolo4ka areas

--- gladyshev.matvey/T3/commands.cpp
+++ gladyshev.matvey/T3/commands.cpp
@@ -1,26 +1,62 @@
 #include "commands.hpp"
 
-gladyshev::findAreas(std::istream& in, const std::vector< Polygon > polys)
+#include <map>
+#include <functional>
+#include <string>
+
+void gladyshev::findAreas(std::istream& in, const std::vector< Polygon > polys)
+{
+  std::map < std::string, std::function < double(const std::vector< Polygon >&) > > cmds;
+  std::string command = "";
+  double sum = 0;
+  cmds["EVEN"] = findEven;
+  cmds["ODD"] = findOdd;
+  cmds["MEAN"] = findMean;
+  in >> command;
+  try
+  {
+    sum = cmds.at(command)(polys);
+  }
+  catch
+  {
+    size_t num = std::stoull(command);
+    sum = findAreaCount(polys, num);
+  }
+  std::cout << sum;
+}
+void gladyshev::findMax(std::istream& in, const std::vector< Polygon > polys)
 {
 
 }
-gladyshev::findMax(std::istream& in, const std::vector< Polygon > polys)
+void gladdyshev::findMin(std::istream& in, const std::vector< Polygon > polys)
 {
 
 }
-gladdyshev::findMin(std::istream& in, const std::vector< Polygon > polys)
+void gladdyshev::processCount(std::istream& in, const std::vector< Polygon > polys)
 {
 
 }
-gladdyshev::processCount(std::istream& in, const std::vector< Polygon > polys)
+void gladyshev::findLessArea(std::istream& in, const std::vector< Polygon > polys)
 {
 
 }
-gladyshev::findLessArea(std::istream& in, const std::vector< Polygon > polys)
+void gladyshev::processEcho(std::istream& in, const std::vector< Polygon > polys);
 {
 
 }
-gladyshev::processEcho(std::istream& in, const std::vector< Polygon > polys);
+double gladyshev::findEven(const std::vector< Polygon >& polys)
+{
+
+}
+double gladyshev::findOdd(const std::vector< Polygon >& polys)
+{
+
+}
+double gladyshev::findMean(const std::vector< Polygon >& polys)
+{
+
+}
+double gladyshev::findAreaCount(const std::vector< Polygon >& polys, size_t n)
 {
 
 }
--- gladyshev.matvey/T3/commands.hpp
+++ gladyshev.matvey/T3/commands.hpp
@@ -7,12 +7,16 @@
 
 namespace gladyshev
 {
-  findAreas(std::istream& in, const std::vector< Polygon > polys);
-  findMax(std::istream& in, const std::vector< Polygon > polys);
-  findMin(std::istream& in, const std::vector< Polygon > polys);
-  processCount(std::istream& in, const std::vector< Polygon > polys);
-  findLessArea(std::istream& in, const std::vector< Polygon > polys);
-  processEcho(std::istream& in, const std::vector< Polygon > polys);
+  void findAreas(std::istream& in, const std::vector< Polygon > polys);
+  void findMax(std::istream& in, const std::vector< Polygon > polys);
+  void findMin(std::istream& in, const std::vector< Polygon > polys);
+  void processCount(std::istream& in, const std::vector< Polygon > polys);
+  void findLessArea(std::istream& in, const std::vector< Polygon > polys);
+  void processEcho(std::istream& in, const std::vector< Polygon > polys);
+  double findEven(const std::vector< Polygon >& polys);
+  double findOdd(const std::vector< Polygon >& polys);
+  double findMean(const std::vector< Polygon >& polys);
+  double findAreaCount(const std::vector< Polygon >& polys, size_t n);
 }
 
 #endif
