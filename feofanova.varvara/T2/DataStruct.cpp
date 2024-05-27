afff829f864d8d2312c9e27fe805e505150053f4
root <root@DESKTOP-HVA9M9A>
2024-05-27 23:40:47 +0300

    less files

--- feofanova.varvara/T2/DataStruct.cpp
+++ feofanova.varvara/T2/DataStruct.cpp
@@ -4,6 +4,78 @@
 #include "Delimeter.hpp"
 #include "formats.hpp"
 
+std::istream& feofanova::operator>>(std::istream& in, LitI&& dest)
+{
+  std::istream::sentry guard(in);
+  if (!guard)
+  {
+    return in;
+  }
+  return in >> dest.value >> IgnoreCaseDelimeter{ "d" };
+}
+
+std::istream& feofanova::operator>>(std::istream& in, BinI&& dest)
+{
+  std::istream::sentry sentry(in);
+  if (!sentry)
+  {
+    return in;
+  }
+  using Delimeter = Delimeter_i;
+  in >> Delimeter{ '0' } >> Delimeter{ 'b' };
+  if (in)
+  {
+    char binary[64]{};
+    char c;
+    size_t i = 0;
+    while (in.get(c))
+    {
+      if (std::isdigit(c))
+      {
+        binary[i] = c;
+        ++i;
+      }
+      else
+      {
+        in.putback(c);
+        break;
+      }
+    }
+    dest.value = std::stoull(binary, nullptr, 2);
+  }
+  return in;
+}
+
+
+std::istream& feofanova::operator>>(std::istream& in, StringI&& dest)
+{
+  std::istream::sentry guard(in);
+  if (!guard)
+  {
+    return in;
+  }
+  using Delimeter = Delimeter_i;
+  in >> Delimeter{ '\"' };
+  std::getline(in, dest.str, '\"');
+  return in;
+}
+
+std::ostream& feofanova::operator<<(std::ostream& out, BinO&& src)
+{
+  std::ostream::sentry guard(out);
+  if (!guard)
+  {
+    return out;
+  }
+  std::string binary = "";
+  if (src.value)
+  {
+    binary = std::bitset<64>(src.value).to_string();
+    binary.erase(0, binary.find_first_not_of('0'));
+  }
+  return out << ('0' + binary);
+}
+
 std::istream& feofanova::operator>>(std::istream& in, DataStruct& data)
 {
   std::istream::sentry guard(in);
--- feofanova.varvara/T2/DataStruct.hpp
+++ feofanova.varvara/T2/DataStruct.hpp
@@ -7,6 +7,30 @@
 
 namespace feofanova
 {
+  struct LitI
+  {
+        double& value;
+  };
+  std::istream& operator>>(std::istream& in, LitI&& num);
+
+  struct BinI
+  {
+        unsigned long long& value;
+  };
+  std::istream& operator>>(std::istream& in, BinI&& num);
+
+  struct BinO
+  {
+    unsigned long long value;
+  };
+  std::ostream& operator<<(std::ostream& out, BinO&& src);
+
+  struct StringI
+  {
+        std::string& str;
+  };
+  std::istream& operator>>(std::istream& in, StringI&& num);
+
   struct DataStruct
   {
     double key1;
