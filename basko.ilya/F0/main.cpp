a54ea1c2dd4ec6f613c36060f60e8866eb2a6cec
IlyaBasko <baskoila200@gmail.com>
2024-05-27 21:10:10 +0300

    Added show_help

--- basko.ilya/F0/main.cpp
+++ basko.ilya/F0/main.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
 
+void show_help()
+{
+  std::cout << "Список доступных команд" << endl;
+  std::cout << "load [filename] - Загрузка файла" << endl;
+  std::cout << "insert [word][number] - Добавление, вводим слово и номер строки" << endl;
+  std::cout << "delete [word] - Удаление" << endl;
+  std::cout << "search [word] - Поиск" << endl;
+  std::cout << "show - Вывод" << endl;
+  std::cout << "exit - Выход" << endl;
+}
+
 int main()
 {
   return 0;
