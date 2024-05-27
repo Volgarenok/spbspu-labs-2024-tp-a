6427db819fd0e901c9effc8d4280f3eec25674d2
IlyaBasko <baskoila200@gmail.com>
2024-05-27 21:19:04 +0300

    Upgrade show_help

--- basko.ilya/F0/main.cpp
+++ basko.ilya/F0/main.cpp
@@ -2,13 +2,21 @@
 
 void show_help()
 {
-  std::cout << "Список доступных команд" << endl;
-  std::cout << "load [filename] - Загрузка файла" << endl;
-  std::cout << "insert [word][number] - Добавление, вводим слово и номер строки" << endl;
-  std::cout << "delete [word] - Удаление" << endl;
-  std::cout << "search [word] - Поиск" << endl;
-  std::cout << "show - Вывод" << endl;
-  std::cout << "exit - Выход" << endl;
+  std::cout << "Commands: " << endl;
+  std::cout << "load [filename]" << endl;
+  std::cout << "add <dict> <key> - <lines>" << endl;
+  std::cout << "top <dict>" << endl;
+  std::cout << "remove <dict> <key>" << endl;
+  std::cout << "merge <combDict> <dict1> <dict2>" << endl;
+  std::cout << "countwords <dict>" << endl;
+  std::cout << "clear <dict>" << endl;
+  std::cout << "search <dict> <key>" << endl;
+  std::cout << "matchWords <dict>" << endl;
+  std::cout << "createDict <dict> <filename>" << endl;
+  std::cout << "getAllEntries <dict>" << endl;
+  std::cout << "intersection <dict> <dict2>" << endl;
+  std::cout << "show" << endl;
+  std::cout << "exit" << endl;
 }
 
 int main()
