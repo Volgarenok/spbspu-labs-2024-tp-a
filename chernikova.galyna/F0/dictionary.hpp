4fc236fa51577f04f6e9237f84d12b6c7a8d11fb
chernikovaG <chernikova.gv@edu.spbstu.ru>
2024-05-27 23:35:46 +0300

    Added class Dictionary

--- chernikova.galyna/F0/dictionary.hpp
+++ chernikova.galyna/F0/dictionary.hpp
@@ -1,5 +1,20 @@
 #ifndef DICTIONARY_H
 #define DICTIONARY_H
 
+#include <unordered_map>
+#include <string>
+#include <set>
+
+namespace chernikova
+{
+  class Dictionary
+  {
+  private:
+    using map = std::unordered_map <std::string, std::set<std::string>>;
+    map data_;
+  };
+
+}
+
 #endif
 
