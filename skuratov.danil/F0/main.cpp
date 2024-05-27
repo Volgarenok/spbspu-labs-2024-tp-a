6556a2f5a826db6f8b1a7609a85770de02b0cfd1
Skuratov09 <skyratovdaniil28@gmail.com>
2024-05-27 17:03:06 +0300

    I count the frequency of symbol repetitions

--- skuratov.danil/F0/main.cpp
+++ skuratov.danil/F0/main.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
+#include <map>
+#include <vector>
+#include <list>
 
-int main()
+template < typename T >
+class Node
 {
-	std::cout << "Initial commit for F0";
+public:
+  int a;
+  Node< T >* left;
+  Node< T >* right;
+};
+
+int main(int argc, char* argv[])
+{
+  std::string s = "it is my striiiiing!!!!";
+  std::map< char, int > m;
+
+  for (int i = 0; i < s.length(); i++)
+  {
+    char c = s[i];
+    m[c]++;
+  }
+
+  std::map< char, int >::iterator i;
+
+  for (i = m.begin(); i != m.end(); ++i)
+  {
+    std::cout << i->first << ":" << i->second << '\n';
+  }
+  return 0;
 }
