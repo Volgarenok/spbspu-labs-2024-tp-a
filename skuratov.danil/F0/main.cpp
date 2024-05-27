e6fb12f393e142224e1a7913a68ad8c852b3e33d
Skuratov09 <skyratovdaniil28@gmail.com>
2024-05-27 18:12:12 +0300

    Wrote the construction of a tree from elements

--- skuratov.danil/F0/main.cpp
+++ skuratov.danil/F0/main.cpp
@@ -2,14 +2,33 @@
 #include <map>
 #include <vector>
 #include <list>
+#include <cstddef>
 
-template < typename T >
 class Node
 {
 public:
-  int a;
-  Node< T >* left;
-  Node< T >* right;
+  int a = {};
+  char c = {};
+  Node* left = {};
+  Node* right = {};
+
+  Node()
+  {}
+
+  Node(Node* L, Node* R)
+  {
+    left = L;
+    right = R;
+    a = L->a + R->a;
+  }
+};
+
+struct MyCompare
+{
+  bool operator()(Node* l, Node* r) const
+  {
+    return l->a < r->a;
+  }
 };
 
 int main(int argc, char* argv[])
@@ -23,11 +42,31 @@ int main(int argc, char* argv[])
     m[c]++;
   }
 
-  std::map< char, int >::iterator i;
+  std::list< Node* > t;
 
-  for (i = m.begin(); i != m.end(); ++i)
+  std::map< char, int >::iterator ii;
+
+  for (ii = m.begin(); ii != m.end(); ++ii)
   {
-    std::cout << i->first << ":" << i->second << '\n';
+    Node* p = new Node;
+    p->c = ii->first;
+    p->a = ii->second;
+    t.push_back(p);
   }
+
+  while (t.size() != 1)
+  {
+    t.sort(MyCompare());
+
+    Node* SonL = t.front();
+    t.pop_front();
+    Node* SonR = t.front();
+    t.pop_front();
+
+    Node* parent = new Node(SonL, SonR);
+    t.push_back(parent);
+  }
+  Node* root = t.front();
+
   return 0;
 }
