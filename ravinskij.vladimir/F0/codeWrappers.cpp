bfc52f521f54a4594d470d830b39be03fce756ee
ravinskij.vladimir <ravunskij0412@gmail.com>
2024-05-28 16:48:55 +0300

    Fixed codeWrappers

--- ravinskij.vladimir/F0/codeWrappers.cpp
+++ ravinskij.vladimir/F0/codeWrappers.cpp
@@ -1,5 +1,6 @@
 #include "codeWrappers.hpp"
 #include <iostream>
+#include <iterator>
 #include <scopeGuard.hpp>
 #include <delimeters.hpp>
 
@@ -11,33 +12,35 @@ std::ostream& rav::operator<<(std::ostream& out, rav::WriteWrapper&& wrapper)
     {
         return out;
     }
-    out << wrapper.symbol << '-';
-    for (bool bit: wrapper.code)
-    {
-        out << bit;
-    }
-    out << ' ' << wrapper.frequency;
+    out << '"' <<  wrapper.symbol << '"' << '-';
+    using output_it_t = std::ostream_iterator< bool >;
+    std::copy(wrapper.code.cbegin(), wrapper.code.cend(), output_it_t{ out, "" });
+    out << '-' << wrapper.frequency;
     return out;
 }
 
-std::istream& rav::operator>>(std::istream& in, rav::ReadWrapper&& wrapper)
+std::istream &rav::operator>>(std::istream& in, rav::ReadWrapper&& wrapper)
 {
-    std::istream::sentry sentry(in);
-    if (!sentry)
-    {
-        return in;
-    }
-    wrapper.symbol = in.get();
-    in >> CharDelimeter{'-'};
-    char bit = 0;
-    in >> std::noskipws;
-    while (bit != '\n')
-    {
-        in >> bit;
-        wrapper.code.push_back(static_cast< bool >(bit - '0'));
-    }
-    wrapper.code.pop_back();
-    in >> wrapper.frequency;
-    in >> std::skipws;
+  std::istream::sentry sentry(in);
+  if (!sentry)
+  {
     return in;
+  }
+  using del = CharDelimeter;
+  in >> std::noskipws;
+  in >> del{'"'} >> wrapper.symbol >> del{'"'} >> del{'-'};
+  if (!in)
+  {
+    return in;
+  }
+  char bit = 0;
+  while (in && bit != '-')
+  {
+    in >> bit;
+    wrapper.code.push_back(static_cast<bool>(bit - '0'));
+  }
+  wrapper.code.pop_back();
+  in >> wrapper.frequency;
+  in >> std::skipws;
+  return in;
 }
