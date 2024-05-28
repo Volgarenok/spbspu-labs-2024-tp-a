d3bdb2bf4582a0d92c2b8ddb326bc708a44a8f6f
chernikovaG <chernikova.gv@edu.spbstu.ru>
2024-05-28 23:29:58 +0300

    Added readData

--- chernikova.galyna/F0/commands.cpp
+++ chernikova.galyna/F0/commands.cpp
@@ -1,4 +1,8 @@
 #include "commands.hpp"
+#include <functional>
+#include <fstream>
+#include <Delimiter.hpp>
+#include "interface.hpp"
 
 void chernikova::doCommand(std::map< std::string, Dictionary >& dataBase, const std::string& command)
 {
@@ -7,5 +11,24 @@ void chernikova::doCommand(std::map< std::string, Dictionary >& dataBase, const 
 
 void chernikova::readData(std::map< std::string, Dictionary >& dataBase, std::istream& input)
 {
+  std::string fileName = "";
+  std::string dictionaryName = "";
 
+  input >> fileName >> PunctuationI{ ' ' } >> dictionaryName >> PunctuationI{ '\n' };
+
+  if (!input)
+  {
+    handleError();
+    return;
+  }
+
+  std::ifstream file(fileName);
+
+  if (!file.is_open())
+  {
+    printError();
+    return;
+  }
+
+  dataBase[dictionaryName].read(file);
 }
--- chernikova.galyna/F0/commands.hpp
+++ chernikova.galyna/F0/commands.hpp
@@ -1,6 +1,7 @@
 #ifndef COMMANDS_HPP
 #define COMMANDS_HPP
 
+#include <iostream>
 #include <map>
 #include <string>
 #include "Dictionary.hpp"
