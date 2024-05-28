dd5f012b778531863d1a2bb52a3ed2cb89d18eda
chernikovaG <chernikova.gv@edu.spbstu.ru>
2024-05-29 00:05:00 +0300

    Added arguements in main

--- chernikova.galyna/F0/main.cpp
+++ chernikova.galyna/F0/main.cpp
@@ -5,12 +5,17 @@
 #include "Dictionary.hpp"
 #include "interface.hpp"
 #include "commands.hpp"
+#include "arguements.hpp"
 
-int main()
+int main(int argc, char* argv[])
 {
   using namespace chernikova;
 
   std::map< std::string, Dictionary > dataBase = {};
+
+  if (!processArguements(argv, argc, dataBase))
+    return 0;
+
   std::string command = "";
 
   while (!std::cin.eof())
