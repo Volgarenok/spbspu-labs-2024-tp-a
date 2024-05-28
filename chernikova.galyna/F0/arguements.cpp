defc86f98b5e7e1bd403314e8f8e555cf08de8ee
chernikovaG <chernikova.gv@edu.spbstu.ru>
2024-05-28 23:53:56 +0300

    Added fillDictionary

--- chernikova.galyna/F0/arguements.cpp
+++ chernikova.galyna/F0/arguements.cpp
@@ -4,3 +4,19 @@ bool chernikova::processArguements(char* argv[], int argc, std::map< std::string
 {
 
 }
+
+void chernikova::fillDictionary(std::map< std::string, Dictionary >& dataBase)
+{
+  std::ifstream file("dictionary");
+
+  if (!file.is_open())
+  {
+    return;
+  }
+
+  Dictionary dictionary;
+  if (dictionary.read(file))
+  {
+    dataBase["dictionary"] = dictionary;
+  }
+}
--- chernikova.galyna/F0/arguements.hpp
+++ chernikova.galyna/F0/arguements.hpp
@@ -3,12 +3,15 @@
 
 #include <map>
 #include <string>
+#include <fstream>
 #include "interface.hpp"
 #include "Dictionary.hpp"
 
 namespace chernikova
 {
   bool processArguements(char* argv[], int argc, std::map< std::string, Dictionary >& dataBase);
+
+  void fillDictionary(std::map< std::string, Dictionary >& dataBase);
 }
 
 #endif
