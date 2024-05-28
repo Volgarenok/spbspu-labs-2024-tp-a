f5d42cea6906986a84901c82c7610ddee9e3bc05
kirillnamestnikov <kir.namestnikov@gmail.com>
2024-05-28 20:12:05 +0300

    fix bind

--- namestnikov.kirill/F0/main.cpp
+++ namestnikov.kirill/F0/main.cpp
@@ -34,21 +34,21 @@ int main(int argc, char * argv[])
     return 1;
   }
   doHelp(std::cout);
-  std::unordered_map< std::string, std::function< void(std::istream &, dictMain &) > > commands;
+  std::unordered_map< std::string, std::function< void(std::istream &) > > commands;
   {
     using namespace std::placeholders;
-    commands["add"] = std::bind(doAdd, _1, _2, std::ref(std::cout));
-    commands["find"] = std::bind(doFind, _1, _2, std::ref(std::cout));
-    commands["postfix"] = doPostfix;
-    commands["remove"] = std::bind(doRemove, _1, _2, std::ref(std::cout));
-    commands["unique"] = std::bind(doUnique, _1, _2, std::ref(std::cout));
-    commands["merge"] = std::bind(doMerge, _1, _2, std::ref(std::cout));
-    commands["export"] = doExport;
-    commands["palindrome"] = std::bind(doPalindrome, _1, _2, std::ref(std::cout));
-    commands["import"] = doImport;
-    commands["prefix"] = doPrefix;
-    commands["create"] = std::bind(doCreate, _1, _2, std::ref(std::cout));
-    commands["suffix"] = doSuffix;
+    commands["add"] = std::bind(doAdd, _1, std::ref(myMap), std::ref(std::cout));
+    commands["find"] = std::bind(doFind, _1, std::ref(myMap), std::ref(std::cout));
+    commands["postfix"] = std::bind(doPostfix, _1, std::ref(myMap));
+    commands["remove"] = std::bind(doRemove, _1, std::ref(myMap), std::ref(std::cout));
+    commands["unique"] = std::bind(doUnique, _1, std::ref(myMap), std::ref(std::cout));
+    commands["merge"] = std::bind(doMerge, _1, std::ref(myMap), std::ref(std::cout));
+    commands["export"] = std::bind(doExport, _1, std::cref(myMap));
+    commands["palindrome"] = std::bind(doPalindrome, _1, std::ref(myMap), std::ref(std::cout));
+    commands["import"] = std::bind(doImport, _1, std::ref(myMap));
+    commands["prefix"] = std::bind(doPrefix, _1, std::ref(myMap));
+    commands["create"] = std::bind(doCreate, _1, std::ref(myMap), std::ref(std::cout));
+    commands["suffix"] = std::bind(doSuffix, _1, std::ref(myMap));
   }
 
   std::string commandName = "";
@@ -56,7 +56,7 @@ int main(int argc, char * argv[])
   {
     try
     {
-      commands.at(commandName)(std::cin, myMap);
+      commands.at(commandName)(std::cin);
     }
     catch (const std::out_of_range &)
     {
