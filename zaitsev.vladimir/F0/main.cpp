2a47576b230db010f9d2b7af9edfeff7533acc6f
v-zaycev <70699650+v-zaycev@users.noreply.github.com>
2024-05-28 21:12:12 +0300

    Added map of commands

--- zaitsev.vladimir/F0/main.cpp
+++ zaitsev.vladimir/F0/main.cpp
@@ -1,14 +1,15 @@
 #define _SILENCE_EXPERIMENTAL_FILESYSTEM_DEPRECATION_WARNING
 #include <iostream>
 #include <fstream>
+#include <functional>
 #include <iomanip>
 #include <string>
 #include <map>
 #include <string.h>
-#include <experimental/filesystem>
 #include <delimiter.hpp>
 #include <stream_guard.hpp>
 #include "i_o_processing.hpp"
+#include "path_search.hpp"
 
 using unit_t = std::map< std::string, int >;
 using graph_t = std::map< std::string, unit_t >;
@@ -58,5 +59,22 @@ int main(int argc, char** argv)
   {
     std::cerr << e.what() << '\n';
   }
-
+  std::map< std::string, std::function< void(std::istream&, std::ostream& out)> > commands;
+  {
+    using namespace std::placeholders;
+    commands["read"] = std::bind(read_graph, std::ref(graphs), _1, _2);
+    commands["write"] = std::bind(write_graph, std::cref(graphs), _1, _2);
+    commands["list_of_graphs"] = std::bind(list_of_graphs, std::cref(graphs), _1, _2);
+    commands["print "] = std::bind(print_graph, std::cref(graphs), _1, _2);
+    commands["create_graph"] = std::bind(create_graph, std::ref(graphs), _1, _2);
+    commands["delete_graph"] = std::bind(delete_graph, std::ref(graphs), _1, _2);
+    commands["add_vertex"] = std::bind(add_vertex, std::ref(graphs), _1, _2);
+    commands["add_edge"] = std::bind(add_edge, std::ref(graphs), _1, _2);
+    commands["merge"] = std::bind(merge_graphs, std::ref(graphs), _1, _2);
+    commands["negative_weight_cycles"] = std::bind(check_negative_weight_cycles, std::cref(graphs), _1, _2);
+    commands["shortest_distance"] = std::bind(shortest_path, std::cref(graphs), _1, _2);
+    commands["shortest_trace"] = std::bind(shortest_path_trace, std::cref(graphs), _1, _2);
+    commands["shortest_path_matrix"] = std::bind(shortest_paths_matrix, std::cref(graphs), _1, _2);
+    commands["dump"] = std::bind(dump, std::ref(graphs), _1, _2);
+  }
  }
