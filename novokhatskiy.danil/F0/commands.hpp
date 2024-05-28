7a521b05a57f9f1f2fdf7b2deffb80f23f89fc62
DanilNovokhatskiy <karuk.vova2005@mail.ru>
2024-05-28 20:19:14 +0300

    Insert and print

--- novokhatskiy.danil/F0/commands.hpp
+++ novokhatskiy.danil/F0/commands.hpp
@@ -4,25 +4,27 @@
 #include <map>
 #include <utility>
 #include <string>
-#include <list>
+#include <set>
 #include <iostream>
+#include "dictionary.hpp"
 
 namespace novokhatskiy
 {
-  using val_t = std::pair< std::string, std::list< std::string > >;
+  using val_t = std::pair< std::string, std::set< std::string > >;
   using mainDict = std::map< std::string, val_t >;
-  mainDict unique(mainDict& dict1, mainDict& dict2);
-  mainDict search(mainDict& res, mainDict& dict1, mainDict& dict2, std::istream& in);
+  using dictionaries = std::map< std::string,  Dictionary >;
+  /*void unique(dictionaries& dict, std::istream& in);
+  void search(mainDict& res, mainDict& dict1, mainDict& dict2, std::istream& in);
   void random(mainDict& res, size_t count, mainDict& dict1, mainDict& dict2);
   void save(const mainDict& dict, const std::string& name);
   void editTranslation(mainDict& dict, std::istream& in);
   void editExample(mainDict& dict, std::istream& in);
-  mainDict merge(mainDict& dict1, mainDict& dict2);
-  void printHelp();
+  void merge(mainDict& dict1, mainDict& dict2);
   void deleteWord(mainDict& dict, const std::string& key);
-  void find(const mainDict& dict, const std::string& key, std::ostream& out);
-  void insert(mainDict& dict, std::istream& in);
-  void print(const mainDict& dict, std::ostream& out);
+  void find(const mainDict& dict, const std::string& key, std::ostream& out);*/
+  void insert(dictionaries& dict, std::istream& in);
+  void printHelp();
+  void print(const dictionaries& dict, std::istream& in, std::ostream& out);
 }
 
 #endif
