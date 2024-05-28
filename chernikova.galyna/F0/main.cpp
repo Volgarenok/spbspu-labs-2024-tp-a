919ed2520f77e6977871d4518253bb84218eed33
chernikovaG <chernikova.gv@edu.spbstu.ru>
2024-05-28 23:45:45 +0300

    Added reading

--- chernikova.galyna/F0/main.cpp
+++ chernikova.galyna/F0/main.cpp
@@ -1,2 +1,41 @@
+#include <iostream>
+#include <map>
+#include <string>
+#include "Dictionary.hpp"
+#include "interface.hpp"
+#include "commands.hpp"
+
 int main()
-{}
+{
+  using namespace chernikova;
+
+  std::map< std::string, Dictionary > dataBase = {};
+  std::string command = "";
+  
+  while (!std::cin.eof())
+  {
+    std::cin >> command;
+
+    if (std::cin.eof())
+    {
+      break;
+    }
+
+    if (!std::cin)
+    {
+      handleError();
+      continue;
+    }
+
+    std::cin >> PunctuationI{ ' ' };
+
+    if (!std::cin)
+    {
+      handleError();
+      continue;
+    }
+
+    doCommand(dataBase, command);
+  }
+  return 0;
+}
