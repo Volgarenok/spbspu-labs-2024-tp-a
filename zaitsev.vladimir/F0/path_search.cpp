c9361e1bbadc99f43119873be698bed737a83553
v-zaycev <70699650+v-zaycev@users.noreply.github.com>
2024-05-28 17:55:04 +0300

    Added negative weight cycle check

--- zaitsev.vladimir/F0/path_search.cpp
+++ zaitsev.vladimir/F0/path_search.cpp
@@ -14,112 +14,11 @@ struct edge
   size_t b;
   int val;
 };
-
-std::vector<std::vector<int>> create_adjacency_matrix(const zaitsev::graph_t& graph)
-{
-  using namespace zaitsev;
-  std::vector<std::vector<int>> matrix(graph.size(), std::vector<int>(graph.size(), inf));
-
-  std::unordered_map< std::string, size_t > vert_indexes(graph.size());
-  size_t index = 0;
-  for (graph_t::const_iterator i = graph.begin(); i != graph.end(); ++i, ++index)
-  {
-    vert_indexes[i->first] = index;
-  }
-
-  size_t i = 0;
-  for (graph_t::const_iterator it_i = graph.begin(); it_i != graph.end(); ++it_i)
-  {
-    for (unit_t::const_iterator it_j = it_i->second.begin(); it_j != it_i->second.end(); ++it_j)
-    {
-      matrix[i][vert_indexes[it_j->first]] = it_j->second;
-    }
-    ++i;
-  }
-  return matrix;
-}
-
-std::vector< edge > extract_edges(const zaitsev::graph_t& graph)
-{
-  using namespace zaitsev;
-
-  std::unordered_map< std::string, size_t > vert_indexes(graph.size());
-  size_t edges_nmb = 0;
-  size_t index = 0;
-  for (graph_t::const_iterator i = graph.begin(); i != graph.end(); ++i, ++index)
-  {
-    edges_nmb += i->second.size();
-    vert_indexes[i->first] = index;
-  }
-  std::vector< edge > edges_list(edges_nmb);
-  size_t i = 0, k = 0;
-  for (graph_t::const_iterator it_i = graph.begin(); it_i != graph.end(); ++it_i)
-  {
-    for (unit_t::const_iterator it_j = it_i->second.begin(); it_j != it_i->second.end(); ++it_j)
-    {
-      edges_list[k] = { i, vert_indexes[it_j->first], it_j->second };
-    }
-    ++i;
-  }
-  return edges_list;
-}
-
-std::vector< std::vector< int > > calc_paths_floyd(const std::vector< std::vector< int > >& matrix)
-{
-  std::vector<std::vector<int>> dist(matrix.size(), std::vector<int>(matrix.size(), inf));
-  for (size_t k = 0; k < matrix.size(); ++k)
-  {
-    for (size_t i = 0; i < matrix.size(); ++i)
-    {
-      for (size_t j = 0; j < matrix.size(); ++j)
-      {
-        if (dist[i][k] < inf && dist[k][j] < inf)
-        {
-          dist[i][j] = std::min(dist[i][j], dist[i][k] + dist[k][j]);
-        }
-      }
-    }
-  }
-  return dist;
-}
-
-std::pair<std::vector< int >, std::vector< int>  > calc_paths_ford(const std::vector< edge >& edges, size_t begin, size_t vert_nmb)
-{
-  std::vector<int> dist(vert_nmb, inf);
-  dist[begin] = 0;
-  std::vector<int> prev(vert_nmb, -1);
-  bool changed = true;
-  size_t phase_nmb = 0;
-  while (changed && phase_nmb < vert_nmb + 1)
-  {
-    for (size_t j = 0; j < edges.size(); ++j)
-      if (dist[edges[j].a] < inf)
-        if (dist[edges[j].b] > dist[edges[j].a] + edges[j].val)
-        {
-          dist[edges[j].b] = dist[edges[j].a] + edges[j].val;
-          prev[edges[j].b] = edges[j].a;
-          changed = true;
-        }
-  }
-  if (phase_nmb == vert_nmb + 1)
-  {
-    std::fill(dist.begin(), dist.end(), inf);
-  }
-
-  return { dist, prev };
- /* if (d[t] == inf)
-    cout << "No path from " << v << " to " << t << ".";
-  else {
-    std::vector<int> path;
-    for (int cur = t; cur != -1; cur = p[cur])
-      path.push_back(cur);
-    reverse(path.begin(), path.end());
-
-    cout << "Path from " << v << " to " << t << ": ";
-    for (size_t i = 0; i < path.size(); ++i)
-      cout << path[i] << ' ';
-  }*/
-}
+using std::vector;
+vector<vector<int>> create_adjacency_matrix(const zaitsev::graph_t& graph);
+vector< edge > extract_edges(const zaitsev::graph_t& graph);
+vector< vector< int > > calc_paths_floyd(const vector< vector< int > >& matrix);
+std::pair<vector< int >, vector< size_t >  > calc_paths_ford(const vector< edge >& edges, size_t begin, size_t vert_nmb);
 
 void zaitsev::shortest_paths_matrix(std::istream& in, std::ostream& out, const base_t& graphs)
 {
@@ -130,8 +29,8 @@ void zaitsev::shortest_paths_matrix(std::istream& in, std::ostream& out, const b
   {
     throw std::invalid_argument("Graph with name \"" + graph_name + "\", doesn't exists.");
   }
-  std::vector< std::vector< int > > adj_matrix = create_adjacency_matrix(it->second);
-  std::vector< std::vector< int > > distances = calc_paths_floyd(adj_matrix);
+  vector< vector< int > > adj_matrix = create_adjacency_matrix(it->second);
+  vector< vector< int > > distances = calc_paths_floyd(adj_matrix);
 
 
   size_t max_int_len = std::to_string(std::numeric_limits< int >::lowest()).size();
@@ -146,7 +45,7 @@ void zaitsev::shortest_paths_matrix(std::istream& in, std::ostream& out, const b
 
 
 
-  std::vector< size_t > names_length;
+  vector< size_t > names_length;
   std::transform(it->second.begin(), it->second.end(), std::back_inserter(names_length), get_len);
   size_t names_column_width = *(std::max_element(names_length.begin(), names_length.end()));
   std::replace_if(names_length.begin(), names_length.end(), need_to_extend, max_int_len);
@@ -176,4 +75,133 @@ void zaitsev::shortest_paths_matrix(std::istream& in, std::ostream& out, const b
     ++ii;
   }
   return;
+}
+
+void zaitsev::check_negative_weight_cycles(std::istream& in, std::ostream& out, const base_t& graphs)
+{
+  std::string graph_name;
+  in >> graph_name;
+  base_t::const_iterator it = graphs.find(graph_name);
+  if (it == graphs.end())
+  {
+    throw std::invalid_argument("Graph with name \"" + graph_name + "\", doesn't exists.");
+  }
+  vector< vector< int > > adj_matrix = create_adjacency_matrix(it->second);
+  vector< vector< int > > distances = calc_paths_floyd(adj_matrix);
+  bool negative_cycles = false;
+  for (size_t i = 0; i < distances.size(); ++i)
+  {
+    if (distances[i][i] < 0)
+    {
+      negative_cycles = true;
+      break;
+    }
+  }
+  out << "Graph " << (negative_cycles ? "contains" : "doesn't contain") << " a negative weight cycle\n";
+}
+
+vector<vector<int>> create_adjacency_matrix(const zaitsev::graph_t& graph)
+{
+  using namespace zaitsev;
+  vector<vector<int>> matrix(graph.size(), vector<int>(graph.size(), inf));
+
+  std::unordered_map< std::string, size_t > vert_indexes(graph.size());
+  size_t index = 0;
+  for (graph_t::const_iterator i = graph.begin(); i != graph.end(); ++i, ++index)
+  {
+    vert_indexes[i->first] = index;
+  }
+
+  size_t i = 0;
+  for (graph_t::const_iterator it_i = graph.begin(); it_i != graph.end(); ++it_i)
+  {
+    for (unit_t::const_iterator it_j = it_i->second.begin(); it_j != it_i->second.end(); ++it_j)
+    {
+      matrix[i][vert_indexes[it_j->first]] = it_j->second;
+    }
+    ++i;
+  }
+  return matrix;
+}
+
+vector< edge > extract_edges(const zaitsev::graph_t& graph)
+{
+  using namespace zaitsev;
+
+  std::unordered_map< std::string, size_t > vert_indexes(graph.size());
+  size_t edges_nmb = 0;
+  size_t index = 0;
+  for (graph_t::const_iterator i = graph.begin(); i != graph.end(); ++i, ++index)
+  {
+    edges_nmb += i->second.size();
+    vert_indexes[i->first] = index;
+  }
+  vector< edge > edges_list(edges_nmb);
+  size_t i = 0, k = 0;
+  for (graph_t::const_iterator it_i = graph.begin(); it_i != graph.end(); ++it_i)
+  {
+    for (unit_t::const_iterator it_j = it_i->second.begin(); it_j != it_i->second.end(); ++it_j)
+    {
+      edges_list[k] = { i, vert_indexes[it_j->first], it_j->second };
+    }
+    ++i;
+  }
+  return edges_list;
+}
+
+std::pair<vector< int >, vector< size_t >  > calc_paths_ford(const vector< edge >& edges, size_t begin, size_t vert_nmb)
+{
+  vector< int > dist(vert_nmb, inf);
+  dist[begin] = 0;
+  vector< size_t > prev(vert_nmb, vert_nmb + 1);
+  bool changed = true;
+  size_t phase_nmb = 0;
+  while (changed && phase_nmb < vert_nmb + 1)
+  {
+    for (size_t j = 0; j < edges.size(); ++j)
+      if (dist[edges[j].a] < inf)
+        if (dist[edges[j].b] > dist[edges[j].a] + edges[j].val)
+        {
+          dist[edges[j].b] = dist[edges[j].a] + edges[j].val;
+          prev[edges[j].b] = edges[j].a;
+          changed = true;
+        }
+  }
+  if (phase_nmb == vert_nmb + 1)
+  {
+    std::fill(dist.begin(), dist.end(), inf);
+  }
+
+  return { dist, prev };
+  /* if (d[t] == inf)
+     cout << "No path from " << v << " to " << t << ".";
+   else {
+     vector<int> path;
+     for (int cur = t; cur != -1; cur = p[cur])
+       path.push_back(cur);
+     reverse(path.begin(), path.end());
+
+     cout << "Path from " << v << " to " << t << ": ";
+     for (size_t i = 0; i < path.size(); ++i)
+       cout << path[i] << ' ';
+   }*/
+}
+
+vector< vector< int > > calc_paths_floyd(const vector< vector< int > >& matrix)
+{
+  vector<vector<int>> dist(matrix.size(), vector<int>(matrix.size(), inf));
+  for (size_t k = 0; k < matrix.size(); ++k)
+  {
+    for (size_t i = 0; i < matrix.size(); ++i)
+    {
+      for (size_t j = 0; j < matrix.size(); ++j)
+      {
+        if (dist[i][k] < inf && dist[k][j] < inf)
+        {
+          dist[i][j] = std::min(dist[i][j], dist[i][k] + dist[k][j]);
+        }
+      }
+    }
+  }
+  return dist;
 }
