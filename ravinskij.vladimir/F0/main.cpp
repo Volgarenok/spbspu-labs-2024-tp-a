a70fdaf48a65dd72f165dbdde31b8846d9dd2263
ravinskij.vladimir <ravunskij0412@gmail.com>
2024-05-28 22:43:07 +0300

    fix CG

--- ravinskij.vladimir/F0/main.cpp
+++ ravinskij.vladimir/F0/main.cpp
@@ -28,23 +28,21 @@ int main(int argc, char* argv[])
 
   using Command = std::function< void(std::istream &) >;
   std::unordered_map< std::string, Command > commands;
-  {
-    using namespace std::placeholders;
-    commands["add-text"] = std::bind(ravinskij::addText, _1, std::ref(files));
-    commands["save-text"] = std::bind(ravinskij::saveText, _1, std::ref(files));
-    commands["delete-text"] = std::bind(ravinskij::deleteText, _1, std::ref(files));
-    commands["print-text"] = std::bind(ravinskij::printText, _1, std::ref(std::cout), std::cref(files));
-    commands["create-encoding"] = std::bind(ravinskij::createEncoding, _1, std::ref(table), std::ref(traverses), std::cref(files));
-    commands["delete-encoding"] = std::bind(ravinskij::deleteEncoding, _1, std::ref(table), std::ref(traverses));
-    commands["encode"] = std::bind(ravinskij::encode, _1, std::cref(table), std::ref(files));
-    commands["decode"] = std::bind(ravinskij::decode, _1, std::cref(traverses), std::ref(files));
-    commands["add-encoding"] = std::bind(ravinskij::addEncoding, _1, std::ref(table), std::ref(traverses));
-    commands["save-encoding"] = std::bind(ravinskij::saveEncoding, _1, std::cref(table), std::cref(traverses));
-    commands["compare-encodings"] = std::bind(ravinskij::compareEncodings, _1, std::ref(std::cout), std::cref(files), std::cref(table));
-    commands["print-files"] = std::bind(ravinskij::printFiles, _1, std::ref(std::cout), std::cref(files));
-    commands["print-texts"] = std::bind(ravinskij::printTexts, _1, std::ref(std::cout), std::cref(files));
-    commands["print-all"] = std::bind(ravinskij::printAll, _1, std::ref(std::cout), std::cref(files));
-  }
+  using namespace std::placeholders;
+  commands["add-text"] = std::bind(ravinskij::addText, _1, std::ref(files));
+  commands["save-text"] = std::bind(ravinskij::saveText, _1, std::ref(files));
+  commands["delete-text"] = std::bind(ravinskij::deleteText, _1, std::ref(files));
+  commands["print-text"] = std::bind(ravinskij::printText, _1, std::ref(std::cout), std::cref(files));
+  commands["create-encoding"] = std::bind(ravinskij::createEncoding, _1, std::ref(table), std::ref(traverses), std::cref(files));
+  commands["delete-encoding"] = std::bind(ravinskij::deleteEncoding, _1, std::ref(table), std::ref(traverses));
+  commands["encode"] = std::bind(ravinskij::encode, _1, std::cref(table), std::ref(files));
+  commands["decode"] = std::bind(ravinskij::decode, _1, std::cref(traverses), std::ref(files));
+  commands["add-encoding"] = std::bind(ravinskij::addEncoding, _1, std::ref(table), std::ref(traverses));
+  commands["save-encoding"] = std::bind(ravinskij::saveEncoding, _1, std::cref(table), std::cref(traverses));
+  commands["compare-encodings"] = std::bind(ravinskij::compareEncodings, _1, std::ref(std::cout), std::cref(files), std::cref(table));
+  commands["print-files"] = std::bind(ravinskij::printFiles, _1, std::ref(std::cout), std::cref(files));
+  commands["print-texts"] = std::bind(ravinskij::printTexts, _1, std::ref(std::cout), std::cref(files));
+  commands["print-all"] = std::bind(ravinskij::printAll, _1, std::ref(std::cout), std::cref(files));
 
   std::string cmd;
 
