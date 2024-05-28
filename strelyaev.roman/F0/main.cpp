db6f9ff30a531e85a2db1e25773c8751c42687a2
Strelyaev R. <roman.strelyaeff@gmail.com>
2024-05-28 15:06:02 +0300

    add command

--- strelyaev.roman/F0/main.cpp
+++ strelyaev.roman/F0/main.cpp
@@ -1,2 +1,47 @@
+#include <map>
+#include <iostream>
+#include <functional>
+#include <string>
+#include <vector>
+#include <stdexcept>
+
+void addDictionary(std::istream& in, std::map< std::string, std::map< std::string, std::vector< std::string > > >& dictionaries)
+{
+  std::string name = "";
+  in >> name;
+  std::map< std::string, std::vector< std::string > > new_dict;
+  try
+  {
+    dictionaries.at(name);
+  }
+  catch (...)
+  {
+    dictionaries[name] = new_dict;
+    return;
+  }
+  throw std::logic_error("<ALREADY EXISTS>");
+}
+
+void addWord(std::istream& in, std::map< std::string, std::map< std::string, std::vector< std::string > > >& dictionaries)
+{
+  std::string name = "";
+  in >> name;
+  std::map< std::string, std::vector< std::string > > needed_dict = dictionaries.at(name);
+}
+
 int main()
-{}
+{
+  std::map< std::string, std::map< std::string, std::vector< std::string > > > dictionaries;
+  
+  while (!std::cin.eof())
+  {
+    try
+    {
+      addDictionary(std::cin, dictionaries);
+    }
+    catch (const std::exception& e)
+    {
+      std::cerr << e.what() << '\n';
+    }
+  }
+}
