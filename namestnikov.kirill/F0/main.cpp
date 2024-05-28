f24582ab7c64fdbc55858e11daf365532361ad8d
kirillnamestnikov <kir.namestnikov@gmail.com>
2024-05-28 19:15:51 +0300

    fixes

--- namestnikov.kirill/F0/main.cpp
+++ namestnikov.kirill/F0/main.cpp
@@ -11,13 +11,9 @@
 
 int main(int argc, char * argv[])
 {
-  using pairWords = std::pair< std::string, std::string >;
   using dictMain = std::unordered_map< std::string, std::unordered_map< std::string, std::string > >;
   using namespace namestnikov;
-
-  std::ifstream in("base.txt");
-  std::unordered_map< std::string, std::unordered_map< std::string, std::string > > myMap;
-  std::cout << argc;
+  dictMain myMap;
   try
   {
     for (int i = 1; i < argc; ++i)
@@ -37,36 +33,7 @@ int main(int argc, char * argv[])
     std::cerr << "Error while trying to read dicts\n";
     return 1;
   }
-  std::cout << myMap["input.txt"]["tree"];
-  std::unordered_map< std::string, std::string > tempMap;
-  std::unordered_map< std::string, std::string > tempMap2;
-  tempMap.insert({"cat", "кошка"});
-  tempMap.insert({"dog", "собака"});
-  tempMap2.insert({"me", "мне"});
-  tempMap2.insert({"I", "я"});
-  tempMap2.insert({"dog", "собака"});
-  tempMap2.insert({"god", "бог"});
-  tempMap2.insert({"melon", "дыня"});
-  tempMap2.insert({"baloon", "шар"});
-
-  //myMap.insert({"first", tempMap});
-  myMap.insert({"second", tempMap2});
-  /*for (auto itr = myMap["first"].begin(); itr != myMap["first"].end(); itr++) { 
-        std::cout << itr->first 
-             << '\t' << itr->second << '\n'; 
-  }*/
-  std::cout << myMap.size();
-  //doImport(std::cin, std::cout, myMap);
-  /*doCreate(std::cin, std::cout, myMap);
-  doUnique(std::cin, std::cout, myMap);
-  doFind(std::cin, std::cout, myMap);*/
-  //std::cout << myMap["first"].size();
-  for (auto itr = myMap["second"].begin(); itr != myMap["second"].end(); itr++) { 
-        std::cout << itr->first 
-             << '\t' << itr->second << '\n'; 
-  }
   doHelp(std::cout);
-  //doPalindrome(std::cin, std::cout, myMap);
   std::unordered_map< std::string, std::function< void(std::istream &, dictMain &, std::ostream &) > > commands;
   {
     using namespace std::placeholders;
@@ -102,10 +69,4 @@ int main(int argc, char * argv[])
     std::cin.clear();
     std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
   }
-  //commands.at("palindrome")(std::cin, myMap, std::cout);
-  //doImport(std::cin, std::cout, myMap);
-  //std::cout << myMap["first"]["dog"];
-  //std::cout << myMap["first"].size();
-  //doSuffix(std::cin, std::cout, myMap);
-  //std::cout << myMap["res"]["melon"];
 }
