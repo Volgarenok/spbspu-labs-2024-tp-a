1cdbbbed1c892869280e558dfd5afb95450fdeaf
Strelyaev R. <roman.strelyaeff@gmail.com>
2024-05-28 19:46:26 +0300

    translate, remove and new cmds

--- strelyaev.roman/F0/main.cpp
+++ strelyaev.roman/F0/main.cpp
@@ -3,6 +3,9 @@
 #include <functional>
 #include <string>
 #include <vector>
+#include <limits>
+#include <algorithm>
+#include <iterator>
 #include <stdexcept>
 
 void addDictionary(std::istream& in, std::map< std::string, std::map< std::string, std::vector< std::string > > >& dictionaries)
@@ -22,26 +25,94 @@ void addDictionary(std::istream& in, std::map< std::string, std::map< std::strin
   throw std::logic_error("<ALREADY EXISTS>");
 }
 
+void deleteDictionary(std::istream& in, std::map< std::string, std::map< std::string, std::vector< std::string > > >& dictionaries)
+{
+  std::string name = "";
+  in >> name;
+  try
+  {
+    dictionaries.at(name);
+  }
+  catch (...)
+  {
+    throw std::logic_error("<BOOK NOT FOUND>");
+  }
+  dictionaries.erase(name);
+}
+
 void addWord(std::istream& in, std::map< std::string, std::map< std::string, std::vector< std::string > > >& dictionaries)
 {
   std::string name = "";
   in >> name;
-  std::map< std::string, std::vector< std::string > > needed_dict = dictionaries.at(name);
+  std::map< std::string, std::vector< std::string > > needed_dict;
+  try
+  {
+    needed_dict = dictionaries.at(name);
+  }
+  catch (...)
+  {
+    throw std::logic_error("<BOOK NOT FOUND>");
+  }
+  std::string word = "";
+  std::string translation = "";
+  in >> word >> translation;
+  needed_dict[word].push_back(translation);
+  dictionaries[name] = needed_dict;
+}
+
+void translate(std::ostream& out, std::istream& in, const std::map< std::string, std::map< std::string, std::vector< std::string > > >& dictionaries)
+{
+  std::string name = "";
+  in >> name;
+  std::map< std::string, std::vector< std::string > > needed_dict;
+  try
+  {
+    needed_dict = dictionaries.at(name);
+  }
+  catch (...)
+  {
+    throw std::logic_error("<BOOK NOT FOUND>");
+  }
+  std::string eng_word = "";
+  in >> eng_word;
+  std::vector< std::string > translations;
+  try
+  {
+    translations = needed_dict.at(eng_word);
+  }
+  catch (...)
+  {
+    throw std::logic_error("<WORD NOT FOUND>");
+  }
+  std::copy(translations.cbegin(), translations.cend(), std::ostream_iterator< std::string >(out, " "));
+  out << "\n";
+  return;
 }
 
 int main()
 {
+  std::setlocale(LC_ALL, "Russian");
   std::map< std::string, std::map< std::string, std::vector< std::string > > > dictionaries;
-  
+  using cmd_func = std::function< void() >;
+  std::map< std::string, cmd_func > cmds;
+  {
+    cmds["new"] = std::bind(addDictionary, std::ref(std::cin), std::ref(dictionaries));
+    cmds["delete"] = std::bind(deleteDictionary, std::ref(std::cin), std::ref(dictionaries));
+    cmds["add"] = std::bind(addWord, std::ref(std::cin), std::ref(dictionaries));
+    cmds["translate"] = std::bind(translate, std::ref(std::cout), std::ref(std::cin), std::ref(dictionaries));
+  }
   while (!std::cin.eof())
   {
     try
     {
-      addDictionary(std::cin, dictionaries);
+      std::string cmd_name = "";
+      std::cin >> cmd_name;
+      cmds.at(cmd_name)();
     }
-    catch (const std::exception& e)
+    catch(const std::exception& e)
     {
       std::cerr << e.what() << '\n';
     }
+    
   }
 }
