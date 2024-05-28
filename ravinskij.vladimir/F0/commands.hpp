c29076ab062c69cde0c6c7215eb11d6c0f0cbf74
ravinskij.vladimir <ravunskij0412@gmail.com>
2024-05-28 16:51:31 +0300

    some refactor of commands.hpp

--- ravinskij.vladimir/F0/commands.hpp
+++ ravinskij.vladimir/F0/commands.hpp
@@ -9,17 +9,18 @@
 
 namespace ravinskij
 {
-  void printHelp();
-
   using traverserTable = std::map< std::string, std::list< nodePtr > >;
   using fileTable = std::map< std::string, std::string >;
+  using encodeMap = std::map< char, std::vector<bool > >;
+  using encodesTable = std::map< std::string, encodeMap >;
+
+  void printHelp();
+
   void addText(std::istream&, fileTable&);
   void saveText(std::istream&, fileTable&);
   void deleteText(std::istream&, fileTable&);
   void printText(std::istream&, std::ostream&, const fileTable&);
 
-  using encodeMap = std::map< char, std::vector<bool > >;
-  using encodesTable = std::map< std::string, encodeMap >;
   void createEncoding(std::istream&, encodesTable&, traverserTable&, const fileTable&);
   void deleteEncoding(std::istream&, encodesTable&, traverserTable&);
   void encode(std::istream&, const encodesTable&, fileTable&);
