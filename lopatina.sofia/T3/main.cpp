07542db0540f725f0bfc3e18f175886df2faabc1
SofiyLop <lopatina.sa@edu.spbstu.ru>
2024-05-29 20:25:11 +0300

    Read file

--- lopatina.sofia/T3/main.cpp
+++ lopatina.sofia/T3/main.cpp
@@ -1,2 +1,30 @@
-int main()
-{}
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+struct Point
+{
+  int x, y;
+};
+
+struct Polygon
+{
+  std::vector<Point> points;
+};
+
+int main(int argc, char ** argv)
+{
+  if (argc < 2)
+  {
+    std::cerr << "Not enough arguments\n";
+    return 1;
+  }
+  std::ifstream input(argv[1]);
+  if (!input)
+  {
+    std::cerr << "No such file\n";
+    return 1;
+  }
+
+  return 0;
+}
