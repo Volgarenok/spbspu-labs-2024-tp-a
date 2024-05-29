2707924aa976f10c847057485bb402e7d2bd1cfc
chernikovaG <chernikova.gv@edu.spbstu.ru>
2024-05-30 00:44:41 +0300

    Fixed main

--- chernikova.galyna/F0/main.cpp
+++ chernikova.galyna/F0/main.cpp
@@ -1,11 +1,9 @@
 #include <iostream>
 #include <map>
 #include <string>
-#include <delimiter.hpp>
-#include <interface.hpp>
-#include "dictionary.hpp"
 #include "commands.hpp"
 #include "arguements.hpp"
+#include "dictionary.hpp"
 
 int main(int argc, char* argv[])
 {
@@ -16,32 +14,13 @@ int main(int argc, char* argv[])
   if (!processArguements(argv, argc, dataBase))
     return 0;
 
-  std::string command;
-
   while (!std::cin.eof())
   {
-    std::cin >> command;
-
-    if (std::cin.eof())
+    if (!doCommand(dataBase))
     {
       break;
     }
-
-    if (!std::cin)
-    {
-      handleError();
-      continue;
-    }
-
-    std::cin >> PunctuationI{ ' ' };
-
-    if (!std::cin)
-    {
-      handleError();
-      continue;
-    }
-
-    doCommand(dataBase, command);
   }
+
   return 0;
 }
