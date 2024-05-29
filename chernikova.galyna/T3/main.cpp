718a9bff412cd8eab3ec6dd74433a5826f2eed0e
chernikovaG <chernikova.gv@edu.spbstu.ru>
2024-05-29 19:57:15 +0300

    Fixed main.cpp

--- chernikova.galyna/T3/main.cpp
+++ chernikova.galyna/T3/main.cpp
@@ -2,59 +2,47 @@
 #include <fstream>
 #include <vector>
 #include <iterator>
-#include <limits>
-#include <deque>
 #include <exception>
-
+#include <interface.hpp>
 #include "polygon.hpp"
 #include "commands.hpp"
 
 int main(int argc, char* argv[])
 {
+  using namespace chernikova;
+
   if (argc != 2)
   {
     std::cout << "Error argument\n";
     return 1;
   }
+
   std::ifstream input(argv[1]);
   if (!input.is_open())
   {
     std::cout << "Invalid file\n";
     return 1;
   }
-  std::vector< chernikova::Polygon > data;
+
+  std::vector< Polygon > data;
+
   while (!input.eof())
   {
     if (!input)
     {
-      input.clear();
-      input.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+      fixStream(input);
     }
-    using iter = std::istream_iterator< chernikova::Polygon >;
+    using iter = std::istream_iterator< Polygon >;
     std::copy(iter(input), iter(), std::back_inserter(data));
   }
-  chernikova::Commands dict_with_cmd;
+
   while (!std::cin.eof())
   {
-    try
-    {
-      std::string command = chernikova::inputCommand(std::cin);
-      dict_with_cmd.doCommand(data, dict_with_cmd, command, std::cout, std::cin);
-    }
-    catch (const std::logic_error& e)
-    {
-      std::cout << e.what();
-      std::cin.clear();
-    }
-    catch (const std::runtime_error& e)
+    if (!doCommand(data, std::cout, std::cin))
     {
       break;
     }
-    if (!std::cin)
-    {
-      std::cout << "<INVALID COMMAND>\n";
-      std::cin.clear();
-      std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
-    }
   }
+
+  return 0;
 }
