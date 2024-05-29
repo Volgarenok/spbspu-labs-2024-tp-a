86ea1f71832e0d1fa4662f2f93d56fed73da98a3
chernikovaG <chernikova.gv@edu.spbstu.ru>
2024-05-29 22:08:36 +0300

    Added forceInsertWords forceInsertLine

--- chernikova.galyna/F0/commands.hpp
+++ chernikova.galyna/F0/commands.hpp
@@ -13,11 +13,13 @@ namespace chernikova
     std::map< std::string, std::function< void(std::map< std::string, Dictionary >&, std::istream&) > >;
 
   Commands initializeCommands();
-  void doCommand(std::map< std::string, Dictionary >& dataBase, const std::string& command);
+  bool doCommand(std::map< std::string, Dictionary >& dataBase);
   void readData(std::map< std::string, Dictionary >& dataBase, std::istream& input);
   void saveData(std::map< std::string, Dictionary >& dataBase, std::istream& input);
   void addDictionary(std::map< std::string, Dictionary >& dataBase, std::istream& input);
   void insertWord(std::map< std::string, Dictionary >& dataBase, std::istream& input);
+  void forceInsertWords(std::map< std::string, Dictionary >& dataBase, std::istream& input);
+  void forceInsertLine(std::map< std::string, Dictionary >& dataBase, std::istream& input);
   void deleteWord(std::map< std::string, Dictionary >& dataBase, std::istream& input);
   void print(std::map< std::string, Dictionary >& dataBase, std::istream& input);
   void editWord(std::map< std::string, Dictionary >& dataBase, std::istream& input);
