1deb3a2d05b519b24632cfd5ea8b939eda43bd19
sonia66666 <147131510+sonia66666@users.noreply.github.com>
2024-05-29 15:06:28 +0300

    update main

--- belokurskaya.sofia/F0/main.cpp
+++ belokurskaya.sofia/F0/main.cpp
@@ -10,11 +10,11 @@
 
 int main(int argc, char* argv[])
 {
- /* if (argc != 2)
+  if (argc != 2)
   {
     std::cerr << "Invalid command line argument\n";
     return 1;
-  }*/
+  }
 
   std::string inputFileName = argv[1];
 
