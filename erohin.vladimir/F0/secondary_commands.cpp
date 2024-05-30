90a5c42566b5ffc17ffa6c0ccb60fdc4d3c1b29b
WarriorVortex <erohinvova01010111@gmail.com>
2024-05-30 08:01:16 +0300

    Changed output of commands --help and --check

--- erohin.vladimir/F0/secondary_commands.cpp
+++ erohin.vladimir/F0/secondary_commands.cpp
@@ -1,17 +1,16 @@
 #include "secondary_commands.hpp"
 #include <iostream>
 #include <fstream>
-#include "format.hpp"
 #include "input_output_collection.hpp"
 
 void erohin::printHelp(std::ostream & output)
 {
   output << "Use: program [options] <freqdict> — starting with use a file <freqdict>\n";
-  output << "Creation and store a frequency dictionary based on the text.";
+  output << "Creation and storage a frequency dictionary based on the text. ";
   output << "The opportunity to change and view the contents of several frequency dictionaries.\n\n";
-  output << "Аргументы командной строки:\n";
-  output << "\t--help — \"помощь\" по программе\n";
-  output << "\t--check — check the data correctness\n";
+  output << "Command line arguments:\n";
+  output << "\t--help — show this help\n";
+  output << "\t--check — check file <freqdict> correctness\n";
   output << "\t--numformat=<format> — starting with use a file <freqdict> with style <format>\n";
   output << "\t<format> = { NUMBER, PROPORTION, FRACTIONAL, PERCANTAGE }\n\n";
   output << "Commands:\n";
@@ -36,32 +35,19 @@ void erohin::printHelp(std::ostream & output)
   output << "\t10. bottom <new_dict> <dict> <bot_number>\n";
   output << "\tCreate a frequency dictionary <new_dict> from the least frequent <top_number> words of <dict>.\n\n";
   output << "\t11. differ <new_dict> <dict1> <dict2>\n";
-  output << "\tCreate a frequency dictionary <new_dict> based on the difference between <dict1> and <dirt2>.\n\n";
+  output << "\tCreate a frequency dictionary <new_dict> based on the difference between <dict1> and <dict2>.\n\n";
   output << "\t12. unite <new_dict> <dict1> <dict2>\n";
-  output << "\tCreate a frequency dictionary <new_dict> based on the union between <dict1> and <dirt2>.\n\n";
+  output << "\tCreate a frequency dictionary <new_dict> based on the union between <dict1> and <dict2>.\n\n";
   output << "\t13. intersect <new_dict> <dict1> <dict2>\n";
-  output << "\tCreate a frequency dictionary <new_dict> based on the intersection between <dict1> and <dirt2>.\n\n";
+  output << "\tCreate a frequency dictionary <new_dict> based on the intersection between <dict1> and <dict2>.\n\n";
 }
 
 void erohin::checkDictionary(const char * file_name, std::ostream & output)
 {
   std::fstream file(file_name, std::ios::in);
   collection dict_context;
-  std::string dict_name;
-  file >> StringFormat{ dict_name, ':' };
-  while (!file.eof())
-  {
-    if (file.fail())
-    {
-      break;
-    }
-    inputDictionary(dict_context[dict_name], file);
-    if (!file)
-    {
-      file.clear();
-    }
-    file >> StringFormat{ dict_name, ':' };
-  }
+  inputCollection(dict_context, file);
   file.close();
-  output << "File contains " << (!file.eof() && file.fail() ? "incorrect" : "correct") << " frequency dictionaries.\n";
+  output << (!file.eof() && file.fail() ? "Incorrect" : "Correct") << " frequency dictionaries.\n";
 }
+
