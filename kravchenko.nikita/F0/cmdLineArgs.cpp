c072ae1272db0560d5319f699f1ba4a6f8fd5203
Glaz0k <nikitakravcenko892@gmail.com>
2024-05-30 20:52:49 +0300

    translated help text

--- kravchenko.nikita/F0/cmdLineArgs.cpp
+++ kravchenko.nikita/F0/cmdLineArgs.cpp
@@ -7,37 +7,34 @@
 
 void kravchenko::printHelp(std::ostream& out)
 {
-  out << "Система команд \"Частотный словарь\":\n";
+  out << "The \"Frequency Dictionary\" command system:\n";
   out << "1. scantext <dict> <text file>\n";
-  out << "Проанализировать текст из файла <text file> на частотность и добавить эти данные в словарь <dict>.\n";
+  out << "Analyze the text from the <text file> for frequency and add this data to the <dict> dictionary\n";
   out << "2. new <dict>\n";
-  out << "Создает пустой новый словарь <dict>.\n";
+  out << "Create an empty new <dict> dictionary\n";
   out << "3. list\n";
-  out << "Вывести через пробел названия всех доступных словарей.\n";
+  out << "Print the names of all available dictionaries separated by a space\n";
   out << "4. remove <dict>\n";
-  out << "Удалить словарь с именем <dict>.\n";
+  out << "Delete a dictionary named <dict>\n";
   out << "5. freq word <dict> <word>\n";
-  out << "Вывести частоту слова <word> в словаре <dict>.\n";
+  out << "Output the frequency of the <word> word in the <dict> dictionary\n";
   out << "6. freq least <dict> [n]\n";
-  out << "Вывести n самых редко встречающихся слов в словаре <dict> в порядке возрастания частоты. Если n не указан,\n";
-  out << "вывести 10 слов\n";
-  out << "7. freq most <dict> [<n>]\n";
-  out << "Вывести n самых часто встречающихся слов в словаре <dict> в порядке убывания частоты. Если n не указан,\n";
-  out << "вывести 10 слов.\n";
+  out << "Output [n] of the most rarely occurring words in the <dict> dictionary in ascending order of frequency\n";
+  out << "If [n] is not specified, output 10 words\n";
+  out << "7. freq most <dict> [n]\n";
+  out << "Output [n] of the most frequently occurring words in the <dict> dictionary in ascending order of frequency\n";
+  out << "If [n] is not specified, output 10 words\n";
   out << "8. save [<dict1> ... <dictn>]\n";
-  out << "Сохранить все доступные словари в соответствующие файлы в формате <dict>.txt при отсутствии дополнительных\n";
-  out << "параметров. Иначе сохранить таким образом только словари <dict1> ... <dictn>.\n";
+  out << "Save the dictionaries [<dict1> ... <dictn>] into appropriate files in <dict>.txt format\n";
+  out << "If dictionaries [<dict1> ... <dictn>] are not specified, save all available dictionaries in this way\n";
   out << "9. intersect <new dict> <dict1> <dict2> [… <dictn>]\n";
-  out << "Создать новый словарь <new dict> в виде пересечения словарей <dict1> и <dict2> … <dictn> (dict1 ∩ dict2 … ∩\n";
-  out << "dictn).\n";
+  out << "Create a new <new dict> dictionary as the intersection of the <dict1> and <dict2> ... <dictn> dictionaries\n";
   out << "10. union <new dict> <dict1> <dict2> [… <dictn>]\n";
-  out << "Создать новый словарь <new dict> в виде объединения словарей <dict1> и <dict2> … <dictn> (dict1 U dict2 … U\n";
-  out << "dictn).\n";
+  out << "Create a new <new dict> dictionary as a union of the <dict1> and <dict2> ... <dictn> dictionaries\n";
   out << "11. difference <new dict> <dict1> <dict2> [… <dictn>]\n";
-  out << "Создать новый словарь <new dict> в виде разности словаря <dict1> и <dict2> … <dictn> (dict1 \\ dict2 … \\ dictn).\n";
+  out << "Create a new dictionary <new dict> as a difference of dictionaries <dict1> and <dict2> ... <dictn>\n";
   out << "12. complement <new dict> <dict1> <dict2> [… <dictn>]\n";
-  out << "Создать новый словарь <new dict> в виде относительного дополнения словаря <dict1> по отношению к <dict2> …\n";
-  out << "<dictn>.\n";
+  out << "Create a new <new dict> dictionary as a relative complement of the <dict1> dictionary with respect to <dict2> ... <dictn>\n"
 }
 
 void kravchenko::loadDict(const std::string& name, DictionaryMap& data)
