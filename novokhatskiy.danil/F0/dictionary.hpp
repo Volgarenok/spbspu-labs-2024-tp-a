113d3834016ff4286b2d97f91812c7c6aa6bd952
DanilNovokhatskiy <karuk.vova2005@mail.ru>
2024-05-30 08:59:53 +0300

    CG

--- novokhatskiy.danil/F0/dictionary.hpp
+++ novokhatskiy.danil/F0/dictionary.hpp
@@ -8,26 +8,26 @@
 
 namespace novokhatskiy
 {
-	class Dictionary
-	{
-	public:
-		using val_t = std::pair< std::string, std::set< std::string > >;
-		Dictionary() = default;
-		~Dictionary() = default;
-		void addValue(Dictionary&& other);
-		val_t& getValue(const std::string& name);
-		std::map< std::string, val_t >& getDict();
-		void remove(const std::string& word);
-		void find(const std::string& word, std::ostream& out) const;
-		size_t size() const noexcept;
-		std::map< std::string, val_t > getDict() const;
-		friend std::istream& operator>>(std::istream& in, Dictionary& dict);
-		friend std::ostream& operator<<(std::ostream& out, const Dictionary& dict);
-	private:
-		std::map< std::string, val_t > dict_;
-	};
-	std::istream& operator>>(std::istream& in, Dictionary& dict);
-	std::ostream& operator<<(std::ostream& out, const Dictionary& dict);
+  class Dictionary
+  {
+  public:
+    using val_t = std::pair< std::string, std::set< std::string > >;
+    Dictionary() = default;
+    ~Dictionary() = default;
+    void addValue(Dictionary&& other);
+    val_t& getValue(const std::string& name);
+    std::map< std::string, val_t >& getDict();
+    void remove(const std::string& word);
+    void find(const std::string& word, std::ostream& out) const;
+    size_t size() const noexcept;
+    std::map< std::string, val_t > getDict() const;
+    friend std::istream& operator>>(std::istream& in, Dictionary& dict);
+    friend std::ostream& operator<<(std::ostream& out, const Dictionary& dict);
+  private:
+    std::map< std::string, val_t > dict_;
+  };
+  std::istream& operator>>(std::istream& in, Dictionary& dict);
+  std::ostream& operator<<(std::ostream& out, const Dictionary& dict);
 }
 
 #endif
