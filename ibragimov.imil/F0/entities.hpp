9bcec68843818e7b980687fef3a91211568a217e
StantStantov <ibragimovimil0@gmail.com>
2024-06-01 13:43:50 +0300

    [feat] Add input and output to Entities

--- ibragimov.imil/F0/entities.hpp
+++ ibragimov.imil/F0/entities.hpp
@@ -1,6 +1,7 @@
 #ifndef ENTITIES_HPP
 #define ENTITIES_HPP
 
+#include <istream>
 #include <map>
 #include <string>
 
@@ -9,31 +10,43 @@ namespace ibragimov
   struct Encodings
   {
     Encodings() = delete;
-    Encodings(const std::map< char, std::string >&);
+    explicit Encodings(const std::map< char, std::string >&);
+    Encodings(const Encodings&) = default;
+    Encodings& operator=(const Encodings&) = default;
     ~Encodings() = default;
 
     std::map< char, std::string > encodingTable;
   };
+  std::istream& operator>>(std::istream&, Encodings&);
+  std::ostream& operator<<(std::ostream&, const Encodings&);
   struct DecodedText
   {
     DecodedText() = delete;
-    DecodedText(const std::string&);
+    explicit DecodedText(const std::string&);
+    DecodedText(const DecodedText&) = default;
+    DecodedText& operator=(const DecodedText&) = default;
     ~DecodedText() = default;
 
     std::string text;
     size_t size;
     size_t bits;
   };
+  std::istream& operator>>(std::istream&, DecodedText&);
+  std::ostream& operator<<(std::ostream&, const DecodedText&);
   struct EncodedText
   {
     EncodedText() = delete;
-    EncodedText(const std::string&);
+    explicit EncodedText(const std::string&);
+    EncodedText(const EncodedText&) = default;
+    EncodedText& operator=(const EncodedText&) = default;
     ~EncodedText() = default;
 
     std::string text;
     size_t size;
     size_t bits;
   };
+  std::istream& operator>>(std::istream&, EncodedText&);
+  std::ostream& operator<<(std::ostream&, const EncodedText&);
 }
 
 #endif
