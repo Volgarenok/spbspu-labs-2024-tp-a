f984612b4c7324a1f66558a9490e0f605dfeb859
sahaki13 <hagiakinh13@gmail.com>
2024-07-14 00:17:33 +0700

    update and fix errors for T2

--- zhakinh.kha/T2/dataStruct.hpp
+++ zhakinh.kha/T2/dataStruct.hpp
@@ -3,18 +3,19 @@
 
 #include <iostream>
 #include <string>
+#include <complex>
 #include <utility>
 
 namespace zhakha {
-    struct DataStruct {
-        double key1;
-        std::pair<long long, unsigned long long> key2;
-        std::string key3;
-    };
+	struct DataStruct {
+		double key1 = 0.0;
+		std::pair<long long, unsigned long long> key2 = { 0, 0 };
+		std::string key3 = "";
+	};
 
-    bool operator<(const DataStruct& lhs, const DataStruct& rhs);
-    std::istream& operator>>(std::istream& in, DataStruct& data);
-    std::ostream& operator<<(std::ostream& out, const DataStruct& data);
+	bool operator<(const DataStruct& lhs, const DataStruct& rhs);
+	std::istream& operator>>(std::istream& in, DataStruct& data);
+	std::ostream& operator<<(std::ostream& out, const DataStruct& data);
 }
 
-#endif // DATASTRUCT_HPP
+#endif
--- zhakinh.kha/T2/streamGuard.hpp
+++ zhakinh.kha/T2/streamGuard.hpp
@@ -1,21 +1,21 @@
 #ifndef STREAMGUARD_HPP
 #define STREAMGUARD_HPP
 
-#include <ios>
+#include <iostream>
 
 namespace zhakha
 {
-    struct StreamGuard
-    {
-    public:
-        StreamGuard(std::basic_ios< char >& s);
-        ~StreamGuard();
+	struct StreamGuard
+	{
+	public:
+		StreamGuard(std::basic_ios< char >& s);
+		~StreamGuard();
 
-    private:
-        std::basic_ios< char >& s_;
-        std::streamsize precision_;
-        std::basic_ios< char >::fmtflags flags_;
-    };
+	private:
+		std::basic_ios< char >& s_;
+		std::streamsize precision_;
+		std::basic_ios< char >::fmtflags flags_;
+	};
 }
 #endif
 
