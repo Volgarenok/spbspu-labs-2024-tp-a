daf2e8e9f6e9a6e72516d50c2a67c3ecf57491aa
sahaki13 <hagiakinh13@gmail.com>
2024-07-14 09:27:26 +0700

    update files, the third final

--- zhakinh.kha/T2/main.cpp
+++ zhakinh.kha/T2/main.cpp
@@ -9,7 +9,7 @@ int main()
   using zhakha::DataStruct;
   using input_it = std::istream_iterator< DataStruct >;
   using output_it = std::ostream_iterator< DataStruct >;
-        std::vector< DataStruct > data;
+  std::vector< DataStruct > data;
   while (!std::cin.eof())
   {
 	std::copy(input_it{ std::cin }, input_it{}, std::back_inserter(data));
