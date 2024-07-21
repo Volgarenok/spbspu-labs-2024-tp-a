914da58fb84cff2e1ff050579c2e0fc1605b1809
AlisaLazarevapi8 <lazarevaalisa283@gmail.com>
2024-07-21 14:58:49 +0300

    fix

--- lazareva.alisa/T2/streamGuard.hpp
+++ lazareva.alisa/T2/streamGuard.hpp
@@ -2,20 +2,23 @@
 #define STREAMGUARD_HPP
 #include <ios>
 
-namespace lazareva
-{
-  struct StreamGuard
-  {
-    public:
-      StreamGuard(std::basic_ios< char >& s);
-      ~StreamGuard();
+namespace lazareva {
+  struct StreamGuard {
+  public:
+    explicit StreamGuard(std::basic_ios<char>& s) : s_(s), precision_(s.precision()), flags_(s.flags()) { } 
 
-    private:
-      std::basic_ios< char >& s_;
-      std::streamsize precision_;
-      std::basic_ios< char >::fmtflags flags_;
+    ~StreamGuard() { 
+      s_.precision(precision_);
+      s_.flags(flags_); 
+    }
+
+  private:
+    std::basic_ios<char>& s_;
+    std::streamsize precision_;
+    std::basic_ios<char>::fmtflags flags_;
   };
 }
 #endif
+
 
 
