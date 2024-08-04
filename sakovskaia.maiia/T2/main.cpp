a90bf91004f6a638e03909dbb76fd2dee7ed23f0
MayaSakovskaya <zloyejik05@gmail.com>
2024-08-04 22:56:51 +0300

    main fix

--- sakovskaia.maiia/T2/main.cpp
+++ sakovskaia.maiia/T2/main.cpp
@@ -9,16 +9,17 @@ using namespace sakovskaia;
 int main()
 {
   std::vector< DataStruct > data_struct;
+  using input_it = std::istream_iterator< DataStruct >;
+  using output_it = std::ostream_iterator< DataStruct >;
   while (!std::cin.eof())
   {
-    std::copy(std::istream_iterator< DataStruct >{std::cin}, std::istream_iterator< DataStruct >{}, std::back_inserter(data_struct));
+    std::copy(input_it{std::cin}, input_it{}, std::back_inserter(data_struct));
     if (!std::cin)
     {
       std::cin.clear();
       std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
-      break;
     }
   }
   std::sort(data_struct.begin(), data_struct.end());
-  std::copy(data_struct.cbegin(), data_struct.cend(), std::ostream_iterator< DataStruct >{std::cout, "\n"});
+  std::copy(data_struct.cbegin(), data_struct.cend(), output_it{std::cout, "\n"});
 }
