4f75c8c94d3c9d04d7aae60e8fa402957e8ed0d0
Rattlerutrap <dimaaupov2005@gmail.com>
2024-08-08 17:03:01 +0300

    INFRAME start(не оч)

--- ayupov.dmitry/T3/main.cpp
+++ ayupov.dmitry/T3/main.cpp
@@ -38,7 +38,7 @@ int main(int argc, char* argv[])
   cmds["MIN"] = std::bind(min, polygon, _1, _2);
   cmds["COUNT"] = std::bind(count, polygon, _1, _2);
   cmds["ECHO"] = std::bind(echo, polygon, _1, _2);
-  cmds["INFRAME"] = std::bind(inFrame, polygon, _1, _2);
+  //cmds["INFRAME"] = std::bind(inFrame, polygon, _1, _2);
   /*cmds["MAXSEQ"] = std::bind(maxSeq, polygon, _1, _2);
   cmds["LESSAREA"] = std::bind(lessArea, polygon, _1, _2);*/
   }
