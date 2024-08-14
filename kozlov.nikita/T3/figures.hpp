9595442c73b8cd1da919552574fb1d0559f6007d
iamneefee <neefee.mail@gmail.com>
2024-08-15 02:10:48 +0300

    added input for poly and point

--- kozlov.nikita/T3/figures.hpp
+++ kozlov.nikita/T3/figures.hpp
@@ -1,5 +1,6 @@
 #ifndef FIGURES_HPP
 #define FIGURES_HPP
+#include <iostream>
 #include <vector>
 
 namespace kozlov
@@ -8,10 +9,12 @@ namespace kozlov
   {
     int x, y;
   };
+  std::istream& operator>>(std::istream& in, Point& point);
 
   struct Polygon
   {
     std::vector< Point > points;
   };
+  std::istream& operator>>(std::istream& in, Polygon& poly);
 }
 #endif
