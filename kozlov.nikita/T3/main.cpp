6af2c4c81ecc4c91450c3d1ae0ffef447f252970
iamneefee <neefee.mail@gmail.com>
2024-08-15 01:40:46 +0300

    added reading from file

--- kozlov.nikita/T3/main.cpp
+++ kozlov.nikita/T3/main.cpp
@@ -1,2 +1,34 @@
-int main()
-{}
+#include <iostream>
+#include <vector>
+#include <iterator>
+#include <limits>
+#include <algorithm>
+#include "figures.hpp"
+
+using namespace kozlov;
+int main(int argc, const char* argv[])
+{
+  if (argc != 2)
+  {
+    std::cerr << "<INVALID ARGUMENTS>\n";
+    return 1;
+  }
+  std::ifstream file(argv[1]);
+  if (!file.is_open())
+  {
+    std::cerr << "<INVALID FILE>\n";
+    return 1;
+  }
+  std::vector< Polygon > polygons;
+  using inIterator = std::istream_iterator< Polygon >;
+  while (!file.eof())
+  {
+    std::copy(inIterator{file}, inIterator{}, std::back_inserter(polygons));
+    if (file.fail())
+    {
+      file.clear();
+      file.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+    }
+  }
+  file.close();
+}
