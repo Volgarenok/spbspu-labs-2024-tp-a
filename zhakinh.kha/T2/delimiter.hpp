f3adad864430c25dcbf030113440775c30545c3e
sahaki13 <hagiakinh13@gmail.com>
2024-08-14 23:28:34 +0700

    Update first try for T3

---
+++ zhakinh.kha/T2/delimiter.hpp
@@ -0,0 +1,15 @@
+#ifndef DELIMITER_HPP
+#define DELIMITER_HPP
+
+#include <iostream>
+
+namespace zhakha
+{
+  struct Delimiter
+  {
+   char expected;
+  };
+  std::istream& operator>>(std::istream& in, const Delimiter& exp);
+}
+
+#endif
