b7717cb42a9633fa63a564a3f7563fa01ea082fb
efeshka <rokie12312@gmail.com>
2024-08-17 21:03:26 +0300

    added cmds

--- altun.efe/T3/main.cpp
+++ altun.efe/T3/main.cpp
@@ -4,7 +4,11 @@
 #include <iterator>
 #include <algorithm>
 #include <limits>
+#include <string>
+#include <functional>
+#include <streamGuard.hpp>
 #include "polygon.hpp"
+#include "commands.hpp"
 
 int main(int argc, char **argv)
 {
@@ -33,4 +37,43 @@ int main(int argc, char **argv)
     }
   }
   file.close();
+
+  std::map< std::string, std::function< bool(const Polygon&) > > args;
+  {
+    using namespace std::placeholders;
+    args["EVEN"] = std::bind(std::equal_to< double >{}, std::bind(std::modulus< size_t >{}, std::bind(getSize, _1), 2), 0);
+    args["ODD"] = std::bind(std::not_equal_to< double >{}, std::bind(std::modulus< size_t >{}, std::bind(getSize, _1), 2), 0);
+    args["MEAN"] = std::bind(std::equal_to< int >{}, 1, 1);
+  }
+
+  std::map<std::string, std::function< bool(const Polygon&) > > args_count;
+  args_count["EVEN"] = args["EVEN"];
+  args_count["ODD"] = args["ODD"];
+
+  std::map< std::string, std::function< void(std::ostream&, std::istream&, const std::vector< Polygon >&) > > cmds;
+  {
+    using namespace std::placeholders;
+    cmds["COUNT"] = std::bind(count, _1, _2, _3, args_count);
+    cmds["AREA"] = std::bind(getArea, _1, _2, _3, args);
+    cmds["MAX"] = std::bind(findMax, _1, _2, _3);
+    cmds["MIN"] = std::bind(findMin, _1, _2, _3);
+    cmds["MAXSEQ"] = std::bind(getMaxSeq, _1, _2, _3);
+  }
+  std::string cmd_name = "";
+  while (std::cin >> cmd_name)
+  {
+    StreamGuard s_guard(std::cout);
+    try
+    {
+      cmds.at(cmd_name)(std::cout, std::cin, polygons);
+      std::cout << "\n";
+    }
+    catch (...)
+    {
+      std::cout << "<INVALID COMMAND>\n";
+    }
+    std::cin.clear();
+    std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+  }
+  return 0;
 }
