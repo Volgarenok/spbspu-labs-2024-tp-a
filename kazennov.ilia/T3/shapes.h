accbcaeda6bf8c17beeb7b3fbec4b421673b1223
kazunovia <iliakazennov@ya.ru>
2024-08-18 04:28:40 +0300

    fix cg

--- kazennov.ilia/T3/shapes.h
+++ kazennov.ilia/T3/shapes.h
@@ -25,7 +25,7 @@ namespace kazennov
     {
       if (p.x > other.x && p.y > other.y)
       {
-	return true;
+        return true;
       }
       return false;
     }
@@ -33,7 +33,7 @@ namespace kazennov
     {
       if (p.x < other.x && p.y < other.y)
       {
-	return true;
+        return true;
       }
       return false;
     }
