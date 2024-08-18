d18a675700182447750ab7e46802f7f0d1eba3a1
iamneefee <neefee.mail@gmail.com>
2024-08-18 20:01:51 +0300

    fix

--- kozlov.nikita/T3/commands.cpp
+++ kozlov.nikita/T3/commands.cpp
@@ -163,12 +163,8 @@ void kozlov::doCmdInframe(std::vector< Polygon >& poly, std::istream& in, std::o
   {
     throw std::logic_error("<WRONG TARGET POLYGON>");
   }
-  std::set< Point > uniquePoints(target.points.begin(), target.points.end());
-  if (uniquePoints.size() != target.points.size())
-  {
-    throw std::invalid_argument("<INVALID COMMAND>");
-  }
-  std::pair< Point, Point > frame = std::accumulate(poly.begin(), poly.end(), {{0, 0}, {0, 0}}, calcFrame);
+  std::pair< Point, Point > initialFrame = {{0, 0}, {0, 0}};
+  std::pair< Point, Point > frame = std::accumulate(poly.begin(), poly.end(), initialFrame, calcFrame);
   auto isPointInFrame = std::bind(checkPointInFrame, frame, std::placeholders::_1);
   bool isPointsInside = std::all_of(target.points.begin(), target.points.end(), isPointInFrame);
   out << (isPointsInside ? "<TRUE>" : "<FALSE>") << '\n';
