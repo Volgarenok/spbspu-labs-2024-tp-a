27540cf31fb70af9dbba0df75e4308e778d5a77c
DaniilPrstrz <Vyzanovdaniil@gmail.com>
2024-08-26 15:00:21 +0300

    add StreamGuard.hpp

---
+++ vyzhanov.daniil/T2/StreamGuard.hpp
@@ -0,0 +1,20 @@
+#ifndef STREAMGUARD_HPP
+#define STREAMGUARD_HPP
+
+#include <iostream>
+
+namespace vyzhanov
+{
+  class StreamGuard
+  {
+  public:
+    StreamGuard(std::basic_ios< char >&);
+    ~StreamGuard();
+  private:
+    std::basic_ios< char >& s_;
+    std::streamsize precision_;
+    std::basic_ios< char >::fmtflags flags_;
+  };
+}
+
+#endif
