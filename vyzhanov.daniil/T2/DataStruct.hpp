1cfc7b67739173a8b48bcde10c0f4c617a89a3df
DaniilPrstrz <Vyzanovdaniil@gmail.com>
2024-08-27 23:18:24 +0300

    забыл убрать

--- vyzhanov.daniil/T2/DataStruct.hpp
+++ vyzhanov.daniil/T2/DataStruct.hpp
@@ -8,7 +8,6 @@ namespace vyzhanov
 {
     struct DataStruct
     {
-        DataStruct(size_t key1, size_t key2, std::string key3);
         size_t key1_;
         size_t key2_;
         std::string key3_;
