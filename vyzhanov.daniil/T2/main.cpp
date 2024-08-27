bebbd52dc565cb2a92623cd325d6e59c845c6bb0
DaniilPrstrz <Vyzanovdaniil@gmail.com>
2024-08-27 23:08:30 +0300

    final?

--- vyzhanov.daniil/T2/main.cpp
+++ vyzhanov.daniil/T2/main.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+#include <limits>
+#include "DataStruct.hpp"
+
 
 int main()
 {
-  std::cout << "Oh shit, here we go again!";
+  using namespace vyzhanov;
+  std::vector< DataStruct > data;
+  while (!std::cin.eof())
+  {
+    std::copy(
+      std::istream_iterator< DataStruct >{ std::cin },
+      std::istream_iterator< DataStruct >{},
+      std::back_inserter(data));
+    if (!std::cin)
+    {
+      std::cin.clear();
+      std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+    }
+  }
+  std::sort(data.begin(), data.end());
+  std::copy(
+    data.cbegin(), data.cend(),
+    std::ostream_iterator< DataStruct >{ std::cout, "\n" }
+  );
+  return 0;
 }
