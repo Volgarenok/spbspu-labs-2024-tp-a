3aad3172709990ae8ad07ff86e9feb7d5f183c93
allaberdiev.ilnar <dedbedblack@gmail.com>
2024-08-28 14:03:46 +0300

    delimiter added

---
+++ allaberdiev.ilnar/T2/delimiter.cpp
@@ -0,0 +1,39 @@
+#include "delimiter.hpp"
+#include <string>
+#include <istream>
+
+std::istream& allaberdiev::operator>>(std::istream& in, Delimiter&& exp)
+{
+  std::istream::sentry guard(in);
+  if (!guard)
+  {
+    return in;
+  }
+  char c = 0;
+  in >> c;
+  if (c != exp.expected)
+  {
+    in.setstate(std::ios::failbit);
+  }
+  return in;
+}
+
+std::istream& allaberdiev::operator>>(std::istream& in, Line&& exp)
+{
+  std::istream::sentry guard(in);
+  if (!guard)
+  {
+    return in;
+  }
+  char c = 0;
+  for (int i = 0; exp.expected[i] != '\0'; i++)
+  {
+    in >> c;
+    if (c != exp.expected[i])
+    {
+      in.setstate(std::ios::failbit);
+    }
+  }
+  return in;
+}
+
