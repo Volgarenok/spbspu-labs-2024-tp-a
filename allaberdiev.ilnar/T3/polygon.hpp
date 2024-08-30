100b21e3cebfa09d1a7bdf80016e261bc8f297c5
allaberdiev.ilnar <dedbedblack@gmail.com>
2024-08-30 15:34:09 +0300

    add polygon structure

---
+++ allaberdiev.ilnar/T3/polygon.hpp
@@ -0,0 +1,28 @@
+#ifndef POLYGON_HPP
+#define POLYGON_HPP
+#include <iosfwd>
+#include <vector>
+
+namespace allaberdiev
+{
+  struct Point
+  {
+    int x;
+    int y;
+  };
+  std::istream& operator>>(std::istream&, Point&);
+
+  struct Polygon
+  {
+    std::vector< Point > points;
+  };
+  std::istream& operator>>(std::istream&, Polygon&);
+  size_t get_size(const Polygon&);
+  int get_x(const Point&);
+  int get_y(const Point&);
+  double get_area(const Polygon&);
+  bool operator==(const Polygon&, const Polygon&);
+  bool operator==(const Point&, const Point &);
+}
+
+#endif
