c3061738a6fd5930412db56cce478d0cc6e6132c
allaberdiev.ilnar <dedbedblack@gmail.com>
2024-08-30 15:40:22 +0300

    add input polygons in main.cpp

--- allaberdiev.ilnar/T3/main.cpp
+++ allaberdiev.ilnar/T3/main.cpp
@@ -16,4 +16,19 @@ int main(int argc, char **argv)
   {
     return 1;
   }
+
+ std::vector< Polygon > polygons;
+
+  using input_it = std::istream_iterator< Polygon >;
+  while (!file.eof())
+  {
+    std::copy(input_it{ file }, input_it{}, std::back_inserter(polygons));
+    if (file.fail())
+    {
+      file.clear();
+      file.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+    }
+  }
+  file.close();
+
 }
