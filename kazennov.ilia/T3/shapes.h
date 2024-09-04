b7c86c4dbc6207638a468b031765d783d1d30dd9
kazunovia <iliakazennov@ya.ru>
2024-09-04 13:46:58 +0300

    find not closed {}

--- kazennov.ilia/T3/shapes.h
+++ kazennov.ilia/T3/shapes.h
@@ -23,6 +23,7 @@ namespace kazennov
     friend bool operator<(const Point& p, const Point& other)
     {
       return (p.x < other.x && p.y < other.y);
+    }
   };
   struct Polygon
   {
@@ -30,7 +31,7 @@ namespace kazennov
   };
   struct TriangleForArea
   {
-    kazennov::Point p1;
+    Point p1;
     double operator()(double area, const Point& p2, const Point& p3)
     {
       area += 0.5 * std::abs((p3.y - p1.y) * (p2.x - p1.x) - (p3.x - p1.x) * (p2.y - p1.y));
