15b16a171713671263f08512962cc893b1937f2b
deadshumz <shumzz@bk.ru>
2024-09-05 21:09:17 +0300

    datastruct hpp

---
+++ petuhov.kirill/T2/datastruct.hpp
@@ -0,0 +1,12 @@
+#ifndef DATASTRUCT_HPP
+#define DATASTRUCT_HPP
+
+#include <string>
+
+struct DataStruct {
+  unsigned long long key1;
+  unsigned long long key2;
+  std::string key3;
+};
+
+#endif
