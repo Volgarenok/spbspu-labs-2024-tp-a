3cc1b32f6d676b308ed22f90e6041acfd27bf626
plitkkka <anastasiatimchishina@yandex.ru>
2024-09-07 22:11:54 +0300

    Added reading from a file in main

--- timchishina.anastasia/T3/main.cpp
+++ timchishina.anastasia/T3/main.cpp
@@ -1,2 +1,35 @@
-int main()
-{}
+#include <iostream>
+#include <fstream>
+#include <iterator>
+#include <algorithm>
+#include <limits>
+#include "figures.hpp"
+
+using namespace timchishina;
+int main(int argc, const char* argv[])
+{
+  if (argc != 2)
+  {
+    std::cerr << "<INVALID ARGUMENTS>\n";
+    return 1;
+  }
+  std::ifstream file(argv[1]);
+  if (!file.is_open())
+  {
+    std::cerr << "<INVALID FILE>\n";
+    return 1;
+  }
+  std::vector< Polygon > polygons;
+  using inIterator = std::istream_iterator< Polygon >;
+  while (!file.eof())
+  {
+    std::copy(inIterator{file}, inIterator{}, std::back_inserter(polygons));
+    if (file.fail())
+    {
+      file.clear();
+      file.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+    }
+  }
+  file.close();
+  return 0;
+}
