24ed768a8f22f476365375ab65d982fc15b355f8
DaniilPrstrz <Vyzanovdaniil@gmail.com>
2024-09-09 15:56:01 +0300

    add point.cpp

--- vyzhanov.daniil/T3/point.hpp
+++ vyzhanov.daniil/T3/point.hpp
@@ -1,12 +1,18 @@
 #ifndef POINT_HPP
 #define POINT_HPP
 
+#include <fstream>
+
 namespace vyzhanov
 {
   struct Point
   {
     size_t x, y;
   };
+
+  std::istream& operator>>(std::istream&, Point&);
+  std::ostream& operator<<(std::ostream&, const Point&);
+  bool operator==(const Point&, const Point&);
 }
 
 #endif
