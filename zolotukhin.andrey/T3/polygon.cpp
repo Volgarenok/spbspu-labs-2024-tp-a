7b2509b682eb3e690111287c3989ffaca59feb7a
andrey <zetandrey24@gmail.com>
2024-09-09 18:16:49 +0300

    add polygon realization

---
+++ zolotukhin.andrey/T3/polygon.cpp
@@ -0,0 +1,91 @@
+#include "polygon.hpp"
+#include <delimiter.hpp>
+#include "geometryFunc.hpp"
+
+std::istream& zolotukhin::operator>>(std::istream& in, Point& point)
+{
+  std::istream::sentry guard(in);
+  if (!guard)
+  {
+    return in;
+  }
+  using del = Delimiter;
+  Point pos = {};
+
+  in >> del{ '(' } >> pos.x >> del{ ';' } >> pos.y >> del{ ')' };
+  if (in)
+  {
+    point = pos;
+  }
+  else
+  {
+    in.setstate(std::ios::failbit);
+  }
+  return in;
+}
+
+std::istream& zolotukhin::operator>>(std::istream& in, Polygon& poly)
+{
+  std::istream::sentry guard(in);
+  if (!guard)
+  {
+    return in;
+  }
+  size_t verticesNumb = {};
+  in >> verticesNumb;
+
+  if (verticesNumb < 3)
+  {
+    in.setstate(std::ios::failbit);
+    return in;
+  }
+  std::vector< Point > p;
+  p.reserve(verticesNumb);
+  using inputItT = std::istream_iterator< Point >;
+  std::copy_n(inputItT{ in }, verticesNumb, std::back_inserter(p));
+
+  if (p.size() == verticesNumb)
+  {
+    poly.points = std::move(p);;
+  }
+  else
+  {
+    in.setstate(std::ios::failbit);
+  }
+  return in;
+}
+
+double zolotukhin::CalculateArea::operator()(double res, const Point& point3)
+{
+  res += 0.5 * (std::abs((point1.x - point3.x) * (point2.y - point3.y) - (point2.x - point3.x) * (point1.y - point3.y)));
+  point2 = point3;
+  return res;
+}
+
+double zolotukhin::Polygon::getArea() const
+{
+  using namespace std::placeholders;
+  auto calcArea = std::bind(CalculateArea{ points[0], points[1] }, _1, _2);
+  return std::accumulate(std::next(points.begin()), points.end(), 0.0, calcArea);
+}
+
+bool zolotukhin::CalcRightCorner::operator()(const Point& point3)
+{
+  Point side1 = { point2.x - point1.x, point2.y - point1.y };
+  Point side2 = { point3.x - point1.x, point3.y - point1.y };
+
+  point1 = point2;
+  point2 = point3;
+
+  return side1.x * side2.x + side1.y * side2.y == 0;
+}
+
+size_t zolotukhin::Polygon::getCorners() const
+{
+  if (points.size() < 3)
+  {
+    throw std::invalid_argument("<INVALID COMMAND>");
+  }
+  auto calcCorner = CalcRightCorner{ points[points.size() - 1], points[points.size() - 2] };
+  return std::find_if(points.cbegin(), points.cend(), calcCorner) != points.cend();
+}
