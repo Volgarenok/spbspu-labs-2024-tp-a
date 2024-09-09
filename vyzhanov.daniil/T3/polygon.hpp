6308af34f93e260aeb9690ed9e9038c938191543
DaniilPrstrz <Vyzanovdaniil@gmail.com>
2024-09-09 16:07:00 +0300

    add class polygon

---
+++ vyzhanov.daniil/T3/polygon.hpp
@@ -0,0 +1,23 @@
+#ifndef POLYGON_HPP
+#define POLYGON_HPP
+
+#include <vector>
+#include <iostream>
+#include "point.hpp"
+
+namespace vyzhanov
+{
+  struct Polygon
+  {
+    std::vector< Point > points;
+  };
+  using points = std::vector< Point >::const_iterator;
+  double getArea(const Polygon&);
+  double calculateArea(points, points, points, points, double);
+  double calculatePair(const Point&, const Point&);
+  std::istream& operator>>(std::istream&, Polygon&);
+  std::ostream& operator<<(std::ostream&, const Polygon&);
+  bool operator==(const Polygon&, const Polygon&);
+}
+
+#endif
