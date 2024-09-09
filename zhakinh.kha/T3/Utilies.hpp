aa9b4b8702285b925a074a7565fbd5e18d258c06
sahaki13 <hagiakinh13@gmail.com>
2024-09-09 13:47:39 +0300

    Update files for T3 - dont know why error: have 2 labs added

---
+++ zhakinh.kha/T3/Utilies.hpp
@@ -0,0 +1,23 @@
+#ifndef PREDICATES_HPP
+#define PREDICATES_HPP
+
+#include "polygon.hpp"
+
+namespace zhakha
+{
+  bool hasEvenVertexesCount(const Polygon& polygon);
+  bool hasOddVertexesCount(const Polygon& polygon);
+  bool hasVertexesCount(const Polygon& polygon, size_t num);
+  bool compareAreas(const Polygon& lhs, const Polygon& rhs);
+  bool compareVertexes(const Polygon& lhs, const Polygon& rhs);
+  bool comparePointsX(const Point& lhs, const Point& rhs);
+  bool comparePointsY(const Point& lhs, const Point& rhs);
+  bool comparePolygonsMinX(const Polygon& lhs, const Polygon& rhs);
+  bool comparePolygonsMinY(const Polygon& lhs, const Polygon& rhs);
+  bool comparePolygonsMaxX(const Polygon& lhs, const Polygon& rhs);
+  bool comparePolygonsMaxY(const Polygon& lhs, const Polygon& rhs);
+  using minmax_pair_t = std::pair< std::vector<Point>::const_iterator, std::vector<Point>::const_iterator >;
+  bool isLayingIn(const minmax_pair_t& in_x, const minmax_pair_t& in_y, const minmax_pair_t& out_x, const minmax_pair_t& out_y);
+}
+
+#endif
