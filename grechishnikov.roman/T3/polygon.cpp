9227eff1a42e6e60e6f784bc932d0ce5bb4847e6
Steuger7 <steuger7@mail.ru>
2024-09-09 22:41:31 +0300

    input change

--- grechishnikov.roman/T3/polygon.cpp
+++ grechishnikov.roman/T3/polygon.cpp
@@ -50,7 +50,7 @@ std::istream& grechishnikov::operator>>(std::istream& in, Polygon& polygon)
   }
   std::vector< Point > temp;
   std::copy_n(std::istream_iterator< Point >{ in }, size, std::back_inserter(temp));
-  if (!in || in.peek() != '\n')
+  if (!in || size != temp.size())
   {
     in.setstate(std::ios::failbit);
     return in;
