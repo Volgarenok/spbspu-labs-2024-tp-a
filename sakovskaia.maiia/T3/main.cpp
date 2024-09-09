906f6b8fa2a16c317cad65d37f05eafec29478e2
MayaSakovskaya <zloyejik05@gmail.com>
2024-09-09 15:33:47 +0300

    main.cpp....

--- sakovskaia.maiia/T3/main.cpp
+++ sakovskaia.maiia/T3/main.cpp
@@ -1,2 +1,33 @@
-int main()
-{}
+#include "polygon.hpp"
+#include "command.hpp"
+#include <fstream>
+#include <vector>
+#include <string>
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+  if (argc < 2)
+  {
+    std::cerr << "Not enough arguments\n";
+    return 1;
+  }
+  std::ifstream input(argv[1]);
+  if (!input)
+  {
+    std::cerr << "Can't open file\n";
+    return 1;
+  }
+  std::vector< sakovskaia::Polygon > polygons;
+  sakovskaia::Polygon polygon;
+  while (input >> polygon)
+  {
+    polygons.push_back(polygon);
+  }
+  std::string command;
+  while (std::getline(std::cin, command))
+  {
+    sakovskaia::getCommand(command, polygons);
+  }
+  return 0;
+}
