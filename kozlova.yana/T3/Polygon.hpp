b1a189be1a4c2ed8a1296bec573de21d61c130ed
AuntMotYA <ikozyanag.2004@gmail.com>
2024-09-15 23:53:05 +0300

    adding operators and functions

--- kozlova.yana/T3/Polygon.hpp
+++ kozlova.yana/T3/Polygon.hpp
@@ -3,21 +3,26 @@
 
 #include <vector>
 #include <istream>
+#include <algorithm>
 
 namespace kozlova
 {
   struct Point
   {
-      int x, y;
+    int x, y;
   };
   std::istream& operator>>(std::istream& in, Point& point);
+  bool operator==(const Point& point1, const Point& point2);
 
   struct Polygon
   {
-      std::vector< Point > points;
+    std::vector< Point > points;
   };
 
   std::istream& operator>>(std::istream& in, Polygon& polygon);
+  bool operator==(const Polygon& polygon1, const Polygon& polygon2);
+  double getAreaPolygon(const Polygon& polygon);
+  double getAreaPolygons(const std::vector< Polygon >& polygons);
 }
 
 #endif
