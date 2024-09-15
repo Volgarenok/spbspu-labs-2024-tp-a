eb5d56c93ac01d7fb1a605efa3aa91f5e6e18072
MayaSakovskaya <zloyejik05@gmail.com>
2024-09-15 22:26:40 +0300

    fix

--- sakovskaia.maiia/T3/commands.hpp
+++ sakovskaia.maiia/T3/commands.hpp
@@ -12,13 +12,23 @@
 
 namespace sakovskaia
 {
-  double count(const std::vector< Polygon > & polygons, std::function< bool(const Polygon &) > filter);
-  double getArea(const Polygon & polygon);
+  double calculateArea(const std::vector< Point > & points, int n, int i);
+  double countArea(const Polygon & polygon);
+  double count(const std::vector< Polygon > & polygons, bool (* filter)(const Polygon &));
+  bool isEven(const Polygon & p);
+  bool isOdd(const Polygon & p);
+  bool alwaysTrue(const Polygon &);
+  double countWithVertexCheck(const std::vector< Polygon > & polygons, int vertex_count);
   double getArea(const std::string & parameter, const std::vector< Polygon > & polygons);
+  bool compareArea(const Polygon & lhs, const Polygon & rhs);
+  bool compareVertices(const Polygon & lhs, const Polygon & rhs);
   void getMaxMin(const std::string & type, const std::vector< Polygon > & polygons);
-  size_t findMaxSeq(const Polygon & pattern, std::vector< Polygon >::const_iterator iter, std::vector< Polygon >::const_iterator end, size_t current_max = 0);
+  bool isNotEqualToPattern(const Polygon & polygon, const Polygon & pattern);
+  size_t findMaxSeq(const Polygon & pattern, std::vector< Polygon >::const_iterator iter, std::vector< Polygon >::const_iterator end, size_t current_max);
   void getMaxSeq(const Polygon & pattern, const std::vector< Polygon > & polygons);
+  bool areEqualPolygons(const Polygon & lhs, const Polygon & rhs, const Polygon & pattern);
   void getRmecho(const Polygon & pattern, std::vector< Polygon > & polygons);
+  double squaredDist(const Point & a, const Point & b);
   bool isRectangle(const Polygon & polygon);
   void getRects(const std::vector< Polygon > & polygons);
   void getCommand(const std::string & command, std::vector< Polygon > & polygons);
