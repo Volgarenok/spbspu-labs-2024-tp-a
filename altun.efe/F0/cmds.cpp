fd71b5dcc91775913bf54665bc4b9fdab6e193d5
efeshka <rokie12312@gmail.com>
2024-09-15 17:37:59 +0300

    added create command

---
+++ altun.efe/F0/cmds.cpp
@@ -0,0 +1,20 @@
+#include "cmds.hpp"
+#include <iostream>
+#include <algorithm>
+#include <functional>
+#include <iterator>
+#include <stdexcept>
+
+void altun::createDictionary(std::istream& in, std::map< std::string, std::map < std::string, std::vector< std::string > > >& dicts)
+{
+  std::string name = "";
+  in >> name;
+
+  if (dicts.find(name) != dicts.end())
+  {
+    throw std::logic_error("<ALREADY EXISTS>");
+  }
+
+  std::map< std::string, std::vector< std::string > > newDict = {};
+  dicts[name] = newDict;
+}
---
+++ altun.efe/F0/cmds.hpp
@@ -0,0 +1,12 @@
+#ifndef CMDS_HPP
+#define CMDS_HPP
+#include <iosfwd>
+#include <map>
+#include <string>
+#include <vector>
+
+namespace altun
+{
+  void createDictionary(std::istream&, std::map< std::string, std::map < std::string, std::vector< std::string > > >&);
+}
+#endif
