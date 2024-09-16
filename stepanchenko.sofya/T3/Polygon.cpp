1ee107be0d6e0dda706e460f4408ef6f5eaca210
loveShrek <sofia.kunaeva@gmail.com>
2024-09-17 00:40:55 +0300

    Added input operator for point struct

--- stepanchenko.sofya/T3/Polygon.cpp
+++ stepanchenko.sofya/T3/Polygon.cpp
@@ -1,4 +1,20 @@
 #include "Polygon.hpp"
 
-std::istream& stepanchenko::operator>>(std::istream& in, Point& point);
+std::istream& stepanchenko::operator>>(std::istream& in, Point& point)
+{
+  std::istream::sentry guard(in);
+  if (!guard)
+  {
+    return in;
+  }
+  using del = DelimiterI;
+  Point t_point{ 0, 0 };
+  in >> del{ '(' } >> t_point.x >> del{ ';' } >> t_point.y >> del{ ')' };
+  if (in)
+  {
+  point = t_point;
+  }
+  return in;
+}
+
 std::istream& stepanchenko::operator>>(std::istream& in, Polygon& polygon);
