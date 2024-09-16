eaef1ca276e984026860f19fada89b3b51042a70
osakasonyboy@gmail.com <sensh0god@gmail.com>
2024-09-16 15:12:42 +0300

    =)

--- arakelyan.daniil/T3/main.cpp
+++ arakelyan.daniil/T3/main.cpp
@@ -9,12 +9,14 @@
 
 #include <formatGuard.hpp>
 
-#include "polygons.hpp"
+#include "commands.hpp"
+#include "polygon.hpp"
 
 int main(int argc, char **argv)
 {
   using namespace arakelyan;
   //maxseq rightshapes
+
   if (argc < 2)
   {
     std::cerr << "<NO SOURCE FILE WITH FIGURES!>\n";
@@ -43,16 +45,16 @@ int main(int argc, char **argv)
 
   file.close();
 
-  // std::map< std::string, std::function< void(std::ostream&, std::istream&, const std::vector< Polygon >&) > > commands;
-  // {
-  //   using namespace std::placeholders;
-  //   // commands["AREA"] = std::bind(getArea, _1, _2, _3);
-  //   // commands["MAX"] = std::bind(findMax, _1, _2, _3);
+  std::map< std::string, std::function< void(std::istream&, std::ostream&, const std::vector< Polygon >&) > > commands;
+  {
+    using namespace std::placeholders;
+    commands["AREA"] = std::bind(doArea, _1, _2, _3);
+    commands["MAX"] = std::bind(findMax, _1, _2, _3);
   //   // commands["MIN"] = std::bind(findMin, _1, _2, _3);
   //   // commands["COUNT"] = std::bind(count, _1, _2, _3);
   //   // commands["MAXSEQ"] = std::bind(maxSeq, _1, _2, _3);
   //   // commands["RIGHTSHAPES"] = std::bind(rightShapes, _1, _2, _3);
-  // }
+  }
 
   std::string cmdName = "";
   while (std::cin >> cmdName)
