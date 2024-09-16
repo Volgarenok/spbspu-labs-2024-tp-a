acd5be9fbc1f9f557e18809e3406157ce5687ca3
loveShrek <sofia.kunaeva@gmail.com>
2024-09-17 00:13:26 +0300

    Added polygon/point operators

--- stepanchenko.sofya/T3/Polygon.hpp
+++ stepanchenko.sofya/T3/Polygon.hpp
@@ -10,13 +10,16 @@ namespace stepanchenko
   {
     int x, y;
   };
-  std::istream& operator>>(std::istream& in, Point& point);
 
   struct Polygon
   {
     std::vector< Point > points;
   };
+
+  std::istream& operator>>(std::istream& in, Point& point);
   std::istream& operator>>(std::istream& in, Polygon& polygon);
+  bool operator==(const Point& right, const Point& left);
+  bool operator==(const Polygon& right, const Polygon& left);
 }
 
 #endif
