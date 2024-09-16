d8c63546f9113c4b3d681c3315db8f693f80da1d
DaniilPrstrz <Vyzanovdaniil@gmail.com>
2024-09-16 18:24:20 +0300

    change main

--- vyzhanov.daniil/T3/main.cpp
+++ vyzhanov.daniil/T3/main.cpp
@@ -1,45 +1,35 @@
-#include <functional>
 #include <iostream>
-#include <limits>
+#include <functional>
 #include <map>
+#include <limits>
+#include <fstream>
 #include <iterator>
 #include <iomanip>
 #include "polygon.hpp"
 #include "commands.hpp"
 
-int main(int argc, char** argv)
+
+int main(int argc, char* argv[])
 {
   using namespace vyzhanov;
   if (argc != 2)
   {
-    std::cerr << "Incorrect input\n";
+    std::cerr << "Incorrect number of arguments!\n";
     return 1;
   }
   std::ifstream input_file(argv[1]);
   std::vector< Polygon > polygons;
-  std::string args;
-
-  if (!input_file.is_open())
-  {
-    std::cerr << "Invalid file name\n";
-    return 1;
-  }
-
   while (!input_file.eof())
   {
-    std::copy(std::istream_iterator< Polygon >{ input_file },
-      std::istream_iterator< Polygon >{}, std::back_inserter(polygons));
-    if (input_file.fail())
-    {
-      input_file.clear();
-      input_file.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
-    }
+    std::copy(std::istream_iterator< Polygon >{ input_file }, std::istream_iterator< Polygon >{}, std::back_inserter(polygons));
+    input_file.clear();
+    input_file.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
   }
 
   std::map< std::string, std::function< void(std::istream&, std::ostream&) > > commands;
   {
     using namespace std::placeholders;
-    commands["AREA"] = std::bind(area, std::cref(polygons), _1, _2);
+    commands["AREA"] = std::bind(area, std::ref(polygons), _1, _2);
     commands["MAX"] = std::bind(max, std::cref(polygons), _1, _2);
     commands["MIN"] = std::bind(min, std::cref(polygons), _1, _2);
     commands["COUNT"] = std::bind(count, std::cref(polygons), _1, _2);
@@ -47,12 +37,13 @@ int main(int argc, char** argv)
     commands["RMECHO"] = std::bind(rmecho, std::ref(polygons), _1, _2);
     commands["RECTS"] = std::bind(rects, std::cref(polygons), _1, _2);
   }
-
-  while (std::cin >> args)
+  std::string arg;
+  std::cout << std::fixed << std::setprecision(1);
+  while (std::cin >> arg)
   {
     try
     {
-      commands.at(args)(std::cin, std::cout);
+      commands.at(arg)(std::cin, std::cout);
     }
     catch (const std::out_of_range&)
     {
