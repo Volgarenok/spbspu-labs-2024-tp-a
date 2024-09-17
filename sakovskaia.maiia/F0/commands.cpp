3126cfb1d445c7a4c0d93e12c543e17499739725
MayaSakovskaya <zloyejik05@gmail.com>
2024-09-17 23:31:10 +0300

    fix

--- sakovskaia.maiia/F0/commands.cpp
+++ sakovskaia.maiia/F0/commands.cpp
@@ -46,6 +46,7 @@ void sakovskaia::newCmd(std::map< std::string, dict_t > & dicts, std::istream & 
     }
     extendDict(dicts[dictName], file);
   }
+  output << "<The dictionary" << dictName << " has been created >\n";
 }
 
 void sakovskaia::deleteCmd(std::map< std::string, dict_t > & dicts, std::istream & input, std::ostream & output)
@@ -58,6 +59,7 @@ void sakovskaia::deleteCmd(std::map< std::string, dict_t > & dicts, std::istream
     throw std::logic_error("<DICTIONARY NOT FOUND>\n");
   }
   dicts.erase(it);
+  output << "<The dictionary" << dictName << " has been deleted>\n";
 }
 
 void sakovskaia::loadCmd(std::map< std::string, dict_t > & dicts, std::istream & input, std::ostream & output)
@@ -75,6 +77,7 @@ void sakovskaia::loadCmd(std::map< std::string, dict_t > & dicts, std::istream &
     throw std::logic_error("<FILE NOT FOUND>\n");
   }
   extendDict(dicts[dictName], file);
+  output << "<The dictionary" << dictName << " has been loaded>\n";
 }
 
 void sakovskaia::addCmd(std::map< std::string, dict_t > & dicts, std::istream & input, std::ostream & output)
@@ -110,6 +113,7 @@ void sakovskaia::saveCmd(std::map< std::string, dict_t > & dicts, std::istream &
   auto end = dict.end();
   sakovskaia::saveDictToFile(file, start, end);
   file.close();
+  output << "<The dictionary" << dictName << " has been saved>\n";
 }
 
 void sakovskaia::removeCmd(std::map< std::string, dict_t > & dicts, std::istream & input, std::ostream & output)
