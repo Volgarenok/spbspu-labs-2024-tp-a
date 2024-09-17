d208ddd43ab943ab7b5ce1e36ea683e006ed3ae3
MayaSakovskaya <zloyejik05@gmail.com>
2024-09-17 19:46:30 +0300

    FIVE MORE!!!!

--- sakovskaia.maiia/F0/commands.cpp
+++ sakovskaia.maiia/F0/commands.cpp
@@ -89,3 +89,110 @@ void sakovskaia::saveCmd(std::map< std::string, dict_t > & dicts, std::istream &
   saveDictToFile(file, start, end);
   file.close();
 }
+
+void sakovskaia::removeCmd(std::map< std::string, dict_t > & dicts, std::istream & input, std::ostream & output)
+{
+  std::string dictName, word;
+  input >> dictName >> word;
+  auto it = dicts.find(dictName);
+  if (it == dicts.end())
+  {
+    throw std::logic_error("<DICTIONARY NOT FOUND>\n");
+  }
+  dict_t & dict = it->second;
+  auto wordIt = dict.find(word);
+  if (wordIt == dict.end())
+  {
+    throw std::logic_error("<WORD NOT FOUND>\n");
+  }
+  dict.erase(wordIt);
+  int totalWords = dict.size();
+  updateFrequencies(dict.begin(), dict.end(), totalWords);
+  output << "<Word '" << word << "' removed and frequencies updated>\n";
+}
+
+void sakovskaia::frequencyCmd(std::map< std::string, dict_t > & dicts, std::istream & input, std::ostream & output)
+{
+  std::string dictName, word;
+  input >> dictName >> word;
+  auto it = dicts.find(dictName);
+  if (it == dicts.end())
+  {
+    throw std::logic_error("<DICTIONARY NOT FOUND>\n");
+  }
+  dict_t & dict = it->second;
+  auto wordIt = dict.find(word);
+  if (wordIt == dict.end())
+  {
+    throw std::logic_error("<WORD NOT FOUND>\n");
+  }
+  output << "Frequency of " << word << " in " << dictName << ": " << wordIt->second << "\n";
+}
+
+void sakovskaia::updateCmd(std::map< std::string, dict_t > & dicts, std::istream & input, std::ostream & output)
+{
+  std::string dictName, word;
+  int newFrequency;
+  input >> dictName >> word >> newFrequency;
+  auto it = dicts.find(dictName);
+  if (it == dicts.end())
+  {
+    throw std::logic_error("<DICTIONARY NOT FOUND>\n");
+  }
+  dict_t & dict = it->second;
+  auto wordIt = dict.find(word);
+  if (wordIt == dict.end())
+  {
+    throw std::logic_error("<WORD NOT FOUND>\n");
+  }
+  wordIt->second = newFrequency;
+  int totalFrequency = calculateTotalFrequencyExcludeOne(dict.begin(), dict.end(), newFrequency);
+  updateFrequenciesWithNew(dict.begin(), dict.end(), totalFrequency, newFrequency);
+  output << "Frequency of " << word << " in " << dictName << " updated to " << newFrequency << ".\n";
+}
+
+void sakovskaia::combiningCmd(std::map< std::string, dict_t > & dicts, std::istream & input, std::ostream & output)
+{
+  std::string dictName1, dictName2, resultDictName;
+  input >> dictName1 >> dictName2 >> resultDictName;
+  auto dictIt1 = dicts.find(dictName1);
+  auto dictIt2 = dicts.find(dictName2);
+  if (dictIt1 == dicts.end() || dictIt2 == dicts.end())
+  {
+    throw std::logic_error("<DICTIONARY NOT FOUND>\n");
+  }
+  dict_t & dict1 = dictIt1->second;
+  dict_t & dict2 = dictIt2->second;
+  if (dicts.find(resultDictName) != dicts.end())
+  {
+    throw std::logic_error("<ALREADY EXISTS>\n");
+  }
+  dicts[resultDictName] = dict_t();
+  dict_t & resultDict = dicts[resultDictName];
+  int totalFrequency = calculateTotalFrequencyForTwo(dict1.begin(), dict1.end(), dict2.begin(), dict2.end());
+  combineDictionaries(dict1.begin(), dict1.end(), dict2.begin(), dict2.end(), resultDict, totalFrequency);
+  output << "Combined dictionaries successfully into " << resultDictName << ".\n";
+}
+
+void sakovskaia::diffCmd(std::map< std::string, dict_t > & dicts, std::istream & input, std::ostream & output)
+{
+  std::string dictName1, dictName2, resultDictName;
+  input >> dictName1 >> dictName2 >> resultDictName;
+  auto dictIt1 = dicts.find(dictName1);
+  auto dictIt2 = dicts.find(dictName2);
+  if (dictIt1 == dicts.end() || dictIt2 == dicts.end())
+  {
+    throw std::logic_error("<DICTIONARY NOT FOUND>\n");
+  }
+  dict_t &dict1 = dictIt1->second;
+  dict_t &dict2 = dictIt2->second;
+  if (dicts.find(resultDictName) != dicts.end())
+  {
+    throw std::logic_error("<ALREADY EXISTS>\n");
+  }
+  dicts[resultDictName] = dict_t();
+  dict_t &resultDict = dicts[resultDictName];
+  int totalFrequency = calculateTotalFrequency(dict1.begin(), dict1.end());
+  createDiffDictionary(dict1.begin(), dict1.end(), dict2, resultDict, totalFrequency);
+  output << "Words unique to " << dictName1 << " saved in " << resultDictName << ".\n";
+}
--- sakovskaia.maiia/F0/helping_commands.cpp
+++ sakovskaia.maiia/F0/helping_commands.cpp
@@ -9,3 +9,92 @@ void sakovskaia::saveDictToFile(std::ofstream & file, dict_t::const_iterator it,
   file << it->first << " " << it->second << "\n";
   saveDictToFile(file, ++it, end);
 }
+
+void sakovskaia::updateFrequencies(dict_t::iterator it, dict_t::iterator end, int totalWords)
+{
+  if (it == end)
+  {
+    return;
+  }
+  it->second = 1 / totalWords;
+  updateFrequencies(++it, end, totalWords);
+}
+
+int sakovskaia::calculateTotalFrequencyExcludeOne(dict_t::iterator it, dict_t::iterator end, int excludeFrequency)
+{
+  if (it == end)
+  {
+    return 0;
+  }
+  if (it->second == excludeFrequency)
+  {
+    return calculateTotalFrequency(++it, end, excludeFrequency);
+  }
+  return it->second + calculateTotalFrequency(++it, end, excludeFrequency);
+}
+
+void sakovskaia::updateFrequenciesWithNew(dict_t::iterator it, dict_t::iterator end, int totalFrequency, int excludeWordFrequency)
+{
+  if (it == end)
+  {
+    return;
+  }
+  if (it->second != excludeWordFrequency)
+  {
+    it->second = (1 / totalFrequency) * it->second;
+  }
+  updateFrequenciesWithNew(++it, end, totalFrequency, excludeWordFrequency);
+}
+
+int sakovskaia::calculateTotalFrequencyForTwo(dict_t::iterator it1, dict_t::iterator end1, dict_t::iterator it2, dict_t::iterator end2)
+{
+  if (it1 == end1 && it2 == end2)
+  {
+    return 0;
+  }
+  int freq1 = (it1 != end1) ? it1->second : 0;
+  int freq2 = (it2 != end2) ? it2->second : 0;
+  return freq1 + freq2 + calculateTotalFrequencyForTwo((it1 != end1) ? ++it1 : it1, end1, (it2 != end2) ? ++it2 : it2, end2);
+}
+
+void sakovskaia::combineDictionaries(dict_t::iterator it1, dict_t::iterator end1, dict_t::iterator it2, dict_t::iterator end2, dict_t & resultDict, int totalFrequency)
+{
+  if (it1 == end1 && it2 == end2)
+  {
+    return;
+  }
+  if (it1 != end1)
+  {
+    resultDict[it1->first] = (static_cast< double >(it1->second) / totalFrequency) * 100;
+    combineDictionaries(++it1, end1, it2, end2, resultDict, totalFrequency);
+    return;
+  }
+  if (it2 != end2)
+  {
+    resultDict[it2->first] = (static_cast< double >(it2->second) / totalFrequency) * 100;
+    combineDictionaries(it1, end1, ++it2, end2, resultDict, totalFrequency);
+  }
+}
+
+int sakovskaia::calculateTotalFrequency(dict_t::iterator it, dict_t::iterator end)
+{
+  if (it == end)
+  {
+    return 0;
+  }
+  return it->second + calculateTotalFrequency(++it, end);
+}
+
+void sakovskaia::createDiffDictionary(dict_t::iterator it1, dict_t::iterator end1, dict_t & dict2, dict_t & resultDict, int totalFrequency)
+{
+  if (it1 == end1)
+  {
+    return;
+  }
+  auto it2 = dict2.find(it1->first);
+  if (it2 == dict2.end() || it1->second > it2->second)
+  {
+    resultDict[it1->first] = static_cast< double >(it1->second) / totalFrequency;
+  }
+  createDiffDictionary(++it1, end1, dict2, resultDict, totalFrequency);
+}
