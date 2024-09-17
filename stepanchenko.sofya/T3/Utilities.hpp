009d5ed83d2b22f012cd4f002fac57c967ea043f
loveShrek <sofia.kunaeva@gmail.com>
2024-09-17 03:02:08 +0300

    a have like a thousand misspellings like I was typing on typewriter or some sh

--- stepanchenko.sofya/T3/Utilities.hpp
+++ stepanchenko.sofya/T3/Utilities.hpp
@@ -10,11 +10,12 @@ namespace stepanchenko
   double areaOdd(const std::vector< Polygon >& polygons);
   double areaMean(const std::vector< Polygon >& polygons);
   double areaNum(const std::vector< Polygon >& polygons, size_t nVertexes);
-  double getArea(const Polygobool isEven(const Polygon& polygon);
+  double getArea(const Polygon& polygon);
+  bool isEven(const Polygon& polygon);
   bool isOdd(const Polygon& polygon);
   bool isEven(const Polygon& polygon);
   int multiply_points(const Point& a, const Point& b);
-  bool hasNumOfVertexes(const stepanchenko::Polygon& polygon, const size_t& num);
+  bool hasNumOfVertexes(const Polygon& polygon, const size_t& num);
 
   void maxArea(std::vector< Polygon >& polygons, std::ostream& out);
   void maxVertexes(std::vector< Polygon >& polygons, std::ostream& out);
