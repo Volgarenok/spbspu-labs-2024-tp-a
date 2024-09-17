542f657b84b7ab9d1a4ccbfc750a53ef62d7d532
andrey <zetandrey24@gmail.com>
2024-09-18 01:45:50 +0300

    add commands

---
+++ zolotukhin.andrey/F0/commands.hpp
@@ -0,0 +1,18 @@
+#ifndef COMMANDS_HPP
+#define COMMANDS_HPP
+
+#include "AVLTree.hpp"
+
+namespace zolotukhin
+{
+	void create(std::map<std::string, Set>& Sets, std::istream& inp, std::ostream& out);
+	void addWord(std::map<std::string, Set>& Sets, std::istream& inp, std::ostream& out);
+	void removeWord(std::map<std::string, Set>& Sets, std::istream& inp, std::ostream& out);
+	void search(std::map<std::string, Set>& Sets, std::istream& inp, std::ostream& out);
+	void display(std::map<std::string, Set>& Sets, std::istream& inp, std::ostream& out);
+	void countWords(std::map<std::string, Set>& Sets, std::istream& inp, std::ostream& out);
+	void clearSet(std::map<std::string, Set>& Sets, std::istream& inp, std::ostream& out);
+	void mergeSets(std::map<std::string, Set>& Sets, std::istream& inp, std::ostream& out);
+	void intersectSets(std::map<std::string, Set>& Sets, std::istream& inp, std::ostream& out);
+}
+#endif
