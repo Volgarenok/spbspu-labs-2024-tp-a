85101e01769788d68501548455b1040f0c83358f
polykisa <s.sanek2005@gmail.com>
2024-09-20 01:57:40 +0300

    read sort output to main.cpp

--- skopchenko.alexander/T2/main.cpp
+++ skopchenko.alexander/T2/main.cpp
@@ -1,4 +1,20 @@
-int main()
-{
-  
+#include "datastruct.h"
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+
+int main() {
+  std::vector<skopchenko::DataStruct> dataStructs;
+
+  std::copy(std::istream_iterator<skopchenko::DataStruct>(std::cin),
+            std::istream_iterator<skopchenko::DataStruct>(),
+            std::back_inserter(dataStructs));
+
+  std::sort(dataStructs.begin(), dataStructs.end(), skopchenko::compare);
+
+  std::copy(dataStructs.begin(), dataStructs.end(),
+            std::ostream_iterator<skopchenko::DataStruct>(std::cout, "\n"));
+
+  return 0;
 }
