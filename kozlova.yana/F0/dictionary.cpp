6c76a54bc68c3bcc264313b417537e6274652c3f
AuntMotYA <ikozyanag.2004@gmail.com>
2024-09-19 23:53:26 +0300

    realize methods Dictionary

--- kozlova.yana/F0/dictionary.cpp
+++ kozlova.yana/F0/dictionary.cpp
@@ -18,4 +18,77 @@ namespace kozlova
   {
     return name_;
   }
+
+  void Dictionary::addWord(const std::string& word, const size_t& number)
+  {
+    auto iterator = dictionary_.find(word);
+    if (iterator != dictionary_.end())
+    {
+      (*iterator).second += number;
+      return;
+    }
+    pair pair = std::make_pair(word, number);
+    dictionary_.insert(pair);
+    }
+
+  void Dictionary::insertWords(std::istream& in)
+  {
+    std::string word;
+    while (in >> word)
+    {
+      std::string cleanWord;
+      for (size_t i = 0; i < word.length(); ++i)
+      {
+        char c = word[i];
+        if (std::isalpha(c))
+        {
+          cleanWord += std::tolower(c);
+        }
+      }
+      if (!cleanWord.empty())
+      {
+        this->addWord(cleanWord, 1);
+      }
+    }
+  }
+
+  void Dictionary::insertPart( std::istream& in, std::string& word1, std::string word2)
+  {
+    std::string word;
+    bool inRange = false;
+    while (in >> word)
+    {
+      std::string cleanWord;
+      for (size_t i = 0; i < word.length(); ++i)
+      {
+        char c = word[i];
+        if (std::isalpha(c))
+        {
+          cleanWord += std::tolower(c);
+        }
+      }
+
+      if (cleanWord == word1)
+      {
+        inRange = true;
+      }
+      if (inRange)
+      {
+        this->addWord(cleanWord, 1);
+      }
+      if (cleanWord == word2)
+      {
+        break;
+      }
+    }
+  }
+
+  void Dictionary::deleteWord(const std::string word)
+  {
+    auto iterator = dictionary_.find(word);
+    if (iterator != dictionary_.end())
+    {
+      dictionary_.erase(word);
+    }
+  }
 }
