514cbdc80383e185f441fdd8582a53054436b82b
efeshka <rokie12312@gmail.com>
2024-09-19 18:49:40 +0300

    micro CG fix

--- altun.efe/F0/cmds.hpp
+++ altun.efe/F0/cmds.hpp
@@ -7,14 +7,14 @@
 
 namespace altun
 {
-  void createDictionary(std::istream&, std::map< std::string, std::map < std::string, std::vector< std::string > > >&);
-  void deleteDictionary(std::istream&, std::map< std::string, std::map < std::string, std::vector< std::string > > >&);
-  void addWord(std::istream&, std::map< std::string, std::map < std::string, std::vector< std::string > > >&);
-  void removeWord(std::istream&, std::map< std::string, std::map < std::string, std::vector< std::string > > >&);
-  void translateWord(std::ostream&, std::istream&, const std::map < std::string, std::map < std::string, std::vector< std::string > > >&);
-  void merge(std::istream&, std::map< std::string, std::map < std::string, std::vector< std::string > > >&);
-  void intersection(std::istream&, std::map < std::string, std::map < std::string, std::vector< std::string > > >&);
-  void combining(std::istream&, std::map< std::string, std::map < std::string, std::vector< std::string > > >&);
+  void createDictionary(std::istream&, std::map< std::string, std::map< std::string, std::vector< std::string > > >&);
+  void deleteDictionary(std::istream&, std::map< std::string, std::map< std::string, std::vector< std::string > > >&);
+  void addWord(std::istream&, std::map< std::string, std::map< std::string, std::vector< std::string > > >&);
+  void removeWord(std::istream&, std::map< std::string, std::map< std::string, std::vector< std::string > > >&);
+  void translateWord(std::ostream&, std::istream&, const std::map< std::string, std::map < std::string, std::vector< std::string > > >&);
+  void merge(std::istream&, std::map< std::string, std::map< std::string, std::vector< std::string > > >&);
+  void intersection(std::istream&, std::map < std::string, std::map< std::string, std::vector< std::string > > >&);
+  void combining(std::istream&, std::map< std::string, std::map< std::string, std::vector< std::string > > >&);
   void difference(std::istream&, std::map< std::string, std::map< std::string, std::vector< std::string > > >&);
 }
 #endif
