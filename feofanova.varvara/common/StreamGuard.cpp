2c146bed6b59e1dfd084e740404be3042bb34ccf
root <root@DESKTOP-HVA9M9A>
2024-09-19 21:27:34 +0800

    common files

---
+++ feofanova.varvara/common/StreamGuard.cpp
@@ -0,0 +1,13 @@
+#include "StreamGuard.hpp"
+
+feofanova::StreamGuard::StreamGuard(std::basic_ios<char>& s):
+  s_(s),
+  precision_(s.precision()),
+  flags_(s.flags())
+{}
+
+feofanova::StreamGuard::~StreamGuard()
+{
+  s_.precision(precision_);
+  s_.flags(flags_);
+}
