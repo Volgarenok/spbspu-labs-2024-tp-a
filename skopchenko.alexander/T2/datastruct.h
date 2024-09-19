6ff17f40c5b32764eee6476ea05fd5c1173c0daf
polykisa <s.sanek2005@gmail.com>
2024-09-20 01:49:33 +0300

    basic DataStruct definition

---
+++ skopchenko.alexander/T2/datastruct.h
@@ -0,0 +1,18 @@
+#ifndef DATASTRUCT_H
+#define DATASTRUCT_H
+
+#include <complex>
+#include <string>
+#include <utility>
+
+namespace skopchenko {
+
+  struct DataStruct {
+    std::complex<double> key1;
+    std::pair<long long, unsigned long long> key2;
+    std::string key3;
+  };
+
+}
+
+#endif
