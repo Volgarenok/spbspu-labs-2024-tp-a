50cac4e22ba68e0f8fe15ac0b840751e90695617
polykisa <s.sanek2005@gmail.com>
2024-09-20 02:04:32 +0300

    new streamguard function to save and restore stream state

---
+++ skopchenko.alexander/T2/streamguard.cpp
@@ -0,0 +1,17 @@
+#include "streamguard.h"
+
+namespace skopchenko
+{
+
+  StreamGuard::StreamGuard(std::basic_ios<char>& s) :
+    s_(s), precision_(s.precision()), flags_(s.flags())
+  {
+  }
+
+  StreamGuard::~StreamGuard()
+  {
+    s_.precision(precision_);
+    s_.flags(flags_);
+  }
+
+}
