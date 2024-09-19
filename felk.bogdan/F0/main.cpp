f904b572c51afb70e57a9c5354842a069e536aaa
true-slon <160644649+true-slon@users.noreply.github.com>
2024-09-19 23:55:03 +0300

    Update main.cpp

--- felk.bogdan/F0/main.cpp
+++ felk.bogdan/F0/main.cpp
@@ -10,15 +10,17 @@ int main()
   using namespace felk;
   using namespace std::placeholders;
   std::unordered_map< std::string, Dict > collection;
-  std::unordered_map< std::string, std::function< void(std::istream&, std::ostream&) > > commands;
-  commands["makedict"] = std::bind(makeDict, _1, _2, std::ref(collection));
-  commands["split"] = std::bind(split, _1, _2, std::ref(collection));
-  commands["getmean"] = std::bind(getMean, _1, _2, std::ref(collection));
-  commands["filter"] = std::bind(filter, _1, _2, std::ref(collection));
-  commands["print"] = std::bind(print, _1, _2, std::ref(collection));
-  commands["getunique"] = std::bind(getUnique, _1, _2, std::ref(collection));
-  commands["lenfilter"] = std::bind(lenFilter, _1, _2, std::ref(collection));
-
+  std::unordered_map< std::string, std::function< void(std::istream&) > > commands;
+  commands["makedict"] = std::bind(makeDict, _1, std::ref(std::cout), std::ref(collection));
+  commands["split"] = std::bind(split, _1, std::ref(std::cout), std::ref(collection));
+  commands["getmean"] = std::bind(getMean, _1, std::ref(std::cout), std::ref(collection));
+  commands["filter"] = std::bind(filter, _1, std::ref(std::cout), std::ref(collection));
+  commands["print"] = std::bind(print, _1, std::ref(std::cout), std::ref(collection));
+  commands["getunique"] = std::bind(getUnique, _1, std::ref(std::cout), std::ref(collection));
+  commands["lenfilter"] = std::bind(lenFilter, _1, std::ref(std::cout), std::ref(collection));
+  commands["setnewnumber"] = std::bind(setNewNumber, _1, std::ref(collection));
+  commands["clear"] = std::bind(clear, _1, std::ref(collection));
+  commands["deletekey"] = std::bind(deleteKey, _1, std::ref(collection));
   std::string command = "";
   while (!std::cin.eof())
   {
@@ -28,23 +30,8 @@ int main()
     }
     catch (const std::out_of_range& e)
     {
-      if (command == "setnewnumber")
-      {
-        setNewNumber(std::cin, collection);
-      }
-      else if (command == "clear")
-      {
-        clear(std::cin, collection);
-      }
-      else if (command == "deletekey")
-      {
-        deleteKey(std::cin, collection);
-      }
-      else
-      {
-        std::cerr << e.what() << "\n";
-        std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
-      }
+      std::cerr << e.what() << "\n";
+      std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
     }
     catch (const std::exception& e)
     {
