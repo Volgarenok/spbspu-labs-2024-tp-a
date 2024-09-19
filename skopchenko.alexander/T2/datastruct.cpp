fc3002c17d16d7e98c212da21a0725f868e98999
polykisa <s.sanek2005@gmail.com>
2024-09-20 02:31:46 +0300

    error handling for incorrect data input

--- skopchenko.alexander/T2/datastruct.cpp
+++ skopchenko.alexander/T2/datastruct.cpp
@@ -5,21 +5,27 @@
 namespace skopchenko
 {
 
-  std::istream& operator>>(std::istream& is, DataStruct& data)
-  {
+  std::istream& operator>>(std::istream& is, DataStruct& data) {
     std::string line;
-    std::getline(is, line);
-    std::regex pattern(R"(\(:key1 #c\(([-+]?\d+\.\d+) ([-+]?\d+\.\d+)\):key2 \(:N ([-+]?\d+):D (\d+)\):key3 "(.+?)":\))");    std::smatch match;
-
-    if (std::regex_search(line, match, pattern))
-    {
-      data.key1 = {std::stod(match[1]), std::stod(match[2])};
-      data.key2 = {std::stoll(match[3]), std::stoull(match[4])};
-      data.key3 = match[5];
+    if (!std::getline(is, line)) {
+      return is;
     }
+
+    std::regex pattern(R"(\(:key1 #c\(([-+]?\d+\.\d+) ([-+]?\d+\.\d+)\):key2 \(:N ([-+]?\d+):D (\d+)\):key3 "(.+?)":\))");
+    std::smatch match;
+
+    if (!std::regex_search(line, match, pattern)) {
+      is.setstate(std::ios::failbit);  // Устанавливаем флаг ошибки, если данные не соответствуют формату
+      return is;
+    }
+
+    data.key1 = {std::stod(match[1]), std::stod(match[2])};
+    data.key2 = {std::stoll(match[3]), std::stoull(match[4])};
+    data.key3 = match[5];
 
     return is;
   }
+
 
   std::ostream& operator<<(std::ostream& os, const DataStruct& data)
   {
