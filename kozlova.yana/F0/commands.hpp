207eb392079fa07f789a2298a0992b029787b1e1
AuntMotYA <ikozyanag.2004@gmail.com>
2024-09-20 02:09:42 +0300

    update commands.hpp

--- kozlova.yana/F0/commands.hpp
+++ kozlova.yana/F0/commands.hpp
@@ -6,12 +6,12 @@ namespace kozlova
 {
   void read(std::map< std::string, Dictionary >& dictionaries, std::istream& in);
   void readPart(std::map< std::string, Dictionary >& dictionaries, std::istream& in);
-  void findMaxFreq(std::istream& in, std::ostream& out);
+  void findMaxFreq(std::map< std::string, Dictionary >& dictionaries, std::istream& in, std::ostream& out);
   void combiningDictionary(std::map< std::string, Dictionary >& dictionaries, std::istream& in);
   void removeWords(std::map< std::string, Dictionary >& dictionaries, std::istream& in);
   void deleteDictionary(std::map< std::string, Dictionary >& dicts, std::istream& in);
   void printFreqWord(std::map< std::string, Dictionary >& dictionaries, std::istream& in, std::ostream& out);
-  void predecessor(std::istream& in, std::ostream& out);
+  void predecessor(std::map< std::string, Dictionary >& dictionaries, std::istream& in, std::ostream& out);
   void printDictCmd(const std::map< std::string, Dictionary >& dicts, std::istream& in, std::ostream& out);
 }
 
