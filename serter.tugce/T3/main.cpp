3a1413bfca99cc65c64163b411b5fa0cfcce3e77
mafeeoet <serter.t@spbspu.ru>
2024-09-20 02:26:59 +0300

    committtt

--- serter.tugce/T3/main.cpp
+++ serter.tugce/T3/main.cpp
@@ -58,11 +58,14 @@ int main(int argc, char* argv[]) {
         if (it != commands.end()) {
             try {
                 it->second(std::cin, std::cout);
-            } catch (const std::logic_error& e) {
+            }
+               catch (...)
+             {
                 std::cout << "<INVALID COMMAND>\n";
                 std::cin.clear();
                 std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
             }
+
         } else {
             std::cout << "<INVALID COMMAND>\n";
         }
