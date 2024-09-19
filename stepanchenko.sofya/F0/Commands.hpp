fa405bc7e108d7b9752cdea565eeebbc2bfc841e
loveShrek <sofia.kunaeva@gmail.com>
2024-09-19 22:48:31 +0300

    CrossRefs iterators included

--- stepanchenko.sofya/F0/Commands.hpp
+++ stepanchenko.sofya/F0/Commands.hpp
@@ -3,6 +3,7 @@
 
 #include <unordered_map>
 #include <string>
+#include <iterator>
 
 namespace stepanchenko
 {
@@ -13,6 +14,11 @@ namespace stepanchenko
     ~CrossRefs() = default;
     CrossRefs(const CrossRefs& other) = default;
     CrossRefs(CrossRefs&& moved) noexcept = default;
+
+    std::unordered_map<std::string, std::list< size_t > >::iterator begin();
+    std::unordered_map<std::string, std::list< size_t > >::iterator end();
+    std::unordered_map<std::string, std::list< size_t > >::const_iterator search(const std::string& word) const;
+
   private:
     std::unordered_map< std::string, std::list< size_t > > table_;
     std::vector< std::string > lines_;
