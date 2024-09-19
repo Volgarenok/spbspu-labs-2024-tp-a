0340d97ab0a26496962bc13d9444916b99270b81
loveShrek <sofia.kunaeva@gmail.com>
2024-09-19 22:57:31 +0300

    createTable method added

--- stepanchenko.sofya/F0/Commands.cpp
+++ stepanchenko.sofya/F0/Commands.cpp
@@ -9,11 +9,38 @@ std::unordered_map<std::string, std::list< size_t > >::iterator stepanchenko::Cr
 {
   return table_.begin();
 }
+
 std::unordered_map<std::string, std::list< size_t > >::iterator stepanchenko::CrossRefs::end()
 {
   return table_.end();
 }
+
 std::unordered_map<std::string, std::list< size_t > >::const_iterator stepanchenko::CrossRefs::search(const std::string& word) const
 {
   return table_.find(word);
+}
+
+void stepanchenko::CrossRefs::createTable(const std::string& tableName, const std::string& fname)
+{
+  name_ = tableName;
+  std::ifstream file;
+  file.open(fname);
+  std::string line;
+  size_t lineNumber = 1;
+  while (std::getline(file, line))
+  {
+    lines_.push_back(line);
+    std::istringstream stream(line);
+    std::string word;
+
+    std::for_each(std::istream_iterator<std::string>(stream),
+      std::istream_iterator<std::string>(),
+      [&](const std::string& word) {
+        std::string lowerWord;
+        std::copy_if(word.begin(), word.end(), std::back_inserter(lowerWord), isalpha);
+        std::transform(lowerWord.begin(), lowerWord.end(), lowerWord.begin(), ::tolower);
+        insert(lowerWord, lineNumber);
+      });
+    ++lineNumber;
+  }
 }
