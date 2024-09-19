b59c7098a789700cf86326d4436476af7211b86a
AuntMotYA <ikozyanag.2004@gmail.com>
2024-09-20 01:08:16 +0300

    add commands.hpp

---
+++ kozlova.yana/F0/commands.hpp
@@ -0,0 +1,18 @@
+#ifndef COMMANDS_HPP
+#define COMMANDS_HPP
+#include "dictionary.hpp"
+
+namespace kozlova
+{
+  void read(std::map< std::string, Dictionary >& dictionaries, std::istream& in);
+  void readPart(std::map< std::string, Dictionary >& dictionaries, std::istream& in);
+  void findMaxFreq(std::istream& in, std::ostream& out);
+  void combiningDictionary(std::map< std::string, Dictionary >& dictionaries, std::istream& in);
+  void removeWords(std::map< std::string, Dictionary >& dictionaries, std::istream& in);
+  void deleteDictionary(std::map< std::string, Dictionary >& dicts, std::istream& in);
+  void printFreqWord(std::map< std::string, Dictionary >& dictionaries, std::istream& in, std::ostream& out);
+  void predecessor(std::istream& in, std::ostream& out);
+  void printDictCmd(const std::map< std::string, Dictionary >& dicts, std::istream& in, std::ostream& out);
+}
+
+#endif
