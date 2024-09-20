ae8e597449e2a915d77d61be51eb4d3a0b6bfa66
loveShrek <sofia.kunaeva@gmail.com>
2024-09-20 23:38:11 +0300

    Commands added

--- stepanchenko.sofya/F0/Commands.hpp
+++ stepanchenko.sofya/F0/Commands.hpp
@@ -12,6 +12,13 @@ namespace stepanchenko
   void create_tableCmd(std::vector< CrossRefs > cross_refs, std::istream & in);
   void add_to_tableCmd(std::vector< CrossRefs > cross_refs, std::istream& in);
   void get_intersectionCmd(std::vector< CrossRefs > cross_refs, std::istream& in);
+  void print_linesCmd(std::vector< CrossRefs > cross_refs, std::istream& in, std::ostream& out);
+  void print(std::vector< CrossRefs > cross_refs, std::istream& in, std::ostream& out);
+  void delete_lines(std::vector< CrossRefs > cross_refs, std::istream& in)
+  void move_lines_downCmd(std::vector< CrossRefs > cross_refs, std::istream& in);
+  void rearrangeCmd(std::vector< CrossRefs > cross_refs, std::istream& in);
+  void get_selectionCmd(std::vector< CrossRefs > cross_refs, std::istream& in);
+  void print_tablesCmd(std::vector< CrossRefs > cross_refs, std::ostream& out);
 }
 
 #endif
