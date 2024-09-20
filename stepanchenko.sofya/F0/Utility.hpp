7bb21aca0a0db87f4eb12de666d2b878da6b0048
loveShrek <sofia.kunaeva@gmail.com>
2024-09-20 23:35:16 +0300

    Utility.hpp changed

--- stepanchenko.sofya/F0/Utility.hpp
+++ stepanchenko.sofya/F0/Utility.hpp
@@ -3,23 +3,18 @@
 
 #include <string>
 #include <algorithm>
+#include <iostream>
+#include <utility>
 
 #include "CrossRefs.hpp"
 
 namespace stepanchenko
 {
-  void checkName(std::vector< CrossRefs > cross_refs, const std::string& name)
-  {
-    auto iter = std::find_if(cross_refs.begin(), cross_refs.end(), [&](const CrossRefs& cr) {
-      return cr.getName() == name;
-    });
-    if (iter != cross_refs.end())
-    {
-      throw std::logic_error("<INVALID COMMAND>");
-    }
-  }
-
-  bool isInIntersection(const CrossRefs& cr1, const CrossRefs& cr2);
+  void checkName(std::vector< CrossRefs >& cross_refs, const std::string& name);
+  void printL(const std::pair < size_t, std::string >& line, std::ostream& out);
+  void writeToFile(const std::vector< std::pair< size_t, std::string > >& lines, std::ofstream& file);
+  void swapLines(const std::vector< std::pair< size_t, std::string > >::iterator first,
+    const std::vector< std::pair< size_t, std::string > >::iterator second, bool pred);
 }
 
 #endif
