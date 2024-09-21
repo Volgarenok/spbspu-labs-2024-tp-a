5a97d46df49423959a1226ce81b9c1b431707789
loveShrek <sofia.kunaeva@gmail.com>
2024-09-21 21:52:16 +0300

    и все кому уже хватит, и все кому этого мало

--- stepanchenko.sofya/F0/Commands.hpp
+++ stepanchenko.sofya/F0/Commands.hpp
@@ -3,22 +3,21 @@
 
 #include <iostream>
 #include <vector>
-#include <algorithm>
 
 #include "CrossRefs.hpp"
 
 namespace stepanchenko
 {
-  void create_tableCmd(std::vector< CrossRefs > cross_refs, std::istream & in);
-  void add_to_tableCmd(std::vector< CrossRefs > cross_refs, std::istream& in);
-  void get_intersectionCmd(std::vector< CrossRefs > cross_refs, std::istream& in);
-  void print_linesCmd(std::vector< CrossRefs > cross_refs, std::istream& in, std::ostream& out);
-  void print(std::vector< CrossRefs > cross_refs, std::istream& in, std::ostream& out);
-  void delete_lines(std::vector< CrossRefs > cross_refs, std::istream& in);
-  void move_lines_downCmd(std::vector< CrossRefs > cross_refs, std::istream& in);
-  void rearrangeCmd(std::vector< CrossRefs > cross_refs, std::istream& in);
-  void get_selectionCmd(std::vector< CrossRefs > cross_refs, std::istream& in);
-  void print_tablesCmd(std::vector< CrossRefs > cross_refs, std::ostream& out);
+  void create_tableCmd(std::vector< CrossRefs > & cross_refs, std::istream & in);
+  void add_to_tableCmd(std::vector< CrossRefs > & cross_refs, std::istream& in);
+  void get_intersectionCmd(std::vector< CrossRefs > & cross_refs, std::istream& in);
+  void print_linesCmd(std::vector< CrossRefs > & cross_refs, std::istream& in, std::ostream& out);
+  void printCmd(std::vector< CrossRefs > & cross_refs, std::istream& in, std::ostream& out);
+  void delete_linesCmd(std::vector< CrossRefs > & cross_refs, std::istream& in);
+  void move_lines_downCmd(std::vector< CrossRefs > & cross_refs, std::istream& in);
+  void viewCmd(std::vector< CrossRefs >& cross_refs, std::ostream& out);
+  void get_selectionCmd(std::vector< CrossRefs > & cross_refs, std::istream& in);
+  void saveCmd(std::istream& in, std::vector< CrossRefs > & cross_refs);
 }
 
 #endif
