2f73e7f2755e781c01ff16bac915be3e66d162a2
loveShrek <sofia.kunaeva@gmail.com>
2024-09-21 21:59:54 +0300

    все исчезло.

--- stepanchenko.sofya/F0/Utility.hpp
+++ stepanchenko.sofya/F0/Utility.hpp
@@ -10,11 +10,14 @@
 
 namespace stepanchenko
 {
+  std::vector<CrossRefs> createCrossRefsFromFile(std::istream& fin);
   void checkName(std::vector< CrossRefs >& cross_refs, const std::string& name);
   void printL(const std::pair < size_t, std::string >& line, std::ostream& out);
-  void writeToFile(const std::vector< std::pair< size_t, std::string > >& lines, std::ofstream& file);
+  void writeToFile(const std::string& name, const std::vector< std::pair< size_t, std::string > >& lines, std::ofstream& file);
   void swapLines(const std::vector< std::pair< size_t, std::string > >::iterator first,
     const std::vector< std::pair< size_t, std::string > >::iterator second, bool pred);
+  bool isInIntersection(std::pair <std::string, std::list< size_t > > entry, CrossRefs& cr);
+  void helpCmd(std::ostream& out);
 }
 
 #endif
