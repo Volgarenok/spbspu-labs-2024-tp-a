945b1a1bbe00489ca61aa72a90e1eb777b0e4f19
puzz13 <puzz13one@gmail.com>
2024-09-22 16:04:51 +0300

    Add draft io w/ DataStruct

--- kovtun.alexander/T2/main.cpp
+++ kovtun.alexander/T2/main.cpp
@@ -1,5 +1,60 @@
+#include <iostream>
+
+namespace kovtun
+{
+  struct DataStruct
+  {
+    DataStruct(int _a):
+      a(_a)
+    {}
+
+    int getValue() const
+    {
+      return a;
+    }
+
+  private:
+    int a;
+  };
+
+  std::istream & operator>> (std::istream & in, DataStruct & dataStruct)
+  {
+    std::istream::sentry guard(in);
+    if (!guard)
+    {
+      return in;
+    }
+
+    int a = 0;
+    in >> a;
+    if (in)
+    {
+      dataStruct = DataStruct(a);
+    }
+
+    return in;
+  }
+
+  std::ostream & operator<< (std::ostream & out, const DataStruct & dataStruct)
+  {
+    std::ostream::sentry guard(out);
+    if (!guard)
+    {
+      return out;
+    }
+
+    out << dataStruct.getValue();
+    return out;
+  }
+}
+
 int main()
 {
+  kovtun::DataStruct someData(0);
+
+  std::cin >> someData;
+  std::cout << someData << "\n";
+
   return 0;
 }
 
