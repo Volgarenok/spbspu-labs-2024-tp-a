37e9583c17cc088f817908aa60d986a8b46eeae4
kumekova <161251059+kumekova@users.noreply.github.com>
2024-09-23 20:39:27 +0300

    Update DataStruct.hpp
    исправлено

--- kumekova.hasiyat/T2/DataStruct.hpp
+++ kumekova.hasiyat/T2/DataStruct.hpp
@@ -6,21 +6,19 @@
 
 namespace kumekova
 {
+    using KeyType = unsigned long long;
 
-using KeyType = unsigned long long;
+    struct DataStruct
+    {
+        KeyType key1{};
+        KeyType key2{};
+        std::string key3;
+    };
 
-struct DataStruct
-{
-  KeyType key1{};
-  KeyType key2{};
-  std::string key3;
-};
+    std::istream& operator>>(std::istream& stream, DataStruct& dataStruct);
+    std::ostream& operator<<(std::ostream& stream, const DataStruct& dataStruct);
 
-std::istream& operator>>(std::istream& stream, DataStruct& dataStruct);
-std::ostream& operator<<(std::ostream& stream, const DataStruct& dataStruct);
-
-bool operator<(const DataStruct& lhs, const DataStruct& rhs);
-
+    bool operator<(const DataStruct& lhs, const DataStruct& rhs);
 }
 
 #endif
