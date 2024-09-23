156abeceec4170b8c84bf5386a699571579d58ac
kumekova <161251059+kumekova@users.noreply.github.com>
2024-09-23 21:15:45 +0300

    Update main.cpp

--- kumekova.hasiyat/T2/main.cpp
+++ kumekova.hasiyat/T2/main.cpp
@@ -2,28 +2,39 @@
 #include <iterator>
 #include <limits>
 #include <vector>
+
 #include "DataStruct.hpp"
 
 int main()
 {
-    using namespace kumekova;
-    using InputIter = std::istream_iterator<DataStruct>;
-    using OutputIter = std::ostream_iterator<DataStruct>;
+  using namespace kumekova;
+  using InputIter = std::istream_iterator<DataStruct>;
+  using OutputIter = std::ostream_iterator<DataStruct>;
 
-    constexpr auto MaxStreamSize = std::numeric_limits<std::streamsize>::max();
-    std::vector<DataStruct> data;
+  constexpr auto MaxStreamSize = std::numeric_limits<std::streamsize>::max();
 
-    while (!std::cin.eof()) {
-        std::copy(InputIter(std::cin), InputIter(), std::back_inserter(data));
-        if (!std::cin) {
-            std::cin.clear();
-            std::cin.ignore(MaxStreamSize, '\n');
-        }
+  std::vector<DataStruct> data;
+
+  InputIter iter(std::cin);
+  InputIter end;
+
+  while (iter != end)
+  {
+    DataStruct dataStruct;
+    *iter >> dataStruct;
+    if (std::cin.fail()) {
+      std::cerr << "Error: invalid input\n";
+      std::cin.clear();
+      std::cin.ignore(MaxStreamSize, '\n');
+    } else {
+      data.push_back(dataStruct);
     }
+    ++iter;
+  }
 
-    std::sort(data.begin(), data.end());
+  std::sort(data.begin(), data.end());
 
-    std::copy(data.begin(), data.end(), OutputIter(std::cout, "\n"));
+  std::copy(data.begin(), data.end(), OutputIter(std::cout, "\n"));
 
-    return 0;
+  return 0;
 }
