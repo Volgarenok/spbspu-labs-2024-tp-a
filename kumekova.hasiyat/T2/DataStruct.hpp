73acd1c1011c0fdf876d76fc1a5ee74b37971ada
kumekova <161251059+kumekova@users.noreply.github.com>
2024-10-03 22:39:29 +0300

    Update DataStruct.hpp

--- kumekova.hasiyat/T2/DataStruct.hpp
+++ kumekova.hasiyat/T2/DataStruct.hpp
@@ -1,26 +1,15 @@
-#ifndef DATA_STRUCT_HPP
-#define DATA_STRUCT_HPP
+#ifndef DATASTRUCT_HPP
+#define DATASTRUCT_HPP
 
 #include <string>
 #include <iostream>
 
-namespace kumekova
-{
+namespace kumekova {
 
-using KeyType = unsigned long long;
-
-struct DataStruct
-{
-  KeyType key1{};
-  KeyType key2{};
-  std::string key3;
+struct DataStruct {
+    unsigned long long key1;
+    unsigned long long key2;
+    std::string key3;
 };
 
-std::istream& operator>>(std::istream& stream, DataStruct& dataStruct);
-std::ostream& operator<<(std::ostream& stream, const DataStruct& dataStruct);
-
-bool operator<(const DataStruct& lhs, const DataStruct& rhs);
-
 }
-
-#endif
