80f7e3f6fb691a1a4401de1d4a22b562b2b4dbac
kumekova <161251059+kumekova@users.noreply.github.com>
2024-10-03 23:04:01 +0300

    Update DataStruct.hpp

--- kumekova.hasiyat/T2/DataStruct.hpp
+++ kumekova.hasiyat/T2/DataStruct.hpp
@@ -10,6 +10,9 @@ struct DataStruct {
     unsigned long long key1;
     unsigned long long key2;
     std::string key3;
+
+    DataStruct(unsigned long long key1, unsigned long long key2, const std::string& key3)
+        : key1(key1), key2(key2), key3(key3) {}
 };
 std::istream& operator>>(std::istream& stream, DataStruct& dataStruct);
 std::ostream& operator<<(std::ostream& stream, const DataStruct& dataStruct);
