de38155f95a8384f5d7e68e1819fe493eaf29f9e
kumekova <161251059+kumekova@users.noreply.github.com>
2024-10-03 19:04:48 +0300

    Update DataStruct.cpp

--- kumekova.hasiyat/T2/DataStruct.cpp
+++ kumekova.hasiyat/T2/DataStruct.cpp
@@ -1,112 +1,80 @@
 #include <algorithm>
 #include <tuple>
-
 #include "DataStruct.hpp"
-
 namespace kumekova
 {
-namespace
-{
-
-constexpr size_t KeyCount = 3;
-constexpr size_t KeyLength = 4;
-constexpr size_t SpaceLength = 1;
-constexpr size_t PrefixLength = 2;
-constexpr size_t SuffixLength = 3;
-
-bool parse(const std::string& part, DataStruct& dataStruct)
-{
-  constexpr auto ValueOffset = KeyLength + SpaceLength;
-
-  const auto partLength = part.size();
-  if (partLength < ValueOffset) {
-    return false;
-  }
-
-  const std::string keyStr(part.data(), KeyLength);
-  const std::string valueStr(part.data() + ValueOffset, partLength - ValueOffset);
-
-  if (keyStr == "key3") {
-    dataStruct.key3 = valueStr;
-    return true;
-  }
-
-  const auto valueLength = valueStr.size();
-
-  if (keyStr == "key2" && valueLength > PrefixLength) {
-    if (valueStr[0] == '0' && std::tolower(valueStr[1]) == 'x') {
-      dataStruct.key2 = std::strtoull(valueStr.data(), nullptr, 16);
-      return true;
+    std::istream& operator>>(std::istream& stream, DataStruct& dataStruct)
+    {
+        char c;
+        std::string token;
+        stream >> c; // read the opening parenthesis
+        if (c != '(') 
+        {
+            stream.setstate(std::ios::failbit);
+            throw std::runtime_error("Invalid input format: expected '('");
+        }
+        while (stream >> token) 
+        {
+            if (token == "key1") 
+            {
+                if (token[0] == '0' && (token[1] == 'x' || token[1] == 'X')) 
+                {
+                    stream >> std::hex >> dataStruct.key1;
+                }
+                else 
+                {
+                    stream.setstate(std::ios::failbit);
+                    throw std::runtime_error("Invalid input format: expected hexadecimal or decimal value for key1");
+                }
+               
+            }
+            else if (token == "key2") 
+            {
+                unsigned long long ull;
+                stream >> ull;
+                if ((token[0] == 'u' && token[1] == 'l' && token[2] == 'l')|| (token[0] == 'U' && token[1] == 'L' && token[2] == 'L'))
+                {
+                    stream >> std::hex >> dataStruct.key2;
+                }
+                else
+                {
+                    stream.setstate(std::ios::failbit);
+                    throw std::runtime_error("Invalid input format: expected hexadecimal or decimal value for key2");
+                }
+            }
+            else if (token == "key3") 
+            {
+                stream >> c; // read the double quote
+                if (c != '"')
+                {
+                    stream.setstate(std::ios::failbit);
+                    throw std::runtime_error("Invalid input format: expected '\"'");
+                }
+                std::getline(stream, dataStruct.key3, '"');
+            }
+           
+        }
+        if (stream.peek() == '\n' || stream.peek() == '\r') {
+            stream.setstate(std::ios::failbit);
+            throw std::runtime_error("Invalid input format: empty string");
+        }
+        return stream;
+	};
+	std::ostream& operator<<(std::ostream& stream, const DataStruct& dataStruct)
+	{
+        stream << "(" << "key1 " << dataStruct.key1 << ":key2 '" << dataStruct.key2 << "':key3 \"" << dataStruct.key3 << "\"";
+        return stream;
+	};
+    bool operator<(const DataStruct& lhs, const DataStruct& rhs)
+    {
+        if (lhs.key1 != rhs.key1) {
+            return lhs.key1 < rhs.key1;
+        }
+        else if (lhs.key2 != rhs.key2) {
+            return lhs.key2 < rhs.key2;
+        }
+        else {
+            return lhs.key3.size() < rhs.key3.size();
+        }
     }
-  }
-
-  if (keyStr == "key1" && valueLength > SuffixLength) {
-    const auto suffix = valueStr.substr(valueLength - SuffixLength);
-
-    if (std::tolower(suffix[0]) == 'u' && std::tolower(suffix[1]) == 'l' && std::tolower(suffix[2]) == 'l') {
-      const auto trimmed = valueStr.substr(0, valueLength - SuffixLength);
-      dataStruct.key1 = std::strtoull(trimmed.data(), nullptr, 10);
-      return true;
-    }
-  }
-
-  return false;
-}
-
-void printHex(std::ostream& stream, const KeyType value, bool uppercase = true)
-{
-  if (uppercase)
-  {
-    stream << std::uppercase;
-  }
-  else
-  {
-    stream << std::nouppercase;
-  }
-  stream << std::hex << std::showbase;
-  stream << value;
-  stream << std::dec << std::noshowbase;
-}
-
-} // namespace
-
-std::istream& operator>>(std::istream& stream, DataStruct& dataStruct)
-{
-  constexpr auto NoPos = std::string::npos;
-  constexpr auto FailBit = std::ios::failbit;
-
-  std::string line;
-  std::getline(stream, line);
-
-  size_t from = line.find(':');
-  if (from == NoPos) {
-    stream.setstate(FailBit);
-    return stream;
-  }
-
-  for (size_t i = 0; i < KeyCount; ++i) {
-    ++from;
-    const auto to = line.find(':', from);
-    if (to == NoPos || !parse(std::string(line.c_str() + from, to - from), dataStruct)) {
-      stream.setstate(FailBit);
-      return stream;
-    }
-    from = to;
-  }
-
-  return stream;
-}
-
-std::ostream& operator<<(std::ostream& stream, const DataStruct& dataStruct)
-{
-  stream << "(:key1 " << dataStruct.key1 << "ULL:key2 ";
-  printHex(stream, dataStruct.key2, true);
-  return stream << ":key3 " << dataStruct.key3 << ":)";
-}
-
-bool operator<(const DataStruct& lhs, const DataStruct& rhs)
-{
-  return std::make_tuple(lhs.key1, lhs.key2, lhs.key3.size()) < std::make_tuple(rhs.key1, rhs.key2, rhs.key3.size());
-}
-
-}
+}//DataStruct.cpp
