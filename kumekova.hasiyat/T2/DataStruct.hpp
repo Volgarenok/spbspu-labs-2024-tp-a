a4bf5f850a119b91f68360905249dd57d134e0c2
kumekova <161251059+kumekova@users.noreply.github.com>
2024-10-03 23:19:12 +0300

    Update DataStruct.hpp

--- kumekova.hasiyat/T2/DataStruct.hpp
+++ kumekova.hasiyat/T2/DataStruct.hpp
@@ -11,6 +11,7 @@ struct DataStruct {
     unsigned long long key2;
     std::string key3;
 
+    DataStruct() : key1(0), key2(0), key3("") {} // default constructor
     DataStruct(unsigned long long key1, unsigned long long key2, const std::string& key3)
         : key1(key1), key2(key2), key3(key3) {}
 };
