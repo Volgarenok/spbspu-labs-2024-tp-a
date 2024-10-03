553ba9b389307357786e24433829be39af22d88d
kumekova <161251059+kumekova@users.noreply.github.com>
2024-10-04 00:12:43 +0300

    Update DataStruct.cpp

--- kumekova.hasiyat/T2/DataStruct.cpp
+++ kumekova.hasiyat/T2/DataStruct.cpp
@@ -1,113 +1,107 @@
+#include <algorithm>
+#include <tuple>
+
 #include "DataStruct.hpp"
 
-namespace kumekova {
+namespace kumekova
+{
+namespace
+{
 
-void printHex(std::ostream& stream, const unsigned long long value) {
-    stream << std::uppercase << std::hex << std::showbase
-           << value
-           << std::nouppercase << std::dec << std::noshowbase;
+constexpr size_t KeyCount = 3;
+constexpr size_t KeyLength = 4; 
+constexpr size_t SpaceLength = 1;
+constexpr size_t PrefixLength = 2;
+constexpr size_t SuffixLength = 3;
+
+bool parse(const std::string& part, DataStruct& dataStruct)
+{
+  constexpr auto ValueOffset = KeyLength + SpaceLength;
+
+  const auto partLength = part.size();
+  if (partLength < ValueOffset) {
+    return false;
+  }
+
+  const std::string keyStr(part.data(), KeyLength);
+  const std::string valueStr(part.data() + ValueOffset, partLength - ValueOffset);
+
+  if (keyStr == "key3") {
+    dataStruct.key3 = valueStr;
+    return true;
+  }
+
+  const auto valueLength = valueStr.size();
+
+  if (keyStr == "key2" && valueLength > PrefixLength) {
+    if (valueStr[0] == '0' && std::tolower(valueStr[1]) == 'x') {
+      dataStruct.key2 = std::strtoull(valueStr.data(), nullptr, 16);
+      return true;
+    }
+  }
+
+  if (keyStr == "key1" && valueLength > SuffixLength) {
+    const auto suffix = valueStr.substr(valueLength - SuffixLength);
+
+    if (std::tolower(suffix[0]) == 'u' && std::tolower(suffix[1]) == 'l' && std::tolower(suffix[2]) == 'l') {
+      const auto trimmed = valueStr.substr(0, valueLength - SuffixLength);
+      dataStruct.key1 = std::strtoull(trimmed.data(), nullptr, 10);
+      return true;
+    }
+  }
+
+  return false;
 }
 
-std::istream& operator>>(std::istream& stream, DataStruct& dataStruct) {
-    std::string line;
-    std::getline(stream, line);
+void printHex(std::ostream& stream, const KeyType value)
+{
+  stream
+    << std::uppercase << std::hex << std::showbase
+    << value
+    << std::nouppercase << std::dec << std::noshowbase;
+}
 
-    size_t from = line.find(':');
-    if (from == std::string::npos) {
-        stream.setstate(std::ios::failbit);
-        return stream;
-    }
+} // namespace
 
-    for (size_t i = 0; i < 3; ++i) {
-        ++from;
-        const auto to = line.find(':', from);
-        if (to == std::string::npos) {
-            stream.setstate(std::ios::failbit);
-            return stream;
-        }
+std::istream& operator>>(std::istream& stream, DataStruct& dataStruct)
+{
+  constexpr auto NoPos = std::string::npos;
+  constexpr auto FailBit = std::ios::failbit;
 
-        std::string part(line.c_str() + from, to - from);
-        if (part == "key1") {
-            from = to;
-            const auto valueFrom = line.find(':', from);
-            if (valueFrom == std::string::npos) {
-                stream.setstate(std::ios::failbit);
-                return stream;
-            }
-            std::string valueStr(line.c_str() + valueFrom + 1);
-            if (valueStr.size() > 2 && valueStr[0] == '0' && (valueStr[1] == 'x' || valueStr[1] == 'X')) {
-                valueStr = valueStr.substr(2);
-                try {
-                    dataStruct.key1 = std::stoull(valueStr, nullptr, 16);
-                } catch (const std::invalid_argument& e) {
-                    stream.setstate(std::ios::failbit);
-                    return stream;
-                }
-            } else {
-                try {
-                    dataStruct.key1 = std::stoull(valueStr, nullptr, 10);
-                } catch (const std::invalid_argument& e) {
-                    stream.setstate(std::ios::failbit);
-                    return stream;
-                }
-            }
-        } else if (part == "key2") {
-            from = to;
-            const auto valueFrom = line.find(':', from);
-            if (valueFrom == std::string::npos) {
-                stream.setstate(std::ios::failbit);
-                return stream;
-            }
-            std::string valueStr(line.c_str() + valueFrom + 1);
-            if (valueStr.size() > 2 && valueStr[valueStr.size() - 2] == 'u' && valueStr[valueStr.size() - 1] == 'l') {
-                valueStr = valueStr.substr(0, valueStr.size() - 2);
-                try {
-                    dataStruct.key2 = std::stoull(valueStr, nullptr, 10);
-                } catch (const std::invalid_argument& e) {
-                    stream.setstate(std::ios::failbit);
-                    return stream;
-                }
-            } else {
-                try {
-                    dataStruct.key2 = std::stoull(valueStr, nullptr, 10);
-                } catch (const std::invalid_argument& e) {
-                    stream.setstate(std::ios::failbit);
-                    return stream;
-                }
-            }
-        } else if (part == "key3") {
-            from = to;
-            const auto valueFrom = line.find(':', from);
-            if (valueFrom == std::string::npos) {
-                stream.setstate(std::ios::failbit);
-                return stream;
-            }
-            std::string valueStr(line.c_str() + valueFrom + 1);
-            dataStruct.key3 = valueStr;
-        }
-    }
+  std::string line;
+  std::getline(stream, line);
 
+  size_t from = line.find(':');
+  if (from == NoPos) {
+    stream.setstate(FailBit);
     return stream;
+  }
+
+  for (size_t i = 0; i < KeyCount; ++i) {
+    ++from;
+    const auto to = line.find(':', from);
+    if (to == NoPos || !parse(std::string(line.c_str() + from, to - from), dataStruct)) {
+      stream.setstate(FailBit);
+      return stream;
+    }
+    from = to;
+  }
+
+  return stream;
 }
 
-std::ostream& operator<<(std::ostream& stream, const DataStruct& dataStruct) {
-    stream << "(:key1 ";
-    if (dataStruct.key1 != 0) {
-        stream << "0x";
-    }
-    printHex(stream, dataStruct.key1);
-    stream << " " << dataStruct.key2 << " " << dataStruct.key3;
-    return stream;
+std::ostream& operator<<(std::ostream& stream, const DataStruct& dataStruct)
+{
+  stream << "(:key1 " << dataStruct.key1 << "ULL:key2 ";
+  printHex(stream, dataStruct.key2);
+  return stream << ":key3 " << dataStruct.key3 << ":)";
 }
 
-bool DataStruct::operator<(const DataStruct& rhs) const {
-    if (key1 != rhs.key1) {
-        return key1 < rhs.key1;
-    } else if (key2 != rhs.key2) {
-        return key2 < rhs.key2;
-    } else {
-        return key3 < rhs.key3;
-    }
+bool operator<(const DataStruct& lhs, const DataStruct& rhs)
+{
+  return
+    std::make_tuple(lhs.key1, lhs.key2, lhs.key3.size()) <
+    std::make_tuple(rhs.key1, rhs.key2, rhs.key3.size());
 }
 
 }
