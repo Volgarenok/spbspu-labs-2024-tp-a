40ab371253664dd4fc729c985249c5b5a9493221
kumekova <161251059+kumekova@users.noreply.github.com>
2024-10-03 23:50:03 +0300

    Update main.cpp

--- kumekova.hasiyat/T2/main.cpp
+++ kumekova.hasiyat/T2/main.cpp
@@ -8,26 +8,21 @@
 int main()
 {
   using namespace kumekova;
-  using InputIter = std::istream_iterator<DataStruct>;
-  using OutputIter = std::ostream_iterator<DataStruct>;
-
-  constexpr auto MaxStreamSize = std::numeric_limits<std::streamsize>::max();
-
   std::vector<DataStruct> data;
 
   while (!std::cin.eof()) {
     DataStruct record;
-        if (std::cin >> record) {
-            data.push_back(record);
-        } else {
-            std::cin.clear();
-            std::cin.ignore(MaxStreamSize, '\n');
-        }
+    if (std::cin >> record) {
+      data.push_back(record);
+    } else {
+      std::cin.clear();
+      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
     }
+  }
 
   std::sort(data.begin(), data.end());
 
-  std::copy(data.begin(), data.end(), OutputIter(std::cout, "\n"));
+  std::copy(data.begin(), data.end(), std::ostream_iterator<DataStruct>(std::cout, "\n"));
 
   return 0;
 }
