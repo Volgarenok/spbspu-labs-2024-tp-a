013e98f96c0899cd25c488fd131d0fe6f7f93666
kumekova <161251059+kumekova@users.noreply.github.com>
2024-10-03 20:05:08 +0300

    Update main.cpp

--- kumekova.hasiyat/T2/main.cpp
+++ kumekova.hasiyat/T2/main.cpp
@@ -8,14 +8,14 @@ int main()
 	using namespace kumekova;
 	std::vector<DataStruct> data;
 	try
-		{
-			std::copy(std::istream_iterator<DataStruct>(std::cin), std::istream_iterator<DataStruct>(), std::back_inserter(data));
-		}
+	{
+		std::copy(std::istream_iterator<DataStruct>(std::cin), std::istream_iterator<DataStruct>(), std::back_inserter(data));
+	}
 	catch (const std::exception& e)
-		{
-			std::cerr << "Error: " << e.what() << std::endl;
-			return 1;
-		}
+	{
+		std::cerr << "Error: " << e.what() << std::endl;
+		return 1;
+	}
 	std::sort(data.begin(), data.end());
 	std::copy(data.begin(), data.end(), std::ostream_iterator<DataStruct>(std::cout, "\n"));
 	return 0;
