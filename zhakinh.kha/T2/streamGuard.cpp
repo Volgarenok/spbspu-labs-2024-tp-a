1c5b949f6e1d49ec06eeebfe2e9a03e085f1d6a5
sahaki13 <hagiakinh13@gmail.com>
2024-10-06 17:56:41 +0300

    fix temp

--- zhakinh.kha/T2/streamGuard.cpp
+++ zhakinh.kha/T2/streamGuard.cpp
@@ -1,9 +1,6 @@
 #include "streamGuard.hpp"
 
-#include <iostream>
-#include <utility>
-
-zhakha::StreamGuard::StreamGuard(std::basic_ios<char>& s):
+zhakha::StreamGuard::StreamGuard(std::basic_ios< char >& s):
   s_(s),
   precision_(s.precision()),
   flags_(s.flags())
