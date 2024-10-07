2cb780e55fb8935bdd7260b3f6ffa1050e549a29
kumekova <161251059+kumekova@users.noreply.github.com>
2024-10-07 04:02:29 +0300

    Update main.cpp

--- kumekova.hasiyat/T2/main.cpp
+++ kumekova.hasiyat/T2/main.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 #include <vector>
 #include <string>
 #include <iterator>
@@ -16,32 +16,32 @@ using kumekova::DataStruct;
 int main()
 {
     try
+    {
+        std::string input = "";
+        std::vector<DataStruct> data;
+
+        while (std::getline(std::cin, input))
         {
-            std::string input = "";
-            std::vector<DataStruct> data;
-            
-            while (std::getline(std::cin, input))
+            std::istringstream iss(input);
+            DataStruct temp;
+            if (iss >> temp)
             {
-                std::istringstream iss(input);
-                DataStruct temp;
-                if (iss >> temp)
-                {
-                    data.push_back(temp);
-                }
+                data.push_back(temp);
             }
-            
-            std::sort(std::begin(data), std::end(data), kumekova::compareDataStruct);
-            
-            std::copy(
-                std::begin(data),
-                std::end(data),
-                std::ostream_iterator<DataStruct>(std::cout, "\n")
-                );
         }
+
+        std::sort(std::begin(data), std::end(data), kumekova::compareDataStruct);
+
+        std::copy(
+            std::begin(data),
+            std::end(data),
+            std::ostream_iterator<DataStruct>(std::cout, "\n")
+        );
+    }
     catch (std::exception& ex)
-        {
-            std::cerr << ex.what();
-            return EXIT_FAILURE;
-        }
+    {
+        std::cerr << ex.what();
+        return EXIT_FAILURE;
+    }
     return EXIT_SUCCESS;
 }
