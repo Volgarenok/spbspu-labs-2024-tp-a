c3f78ca8a55d1143a8449a76a187228aa9b11531
vktrchh <viskas6969@gmail.com>
2024-10-09 15:54:00 +0300

    add files from T2

---
+++ vojuck.victoria/T3/delimiter.hpp
@@ -0,0 +1,40 @@
+#ifndef DELIMETER_HPP
+#define DELIMETER_HPP
+
+#include <string>
+#include <iosfwd>
+#include <iostream>
+
+namespace vojuck
+{
+  struct DelimiterIO
+  {
+    char exp;
+  };
+
+  struct StringIO
+  {
+    std::string &ref;
+  };
+
+  struct HexULL
+  {
+    unsigned long long &ref;
+  };
+
+  struct LabelIO
+  {
+    std::string exp;
+  };
+
+  struct CharIO
+  {
+    char &ref;
+  };
+  std::istream &operator>>(std::istream &in, DelimiterIO &&dest);
+  std::istream &operator>>(std::istream &in, StringIO &&dest);
+  std::istream &operator>>(std::istream &in, HexULL &&dest);
+  std::istream &operator>>(std::istream &in, LabelIO &&dest);
+  std::istream &operator>>(std::istream &in, CharIO &&dest);
+}
+#endif
