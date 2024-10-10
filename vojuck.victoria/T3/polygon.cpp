b7d0243cd61fd0d9f1d4d01ef296d5f06806d842
vktrchh <viskas6969@gmail.com>
2024-10-10 22:13:02 +0300

    brain revolution girl reference (int -> size_t)

--- vojuck.victoria/T3/polygon.cpp
+++ vojuck.victoria/T3/polygon.cpp
@@ -61,7 +61,7 @@ bool vojuck::Point::operator==(const vojuck::Point& point) const
 bool vojuck::Polygon::operator==(const vojuck::Polygon& poly) const
 {
   if (points.size() != poly.points.size()) return 0;
-  for (int i = 0; i < points.size(); i++)
+  for (size_t i = 0; i < points.size(); i++)
   {
     if (!(points[i] == poly.points[i])) return 0;
   }
