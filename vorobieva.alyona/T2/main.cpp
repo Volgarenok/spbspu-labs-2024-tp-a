68e55675dfc100e76bd2288385f0514f6fa1a3c0
VorobievaA <listiknew@gmail.com>
2024-10-13 14:47:31 +0300

    test case

---
+++ vorobieva.alyona/T2/main.cpp
@@ -0,0 +1,26 @@
+//#include "DataStruct.hpp"
+#include <algorithm>
+#include <complex>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <vector>
+
+int main() {
+  //using namespace vorobieva;
+
+  //using input_it = std::istream_iterator<DataStruct>;
+  //using output_it = std::ostream_iterator<DataStruct>;
+
+  //std::vector<DataStruct> data;
+
+  //while (!std::cin.eof()) {
+    //if (std::cin.fail()) {
+      //std::cin.clear();
+      //std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    //}
+    //std::copy(input_it{std::cin}, input_it{}, std::back_inserter(data));
+  //}
+  //std::sort(data.begin(), data.end());
+  //std::copy(std::begin(data), std::end(data), output_it{std::cout, "\n"});
+}
