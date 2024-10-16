3a12bb45074b3cc04d4aea3fb36166cff02dcfb3
Gorgor777 <sergei-agarkov07@mail.ru>
2024-10-17 01:27:19 +0300

    types proto

--- agarkov.sergey/T2/iotypes.hpp
+++ agarkov.sergey/T2/iotypes.hpp
@@ -1,7 +1,35 @@
 #ifndef IOTYPES_HPP
 #define IOTYPES_HPP
 
-#include <string>
 #include <complex>
+
+namespace agarkov
+{
+  struct DelimiterIO
+  {
+    char exp;
+  };
+
+  struct CharIO
+  {
+    char& num;
+  };
+
+  struct ComplexIO
+  {
+    std::complex< double >& cmp;
+  };
+
+  struct StringIO
+  {
+    std::string& str;
+  };
+
+  std::istream& operator>>(std::istream& in, DelimiterIO&& dest);
+  std::istream& operator>>(std::istream& in, CharIO&& dest);
+  std::istream& operator>>(std::istream& in, ComplexIO&& dest);
+  std::istream& operator>>(std::istream& in, StringIO&& dest);
+}
+
 
 #endif
