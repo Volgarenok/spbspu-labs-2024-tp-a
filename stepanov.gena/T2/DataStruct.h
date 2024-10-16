8b3e85923cbd395e04d16e99a34a5c1ceb6c971d
stepanov.gr@edu.spbstu.ru <stepanov.gr@edu.spbstu.ru>
2024-10-16 18:31:32 +0300

    DataStruct header

---
+++ stepanov.gena/T2/DataStruct.h
@@ -0,0 +1,20 @@
+#ifndef DATASTRUCT_H
+#define DATASTRUCT_H
+
+#include <string>
+
+namespace stepanov
+{
+  struct DataStruct
+  {
+    unsigned long long key1_;
+    unsigned long long key2_;
+    std::string key3_;
+  };
+
+  std::istream& operator>>(std::istream& in, DataStruct& dest);
+  std::ostream& operator<<(std::ostream& out, const DataStruct& dest);
+  bool operator<(const DataStruct& first, const DataStruct& second);
+}
+
+#endif
