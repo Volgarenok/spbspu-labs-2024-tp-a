a44fce53a8383348e22123fba0583a4c9cc1109d
Gorgor777 <sergei-agarkov07@mail.ru>
2024-10-17 01:20:07 +0300

    scope guard

---
+++ agarkov.sergey/T2/scopeguard.hpp
@@ -0,0 +1,23 @@
+#ifndef SCOPEGUARD_HPP
+#define SCOPEGUARD_HPP
+
+#include <ios>
+
+namespace agarkov
+{
+  class iofmtguard
+  {
+  public:
+    explicit iofmtguard(std::basic_ios< char >& s);
+    ~iofmtguard();
+  private:
+    std::basic_ios< char >& s_;
+    char fill_;
+    std::streamsize precision_;
+    std::basic_ios< char >::fmtflags fmt_;
+  };
+}
+
+#endif
+
+ 
