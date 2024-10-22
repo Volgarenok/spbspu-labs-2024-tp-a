7ded5873ad0f448b1b8bbd51a523c7856bd4ef4e
Gorgor777 <sergei-agarkov07@mail.ru>
2024-10-22 21:55:14 +0300

    == poly

--- agarkov.sergey/T3/polygon.cpp
+++ agarkov.sergey/T3/polygon.cpp
@@ -23,3 +23,32 @@ bool agarkov::operator==(const Point& lhs, const Point& rhs)
   return (lhs.x_ == rhs.x_) && (lhs.y_ == rhs.y_);
 }
 
+std::istream& agarkov::operator>>(std::istream& in, Polygon& dest)
+{
+  std::istream::sentry sentry(in);
+  if (!sentry)
+  {
+    return in;
+  }
+  size_t count = 0;
+  in >> count;
+  if (count < 3)
+  {
+    in.setstate(std::ios::failbit);
+    return in;
+  }
+  using iter = std::istream_iterator< Point >;
+  dest.points_.clear();
+  std::copy_n(iter(in), count, std::back_inserter(dest.points_));
+  if (dest.points_.size() != count)
+  {
+    in.setstate(std::ios::failbit);
+    return in;
+  }
+  return in;
+}
+
+bool agarkov::operator==(const Polygon& lhs, const Polygon& rhs)
+{
+  return lhs.points_ == rhs.points_;
+}
