1cf22b9911cf9bf0ad89547aac0ea455a8a78978
Gorgor777 <sergei-agarkov07@mail.ru>
2024-10-22 21:07:24 +0300

    input point

--- agarkov.sergey/T3/polygon.cpp
+++ agarkov.sergey/T3/polygon.cpp
@@ -1,1 +1,20 @@
+#include "polygon.hpp"
+#include <iterator>
+#include <algorithm>
+#include <iotypes.hpp>
+
+std::istream& agarkov::operator>>(std::istream& in, Point& dest)
+{
+  std::istream::sentry sentry(in);
+  if (!sentry)
+  {
+    return in;
+  }
+  in >> DelimiterIO{'('};
+  in >> dest.x_;
+  in >> DelimiterIO{';'};
+  in >> dest.y_;
+  in >> DelimiterIO{')'};
+  return in;
+}
  
