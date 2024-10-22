07b6581ef703cd3a2978959f50a356af559e1c71
Gorgor777 <sergei-agarkov07@mail.ru>
2024-10-22 22:16:00 +0300

    calc area

--- agarkov.sergey/T3/commands.cpp
+++ agarkov.sergey/T3/commands.cpp
@@ -1,1 +1,12 @@
+#include "command.hpp"
+#include <cmath>
+#include <scopeguard.hpp>
+#include "polygon.hpp"
  
+namespace
+{
+  double calcArea(const agarkov::Point & left, const agarkov::Point & right)
+  {
+    return 0.5 * (left.x_ * right.y_ - right.x_ * left.y_);
+  }
+}
--- agarkov.sergey/T3/commands.hpp
+++ agarkov.sergey/T3/commands.hpp
@@ -1,1 +1,14 @@
- 
+#ifndef COMMAND_HPP
+#define COMMAND_HPP
+
+#include <vector>
+#include <ostream>
+#include "polygon.hpp"
+
+namespace agarkov
+{
+  void getAreaEven(const std::vector< Polygon >& polygons, std::ostream& out);
+  void getAreaOdd(const std::vector< Polygon >& polygons, std::ostream& out);
+}
+
+#endif
