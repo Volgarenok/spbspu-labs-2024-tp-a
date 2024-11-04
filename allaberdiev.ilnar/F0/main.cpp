422973bd698dde8f998038f2416bb066e74a8638
allaberdiev.ilnar <dedbedblack@gmail.com>
2024-11-04 15:26:37 +0300

    added commands in main.cpp

--- allaberdiev.ilnar/F0/main.cpp
+++ allaberdiev.ilnar/F0/main.cpp
@@ -1,3 +1,43 @@
-int main(){
-    return 0;
+#include <map>
+#include <iostream>
+#include <functional>
+#include <string>
+#include <vector>
+#include <limits>
+#include "commands.hpp"
+
+int main() {
+  using namespace allaberdiev;
+  std::map< std::string, std::map< std::string, std::vector< std::string > > > dicts = {};
+  using funcName = std::function< void(std::istream&, std::map< std::string, std::map< std::string, std::vector< std::string > > >&) >;
+  std::map< std::string, funcName > commands = {};
+  {
+    using namespace std::placeholders;
+    commands["create"] = createDict;
+    commands["delete"] = deleteDict;
+    commands["add"] = addWord;
+    commands["remove"] = removeWord;
+    commands["translate"] = std::bind(translateWord, std::ref(std::cout), _1, _2);
+    commands["merge"] = mergeDict;
+  }
+
+  while (!std::cin.eof())
+  {
+    try
+    {
+      std::string commandName = "";
+      std::cin >> commandName;
+      commands.at(commandName)(std::cin, dicts);
+    }
+    catch (const std::out_of_range& e)
+    {
+      std::cerr << "<INVALID COMMAND>\n";
+    }
+    catch(const std::exception& e)
+    {
+      std::cerr << e.what() << '\n';
+    }
+    std::cin.clear();
+    std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+  }
 }
