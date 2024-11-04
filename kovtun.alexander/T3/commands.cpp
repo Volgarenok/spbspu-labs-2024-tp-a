6137e04c6819c782aa5d73ab7edf7ddbc09ca7bc
puzz13 <puzz13one@gmail.com>
2024-11-05 01:49:26 +0300

    Add draft arg handler

--- kovtun.alexander/T3/commands.cpp
+++ kovtun.alexander/T3/commands.cpp
@@ -3,8 +3,26 @@
 
 void kovtun::area(const std::vector< kovtun::Polygon > & polygons, std::istream & in, std::ostream & out)
 {
-    for (int i = 0; i < polygons.size(); i++)
-    {
-        out << "area: " << kovtun::getArea(polygons[i]) << "\n";
-    }
+  std::map< std::string, std::function< bool(const kovtun::Polygon & polygon) > > args
+  {
+    {"EVEN", isEven}
+  };
+
+  std::string arg;
+  in >> arg;
+
+  std::vector< kovtun::Polygon > selection;
+  if (args.find(arg) != args.end())
+  {
+    std::copy_if(polygons.begin(), polygons.end(), std::back_inserter(selection), args.at(arg));
+  }
+
+  std::vector< double > result(selection.size());
+  std::transform(selection.cbegin(), selection.cend(), result.begin(), getArea);
+  out << std::accumulate(result.cbegin(), result.cend(), 0.0);
+}
+
+bool kovtun::isEven(const kovtun::Polygon & polygon)
+{
+  return polygon.points.size() % 2 == 0;
 }
--- kovtun.alexander/T3/commands.hpp
+++ kovtun.alexander/T3/commands.hpp
@@ -2,6 +2,8 @@
 #define COMMANDS_HPP
 
 #include <iostream>
+#include <map>
+#include <functional>
 
 #include "polygon.hpp"
 
@@ -9,6 +11,8 @@
 namespace kovtun
 {
   void area(const std::vector< kovtun::Polygon > & polygons, std::istream & in, std::ostream & out);
+
+  bool isEven(const kovtun::Polygon & polygon);
 }
 
 #endif
