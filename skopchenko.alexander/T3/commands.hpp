d1b5df5284bf725cc846cbcbc291f2b9bb1e1ed7
polykisa <s.sanek2005@gmail.com>
2024-11-06 02:43:55 +0300

    added headers for remaining comms

--- skopchenko.alexander/T3/commands.hpp
+++ skopchenko.alexander/T3/commands.hpp
@@ -1,5 +1,5 @@
-#ifndef COMMANDS_HPP
-#define COMMANDS_HPP
+#ifndef COMANDS_HPP
+#define COMANDS_HPP
 
 #include <iostream>
 #include <vector>
@@ -12,6 +12,8 @@ namespace skopchenko
   void getMin(const std::vector< Polygon >& data, std::istream& in, std::ostream& out);
   void getCount(const std::vector< Polygon >& data, std::istream& in, std::ostream& out);
   void getIntersections(const std::vector< Polygon >& data, std::istream& in, std::ostream& out);
+  void getMaxSeq(std::vector< Polygon > polygons, std::istream& in, std::ostream& out);
+  void getRmecho(std::vector< Polygon > polygons, std::istream& in, std::ostream& out);
   struct SeqCounter
   {
     size_t count{ 0 };
