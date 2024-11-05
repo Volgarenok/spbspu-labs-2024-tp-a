322a2300737cc9f5437f2576a91d6325ba4cbb93
puzz13 <puzz13one@gmail.com>
2024-11-05 15:46:17 +0300

    Fix input

--- kovtun.alexander/T3/main.cpp
+++ kovtun.alexander/T3/main.cpp
@@ -58,6 +58,8 @@ int main(int argc, char * argv[])
     {
       std::cout << "<INVALID COMMAND>\n";
     }
+    std::cin.clear();
+    std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
   }
 
   return 0;
