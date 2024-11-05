4d04e4a95f97208b52c8610aab08c53361ee9731
polykisa <s.sanek2005@gmail.com>
2024-11-06 01:23:17 +0300

    added implementation of getarea and a few support functions so that it works

--- skopchenko.alexander/T3/commands.cpp
+++ skopchenko.alexander/T3/commands.cpp
@@ -6,6 +6,101 @@
 #include "polygon.hpp"
 #include "stream_guard.hpp"
 
+bool isPolygon(const skopchenko::Polygon& polygon)
+{
+  return (polygon.points.size() > 2);
+}
+
+bool isEven(const skopchenko::Polygon& polygon)
+{
+  return (isPolygon(polygon)) && (polygon.points.size() % 2 == 0);
+}
+
+bool isOdd(const skopchenko::Polygon& polygon)
+{
+  return !isEven(polygon);
+}
+
+size_t maxSeq(std::vector< skopchenko::Polygon > polygons, const skopchenko::Polygon& given)
+{
+  std::vector< size_t > count;
+  skopchenko::SeqCounter seqCount;
+  using namespace std::placeholders;
+  std::transform(polygons.begin(), polygons.end(), std::back_inserter(count), std::bind(seqCount, _1, given));
+  return *(std::max_element(count.begin(), count.end()));
+}
+
+bool isProperSize(const skopchenko::Polygon& polygon, size_t number)
+{
+  return (polygon.points.size() == number);
+}
+
+void skopchenko::getCount(const std::vector< Polygon >& data, std::istream& in, std::ostream& out)
+{
+  std::string argument = "";
+  in >> argument;
+  if (argument == "EVEN")
+  {
+    out << std::count_if(data.begin(), data.end(), isEven);
+  }
+  else if (argument == "ODD")
+  {
+    out << std::count_if(data.begin(), data.end(), isOdd);
+  }
+  else
+  {
+    size_t pointsCount = std::stoull(argument);
+    if (pointsCount < 3)
+    {
+      throw std::logic_error("Wrong number of points");
+    }
+    using namespace std::placeholders;
+    std::function< bool(const Polygon&) > isRightCount = std::bind(isProperSize, _1, pointsCount);
+    out << std::count_if(data.begin(), data.end(), isRightCount);
+  }
+}
+
+void skopchenko::getArea(const std::vector< Polygon >& data, std::istream& in, std::ostream& out)
+{
+  StreamGuard guard(out);
+  out << std::setprecision(1) << std::fixed;
+  std::string argument = "";
+  in >> argument;
+  std::vector< Polygon > shapes;
+  shapes.reserve(data.size());
+  using namespace std::placeholders;
+  if (argument == "EVEN")
+  {
+    std::copy_if(data.cbegin(), data.cend(), std::back_inserter(shapes), isEven);
+  }
+  else if (argument == "ODD")
+  {
+    std::copy_if(data.cbegin(), data.cend(), std::back_inserter(shapes), isOdd);
+  }
+  else if (argument == "MEAN")
+  {
+    if (data.empty())
+    {
+      throw std::logic_error("No shapes to accumulate area");
+    }
+    std::copy(data.cbegin(), data.cend(), std::back_inserter(shapes));
+  }
+  else
+  {
+    size_t pointsCount = std::stoull(argument);
+    if (pointsCount < 3)
+    {
+      throw std::logic_error("Wrong number of points");
+    }
+    std::function< bool(const Polygon&) > isRightCount = std::bind(isProperSize, _1, pointsCount);
+    std::copy_if(data.cbegin(), data.cend(), std::back_inserter(shapes), isRightCount);
+  }
+  std::vector< double > areas(shapes.size());
+  std::transform(shapes.cbegin(), shapes.cend(), std::back_inserter(areas), getPolygonArea);
+  double result = std::accumulate(areas.cbegin(), areas.cend(), 0.0);
+  (argument == "MEAN") ? (out << result / data.size()) : (out << result);
+}
+
 bool comparePoints(const skopchenko::Polygon& first, const skopchenko::Polygon& second)
 {
   return (first.points.size() < second.points.size());
--- skopchenko.alexander/T3/commands.hpp
+++ skopchenko.alexander/T3/commands.hpp
@@ -7,8 +7,16 @@
 
 namespace skopchenko
 {
+  void getArea(const std::vector< Polygon >& data, std::istream& in, std::ostream& out);
   void getMax(const std::vector< Polygon >& data, std::istream& in, std::ostream& out);
   void getMin(const std::vector< Polygon >& data, std::istream& in, std::ostream& out);
+  void getCount(const std::vector< Polygon >& data, std::istream& in, std::ostream& out);
+ struct SeqCounter
+  {
+    size_t count{ 0 };
+    size_t operator()(const Polygon& polygon, const Polygon& given);
+    bool operator>(const size_t& n);
+  };
 }
 
 #endif
