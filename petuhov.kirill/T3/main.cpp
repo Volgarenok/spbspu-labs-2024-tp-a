d0bb478b7e9d6b6e3ad1a01fbdbd64bd461fd068
deadshumz <shumzz@bk.ru>
2024-11-07 02:23:17 +0300

    remove stringstream

--- petuhov.kirill/T3/main.cpp
+++ petuhov.kirill/T3/main.cpp
@@ -3,72 +3,118 @@
 #include "commands.hpp"
 #include <iostream>
 #include <string>
-#include <sstream>
 #include <vector>
 #include <iomanip>
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[])
+{
   using namespace petuhov;
 
-  if (argc != 2) {
+  if (argc != 2)
+  {
     std::cerr << "Using: ./program filename\n";
     return 1;
   }
 
-  try {
+  try
+  {
     std::vector<Polygon> polygons = read_polygons(argv[1]);
 
     std::string line;
-    while (std::getline(std::cin, line)) {
-      std::istringstream iss(line);
-      std::string command;
-      iss >> command;
+    while (std::getline(std::cin, line))
+    {
+      size_t space_pos = line.find(' ');
+      std::string command = line.substr(0, space_pos);
+      std::string param = (space_pos != std::string::npos) ? line.substr(space_pos + 1) : "";
 
-      if (command == "AREA") {
-        std::string param;
-        iss >> param;
-        if (param == "EVEN") {
+      if (command == "AREA")
+      {
+        if (param == "EVEN")
+        {
           std::cout << std::fixed << std::setprecision(1) << sum_area(polygons, true) << "\n";
-        } else if (param == "ODD") {
-          std::cout << std::fixed << std::setprecision(1) << sum_area(polygons, false) << "\n";
-        } else if (param == "MEAN") {
-          std::cout << std::fixed << std::setprecision(1) << mean_area(polygons) << "\n";
-        } else {
-          int vertex_count = std::stoi(param);
-          std::cout << std::fixed << std::setprecision(1) << sum_area_by_vertex_count(polygons, vertex_count) << "\n";
         }
-      } else if (command == "MAX") {
-        std::string param;
-        iss >> param;
-        if (param == "AREA") {
+        else if (param == "ODD")
+        {
+          std::cout << std::fixed << std::setprecision(1) << sum_area(polygons, false) << "\n";
+        }
+        else if (param == "MEAN")
+        {
+          std::cout << std::fixed << std::setprecision(1) << mean_area(polygons) << "\n";
+        }
+        else
+        {
+          try
+          {
+            int vertex_count = std::stoi(param);
+            std::cout << std::fixed << std::setprecision(1) << sum_area_by_vertex_count(polygons, vertex_count) << "\n";
+          }
+          catch (const std::invalid_argument &)
+          {
+            std::cout << "<INVALID COMMAND>\n";
+          }
+        }
+      }
+      else if (command == "MAX")
+      {
+        if (param == "AREA")
+        {
           std::cout << std::fixed << std::setprecision(1) << max_area(polygons) << "\n";
-        } else if (param == "VERTEXES") {
+        }
+        else if (param == "VERTEXES")
+        {
           std::cout << max_vertex_count(polygons) << "\n";
         }
-      } else if (command == "MIN") {
-        std::string param;
-        iss >> param;
-        if (param == "AREA") {
+        else
+        {
+          std::cout << "<INVALID COMMAND>\n";
+        }
+      }
+      else if (command == "MIN")
+      {
+        if (param == "AREA")
+        {
           std::cout << std::fixed << std::setprecision(1) << min_area(polygons) << "\n";
-        } else if (param == "VERTEXES") {
+        }
+        else if (param == "VERTEXES")
+        {
           std::cout << min_vertex_count(polygons) << "\n";
         }
-      } else if (command == "COUNT") {
-        std::string param;
-        iss >> param;
-        if (param == "EVEN") {
-          std::cout << count_by_vertex_parity(polygons, true) << "\n";
-        } else if (param == "ODD") {
-          std::cout << count_by_vertex_parity(polygons, false) << "\n";
-        } else {
-          int vertex_count = std::stoi(param);
-          std::cout << count_by_vertex_count(polygons, vertex_count) << "\n";
+        else
+        {
+          std::cout << "<INVALID COMMAND>\n";
         }
-      } else {
+      }
+      else if (command == "COUNT")
+      {
+        if (param == "EVEN")
+        {
+          std::cout << count_by_vertex_parity(polygons, true) << "\n";
+        }
+        else if (param == "ODD")
+        {
+          std::cout << count_by_vertex_parity(polygons, false) << "\n";
+        }
+        else
+        {
+          try
+          {
+            int vertex_count = std::stoi(param);
+            std::cout << count_by_vertex_count(polygons, vertex_count) << "\n";
+          }
+          catch (const std::invalid_argument &)
+          {
+            std::cout << "<INVALID COMMAND>\n";
+          }
+        }
+      }
+      else
+      {
         std::cout << "<INVALID COMMAND>\n";
       }
     }
-  } catch (const std::exception& e) {
+  }
+  catch (const std::exception &e)
+  {
     std::cerr << "Ошибка: " << e.what() << "\n";
     return 1;
   }
--- petuhov.kirill/T3/polygonutils.cpp
+++ petuhov.kirill/T3/polygonutils.cpp
@@ -7,7 +7,7 @@
 
 namespace petuhov
 {
-  std::vector< Polygon > read_polygons(const std::string &filename)
+  std::vector<Polygon> read_polygons(const std::string &filename)
   {
     std::ifstream file(filename);
     if (!file)
@@ -15,19 +15,39 @@ namespace petuhov
       throw std::runtime_error("Error while opening file");
     }
 
-    std::vector< Polygon > polygons;
-    Polygon polygon;
+    std::vector<Polygon> polygons;
     std::string line;
 
     while (std::getline(file, line))
     {
-      std::istringstream iss(line);
-      StreamGuard guard(iss);
-      if (iss >> polygon)
+      Polygon polygon;
+      size_t pos = 0;
+      try
       {
+        pos = line.find(' ');
+        int num_vertices = std::stoi(line.substr(0, pos));
+        size_t new_pos = pos + 1;
+
+        for (int i = 0; i < num_vertices; ++i)
+        {
+          pos = line.find('(', new_pos);
+          size_t semicolon_pos = line.find(';', pos + 1);
+          size_t end_pos = line.find(')', semicolon_pos + 1);
+
+          int x = std::stoi(line.substr(pos + 1, semicolon_pos - pos - 1));
+          int y = std::stoi(line.substr(semicolon_pos + 1, end_pos - semicolon_pos - 1));
+
+          polygon.points.push_back({x, y});
+          new_pos = end_pos + 1;
+        }
         polygons.push_back(polygon);
       }
+      catch (const std::exception &e)
+      {
+        std::cerr << e.what() << "\n";
+      }
     }
+
     return polygons;
   }
 }
