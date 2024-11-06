413746bdb072e6d319c853a4c067497c61a159b2
deadshumz <shumzz@bk.ru>
2024-11-07 00:24:28 +0300

    replace endl with \n

--- petuhov.kirill/T3/main.cpp
+++ petuhov.kirill/T3/main.cpp
@@ -28,48 +28,48 @@ int main(int argc, char* argv[]) {
         std::string param;
         iss >> param;
         if (param == "EVEN") {
-          std::cout << std::fixed << std::setprecision(1) << sum_area(polygons, true) << std::endl;
+          std::cout << std::fixed << std::setprecision(1) << sum_area(polygons, true) << "\n";
         } else if (param == "ODD") {
-          std::cout << std::fixed << std::setprecision(1) << sum_area(polygons, false) << std::endl;
+          std::cout << std::fixed << std::setprecision(1) << sum_area(polygons, false) << "\n";
         } else if (param == "MEAN") {
-          std::cout << std::fixed << std::setprecision(1) << mean_area(polygons) << std::endl;
+          std::cout << std::fixed << std::setprecision(1) << mean_area(polygons) << "\n";
         } else {
           int vertex_count = std::stoi(param);
-          std::cout << std::fixed << std::setprecision(1) << sum_area_by_vertex_count(polygons, vertex_count) << std::endl;
+          std::cout << std::fixed << std::setprecision(1) << sum_area_by_vertex_count(polygons, vertex_count) << "\n";
         }
       } else if (command == "MAX") {
         std::string param;
         iss >> param;
         if (param == "AREA") {
-          std::cout << std::fixed << std::setprecision(1) << max_area(polygons) << std::endl;
+          std::cout << std::fixed << std::setprecision(1) << max_area(polygons) << "\n";
         } else if (param == "VERTEXES") {
-          std::cout << max_vertex_count(polygons) << std::endl;
+          std::cout << max_vertex_count(polygons) << "\n";
         }
       } else if (command == "MIN") {
         std::string param;
         iss >> param;
         if (param == "AREA") {
-          std::cout << std::fixed << std::setprecision(1) << min_area(polygons) << std::endl;
+          std::cout << std::fixed << std::setprecision(1) << min_area(polygons) << "\n";
         } else if (param == "VERTEXES") {
-          std::cout << min_vertex_count(polygons) << std::endl;
+          std::cout << min_vertex_count(polygons) << "\n";
         }
       } else if (command == "COUNT") {
         std::string param;
         iss >> param;
         if (param == "EVEN") {
-          std::cout << count_by_vertex_parity(polygons, true) << std::endl;
+          std::cout << count_by_vertex_parity(polygons, true) << "\n";
         } else if (param == "ODD") {
-          std::cout << count_by_vertex_parity(polygons, false) << std::endl;
+          std::cout << count_by_vertex_parity(polygons, false) << "\n";
         } else {
           int vertex_count = std::stoi(param);
-          std::cout << count_by_vertex_count(polygons, vertex_count) << std::endl;
+          std::cout << count_by_vertex_count(polygons, vertex_count) << "\n";
         }
       } else {
         std::cout << "<INVALID COMMAND>\n";
       }
     }
   } catch (const std::exception& e) {
-    std::cerr << "Ошибка: " << e.what() << std::endl;
+    std::cerr << "Ошибка: " << e.what() << "\n";
     return 1;
   }
 
