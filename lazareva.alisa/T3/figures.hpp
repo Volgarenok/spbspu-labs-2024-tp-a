714b3edba23b964b5f7f76c2f26b2e9b670d8a36
AlisaLazarevapi8 <lazarevaalisa283@gmail.com>
2024-11-06 11:51:58 +0300

    add figures

--- lazareva.alisa/T3/figures.hpp
+++ lazareva.alisa/T3/figures.hpp
@@ -1,2 +1,30 @@
-int main()
-{}
+#ifndef POLYGON_HPP
+#define POLYGON_HPP
+
+#include "delimeter.hpp"
+#include <iostream>
+#include <algorithm>
+#include <iterator>
+#include <numeric>
+#include <string>
+#include <vector>
+
+namespace feofanova
+{
+    struct Point
+    {
+        int x, y;
+    };
+    std::istream& operator>>(std::istream& in, Point& p);
+    std::ostream& operator<<(std::ostream& out, const Point& p);
+    bool operator==(const Point& point1, const Point& point2);
+
+    struct Polygon
+    {
+        std::vector< Point > points;
+    };
+    std::istream& operator>>(std::istream& in, Polygon& p);
+    std::ostream& operator<<(std::ostream& out, const Polygon& p);
+}
+
+#endif
