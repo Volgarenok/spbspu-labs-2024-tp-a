27164acf6eed205b446b7886d59fdb7864f8af2d
deadshumz <shumzz@bk.ru>
2024-11-07 00:02:08 +0300

    fix RIGHTSHAPES

--- petuhov.kirill/T3/commands.cpp
+++ petuhov.kirill/T3/commands.cpp
@@ -20,18 +20,16 @@ namespace petuhov
 
 
 
-  bool has_right_angle(const Polygon &poly)
-  {
+  bool has_right_angle(const Polygon &poly) {
     size_t size = poly.points.size();
     if (size < 3) {
       return false;
     }
 
-    int right_angle_count = 0;
     for (size_t i = 0; i < size; ++i) {
-      Point a = poly.points[i];
-      Point b = poly.points[(i + 1) % size];
-      Point c = poly.points[(i + 2) % size];
+      const Point &a = poly.points[i];
+      const Point &b = poly.points[(i + 1) % size];
+      const Point &c = poly.points[(i + 2) % size];
 
       int dx1 = b.x_ - a.x_;
       int dy1 = b.y_ - a.y_;
@@ -39,12 +37,10 @@ namespace petuhov
       int dy2 = c.y_ - b.y_;
 
       if (dx1 * dx2 + dy1 * dy2 == 0) {
-        right_angle_count++;
-        if (right_angle_count >= 2) {
-          return true;
-        }
+        return true;
       }
     }
+
     return false;
   }
 
@@ -74,19 +70,19 @@ namespace petuhov
     return normalized_points;
   }
 
-  bool is_permutation(const petuhov::Polygon &a, const petuhov::Polygon &b) {
+  bool is_permutation(const Polygon &a, const Polygon &b) {
   if (a.points.size() != b.points.size()) {
     return false;
   }
 
-  std::vector<petuhov::Point> sorted_a = a.points;
-  std::vector<petuhov::Point> sorted_b = b.points;
+  std::vector<Point> sorted_a = a.points;
+  std::vector<Point> sorted_b = b.points;
 
-  std::sort(sorted_a.begin(), sorted_a.end(), [](const petuhov::Point &p1, const petuhov::Point &p2) {
+  std::sort(sorted_a.begin(), sorted_a.end(), [](const Point &p1, const Point &p2) {
     return std::tie(p1.x_, p1.y_) < std::tie(p2.x_, p2.y_);
   });
 
-  std::sort(sorted_b.begin(), sorted_b.end(), [](const petuhov::Point &p1, const petuhov::Point &p2) {
+  std::sort(sorted_b.begin(), sorted_b.end(), [](const Point &p1, const Point &p2) {
     return std::tie(p1.x_, p1.y_) < std::tie(p2.x_, p2.y_);
   });
 
