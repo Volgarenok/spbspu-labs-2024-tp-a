b230403ab3ca6aa20eebdb48fc29fbe5c4822310
puzz13 <puzz13one@gmail.com>
2024-11-06 16:29:07 +0300

    Add base program structure

---
+++ kovtun.alexander/F0/commands.hpp
@@ -0,0 +1,9 @@
+#ifndef COMMANDS_HPP
+#define COMMANDS_HPP
+
+namespace kovtun
+{
+
+}
+
+#endif
--- kovtun.alexander/F0/main.cpp
+++ kovtun.alexander/F0/main.cpp
@@ -1,4 +1,25 @@
+#include <iostream>
+#include <map>
+#include <string>
+#include <functional>
+
 int main()
 {
-  
+  using fun_t = std::function< void(std::istream &, std::ostream &) >;
+  std::map< std::string, fun_t > commands;
+
+  std::string command;
+  while (std::cin >> command)
+  {
+    try
+    {
+      commands.at(command)(std::cin, std::cout);
+    }
+    catch (...)
+    {
+      std::cout << "<INVALID COMMAND>\n";
+    }
+    std::cin.clear();
+    std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+  }
 }
