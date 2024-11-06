8ae8bb45dedc4ec3d64fb2f999f005b735a9d2be
AlisaLazarevapi8 <lazarevaalisa283@gmail.com>
2024-11-06 11:40:21 +0300

    add common files

---
+++ lazareva.alisa/common/streamGuard.cpp
@@ -0,0 +1,12 @@
+#include "streamGuard.hpp"
+lazareva::StreamGuard::StreamGuard(std::basic_ios< char>& s) :
+	s_(s),
+	precision_(s.precision()),
+	flags_(s.flags())
+{}
+
+lazareva::StreamGuard::~StreamGuard()
+{
+	s_.precision(precision_);
+	s_.flags(flags_);
+}
