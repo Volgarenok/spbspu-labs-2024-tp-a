fff74e8c7c87834a46d1bab9665195b28d5ee870
deadshumz <shumzz@bk.ru>
2024-11-07 12:14:45 +0300

    cg

--- petuhov.kirill/F0/dictionarymanager.cpp
+++ petuhov.kirill/F0/dictionarymanager.cpp
@@ -1,91 +1,93 @@
-#include "dictionarymanager.hpp"
+#include "DictionaryManager.hpp"
 #include <iostream>
+
+using namespace std;
 
 namespace petuhov
 {
 
-  void DictionaryManager::create(const std::string &name)
+  void DictionaryManager::create(const string &name)
   {
     if (dictionaries_.find(name) != dictionaries_.end())
     {
-      std::cout << "<ALREADY EXISTS>\n";
+      cout << "<ALREADY EXISTS>\n";
       return;
     }
     dictionaries_[name] = Dictionary();
   }
 
-  void DictionaryManager::deleteDictionary(const std::string &name)
+  void DictionaryManager::deleteDictionary(const string &name)
   {
     if (dictionaries_.erase(name) == 0)
     {
-      std::cout << "<DICTIONARY NOT FOUND>\n";
+      cout << "<DICTIONARY NOT FOUND>\n";
     }
   }
 
-  void DictionaryManager::insert(const std::string &dictName, const std::string &word, const std::string &translation)
+  void DictionaryManager::insert(const string &dictName, const string &word, const string &translation)
   {
-    auto it = dictionaries_.find(dictName);
+    map< string, Dictionary >::iterator it = dictionaries_.find(dictName);
     if (it == dictionaries_.end())
     {
-      std::cout << "<DICTIONARY NOT FOUND>\n";
+      cout << "<DICTIONARY NOT FOUND>\n";
       return;
     }
     it->second.insert(word, translation);
   }
 
-  void DictionaryManager::remove(const std::string &dictName, const std::string &word)
+  void DictionaryManager::remove(const string &dictName, const string &word)
   {
-    auto it = dictionaries_.find(dictName);
+    map< string, Dictionary >::iterator it = dictionaries_.find(dictName);
     if (it == dictionaries_.end())
     {
-      std::cout << "<DICTIONARY NOT FOUND>\n";
+      cout << "<DICTIONARY NOT FOUND>\n";
       return;
     }
     it->second.remove(word);
   }
 
-  void DictionaryManager::translate(const std::string &dictName, const std::string &word) const
+  void DictionaryManager::translate(const string &dictName, const string &word) const
   {
-    auto it = dictionaries_.find(dictName);
+    map< string, Dictionary >::const_iterator it = dictionaries_.find(dictName);
     if (it == dictionaries_.end())
     {
-      std::cout << "<DICTIONARY NOT FOUND>\n";
+      cout << "<DICTIONARY NOT FOUND>\n";
       return;
     }
     it->second.translate(word);
   }
 
-  void DictionaryManager::merge(const std::string &dict1, const std::string &dict2)
+  void DictionaryManager::merge(const string &dict1, const string &dict2)
   {
-    auto it1 = dictionaries_.find(dict1);
-    auto it2 = dictionaries_.find(dict2);
+    map< string, Dictionary >::iterator it1 = dictionaries_.find(dict1);
+    map< string, Dictionary >::iterator it2 = dictionaries_.find(dict2);
     if (it1 == dictionaries_.end() || it2 == dictionaries_.end())
     {
-      std::cout << "<DICTIONARY NOT FOUND>\n";
+      cout << "<DICTIONARY NOT FOUND>\n";
       return;
     }
     it1->second.merge(it2->second);
   }
 
-  void DictionaryManager::combining(const std::string &dict1, const std::string &dict2) const
+  void DictionaryManager::combining(const string &dict1, const string &dict2) const
   {
-    auto it1 = dictionaries_.find(dict1);
-    auto it2 = dictionaries_.find(dict2);
+    map< string, Dictionary >::const_iterator it1 = dictionaries_.find(dict1);
+    map< string, Dictionary >::const_iterator it2 = dictionaries_.find(dict2);
     if (it1 == dictionaries_.end() || it2 == dictionaries_.end())
     {
-      std::cout << "<DICTIONARY NOT FOUND>\n";
+      cout << "<DICTIONARY NOT FOUND>\n";
       return;
     }
     it1->second.combining(it2->second);
   }
 
-  void DictionaryManager::difference(const std::string &dict1, const std::string &dict2) const
+  void DictionaryManager::difference(const string &dict1, const string &dict2) const
   {
-    auto it1 = dictionaries_.find(dict1);
-    auto it2 = dictionaries_.find(dict2);
+    map< string, Dictionary >::const_iterator it1 = dictionaries_.find(dict1);
+    map< string, Dictionary >::const_iterator it2 = dictionaries_.find(dict2);
     if (it1 == dictionaries_.end() || it2 == dictionaries_.end())
     {
-      std::cout << "<DICTIONARY NOT FOUND>\n";
+      cout << "<DICTIONARY NOT FOUND>\n";
       return;
     }
     it1->second.difference(it2->second);
