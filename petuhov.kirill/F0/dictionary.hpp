c69e599022c1e20468edb6201bcb09dc1a5d991a
deadshumz <shumzz@bk.ru>
2024-11-07 11:47:42 +0300

    preparing dictionary to new command system

--- petuhov.kirill/F0/dictionary.hpp
+++ petuhov.kirill/F0/dictionary.hpp
@@ -3,20 +3,30 @@
 
 #include <map>
 #include <string>
+#include <set>
+#include <vector>
+#include <iostream>
 #include <stdexcept>
 
-namespace petuhov {
+namespace petuhov
+{
   using namespace std;
 
-  class Dictionary {
-    public:
-      void insert(const string& word, const string& translation, const string& additionalInfo);
-      void search(const string& word) const;
-      void remove(const string& word);
+  class Dictionary
+  {
+  public:
+    void create(const string &name);
+    void deleteDictionary(const string &name);
+    void insert(const string &word, const string &translation);
+    void remove(const string &word);
+    void translate(const string &word) const;
+    void merge(const Dictionary &other);
+    void combining(const Dictionary &other) const;
+    void difference(const Dictionary &other) const;
 
-    private:
-      map< string, map< string, string > > dictionary_;
-};
+  private:
+    map< string, set< string > > dictionary_;
+  };
 }
 
 #endif
