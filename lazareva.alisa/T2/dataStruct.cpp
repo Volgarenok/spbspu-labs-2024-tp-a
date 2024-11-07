6425fbc06aa556d4006f267641ed6e13513fe584
AlisaLazarevapi8 <lazarevaalisa283@gmail.com>
2024-11-07 03:38:16 +0300

    fix

--- lazareva.alisa/T2/dataStruct.cpp
+++ lazareva.alisa/T2/dataStruct.cpp
@@ -1,75 +1,113 @@
-#include "dataStruct.hpp"
+#include "dataStruct.h"
 #include <iostream>
 #include <iomanip>
 #include "delimiter.hpp"
 #include "streamGuard.hpp"
+
+std::istream& lazareva::operator>>(std::istream& in, Dbl&& dest)
+{
+    std::istream::sentry guard(in);
+    if (!guard)
+    {
+        return in;
+    }
+    return in >> dest.value >> IgnoreCaseDelimeter{ "d" };
+}
+
+std::istream& lazareva::operator>>(std::istream& in, Sll&& key)
+{
+    std::istream::sentry sentry(in);
+    if (!sentry)
+    {
+        return in;
+    }
+    using delS = DelimiterI;
+    in >> key.value >> delS{ "ll" };
+    return in;
+}
+
+std::istream& lazareva::operator>>(std::istream& in, String&& dest)
+{
+    std::istream::sentry guard(in);
+    if (!guard)
+    {
+        return in;
+    }
+    using Delimeter = DelimeterI;
+    in >> Delimeter{ '\"' };
+    std::getline(in, dest.str, '\"');
+    return in;
+}
+
 bool lazareva::operator<(const DataStruct& lhs, const DataStruct& rhs)
 {
-  if (lhs.key1 != rhs.key1)
-  {
-    return lhs.key1 < rhs.key1;
-  }
-  if (lhs.key2 != rhs.key2)
-  {
-    return lhs.key2 < rhs.key2;
-  }
-  return lhs.key3.size() < rhs.key3.size();
+    if (lhs.key1 != rhs.key1)
+    {
+        return lhs.key1 < rhs.key1;
+    }
+    if (lhs.key2 != rhs.key2)
+    {
+        return lhs.key2 < rhs.key2;
+    }
+    return lhs.key3.size() < rhs.key3.size();
 }
 
 std::istream& lazareva::operator>>(std::istream& in, DataStruct& data)
 {
-  std::istream::sentry guard(in);
-  if (!guard)
-  {
+    std::istream::sentry guard(in);
+    if (!guard)
+    {
+        return in;
+    }
+    StreamGuard s_guard(in);
+    using del = lazareva::Delimiter;
+    in >> del{ '(' };
+    for (int b = 0; b < 3; b++)
+    {
+        in >> del{ ':' }
+        >> del{ 'k' } >> del{ 'e' } >> del{ 'y' };
+        int i = 0;
+        in >> i;
+        if (i == 1)
+        {
+            double value = 0;
+            in >> value;
+            data.key1 = value;
+            in >> del{ 'd' };
+        }
+        else if (i == 2)
+        {
+            long long value = 0;
+            in >> value;
+            data.key2 = value;
+            in >> del{ 'l' } >> del{ 'l' };
+        }
+        else if (i == 3)
+        {
+            std::string value = "";
+            in >> del{ '"' };
+            std::getline(in, value, '\"');
+            data.key3 = value;
+        }
+        else
+        {
+            in.setstate(std::ios::failbit);
+        }
+    }
+    in >> del{ ':' } >> del{ ')' };
     return in;
-  }
-  StreamGuard s_guard(in);
-  using del = lazareva::Delimiter;
-  in >> del{'('};
-  for (int b = 0; b < 3; b++)
-  {
-    in  >> del{':'} >> del{'k'} >> del{'e'} >> del{'y'};
-    int i = 0;
-    in >> i;
-    if (i == 1)
-    {
-      double value = 0;
-      in >> value;
-      data.key1 = value;
-      in >> del{'d'};
-    }
-    else if (i == 2)
-    {
-      long long value = 0;
-      in >> value;
-      data.key2 = value;
-      in >> del{'l'} >> del{'l'};
-    }
-    else if (i == 3)
-    {
-      std::string value = "";
-      in >> del{'"'};
-      std::getline(in, value, '\"');
-      data.key3 = value;
-    }
-    else
-    {
-      in.setstate(std::ios::failbit);
-    }
-  }
-  in >> del{':'} >> del{')'};
-  return in;
 }
 
 std::ostream& lazareva::operator<<(std::ostream& out, const DataStruct& data)
 {
-  std::ostream::sentry guard(out);
-  if (!guard)
-  {
+    std::ostream::sentry guard(out);
+    if (!guard)
+    {
+        return out;
+    }
+    StreamGuard s_guard(out);
+    out << std::fixed;
+    out << "(:key1 " << std::setprecision(1) << data.key1
+        << "d:key2 " << data.key2 << "ll:key3 \"" << data.key3 << "\":)";
     return out;
-  }
-  StreamGuard s_guard(out);
-  out << std::fixed;
-  out << "(:key1 " << std::setprecision(1) <<  data.key1 << "d:key2 " << data.key2 << "ll:key3 \"" << data.key3 << "\":)";
-  return out;
 }
--- lazareva.alisa/T2/dataStruct.hpp
+++ lazareva.alisa/T2/dataStruct.hpp
@@ -1,19 +1,38 @@
+#pragma once
 #ifndef DATASTRUCT_HPP
 #define DATASTRUCT_HPP
 #include <iostream>
 #include <string>
 namespace lazareva
 {
- struct DataStruct
-  {
-    double key1;
-    long long key2;
-    std::string key3;
-  };
+    struct Dbl
+    {
+        double& value;
+    };
+    std::istream& operator>>(std::istream& in, Dbl&& num);
 
-  bool operator<(const DataStruct& lhs, const DataStruct& rhs);
-  std::istream& operator>>(std::istream& in, DataStruct& data);
-  std::ostream& operator<<(std::ostream& out, const DataStruct& data);
+    struct Sll
+    {
+        unsigned long long value;
+    };
+    std::ostream& operator<<(std::ostream& out, Sll&& src);
+
+    struct String
+    {
+        std::string& str;
+    };
+    std::istream& operator>>(std::istream& in, String&& num);
+
+    struct DataStruct
+    {
+        double key1;
+        long long key2;
+        std::string key3;
+    };
+
+    bool operator<(const DataStruct& lhs, const DataStruct& rhs);
+    std::istream& operator>>(std::istream& in, DataStruct& data);
+    std::ostream& operator<<(std::ostream& out, const DataStruct& data);
 
 }
 
