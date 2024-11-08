f3a650ec80bb324521a44149d0b26f0a18850a2a
fncg <genastepanow05@gmail.com>
2024-11-08 22:40:43 +0300

    start getAreaEven

--- stepanov.gena/T3/Commands.h
+++ stepanov.gena/T3/Commands.h
@@ -1,5 +1,14 @@
 #ifndef COMMANDS_H
 #define COMMANDS_H
 
+#include <vector>
+#include <ostream>
+#include "DataStruct.h"
+
+
+namespace stepanov
+{
+  void getAreaEven(const std::vector< Polygon >& polygons, std::ostream& out);
+}
 
 #endif
