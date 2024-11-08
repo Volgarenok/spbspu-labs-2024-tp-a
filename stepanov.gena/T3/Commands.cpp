89f5d39644f1d2a127e26132ea427953eb7b5349
fncg <genastepanow05@gmail.com>
2024-11-08 22:49:56 +0300

    is even

--- stepanov.gena/T3/Commands.cpp
+++ stepanov.gena/T3/Commands.cpp
@@ -1,1 +1,17 @@
- 
+#include "Commands.h"
+#include <vector>
+#include <ostream>
+#include "DataStruct.h"
+
+namespace stepanov
+{
+  bool isEven(const Polygon& polygon)
+  {
+    return polygon.points_.size() % 2 == 0;
+  }
+
+  void getAreaEven(const std::vector< Polygon >& polygons, std::ostream& out)
+  {
+    std::vector< Polygon > even_polygons;
+  }
+}
