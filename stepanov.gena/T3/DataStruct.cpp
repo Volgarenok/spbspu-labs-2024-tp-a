8a5a8dcb33289ea1716a55d68523bdb8f314c87e
fncg <genastepanow05@gmail.com>
2024-11-08 22:16:46 +0300

    input poly

--- stepanov.gena/T3/DataStruct.cpp
+++ stepanov.gena/T3/DataStruct.cpp
@@ -1,1 +1,22 @@
+#include "DataStruct.h"
+#include <iterator>
+#include <algorithm>
+#include <StreamGuard.h>
+#include <HelperStructsIO.h>
+
+std::istream& stepanov::operator>>(std::istream& in, Point& dest)
+{
+  std::istream::sentry sentry(in);
+  if (!sentry)
+  {
+    return in;
+  }
+  using sep = DelimeterIO;
+  in >> sep{'('};
+  in >> dest.x_;
+  in >> sep{';'};
+  in >> dest.y_;
+  in >> sep{')'};
+  return in;
+}
  
