380c2f71cb7382f30601f2326263bf6650a28f3c
fncg <genastepanow05@gmail.com>
2024-11-10 01:21:13 +0300

    start commands class

--- stepanov.gena/T3/Commands.h
+++ stepanov.gena/T3/Commands.h
@@ -22,6 +22,30 @@ namespace stepanov
   void rmEcho(std::vector< Polygon >& polygons, const Polygon& polygon, std::ostream& out);
   void getRects(const std::vector< Polygon >& polygons, std::ostream& out);
   void getIntersections(const std::vector< Polygon >& polygons, const Polygon& polygon, std::ostream& out);
+  class Commands
+  {
+    public:
+      Commands();
+      void doCommand(const std::vector< Polygon >& polygons,
+          const std::string& cmd,
+          std::ostream& out) const;
+      void doCommand(const std::vector< Polygon >& polygons,
+          const std::string& cmd,
+          size_t count,
+          std::ostream& out) const;
+      void doCommand(std::vector< Polygon >& polygons,
+          const std::string& cmd,
+          const Polygon& polygon,
+          std::ostream& out) const;
+    private:
+      using com1 = void (*)(const std::vector< Polygon >& polygons, std::ostream& out);
+      using com2 = void (*)(const std::vector< Polygon >& polygons, size_t count, std::ostream& out);
+      using com3 = void (*)(std::vector< Polygon >& polygons, const Polygon& polygon, std::ostream& out);
+      std::map< std::string, com1 > dict1_;
+      std::map< std::string, com2 > dict2_;
+      std::map< std::string, com3 > dict3_;
+  };
+
 }
 
 #endif
