1b9520a1b587ae993863fc8491f9663c141428b1
fncg <genastepanow05@gmail.com>
2024-11-13 03:01:49 +0300

    remove while from input

--- stepanov.gena/F0/Commands.cpp
+++ stepanov.gena/F0/Commands.cpp
@@ -4,98 +4,126 @@
 #include <vector>
 #include <algorithm>
 #include <map>
+#include <functional>
+#include <iterator>
+
 
 using TextMap = std::multimap<std::string, std::pair<size_t, size_t>>;
 
+void addWordToMap(const std::string& word, size_t lineNumber, size_t& wordNumber, TextMap& map)
+{
+  map.emplace(word, std::make_pair(lineNumber, wordNumber));
+  ++wordNumber;
+}
+
 void processWord(const std::string& word, size_t lineNumber, size_t& wordNumber, TextMap& map)
 {
- map.emplace(word, std::make_pair(lineNumber, wordNumber));
- ++wordNumber;
+  addWordToMap(word, lineNumber, wordNumber, map);
 }
 
 void splitLineIntoWords(const std::string& line, size_t lineNumber, TextMap& map)
 {
- size_t wordNumber = 0;
- size_t start = 0;
- size_t end = 0;
+  size_t wordNumber = 0;
+  auto start = line.begin();
 
- while (end < line.length()) {
-  while (end < line.length() && line[end] != ' ') ++end;
-  if (end > start)
-  {
-   processWord(line.substr(start, end - start), lineNumber, wordNumber, map);
-  }
-  end++;
-  start = end;
- }
+  std::for_each(line.begin(), line.end(), [&](char) {
+    auto end = std::find(start, line.end(), ' ');
+    if (start != end) {
+      std::string word(start, end);
+      processWord(word, lineNumber, wordNumber, map);
+    }
+    start = (end == line.end()) ? end : std::next(end);
+  });
+}
+
+void processLine(const std::string& line, size_t& lineNumber, TextMap& map)
+{
+  splitLineIntoWords(line, lineNumber, map);
+  ++lineNumber;
 }
 
 void stepanov::input(std::map<std::string, TextMap>& textMaps, const std::string& mapName, const std::string& fileName)
 {
- if (textMaps.find(mapName) != textMaps.end())
- {
-  std::cout << "<INVALID COMMAND>\n";
-  return;
- }
-
- std::ifstream file(fileName);
- if (!file.is_open())
- {
-  std::cout << "<INVALID COMMAND>\n";
-  return;
- }
-
- TextMap map;
- std::string line;
- size_t lineNumber = 0;
-
- while (std::getline(file, line))
- {
-  splitLineIntoWords(line, lineNumber, map);
-  ++lineNumber;
- }
-
- if (map.empty())
- {
-  std::cout << "<INVALID COMMAND>\n";
- }
- else
- {
-  textMaps[mapName] = map;
- }
-}
-
-void stepanov::output(const std::map<std::string, TextMap>& textMaps, const std::string& mapName)
-{
-  auto it = textMaps.find(mapName);
-  if (it == textMaps.end())
+  if (textMaps.find(mapName) != textMaps.end())
   {
     std::cout << "<INVALID COMMAND>\n";
     return;
   }
 
+  std::ifstream file(fileName);
+  if (!file.is_open())
+  {
+    std::cout << "<INVALID COMMAND>\n";
+    return;
+  }
+
+  TextMap map;
+  std::string line;
+  size_t lineNumber = 0;
+
+  while (std::getline(file, line))
+  {
+    processLine(line, lineNumber, map);
+  }
+
+  if (map.empty())
+  {
+    std::cout << "<INVALID COMMAND>\n";
+  }
+  else
+  {
+    textMaps[mapName] = map;
+  }
+}
+
+void populateLines(std::map<size_t, std::vector<std::pair<std::string, size_t>>>& lines,
+           const std::pair<const std::string, std::pair<size_t, size_t>>& entry)
+{
+  const std::string& word = entry.first;
+  size_t lineNum = entry.second.first;
+  size_t wordPos = entry.second.second;
+  lines[lineNum].push_back({word, wordPos});
+}
+
+bool compareByWordPos(const std::pair<std::string, size_t>& a, const std::pair<std::string, size_t>& b)
+{
+  return a.second < b.second;
+}
+
+void sortLineEntries(std::pair<const size_t, std::vector<std::pair<std::string, size_t>>>& lineEntry)
+{
+  using namespace std::placeholders;
+  auto comp = std::bind(compareByWordPos, _1, _2);
+  std::sort(lineEntry.second.begin(), lineEntry.second.end(), comp);
+}
+
+void printLine(const std::pair<const size_t, std::vector<std::pair<std::string, size_t>>>& lineIt)
+{
+  for (size_t i = 0; i < lineIt.second.size(); ++i)
+  {
+  std::cout << lineIt.second[i].first;
+  if (i < lineIt.second.size() - 1)
+  {
+    std::cout << " ";
+  }
+  }
+  std::cout << "\n";
+}
+
+void stepanov::output(const std::map<std::string, TextMap>& textMaps, const std::string& mapName)
+{
+  using namespace std::placeholders;
+  auto it = textMaps.find(mapName);
+  if (it == textMaps.end())
+  {
+  std::cout << "<INVALID COMMAND>\n";
+  return;
+  }
+
   const TextMap& map = it->second;
   std::map<size_t, std::vector<std::pair<std::string, size_t>>> lines;
 
-  std::for_each(map.begin(), map.end(), [&lines](const auto& entry) {
-    const std::string& word = entry.first;
-    size_t lineNum = entry.second.first;
-    size_t wordPos = entry.second.second;
-    lines[lineNum].push_back({word, wordPos});
-  });
-
-  std::for_each(lines.begin(), lines.end(), [](auto& lineEntry) {
-    std::sort(lineEntry.second.begin(), lineEntry.second.end(),
-      [](const std::pair<std::string, size_t>& a, const std::pair<std::string, size_t>& b) {
-        return a.second < b.second;
-      });
-  });
-
-  std::for_each(lines.begin(), lines.end(), [](const auto& lineIt) {
-    std::for_each(lineIt.second.begin(), lineIt.second.end(), [&lineIt](const auto& wordPair) {
-      std::cout << wordPair.first;
-      if (&wordPair != &lineIt.second.back()) std::cout << " ";
-    });
-    std::cout << "\n";
-  });
+  std::for_each(map.begin(), map.end(), std::bind(populateLines, std::ref(lines), _1));
+  std::for_each(lines.begin(), lines.end(), std::bind(sortLineEntries, _1));
+  std::for_each(lines.begin(), lines.end(), std::bind(printLine,_1));
 }
--- stepanov.gena/F0/main.cpp
+++ stepanov.gena/F0/main.cpp
@@ -3,10 +3,10 @@
 #include "Commands.h"
 
 int main() {
-    std::map<std::string, TextMap> textMaps;  // Карта, которая будет передаваться в функции
+    std::map<std::string, TextMap> textMaps;
 
     std::cout << "Testing INPUT command:\n";
-    stepanov::input(textMaps, "map1", "input.txt");  // "file1.txt" содержит несколько строк текста
+    stepanov::input(textMaps, "map1", "input.txt");
     stepanov::output(textMaps, "map1");
 
     return 0;
