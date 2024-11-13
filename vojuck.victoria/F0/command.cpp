e7785207518156e4f1450b54fd69ccda424bb4fc
vktrchh <viskas6969@gmail.com>
2024-11-13 21:58:04 +0300

    added min/max_freq

--- vojuck.victoria/F0/command.cpp
+++ vojuck.victoria/F0/command.cpp
@@ -2,7 +2,7 @@
 
 void vojuck::printHelp(std::ostream & output)
 {
-//1 2 3 4 5 6 7 8 9 10
+//1 2 3 4 5 6 7 8 9 10 11 12
   output << "1.Make new  dictionary:\n";
   output << "  make <dictionary_name>\n";
   output << "2.Delete dictionary:\n";
@@ -246,7 +246,7 @@ void vojuck::mergeDict(std::map<std::string, dict_t>& dicts, std::istream& input
   {
     if (mergedDict.find(entry.first) != mergedDict.end())
     {
-    mergedDict[entry.first] += entry.second; // Суммируем частоты
+    mergedDict[entry.first] += entry.second;
     }
     else
     {
@@ -261,7 +261,7 @@ void vojuck::mergeDict(std::map<std::string, dict_t>& dicts, std::istream& input
 void vojuck::intersection(std::map<std::string, dict_t>& dicts, std::istream& input, std::ostream& output)
 {
   std::string dictName1, dictName2, totalName;
-  input >> dictName1 >> dictName2 >> totalName; // Считываем имена словарей
+  input >> dictName1 >> dictName2 >> totalName;
 
   auto dictIt1 = dicts.find(dictName1);
   auto dictIt2 = dicts.find(dictName2);
@@ -288,6 +288,140 @@ void vojuck::intersection(std::map<std::string, dict_t>& dicts, std::istream& in
   }
     dicts[totalName] = intersectionDict;
     output << "The intersection dictionary '" << totalName << "' has been created.\n";
+}
+
+//11
+void vojuck::uniqueFreq(std::map<std::string, dict_t>& dicts, std::istream& input, std::ostream& output)
+{
+  std::string dictName1, dictName2, totalName;
+  input >> dictName1 >> dictName2 >> totalName; // Считываем имена словарей
+
+  auto dictIt1 = dicts.find(dictName1);
+  auto dictIt2 = dicts.find(dictName2);
+
+  if (dictIt1 == dicts.end() || dictIt2 == dicts.end())
+  {
+    throw std::logic_error("<ONE OR BOTH DICTIONARIES NOT FOUND>\n");
+  }
+
+  const dict_t& dict1 = dictIt1->second;
+  const dict_t& dict2 = dictIt2->second;
+
+  dict_t uniqueDict;
+
+  for (const auto& entry : dict1)
+  {
+    if (dict2.find(entry.first) == dict2.end())
+    {
+      uniqueDict[entry.first] = entry.second;
+    }
+  }
+
+  for (const auto& entry : dict2)
+  {
+    if (dict1.find(entry.first) == dict1.end())
+    {
+      uniqueDict[entry.first] = entry.second;
+    }
+  }
+  dicts[totalName] = uniqueDict;
+  output << "The unique frequency dictionary '" << totalName << "' has been created.\n";
+}
+
+//12
+void vojuck::minFreq(std::map<std::string, dict_t>& dicts, std::istream& input, std::ostream& output)
+{
+  std::vector<std::string> dictNames;
+  std::string dictName;
+  while (input.peek() != '\n')
+  {
+    input >> dictName;
+    dictNames.push_back(dictName);
+  }
+
+  if (dictNames.empty())
+  {
+     throw std::logic_error("<DICTIONARIES NOT FOUND>\n");
+  }
+
+  std::string minWord;
+  int minFrequency = std::numeric_limits<int>::max();
+
+  for (const auto& dictName : dictNames)
+  {
+    auto dictIt = dicts.find(dictName);
+    if (dictIt == dicts.end())
+    {
+      continue;
+    }
+
+    const dict_t& dict = dictIt->second;
+    for (const auto& entry : dict)
+    {
+      if (entry.second < minFrequency)
+      {
+        minFrequency = entry.second;
+        minWord = entry.first;
+      }
+    }
+  }
+
+  if (!minWord.empty())
+  {
+    output << minWord << " " << minFrequency << "\n";
+  }
+  else
+  {
+    throw std::logic_error("<WORD NOT FOUND>\n");
+  }
+}
+
+//13
+void vojuck::maxFreq(std::map<std::string, dict_t>& dicts, std::istream& input, std::ostream& output)
+{
+  std::vector<std::string> dictNames;
+  std::string dictName;
+  while (input.peek() != '\n')
+  {
+    input >> dictName;
+    dictNames.push_back(dictName);
+  }
+
+  if (dictNames.empty())
+  {
+     throw std::logic_error("<DICTIONARIES NOT FOUND>\n");
+  }
+
+  std::string maxWord;
+  int maxFrequency = std::numeric_limits<int>::min();
+
+  for (const auto& dictName : dictNames)
+  {
+    auto dictIt = dicts.find(dictName);
+    if (dictIt == dicts.end())
+    {
+      continue;
+    }
+
+    const dict_t& dict = dictIt->second;
+    for (const auto& entry : dict)
+    {
+      if (entry.second > minFrequency)
+      {
+        maxFrequency = entry.second;
+        maxWord = entry.first;
+      }
+    }
+  }
+
+  if (!maxWord.empty())
+  {
+    output << maxWord << maxFrequency << "\n";
+  }
+  else
+  {
+    throw std::logic_error("<WORD NOT FOUND>\n");
+  }
 }
 
 
